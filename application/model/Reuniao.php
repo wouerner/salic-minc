e60e9a0aa75c0558848e663f792250fdc4a2b10a
Alysson Vicuna <alysson.oliveira@cultura.gov.br>
2015-11-27 16:43:52 -0200

    Realizando Implementação do relatório de Consulta Publica a todos os Projetos e Reunioes do SALIC (CNIC)

--- application/model/Reuniao.php
+++ application/model/Reuniao.php
@@ -14,6 +14,10 @@ class Reuniao extends GenericModel{
     protected $_banco = 'SAC';
     protected $_name  = 'dbo.tbReuniao';
 
+    /**
+     * Retorna a Ultima Reiniao Aberta
+     * @return null|Zend_Db_Table_Row_Abstract
+     */
     public function buscarReuniaoAberta(){
         $select = $this->select();
         $select->from(
@@ -30,6 +34,63 @@ class Reuniao extends GenericModel{
         $select->where("stEstado = ?", 0);
         return $this->fetchRow($select);
     }
+
+    /**
+     * Retorna uma Reuniao filtrando pelo NrReuniao
+     * @param $nrReuniao
+     * @return null|Zend_Db_Table_Row_Abstract
+     */
+    public function buscarReuniaoPorNumero($nrReuniao){
+        $select = $this->select();
+        $select->from(
+            $this,
+            array
+            (
+                "idNrReuniao",
+                "NrReuniao",
+                "stPlenaria",
+                "DtInicio",
+                "DtFinal"
+            )
+        );
+        $select->where("NrReuniao = ?", $nrReuniao);
+        return $this->fetchRow($select);
+    }
+
+    public function buscarTodasReunioes(){
+        $select = $this->select();
+        $select->from(
+            $this,
+            array
+            (
+                "idNrReuniao",
+                "NrReuniao",
+                "stPlenaria",
+                "DtInicio",
+                "DtFinal"
+            )
+        );
+
+        return $this->fetchAll($select);
+    }
+
+    public function buscarReuniaoPorId($idNrReuniao){
+        $select = $this->select();
+        $select->from(
+            $this,
+            array
+            (
+                "idNrReuniao",
+                "NrReuniao",
+                "stPlenaria",
+                "DtInicio",
+                "DtFinal"
+            )
+        );
+        $select->where("idNrReuniao = ?", $idNrReuniao);
+        return $this->fetchRow($select);
+    }
     
 }
+
 ?>
