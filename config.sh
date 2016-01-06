9c3cb5c811ba70d23aa43c7cf93848ca90a4cf95
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-01-06 14:28:59 -0200

    Fix no script config.sh que faz as conexões com o banco

--- config.sh
+++ config.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-CAMINHO="/var/www/novosalic"
 HOST="HOST"
 USERNAME="USERNAME"
 PASS="PASSWORD"
 
-chown -R www-data: $CAMINHO/
+cp -av application/configs/bancos_treinamento.ini-exemplo application/configs/bancos_treinamento.ini
+cp -av application/configs/config.ini-exemplo application/configs/config.ini
 
-sed -i 's/$DIR_BANCO        = "bancos_AMBIENTE";/$DIR_BANCO      = "bancos_treinamento";/g' $CAMINHO/index.php
-sed -i 's/$DIR_BANCOP     = "conexao_AMBIENTE";/$DIR_BANCOP     = "conexao_02";/g' $CAMINHO/index.php
 
-sed -i "s/db.params.host = MudePorAmbiente/db.params.host = $HOST/g" $CAMINHO/application/configs/bancos_treinamento.ini
-sed -i "s/db.params.username = MudePorAmbiente/db.params.username = $USERNAME/g" $CAMINHO/application/configs/bancos_treinamento.ini
-sed -i "s/db.params.password = MudePorAmbiente/db.params.password = $PASS/g" $CAMINHO/application/configs/bancos_treinamento.ini
+sed -i 's/$DIR_BANCO        = "bancos_AMBIENTE";/$DIR_BANCO      = "bancos_treinamento";/g' index.php
+sed -i 's/$DIR_BANCOP     = "conexao_AMBIENTE";/$DIR_BANCOP     = "conexao_02";/g' index.php
 
-sed -i "s/db.params.host = MudePorAmbiente/db.params.host = $HOST/g" $CAMINHO/application/configs/config.ini
-sed -i "s/db.params.username = MudePorAmbiente/db.params.username = $USERNAME/g" $CAMINHO/application/configs/config.ini
-sed -i "s/db.params.password = MudePorAmbiente/db.params.password = $PASS/g" $CAMINHO/application/configs/config.ini
+sed -i "s/db.params.host = MudePorAmbiente/db.params.host = $HOST/g" application/configs/bancos_treinamento.ini
+sed -i "s/db.params.username = MudePorAmbiente/db.params.username = $USERNAME/g" application/configs/bancos_treinamento.ini
+sed -i "s/db.params.password = MudePorAmbiente/db.params.password = $PASS/g" application/configs/bancos_treinamento.ini
+
+sed -i "s/db.params.host = MudePorAmbiente/db.params.host = $HOST/g" application/configs/config.ini
+sed -i "s/db.params.username = MudePorAmbiente/db.params.username = $USERNAME/g" application/configs/config.ini
+sed -i "s/db.params.password = MudePorAmbiente/db.params.password = $PASS/g" application/configs/config.ini
