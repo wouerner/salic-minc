58fcb12e7f81c2205030990e539229683260337a
Fernão Lopes <fernao.lara@cultura.gov.br>
2016-02-22 15:08:58 -0300

    Realizando correções nas Querys de Comprovação financeira para que contemplem as alterações enviadas pelo Romulo no dia 22/02/2016 as 15:00

--- application/model/PlanilhaItem.php
+++ application/model/PlanilhaItem.php
@@ -42,10 +42,16 @@ class PlanilhaItem   extends GenericModel
                         FROM BDCORPORATIVO.scSAC.tbComprovantePagamentoxPlanilhaAprovacao AS a1
                         INNER JOIN BDCORPORATIVO.scSAC.tbComprovantePagamento AS b1 ON a1.idComprovantePagamento = b1.idComprovantePagamento
                         INNER JOIN SAC.dbo.tbPlanilhaAprovacao AS c1 ON a1.idPlanilhaAprovacao = c1.idPlanilhaAprovacao
-                        WHERE
-                            c1.idPlanilhaItem = pa.idPlanilhaItem
-                             AND (c1.idPronac = pa.idPronac)
-                           GROUP BY c1.idPlanilhaItem) AS vlComprovado,
+                        WHERE c1.idPlanilhaItem = pa.idPlanilhaItem
+                            AND c1.nrFonteRecurso = pa.nrFonteRecurso
+                            AND c1.idProduto = pa.idProduto
+                            AND c1.idEtapa = pa.idEtapa
+                            AND c1.idUFDespesa = pa.idUFDespesa
+                            AND c1.idMunicipioDespesa = pa.idMunicipioDespesa
+                             AND c1.idPronac = pa.idPronac
+                             GROUP BY c1.idPlanilhaItem
+                    ) AS vlComprovado,
+
                     ROUND((pa.QtItem * pa.nrOcorrencia * pa.VlUnitario),2) as valorAprovado
                 FROM SAC.dbo.tbPlanilhaAprovacao AS pa
                     INNER JOIN SAC.dbo.tbPlanilhaItens as pit ON pit.idPlanilhaItens = pa.idPlanilhaItem
@@ -54,12 +60,14 @@ class PlanilhaItem   extends GenericModel
                 WHERE
                     pa.idPlanilhaAprovacao = ?
                     AND pa.stAtivo = 'S'
+                    AND pa.nrFonteRecurso = 109 --BATIZADO
                 ORDER BY prod.Descricao ASC";
 
+        #xd($select);
 
         $statement = $this->getAdapter()->query($select, array($item));
 
-        #xd($select);
+
 
         return $statement->fetchObject();
     }
