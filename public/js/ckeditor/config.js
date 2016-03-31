b3d37e30c524e9156a53330cfd5a0093adb112c2
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-03-31 11:29:10 -0300

    Colocado versão anterior do CKeditor que já possuia no salic

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -1,83 +1,24 @@
-/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-        config.language ='pt-br';
-        config.uiColor = '#AADC6E';
-
-	config.toolbarGroups = [
-//		{ name: 'clipboard'},
-//		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-//		{ name: 'links' },
-//		{ name: 'insert' },
-//		{ name: 'forms' },
-//		{ name: 'tools' },
-//		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-//		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', /* groups: [ 'basicstyles', 'cleanup' ] */},
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-//		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'undo' }
-	];
-
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-};
-
-   CKEDITOR.config.scayt_autoStartup = false; //desabilita corretor ortografico
-   CKEDITOR.config.forcePasteAsPlainText = true; //Elimina todo css em um texto copiado e colado dentro do CKEDITOR.
-   CKEDITOR.config.fontSize_sizes = '12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;'; //limita tamanho de fontes possiveis
-   
-CKEDITOR.config.wordcount = {
-
-    // Whether or not you want to show the Paragraphs Count
-    showParagraphs: false,
-
-    // Whether or not you want to show the Word Count
-    showWordCount: false,
-
-    // Whether or not you want to show the Char Count
-    showCharCount: true,
-
-    // Whether or not you want to count Spaces as Chars
-    countSpacesAsChars: false,
-
-    // Whether or not to include Html chars in the Char Count
-    countHTML: false,
+/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.editorConfig = function( config )
+{
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
     
-    // Maximum allowed Word Count, -1 is default for unlimited
-  //  maxWordCount: -1,
-
-    // Maximum allowed Char Count, -1 is default for unlimited
-    maxCharCount: 8000,
-
-    // Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
-    filter: new CKEDITOR.htmlParser.filter({
-        elements: {
-            div: function( element ) {
-                if(element.attributes.class == 'mediaembed') {
-                    return false;
-                }
-            }
-        }
-    })
+    /* Adicionado por Tarcisio Angelo */
+    config.toolbar = [
+//        ['Cut', 'Copy', 'Paste', 'PasteText', '-', 'Print'],
+//        ['Bold','Italic','Underline','Strike','-','Subscript'],
+//        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+//        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+//        ['Link','Unlink','Anchor'],
+//        ['Font','FontSize'],
+//        ['TextColor','BGColor']
+    ];
 };
-
-
-                                                                                                                                      
+CKEDITOR.config.scayt_autoStartup = false; //desabilita corretor ortografico
+CKEDITOR.config.fontSize_sizes = '12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;'; //limita tamanho de fontes possiveis
+CKEDITOR.config.forcePasteAsPlainText = true; //Elimina todo css em um texto copiado e colado dentro do CKEDITOR.
