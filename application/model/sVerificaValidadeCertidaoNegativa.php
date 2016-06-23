849560a9d2d078bc7297e6020e66e977b1bcf281
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-06-23 16:23:32 -0300

    Correção segurança - sVerificaValidadeCertidaoNegativa com método buscarDadosSemSP implementado em esquema zend

--- application/model/sVerificaValidadeCertidaoNegativa.php
+++ application/model/sVerificaValidadeCertidaoNegativa.php
@@ -19,41 +19,45 @@ class sVerificaValidadeCertidaoNegativa extends GenericModel {
         return $db->fetchAll($sql);
     }
     
-    public function buscarDadosSemSP($cpfcnpj){
-        $sql = "
-                SELECT 
-                    CASE
-                        WHEN c.CodigoCertidao = '49'
-                        THEN 'Quitação de Tributos Federais'
-                        WHEN c.CodigoCertidao = '51'
-                        THEN 'FGTS'
-                        WHEN c.CodigoCertidao = '52'
-                        THEN 'INSS'
-                        WHEN c.CodigoCertidao = '244'
-                        THEN 'CADIN'
-                END AS dsCertidao,
-                c.CodigoCertidao, c.DtEmissao, c.DtValidade, c.AnoProjeto+c.Sequencial AS Pronac,
-                CASE
-                    WHEN c.CodigoCertidao = 244
-                    THEN 
+    public function buscarDadosSemSP($cpfcnpj)
+    {
+        $table = Zend_Db_Table::getDefaultAdapter();
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+
+        $select = $table->select()
+            ->from('CertidoesNegativas',
+                array(new Zend_Db_Expr('
+                   CASE
+                            WHEN CodigoCertidao = \'49\'
+                            THEN \'Quitação de Tributos Federais\'
+                            WHEN CodigoCertidao = \'51\'
+                            THEN \'FGTS\'
+                            WHEN CodigoCertidao = \'52\'
+                            THEN \'INSS\'
+                            WHEN CodigoCertidao = \'244\'
+                            THEN \'CADIN\'
+                        END AS dsCertidao,
+                        CodigoCertidao, DtEmissao, DtValidade, AnoProjeto+Sequencial AS Pronac,
                         CASE
-                            WHEN c.cdSituacaoCertidao = 0
-                            THEN 'Pendente'
-                            ELSE 'Não Pendente'
-                        END
-                END AS Situacao,
-                CASE
-                    WHEN c.CodigoCertidao = 244
-                    THEN DateDiff(dy,getdate(),c.DtEmissao)
-                    ELSE DateDiff(dy,getdate(),c.DtValidade)
-                END AS qtDias
-            FROM SAC.dbo.CertidoesNegativas c
-            WHERE c.CgcCpf = '".$cpfcnpj."'
-            ORDER BY 2";
-        
-        $db = Zend_Registry :: get('db');
-        $db->setFetchMode(Zend_DB :: FETCH_OBJ);
-        return $db->fetchAll($sql);
+                            WHEN CodigoCertidao = 244
+                            THEN
+                                CASE
+                                    WHEN cdSituacaoCertidao = 0
+                                    THEN \'Pendente\'
+                                    ELSE \'Não Pendente\'
+                                END
+                        END AS Situacao,
+                        CASE
+                            WHEN CodigoCertidao = 244
+                            THEN DateDiff(dy,getdate(),DtEmissao)
+                            ELSE DateDiff(dy,getdate(),DtValidade)
+                   END AS qtDias
+                ')),
+                'SAC.dbo')
+            ->where('CgcCpf = ?',$cpfcnpj);
+
+        return $db->fetchAll($select);
     }
 }
 ?>
