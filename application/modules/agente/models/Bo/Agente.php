163db4bdef3dec92a8c855d65119f6985fc87714
Wouerner <12307444793@cultura.gov.br>
2016-07-15 17:01:38 -0300

    Refatoração de metodo listarPropostasResultado, model PreProjeto

--- application/modules/agente/models/Bo/Agente.php
+++ application/modules/agente/models/Bo/Agente.php
@@ -1,1 +1,9 @@
- 
+<?php
+/**
+ * Agente_Model_Bo_Agentes
+ *
+ * @author  wouerner <wouerner@gmail.com>
+ */
+class Agente_Model_Bo_Agentes
+{
+}
--- application/modules/agente/models/Orm/AgenteTable.php
+++ application/modules/agente/models/Orm/AgenteTable.php
@@ -1,1 +1,16 @@
- 
+<?php
+
+/**
+ * Agente_Model_Orm_AgentesTable
+ *
+ * @uses GenericModel
+ * @author  woeurner <wouerner@gmail.com>
+ */
+class Agente_Model_Orm_AgentesTable extends GenericModel
+{
+    protected $_name = 'agentes';
+    protected $_primary = 'idAgente';
+    protected $_schema = 'agentes.dbo';
+
+    protected $_rowClass = 'Agente_Model_Vo_Agente';
+}
