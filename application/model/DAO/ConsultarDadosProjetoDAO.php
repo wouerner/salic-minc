a72d9c6f4739a77a0e45be85dd2254ab356ea905
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-08-08 17:40:11 -0300

    [ConsultarDadosProjetoDAO] - obterDadosProjeto - Alterando para esquema zend_db

--- application/model/DAO/ConsultarDadosProjetoDAO.php
+++ application/model/DAO/ConsultarDadosProjetoDAO.php
@@ -2,58 +2,88 @@
 
 class ConsultarDadosProjetoDAO extends Zend_Db_Table {
 
-    public static function obterDadosProjeto($dados = array()) {
-        /**
-         * @todo: Implementar maneira correta utilizando models.
-         */
+    public static function obterDadosProjeto($dados = array())
+    {
         $retorno = false;
         if ($dados['idPronac']) {
-            $sql = "SELECT p.IdPRONAC, p.idProjeto, p.AnoProjeto+p.Sequencial as NrProjeto,p.NomeProjeto,UfProjeto,a.Descricao as Area,
-                s.Descricao as Segmento, m.Descricao as Mecanismo,p.Situacao + ' - ' + si.Descricao as Situacao,
-                convert(varchar(10),DtSituacao,103) as DtSituacao,ProvidenciaTomada,
-                isnull(sac.dbo.fnValorDaProposta(idProjeto),sac.dbo.fnValorSolicitado(p.AnoProjeto,p.Sequencial)) as ValorProposta,
-                sac.dbo.fnValorSolicitado(p.AnoProjeto,p.Sequencial) as ValorSolicitado,
-                sac.dbo.fnOutrasFontes(p.idPronac) as OutrasFontes,
-                case
-                when p.Mecanismo ='2' or p.Mecanismo ='6'
-                then sac.dbo.fnValorAprovadoConvenio(p.AnoProjeto,p.Sequencial)
-                else sac.dbo.fnValorAprovado(p.AnoProjeto,p.Sequencial)
-                end as ValorAprovado,
-                case
-                when p.Mecanismo ='2' or p.Mecanismo ='6'
-                then sac.dbo.fnValorAprovadoConvenio(p.AnoProjeto,p.Sequencial)
-                else sac.dbo.fnValorAprovado(p.AnoProjeto,p.Sequencial) + sac.dbo.fnOutrasFontes(p.idPronac) end as ValorProjeto,
-                sac.dbo.fnCustoProjeto (p.AnoProjeto,p.Sequencial) as ValorCaptado,
-                p.CgcCPf,Nome as Proponente,sac.dbo.fnFormataProcesso(p.idPronac) as Processo,
-                tabelas.dbo.fnEstruturaOrgao(p.Orgao,0) as Origem,
-                h.Destino,h.DtTramitacaoEnvio,h.dtTramitacaoRecebida,h.Situacao as Estado,
-                sac.dbo.fnNomeUsuario(idUsuarioEmissor)  as Emissor,
-                sac.dbo.fnNomeUsuario(idUsuarioReceptor) as Receptor,
-                h.meDespacho,p.ResumoProjeto,case
-                when Enquadramento = '1'
-                then 'Artigo 26'
-                when Enquadramento = '2'
-                then 'Artigo 18'
-                else 'Não enquadrado'
-                end as Enquadramento, p.Situacao as codSituacao
-                FROM SAC.dbo.Projetos p
-                LEFT JOIN SAC.dbo.Enquadramento e on (p.idPronac = e.idPronac)
-                INNER JOIN SAC.dbo.Interessado i on (p.CgcCPf = i.CgcCPf)
-                INNER JOIN SAC.dbo.Area a on (p.Area = a.Codigo)
-                INNER JOIN SAC.dbo.Segmento s on (p.Segmento = s.Codigo)
-                INNER JOIN SAC.dbo.Mecanismo m on (p.Mecanismo = m.Codigo)
-                INNER JOIN SAC.dbo.Situacao si on (p.Situacao = si.Codigo)
-                LEFT JOIN SAC.dbo.vwTramitarProjeto h on (p.idPronac = h.idPronac)
-                WHERE p.IdPRONAC='{$dados['idPronac']}'";
-                //WHERE p.AnoProjeto+p.Sequencial='{$dados['idPronac']}'";
+
+            $table = Zend_Db_Table::getDefaultAdapter();
+
+            $select = $table->select()
+                ->from(array('p' => 'Projetos'),
+                    array('IdPRONAC',
+                        'idProjeto',
+                        'CgcCPf',
+                        'ResumoProjeto',
+                        'NomeProjeto',
+                        'UfProjeto',
+                        new Zend_Db_Expr('p.AnoProjeto+p.Sequencial AS NrProjeto'),
+                        new Zend_Db_Expr('SAC.dbo.fnCustoProjeto (p.AnoProjeto,p.Sequencial) AS ValorCaptado'),
+                        new Zend_Db_Expr('Nome as Proponente,sac.dbo.fnFormataProcesso(p.idPronac) AS Processo'),
+                        new Zend_Db_Expr('p.Situacao + \' - \' + si.Descricao AS Situacao'),
+                        new Zend_Db_Expr('SAC.dbo.fnNomeUsuario(idUsuarioEmissor)  AS Emissor'),
+                        new Zend_Db_Expr('SAC.dbo.fnNomeUsuario(idUsuarioReceptor) as Receptor'),
+                        new Zend_Db_Expr('TABELAS.dbo.fnEstruturaOrgao(p.Orgao,0) AS Origem'),
+                        new Zend_Db_Expr('convert(varchar(10),DtSituacao,103) as DtSituacao,ProvidenciaTomada'),
+                        new Zend_Db_Expr('isnull(sac.dbo.fnValorDaProposta(idProjeto)'),
+                        new Zend_Db_Expr('SAC.dbo.fnValorSolicitado(p.AnoProjeto,p.Sequencial)) AS ValorProposta'),
+                        new Zend_Db_Expr('SAC.dbo.fnValorSolicitado(p.AnoProjeto,p.Sequencial) AS ValorSolicitado'),
+                        new Zend_Db_Expr('SAC.dbo.fnOutrasFontes(p.idPronac) AS OutrasFontes'),
+                        new Zend_Db_Expr('tabelas.dbo.fnEstruturaOrgao(p.Orgao,0) AS Origem'),
+                        new Zend_Db_Expr('
+                            CASE
+                                WHEN p.Mecanismo =\'2\' OR p.Mecanismo =\'6\' THEN sac.dbo.fnValorAprovadoConvenio(p.AnoProjeto,p.Sequencial)
+                                ELSE sac.dbo.fnValorAprovado(p.AnoProjeto,p.Sequencial)
+                            END AS ValorAprovado,
+                            CASE
+                                WHEN p.Mecanismo =\'2\' OR p.Mecanismo =\'6\' THEN sac.dbo.fnValorAprovadoConvenio(p.AnoProjeto,p.Sequencial)
+                                ELSE sac.dbo.fnValorAprovado(p.AnoProjeto,p.Sequencial) + sac.dbo.fnOutrasFontes(p.idPronac)
+                            END AS ValorProjeto,
+                            CASE
+                                WHEN Enquadramento = \'1\' THEN \'Artigo 26\'
+                                WHEN Enquadramento = \'2\' THEN \'Artigo 18\'
+                                ELSE \'Não enquadrado\'
+                            END as Enquadramento, p.Situacao AS codSituacao')
+                         ),
+                    'SAC.dbo')
+                ->joinLeft(array('e' => 'Enquadramento'),
+                    'p.idPronac = e.idPronac',
+                    array(''),
+                    'SAC.dbo')
+                ->joinInner(array('i' => 'Interessado'),
+                    'p.CgcCPf = i.CgcCPf',
+                    array(''),
+                    'SAC.dbo')
+                ->joinInner(array('a' => 'Area'),
+                    'p.Area = a.Codigo',
+                    array(new Zend_Db_Expr('a.Descricao as Area')),
+                    'SAC.dbo')
+                ->joinInner(array('s' => 'Segmento'),
+                    'p.Segmento = s.Codigo',
+                    array(new Zend_Db_Expr('s.Descricao AS Segmento')),
+                    'SAC.dbo')
+                ->joinInner(array('m' => 'Mecanismo'),
+                    'p.Mecanismo = m.Codigo',
+                    array(new Zend_Db_Expr('m.Descricao as Mecanismo')),
+                    'SAC.dbo')
+                ->joinInner(array('si' => 'Situacao'),
+                    'p.Situacao = si.Codigo',
+                    array(''),
+                    'SAC.dbo')
+                ->joinLeft(array('h' => 'vwTramitarProjeto'),
+                    'p.idPronac = h.idPronac',
+                    array('Destino', 'DtTramitacaoEnvio', 'dtTramitacaoRecebida', new Zend_Db_Expr('h.Situacao AS Estado'), 'meDespacho'),
+                    'SAC.dbo')
+                ->where('p.IdPRONAC = ?', $dados['idPronac']);
+
             try {
                 $db = Zend_Registry::get('db');
                 $db->setFetchMode(Zend_DB::FETCH_OBJ);
             } catch (Zend_Exception_Db $e) {
                 $this->view->message = "Falha ao buscar projeto: " . $e->getMessage();
             }
-//            xd($sql);
-            $retorno = $db->fetchAll($sql);
+
+            $retorno = $db->fetchAll($select);
         }
         return $retorno;
     }
