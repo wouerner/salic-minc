32978d8138a37a9f06f677c0e2b80ff8a91372e3
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-08-11 17:16:15 -0300

    Remoção de injection em paRegularidade.php

--- application/model/paRegularidade.php
+++ application/model/paRegularidade.php
@@ -14,8 +14,12 @@ class paRegularidade extends GenericModel {
     protected $_name  = 'paRegularidade';
 
     public function exec($CNPJCPF){
+
+        $CNPJCPF = preg_replace("/[^0-9]/","",$CNPJCPF); //REMOVE injections
+
         $sql = "exec ".$this->_banco.".dbo.".$this->_name." '$CNPJCPF' ";
         $db = Zend_Registry :: get('db');
+
         $db->setFetchMode(Zend_DB :: FETCH_OBJ);
         return $db->fetchAll($sql);
     }
