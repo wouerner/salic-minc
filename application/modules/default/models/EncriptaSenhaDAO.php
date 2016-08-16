f1340a40de8e26fa181761d82f0394977f85940e
Ruy Ferreira <ruyjfs@gmail.com>
2016-08-15 21:30:08 -0300

    [ruy.silva] - Refatorar a função encryptaSenha do banco SQLServer para o forma do php - Trocar as chamads que usam o encryptaSenha.

--- application/modules/default/models/EncriptaSenhaDAO.php
+++ application/modules/default/models/EncriptaSenhaDAO.php
@@ -9,15 +9,113 @@
  *
  * @author tisomar
  */
-class EncriptaSenhaDAO extends Zend_Db_Table {
+class EncriptaSenhaDAO extends Zend_Db_Table
+{
 
-    public static function encriptaSenha($cpf, $senha)
-	{
-		$sql = "SELECT tabelas.dbo.fnEncriptaSenha('" . $cpf . "', '$senha' ) as senha";
-                $db = Zend_Registry::get('db');
-                $db->setFetchMode(Zend_DB::FETCH_OBJ);
-                return $db->fetchAll($sql);
-	} // fecha método enviarEmail()
+    /**
+     * funcao do banco de dados do sql server no formato PHP.
+     *
+     * @name encriptaSenha
+     * @param $username
+     * @param $password
+     * @return string
+     *
+     * @author Ruy Junior Ferreira Silva ruyjfs@gmail.com
+     * @since 12/08/2016
+     *
+     * Exemplo do formado do SQL Server:
+     *
+     *        BEGIN
+     *    DECLARE
+     *    @w	varchar(30),
+     *        @s      varchar(15),
+     *        @t1	int,
+     *	@t2	int,
+     *        @k      int,
+     *        @i      int,
+     *        @j      int,
+     *        @f      int,
+     *        @v      int
+     *
+     *    SET @w = RTRIM(LTRIM(@p_senha))
+     *    SET @t1 = LEN(RTRIM(LTRIM(@p_identificacao)))
+     *    SET @t2 = LEN(@w)
+     *    IF @t2 < 1
+     *    BEGIN
+     *        SET @p_senha = '??????'
+     *        SET @w = '??????'
+     *        SET @t2 = 6
+     *    END
+     *    WHILE LEN(@w) < 15
+     *    BEGIN
+     *        SET @w = @w + @w
+     *    END
+     *    SET @w = SUBSTRING(@w, 1, 15)
+     *    SET @k = ASCII(SUBSTRING(@w, 1, 1)) + 2
+     *    SET @s = ''
+     *    SET @i = 0
+     *    WHILE @i < 15
+     *    BEGIN
+     *        SET @i = @i + 1
+     *        SET @v = (@t1 + @t2) * @k / @i
+     *        SET @f = ASCII(SUBSTRING(@w, 1, 1))
+     *        SET @w = SUBSTRING(@w, 2, 15)
+     *        SET @j = ((@f * @k) + @t1 + (@t2 * @f)) / @i
+     *        SET @v = @v + @j
+     *        IF @v < 33
+     *        BEGIN
+     *            SET @v = @v + (@t1 * @i)
+     *        END
+     *        SET @j = @v % 94
+     *        SET @s = @s + CHAR(33 + @j)
+     *    END
+     *    RETURN @s
+     */
+    public static function encriptaSenha($username, $password)
+    {
+        $w = trim($password);
+        $t1 = strlen(trim($username));
+        $t2 = strlen($w);
+
+        if ($t2 < 1) {
+            $password = '??????';
+            $w = '??????';
+            $t2 = 6;
+        }
+
+        while (strlen($w) < 15) {
+            $w = $w . $w;
+        }
+
+        $w = substr($w, 1, 15);
+        $k = ord((substr($w, 1, 1))) + 2;
+        $s = '';
+        $i = 0;
+
+        while ($i < 15) {
+            $i = $i + 1;
+            $v = ($t1 + $t2) * $k / $i;
+            $f = ord(substr($w, 1, 1));
+            $w = substr($w, 2, 15);
+            $j = (($f * $k) + $t1 + ($t2 * $f)) / $i;
+            $v = $v + $j;
+            if ($v < 33) {
+                $v = $v + ($t1 * $i);
+            }
+            $j = $v % 94;
+            $s = $s . (33 + $j);
+        }
+
+        return $s;
+    }
+
+//    public static function encriptaSenha($cpf, $senha)
+//	{
+//		$sql = "SELECT tabelas.dbo.fnEncriptaSenha('" . $cpf . "', '$senha' ) as senha";
+//                $db = Zend_Registry::get('db');
+//                $db->setFetchMode(Zend_DB::FETCH_OBJ);
+//                return $db->fetchAll($sql);
+//	} // fecha mï¿½todo enviarEmail()
 
 }
 ?>
