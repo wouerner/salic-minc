f0afa9d170566d7cb38e5e2f3a2ba344cf04e24a
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-08-18 15:08:01 -0300

    [ update ] Ajustes para que o vínculo de responsáveis funcione corretamente. Refatoração de código-fonte.

--- application/modules/default/models/tbVinculoPropostaResponsavelProjeto.php
+++ application/modules/default/models/tbVinculoPropostaResponsavelProjeto.php
@@ -1,41 +1,40 @@
 <?php
+
 /**
- * tbVinculoPropostaResponsavelProjeto
- *
+ * Class tbVinculoPropostaResponsavelProjeto
  * @uses GenericModel
  * @author  wouerner <wouerner@gmail.com>
+ * @author Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
+ * @since 18/08/2016 14:29
  */
-class tbVinculoPropostaResponsavelProjeto extends GenericModel{
-
-    protected $_banco = 'Agentes';
-    protected $_name = 'tbVinculoProposta';
-    protected $_schema = 'Agentes';
+class tbVinculoPropostaResponsavelProjeto extends GenericModel
+{
+    protected $_banco = 'agentes';
+    protected $_name = 'tbvinculoproposta';
+    protected $_schema = 'agentes';
+    protected $_primary = 'idvinculoproposta';
 
     /**
-     * buscarResponsaveisProponentes
-     *
-     * @param bool $where
-     * @access public
-     * @return void
+     * @param array $where
+     * @return Zend_Db_Table_Rowset_Abstract
      */
-    public function buscarResponsaveisProponentes($where=array())
+    public function buscarResponsaveisProponentes($where = array())
     {
         $slct = $this->select();
         $slct->distinct();
         $slct->setIntegrityCheck(false);
 
         $slct->from(
-                array('VP' => $this->_name),
-                array('*')
+            array('VP' => $this->_name),
+            array('*')
         );
 
         $slct->joinInner(
-                array('VI' => 'tbVinculo'),'VI.idVinculo = VP.idVinculo',
-                array('*')
+            array('VI' => 'tbVinculo'), 'VI.idVinculo = VP.idVinculo',
+            array('*')
         );
 
-        foreach ($where as $coluna => $valor)
-        {
+        foreach ($where as $coluna => $valor) {
             $slct->where($coluna, $valor);
         }
 
