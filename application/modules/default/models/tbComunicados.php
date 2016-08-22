3733b7c35f5d424388e874d7a4272e8a9055a9a3
Ruy Ferreira <ruyjfs@gmail.com>
2016-08-22 17:49:30 -0300

    [ruy.silva] Estrtuturando o codigo para rodar com PostgreSQL e SQLServer, fatorando tambem os acentos.

--- application/modules/default/models/tbComunicados.php
+++ application/modules/default/models/tbComunicados.php
@@ -27,6 +27,8 @@ class tbComunicados extends GenericModel {
      * @param int $tamanho - numero de registros que deve retornar
      * @param int $inicio - offset
      * @return Zend_Db_Table_Rowset_Abstract
+     *
+     * @todo trocar as conversoes de datas conforme o banco
      */
     public function listarComunicados($where=array(), $order=array(), $tamanho=-1, $inicio=-1, $count=false) {
         $slct = $this->select();
