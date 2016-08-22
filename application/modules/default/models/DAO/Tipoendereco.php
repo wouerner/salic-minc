0c831d6e2bd7544703f72da6e5349b5b41e1621f
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-08-22 17:51:23 -0300

    [ update ] - Ajustes para que o cadastro de login, alteração de senha e acesso ao cadastro de proponente para o usuário que ainda não tem perfil de proponente se cadastre. - Refatoração de código-fonte; - Adição de chamadas estáticas para GenericModel em casos de classes que possui métodos são estáticos e precisam obter corretamente o nome da tabela e seu schema. - Adição de propriedades em entidades. - Implementação de Singleton + inversão de controle em algumas classes, para evitar o uso de chamadas estáticas.

--- application/modules/default/models/DAO/Tipoendereco.php
+++ application/modules/default/models/DAO/Tipoendereco.php
@@ -1,44 +1,63 @@
 <?php
+
 /**
  * Modelo Tipoendereco
  * @author Equipe RUP - Politec
+ *
  * @since 29/03/2010
  * @version 1.0
  * @package application
  * @subpackage application.models
- * @copyright © 2010 - MinistÈrio da Cultura - Todos os direitos reservados.
+ * @copyright ÔøΩ 2010 - MinistÔøΩrio da Cultura - Todos os direitos reservados.
  * @link http://www.cultura.gov.br
  */
-
 class Tipoendereco extends Zend_Db_Table
 {
-	protected $_name = 'AGENTES.dbo.Verificacao'; // nome da tabela
+    protected $_banco = 'agentes';
+    protected $_schema = 'agentes';
+    protected $_name = 'verificacao';
 
+    /**
+     * @var Zend_Db_Table
+     */
+    private static $instancia;
 
+    /**
+     * Respons√°vel por implementar o Singleton, retornando apenas uma instancia da classe
+     * utilizando uma chamada est√°tica.
+     * @return Zend_Db_Table
+     * @author Vin√≠cius Feitosa da Silva <viniciusfesil@mail.com>
+     */
+    public static function obterInstancia()
+    {
+        if (!self::$instancia) {
+            self::$instancia = new Tipoendereco();
+        }
+        return self::$instancia;
+    }
 
-	/**
-	 * MÈtodo para buscar todos os tipos de endereÁos
-	 * @access public
-	 * @param void
-	 * @return object $db->fetchAll($sql)
-	 */
-	public static function buscar()
-	{
-		$sql = "SELECT idVerificacao AS id, descricao ";
-		$sql.= "FROM AGENTES.dbo.Verificacao ";
-		$sql.= "WHERE idTipo = 2 ";
-		$sql.= "ORDER BY descricao;";
+    /**
+     * M√©todo para buscar todos os tipos de endere√ßos
+     * @access public
+     * @author Vin√≠cius Feitosa da Silva <viniciusfesil@mail.com>
+     * @param void
+     * @return array
+     */
+    public static function buscar()
+    {
+        $objEstado = self::obterInstancia();
+        $sql = "SELECT idVerificacao AS id, descricao ";
+        $sql .= "FROM  " . GenericModel::getStaticTableName($objEstado->_schema, $objEstado->_name);
+        $sql .= " WHERE idtipo = 2 ";
+        $sql .= "ORDER BY descricao;";
 
-		try
-		{
-			$db  = Zend_Registry::get('db');
-			$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		}
-		catch (Zend_Exception_Db $e)
-		{
-			$this->view->message = "Erro ao buscar Tipos de EndereÁos: " . $e->getMessage();
-		}
+        try {
+            $db = Zend_Registry::get('db');
+            $db->setFetchMode(Zend_DB::FETCH_OBJ);
+            return $db->fetchAll($sql);
+        } catch (Zend_Exception_Db $objException) {
+            throw new Exception("Erro ao buscar Tipos de EndereÔøΩos: " . $objException->getMessage(), 0, $objException);
+        }
 
-		return $db->fetchAll($sql);
-	} // fecha buscar()
-} // fecha class
+    }
+}
--- application/modules/default/models/DAO/UsuarioDAO.php
+++ application/modules/default/models/DAO/UsuarioDAO.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Classe Usu�rio DAO
  * @author Equipe RUP - Politec
@@ -9,21 +10,24 @@
  * @copyright � 2010 - Minist�rio da Cultura - Todos os direitos reservados.
  * @link http://www.cultura.gov.br
  */
-
-class UsuarioDAO extends Zend_Db_Table
+class UsuarioDAO extends GenericModel
 {
+    protected $_banco = "tabelas";
+    protected $_name = 'usuarios';
+    protected $_schema = 'tabelas';
+
     /**
-	 * M�todo para buscar os dados do usu�rio de acordo com login e senha
-	 * @access public
-	 * @static
-	 * @param @username (cpf ou cnpj do usu�rio)
-	 * @param @password (senha do usu�rio criptografada)
-	 * @return bool
-	 */
-	public static function login($username, $password)
-	{
-		// busca o usu�rio de acordo com o login e a senha
-		$sql = "SELECT usu_codigo
+     * M�todo para buscar os dados do usu�rio de acordo com login e senha
+     * @access public
+     * @static
+     * @param @username (cpf ou cnpj do usu�rio)
+     * @param @password (senha do usu�rio criptografada)
+     * @return bool
+     */
+    public static function login($username, $password)
+    {
+        // busca o usu�rio de acordo com o login e a senha
+        $sql = "SELECT usu_codigo
 					,usu_nome
 					,usu_identificacao
 					,usu_senha
@@ -36,83 +40,79 @@ class UsuarioDAO extends Zend_Db_Table
 									 WHERE usu_identificacao = '" . $username . "')
 					AND usu_status = 1";
 
-		$db = Zend_Registry::get('db');
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		$buscar = $db->fetchAll($sql);
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        $buscar = $db->fetchAll($sql);
 
-		if ($buscar) // realiza a autentica��o
-		{
-			// configura��es do banco
-			$authAdapter = new Zend_Auth_Adapter_DbTable(Zend_Registry::get('db'));
-			$authAdapter->setTableName('dbo.Usuarios') // TABELAS.dbo.Usuarios
-				->setIdentityColumn('usu_identificacao')
-				->setCredentialColumn('usu_senha');
+        if ($buscar) // realiza a autentica��o
+        {
+            // configura��es do banco
+            $authAdapter = new Zend_Auth_Adapter_DbTable(Zend_Registry::get('db'));
+            $authAdapter->setTableName('dbo.Usuarios')// TABELAS.dbo.Usuarios
+            ->setIdentityColumn('usu_identificacao')
+                ->setCredentialColumn('usu_senha');
 
-			// seta as credenciais informada pelo usu�rio
-			$authAdapter
-				->setIdentity($buscar[0]->usu_identificacao)
-				->setCredential($buscar[0]->usu_senha);
+            // seta as credenciais informada pelo usu�rio
+            $authAdapter
+                ->setIdentity($buscar[0]->usu_identificacao)
+                ->setCredential($buscar[0]->usu_senha);
 
-			// tenta autenticar o usu�rio
-			$auth   = Zend_Auth::getInstance();
-			$acesso = $auth->authenticate($authAdapter);
+            // tenta autenticar o usu�rio
+            $auth = Zend_Auth::getInstance();
+            $acesso = $auth->authenticate($authAdapter);
 
-			// verifica se o acesso foi permitido
-			if ($acesso->isValid())
-			{
-				// pega os dados do usu�rio com exce��o da senha
-				$authData = $authAdapter->getResultRowObject(null, 'usu_senha');
+            // verifica se o acesso foi permitido
+            if ($acesso->isValid()) {
+                // pega os dados do usu�rio com exce��o da senha
+                $authData = $authAdapter->getResultRowObject(null, 'usu_senha');
 
-				// armazena os dados do usu�rio
-				$objAuth = $auth->getStorage()->write($authData);
+                // armazena os dados do usu�rio
+                $objAuth = $auth->getStorage()->write($authData);
 
-				return true;
-			} // fecha if
-			else // caso n�o tenha sido validado
-			{
-				return false;
-			}
-		} // fecha if
-		else
-		{
-			return false;
-		}
-	} // fecha m�todo login()
+                return true;
+            } // fecha if
+            else // caso n�o tenha sido validado
+            {
+                return false;
+            }
+        } // fecha if
+        else {
+            return false;
+        }
+    } // fecha m�todo login()
 
 
-
-	/**
-	 * M�todo para alterar a senha do usu�rio do sistema
-	 * @access public
-	 * @param @username (cpf ou cnpj do usu�rio)
-	 * @param @password (nova senha do usu�rio)
-	 * @return object
-	 */
-	public function alterarSenha($username, $password)
-	{
-		$sql = "UPDATE Tabelas.dbo.Usuarios
+    /**
+     * M�todo para alterar a senha do usu�rio do sistema
+     * @access public
+     * @param @username (cpf ou cnpj do usu�rio)
+     * @param @password (nova senha do usu�rio)
+     * @return object
+     */
+    public function alterarSenha($username, $password)
+    {
+        $sql = "UPDATE Tabelas.dbo.Usuarios
 					SET usu_senha = TABELAS.dbo.fnEncriptaSenha('" . $username . "', '" . $password . "')
 				WHERE usu_identificacao = '" . $username . "'";
 
-		$db = Zend_Registry::get('db');
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		return $db->fetchAll($sql);
-	} // fecha m�todo alterarSenha()
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        return $db->fetchAll($sql);
+    } // fecha m�todo alterarSenha()
 
 
-
-	/**
-	 * M�todo para buscar as unidades autorizadas do usu�rio do sistema
-	 * @access public
-	 * @param @usu_codigo (c�digo do usu�rio)
-	 * @param @sis_codigo (c�digo sistema)
-	 * @param @gru_codigo (c�digo do grupo)
-	 * @param @uog_orgao  (c�digo do �rg�o)
-	 * @return object
-	 */
-	public function buscarUnidades($usu_codigo, $sis_codigo = null, $gru_codigo = null, $uog_orgao = null)
-	{
-		$sql = "SELECT usu_orgao
+    /**
+     * M�todo para buscar as unidades autorizadas do usu�rio do sistema
+     * @access public
+     * @param @usu_codigo (c�digo do usu�rio)
+     * @param @sis_codigo (c�digo sistema)
+     * @param @gru_codigo (c�digo do grupo)
+     * @param @uog_orgao  (c�digo do �rg�o)
+     * @return object
+     */
+    public function buscarUnidades($usu_codigo, $sis_codigo = null, $gru_codigo = null, $uog_orgao = null)
+    {
+        $sql = "SELECT usu_orgao
 					,usu_orgaolotacao
 					,uog_orgao
 					,org_siglaautorizado
@@ -126,188 +126,145 @@ class UsuarioDAO extends Zend_Db_Table
 
 				WHERE usu_codigo = $usu_codigo ";
 
-		if (!empty($sis_codigo))
-		{
-			$sql.= "AND sis_codigo = $sis_codigo ";
-		}
-		if (!empty($gru_codigo))
-		{
-			$sql.= "AND gru_codigo = $gru_codigo ";
-		}
-		if (!empty($uog_orgao))
-		{
-			$sql.= "AND uog_orgao = $uog_orgao ";
-		}
+        if (!empty($sis_codigo)) {
+            $sql .= "AND sis_codigo = $sis_codigo ";
+        }
+        if (!empty($gru_codigo)) {
+            $sql .= "AND gru_codigo = $gru_codigo ";
+        }
+        if (!empty($uog_orgao)) {
+            $sql .= "AND uog_orgao = $uog_orgao ";
+        }
 
-		$sql.= "ORDER BY org_siglaautorizado";
-		//die($sql);
+        $sql .= "ORDER BY org_siglaautorizado";
+        //die($sql);
 
-		$db = Zend_Registry::get('db');
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		return $db->fetchAll($sql);
-	}
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        return $db->fetchAll($sql);
+    }
 
 
-
-	/**
-	 * M�todo para pegar o id do usu�rio logado
-	 * @access public
-	 * @static
-	 * @param integer
-	 * @return object
-	 */
-	public static function getIdUsuario($usu_codigo)
-	{
-		$sql = "SELECT usu_codigo
+    /**
+     * M�todo para pegar o id do usu�rio logado
+     * @access public
+     * @static
+     * @param integer
+     * @return object
+     */
+    public static function getIdUsuario($usu_codigo)
+    {
+        $sql = "SELECT usu_codigo
 					,idAgente
 				FROM Tabelas.dbo.Usuarios u
 					INNER JOIN Agentes.dbo.Agentes a ON (u.usu_identificacao = a.CNPJCPF)
 				WHERE usu_codigo = $usu_codigo";
 
-		try
-		{
-			$db = Zend_Registry::get('db');
-			$db->setFetchMode(Zend_DB::FETCH_ASSOC);
-			return $db->fetchRow($sql);
-		}
-		catch (Zend_Exception_Db $e)
-		{
-			$this->view->message = $e->getMessage();
-		}
-	} // fecha m�todo getIdUsuario()
-
-
-
-	/**
-	 * Metodo para buscar todos os dados do usuario de acordo com o seu codigo vindo do SCRIPTCASE
-	 * @access public
-	 * @static
-	 * @param @cod (codigo do usuario)
-	 * @return object
-     *
-     * @todo melhorar codigo pois esta fazendo a mesma verificacao de tipo de banco que na abstract da model.
-	 */
-	public static function buscarUsuarioScriptcase($cod)
-	{
-        $db = Zend_Registry::get('db');
-
-        $schema = 'controledeacesso';
-        if ($db instanceof Zend_Db_Adapter_Pdo_Mssql){
-            $schema = $schema . '.dbo';
+        try {
+            $db = Zend_Registry::get('db');
+            $db->setFetchMode(Zend_DB::FETCH_ASSOC);
+            return $db->fetchRow($sql);
+        } catch (Zend_Exception_Db $objException) {
+            throw new Exception($e->getMessage(), 0, $objException);
         }
+    } // fecha m�todo getIdUsuario()
 
-		$sql = "SELECT *
-				FROM {$schema}.sgcacesso
-				WHERE idusuario = $cod";
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
 
-		return $db->fetchAll($sql);
-	} // fecha m�todo buscarUsuarioScriptcase()
+    /**
+     * Metodo para buscar todos os dados do usuario de acordo com o seu codigo vindo do SCRIPTCASE
+     * @access public
+     * @static
+     * @param @cod (codigo do usuario)
+     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
+     * @return object
+     */
+    public static function buscarUsuarioScriptcase($idusuario)
+    {
+        return (new Autenticacao_Model_Sgcacesso())->buscar(array('idusuario = ?' => $idusuario));
+    }
 
-	public static function buscarUsuario($cod)
-	{
-		$sql = "SELECT *
+    public static function buscarUsuario($cod)
+    {
+        $sql = "SELECT *
 				FROM TABELAS.dbo.Usuarios
 				WHERE usu_codigo = $cod";
 
-		$db = Zend_Registry::get('db');
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		return $db->fetchAll($sql);
-	}
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        return $db->fetchAll($sql);
+    }
 
-	public static function buscarUsuarioCpf($cpf)
-	{
-		$sql = "SELECT *
+    public static function buscarUsuarioCpf($cpf)
+    {
+        $sql = "SELECT *
 				FROM TABELAS.dbo.Usuarios
 				WHERE usu_identificacao = $cpf";
 
-		$db = Zend_Registry::get('db');
-		$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		return $db->fetchAll($sql);
-	}
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        return $db->fetchAll($sql);
+    }
 
-	/**
-	 * M�todo para buscar os dados do usu�rio de acordo com id (login scriptcase)
-	 * @access public
-	 * @static
-	 * @param @cod (c�digo do usu�rio)
-	 * @return bool
-	 */
-	public static function loginScriptcase($cod)
-	{
-		// busca pelo usu�rio no banco de dados
-		$buscar = UsuarioDAO::buscarUsuarioScriptcase($cod);
+    /**
+     * M�todo para buscar os dados do usu�rio de acordo com id (login scriptcase)
+     * @access public
+     * @static
+     * @param @cod (c�digo do usu�rio)
+     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
+     * @return bool
+     */
+    public static function loginScriptcase($cod)
+    {
+        // busca pelo usu�rio no banco de dados
+        $buscar = UsuarioDAO::buscarUsuarioScriptcase($cod);
+
         $conexao = Zend_Registry::get('conexao_banco');
 
-        if ($conexao == "conexao_01")
-        {
+        if ($conexao == "conexao_01") {
             $conexao_scriptcase = "conexao_scriptcase_01";
-        }
-        else if ($conexao == "conexao_02")
-        {
+        } else if ($conexao == "conexao_02") {
             $conexao_scriptcase = "conexao_scriptcase_02";
-        }
-        else if ($conexao == "conexao_03")
-        {
+        } else if ($conexao == "conexao_03") {
             $conexao_scriptcase = "conexao_scriptcase_03";
-        }
-        else if ($conexao == "conexao_04")
-        {
+        } else if ($conexao == "conexao_04") {
             $conexao_scriptcase = "conexao_scriptcase_04";
-        }
-        else if ($conexao == "conexao_05")
-        {
+        } else if ($conexao == "conexao_05") {
             $conexao_scriptcase = "conexao_scriptcase_05";
+        } else if ($conexao == "conexao_xti") {
+            $conexao_scriptcase = "conexao_xti_controle_acesso";
         }
-                else if($conexao == "conexao_xti")
-                {
-                        $conexao_scriptcase = "conexao_xti_controle_acesso";
-                }
 
         // configura��es do banco de dados (seta uma nova conex�o no arquivo config.ini)
         $config = new Zend_Config_Ini('./application/configs/config.ini', $conexao_scriptcase);
-                //xd($config);
-        $db     = Zend_Db::factory($config->db);
-;
+        //xd($config);
+        $db = Zend_Db::factory($config->db);;
         Zend_Db_Table::setDefaultAdapter($db);
 
-		if ($buscar) // realiza a autentica��o
-		{
-			// configura��es do banco
-			$authAdapter = new Zend_Auth_Adapter_DbTable($db);
-			$authAdapter->setTableName('dbo.SGCacesso') // ControleDeAcesso.dbo.SGCacesso
-				->setIdentityColumn('Cpf')
-				->setCredentialColumn('Senha');
+        if ($buscar) {
+            $authAdapter = new Zend_Auth_Adapter_DbTable($db);
+            $objSgcAcesso = new Autenticacao_Model_Sgcacesso();
 
-			// seta as credenciais informada pelo usu�rio
-			$authAdapter
-				->setIdentity($buscar[0]->Cpf)
-				->setCredential($buscar[0]->Senha);
+            $authAdapter->setTableName($objSgcAcesso->getTableName())
+                ->setIdentityColumn('cpf')
+                ->setCredentialColumn('senha');
 
-			// tenta autenticar o usu�rio
-			$auth   = Zend_Auth::getInstance();
-			$acesso = $auth->authenticate($authAdapter);
+            // seta as credenciais informada pelo usu�rio
+            $authAdapter
+                ->setIdentity($buscar[0]->cpf)
+                ->setCredential($buscar[0]->senha);
 
-			// verifica se o acesso foi permitido
-			if ($acesso->isValid())
-			{
-				// pega os dados do usu�rio com exce��o da senha
-				$authData = $authAdapter->getResultRowObject(null, 'Senha');
+            // tenta autenticar o usu�rio
+            $auth = Zend_Auth::getInstance();
 
-				// armazena os dados do usu�rio
-				$objAuth = $auth->getStorage()->write($authData);
+            $acesso = $auth->authenticate($authAdapter);
+            // verifica se o acesso foi permitido
+            if ($acesso->isValid()) {
 
-				return true;
-			}
-			else // caso n�o tenha sido validado
-			{
-				return false;
-			}
-		} // fecha if
-		else
-		{
-			return false;
-		}
-	} // fecha m�todo loginScriptcase()
+                $authData = $authAdapter->getResultRowObject(null, 'senha');
+                $auth->getStorage()->write($authData);
 
-} // fecha class
+                return true;
+            }
+        }
+    }
+}
