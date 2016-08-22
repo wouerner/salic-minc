295a7a4e0eb3daf8a83546a1631cfd9799e9f0c4
Rafael.Gloria <rafael.gloria@cultura.gov.br>
2016-08-22 11:01:25 -0300

    Funcionalidade de exibir mensagens novas ao iniciar o aplicativo.

--- application/controller/MensagemRestController.php
+++ application/controller/MensagemRestController.php
@@ -20,26 +20,74 @@ class MensagemRestController extends Minc_Controller_AbstractRest{
         parent::init();
     }
 
-    public function postAction() {
-        # Pegando parametros via POST no formato JSON
-//        $body = $this->getRequest()->getRawBody();
-//        $post = Zend_Json::decode($body);
-//        $registrationId = $post['registrationId'];
-//        $cpf = isset($post['cpf'])? $post['cpf']: NULL;
-//        
-//        $modelDispositivoMovel = new Dispositivomovel();
-//        $dispositivo = $modelDispositivoMovel->salvar($registrationId, $cpf);
-
-        # Resposta da autenticação.
-        $this->getResponse()->setHttpResponseCode(200)->setBody(json_encode($dispositivo));
-    }
+    public function postAction(){}
     
-    public function indexAction(){}
+    public function indexAction(){
+        $next = $this->_request->getParam('next');
+        $offset = $this->_request->getParam('offset');
+        $total = $this->_request->getParam('total');
+        $idRegistration = $this->_request->getParam('idRegistration');
+        $new = $this->_request->getParam('new');
+        
+        $listaMensagens = array();
+        $modelMensagem = new Mensagem();
+        $objParam = (object) array(
+            'next' => $next,
+            'offset' => $offset,
+            'idRegistration' => $idRegistration,
+            'new' => $new);
+        # Verifica se existe necessidade de buscar o número total de registros da consulta
+        if(!$total){
+            $total = $modelMensagem->buscarTotalListarDeDispositivo($objParam);
+        }
+        # Busca os dados da lista
+        $objListaRs = $modelMensagem->listarDeDispositivo($objParam);
+        if($objListaRs){
+            $listaMensagens = $this->formatarUtf8Mensagens($objListaRs->toArray());
+        }
+        # Resposta do serviço.
+        $this->getResponse()->setHttpResponseCode(200)->setBody(json_encode(array(
+            'list' => $listaMensagens,
+            'total' => (int)$total)
+        ));
+    }
     
     public function getAction(){}
 
-    public function putAction(){}
+    public function putAction(){
+        $idMensagem = $this->_request->getParam('idMensagem');
+        $result = new stdClass();
+        if($idMensagem){
+            $modelMensagem = new Mensagem();
+            $mensagem = $modelMensagem->find($idMensagem)->current();
+            $mensagem->dtAcesso = new Zend_Db_Expr('GETDATE()');
+            $mensagem->save();
+            $result = (object)$mensagem->toArray();
+            
+        }
+        
+        # Resposta do serviço.
+        $this->getResponse()->setHttpResponseCode(200)->setBody(json_encode($result));
+    }
 
     public function deleteAction(){}
 
+    /**
+     * Formata lista de mensagens para o formato utf8.
+     * 
+     * @param array $arrListaRs
+     * @return array $listaMensagens
+     */
+    private function formatarUtf8Mensagens(array $arrListaRs = NULL) {
+        $listaMensagens = array();
+        if($arrListaRs){
+            foreach ($arrListaRs as $mensagem) {
+                $mensagem['titulo'] = utf8_encode($mensagem['titulo']);
+                $mensagem['descricao'] = utf8_encode($mensagem['descricao']);
+                $listaMensagens[] = (object)$mensagem;
+            }
+        }
+        
+        return $listaMensagens;
+    }
 }
