f872bc7225712d55bf9a5442b593bd25b92192f2
Ruy Ferreira <ruyjfs@gmail.com>
2016-09-02 19:14:55 -0300

    [ruy.silva] - Alteracao em models, populando dados, criado model, dbtable, mapper de DDD e Municipios.

---
+++ application/modules/agente/models/MunicipiosMapper.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Class Agente_Model_MunicipiosMapper
+ *
+ * @name Agente_Model_MunicipiosMapper
+ * @package Modules/Agente
+ * @subpackage Models
+ * @version $Id$
+ *
+ * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
+ * @since 01/09/2016
+ *
+ * @copyright © 2012 - Ministerio da Cultura - Todos os direitos reservados.
+ * @link http://salic.cultura.gov.br
+ */
+class Agente_Model_MunicipiosMapper extends MinC_Db_Mapper
+{
+    public function __construct()
+    {
+        parent::setDbTable('Agente_Model_DbTable_Municipios');
+    }
+
+    /**
+     *
+     * @name fetchPairs
+     * @param string $key
+     * @param string $value
+     * @param array $where
+     * @param string $order
+     * @return array
+     *
+     * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
+     * @since  02/09/2016
+     *
+     * @todo retirar futuramente, metodo feito para utilizar no xml.
+     */
+    public function fetchPairs($key, $value, array $where = [], $order = '')
+    {
+        $result = parent::fetchPairs($key, $value, $where, $order);
+        $resultNew = array();
+        foreach ($result as $key => $value) {
+            $stdClass = new stdClass();
+            $stdClass->id = $key;
+            $stdClass->descricao = $value;
+            $resultNew[] = $stdClass;
+        }
+
+        return $resultNew;
+    }
+}
