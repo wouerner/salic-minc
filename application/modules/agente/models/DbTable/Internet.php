caf1d938923e94f60ba6740cbfacf9d631fe0ae0
Ruy Ferreira <ruyjfs@gmail.com>
2016-09-06 19:19:52 -0300

    [ruy.silva] - Refatoracao por completo na action ManteragentesController::gravaragentecompletoAction. Retirando a logica da controller, usando beginTransaction ao salvar em 6 tabelas diferentes. -  Criado as DbTable, Mapper e Model das seguintes tabelas: -- agentes.internet -- agentes.enderecoNacional, -- agentes.Telefones -- agentes.Nomes -- agentes.tbTitulacaoConselheiro -- agentes.visao - Criado metodos na abstract da MinC_Db_Mapper e MinC_Db_Table:  -- getSequence para pegar o objeto protegido _sequence  -- deleteBy para deletar varias rows conforme o array de where  -- begin, rolback e commit para as trans  -- toArray para a model  - Deletado a model Telefone  - Criado sequence para as 6 tabelas utilizadas.

---
+++ application/modules/agente/models/DbTable/Internet.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Class Agente_Model_DbTable_Internet
+ *
+ * @name Agente_Model_DbTable_Internet
+ * @package Modules/Agente
+ * @subpackage Models/DbTable
+ * @version $Id$
+ *
+ * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
+ * @since 06/09/2016
+ *
+ * @copyright © 2012 - Ministerio da Cultura - Todos os direitos reservados.
+ * @link http://salic.cultura.gov.br
+ */
+class Agente_Model_DbTable_Internet extends MinC_Db_Table_Abstract
+{
+    /**
+     * _schema
+     *
+     * @var string
+     * @access protected
+     */
+    protected $_schema = 'agentes';
+
+    /**
+     * _name
+     *
+     * @var bool
+     * @access protected
+     */
+    protected $_name = 'internet';
+
+    /**
+     * _primary
+     *
+     * @var bool
+     * @access protected
+     */
+    protected $_primary = 'idinternet';
+
+    /**
+     * M�todo para envio de e-mail
+     * @access public
+     * @param string $email
+     * @param string $assunto
+     * @param string $texto
+     * @param string $perfil
+     * @param string $formato
+     * @return void
+     * @todo retirar SP, n�o foi encontrada uso do metodo no sistema, proposta de remo��o.
+     */
+    public function enviarEmail($email, $assunto, $texto, $perfil = "PerfilGrupoPRONAC", $formato = "HTML")
+    {
+        $sql = "EXEC msdb.dbo.sp_send_dbmail
+                    @profile_name          = '" . $perfil . "'
+                    ,@recipients           = '" . $email . "'
+                    ,@body                 = '" . $texto . "'
+                    ,@body_format          = '" . $formato . "'
+                    ,@subject              = '" . $assunto . "'
+                    ,@exclude_query_output = 1;";
+
+        return $this->getAdapter()->query($sql);
+    }
+
+    /**
+     * M�todo para buscar o(s) e-mail(s) do agente
+     * @access public
+     * @param string $cpfcnpj
+     * @param integer $idAgente
+     * @param integer $statusEmail (1 = ATIVADO, 0 = DESATIVADO)
+     * @param integer $statusDivulgacao (1 = ATIVADO, 0 = DESATIVADO)
+     * @param boolean $buscarTodos (informa se busca todos ou somente um)
+     * @return array || object
+     */
+    public function buscarEmailAgente($cpfcnpj = null, $idAgente = null, $statusEmail = null, $statusDivulgacao = null, $buscarTodos = true)
+    {
+        $select = $this->select();
+        $select->setIntegrityCheck(false);
+        $select->from(
+            array("e" => $this->_name),
+            array("e.idInternet AS idEmail"
+            ,"e.TipoInternet AS TipoEmail"
+            ,"e.Descricao    AS Email"
+            ,"e.Status"
+            ,"e.Divulgar"),
+            'Agentes.dbo'
+        );
+
+        $select->join(
+            array("a" => "Agentes")
+            ,"a.idAgente = e.idAgente"
+            ,array(), 'Agentes.dbo'
+        );
+
+        // busca pelo cnpj ou cpf
+        if (!empty($cpfcnpj))
+        {
+            $select->where("a.CNPJCPF = ?", $cpfcnpj);
+        }
+
+        // busca pelo id do agente
+        if (!empty($idAgente))
+        {
+            $select->where("a.idAgente = ?", $idAgente);
+        }
+
+        // busca pelo email ativado/desativado
+        if (!empty($statusEmail))
+        {
+            $select->where("e.Status = ?", $statusEmail);
+        }
+
+        // busca pelo email de divulgacao
+        if (!empty($statusDivulgacao))
+        {
+            $select->where("e.Divulgar = ?", $statusDivulgacao);
+        }
+
+        return $buscarTodos ? $this->fetchAll($select) : $this->fetchRow($select);
+    }
+
+    /**
+     * M�todo para cadastrar
+     * @access public
+     * @param array $dados
+     * @return integer (retorna o �ltimo id cadastrado)
+     */
+    public function cadastrarEmailAgente($dados)
+    {
+        return $this->insert($dados);
+    }
+
+    /**
+     * Metodo para excluir
+     * @access public
+     * @param integer $idAgente (excluir todos os e-mails de um agente)
+     * @param integer $idInternet (excluir um determinado e-mail)
+     * @return integer (quantidade de registros exclu�dos)
+     */
+    public function excluirEmailAgente($idAgente = null, $idInternet = null)
+    {
+        // exclui todos os e-mails de um agente
+        if (!empty($idAgente))
+        {
+            $where['idAgente = ?'] = $idAgente;
+        }
+
+        // exclui um determinado e-mail
+        else if (!empty($idInternet))
+        {
+            $where['idInternet = ?'] = $idInternet;
+        }
+
+        return $this->delete($where);
+    }
+
+}
