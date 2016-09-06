42900961e5e576224afba7fb9daf069a97573323
minc <guiadler@hotmail.com>
2016-09-06 10:39:48 -0300

    -Troca da funcao 'buscarAreasCulturais' por mapperArea->fetchpairs -E inabilitacao da mesma funcao

--- application/modules/default/controllers/VerprojetosController.php
+++ application/modules/default/controllers/VerprojetosController.php
@@ -30,6 +30,7 @@ class VerProjetosController extends MinC_Controller_Action_Abstract {
      */
 
          public function init() {
+             $mapperArea = new Agente_Model_AreaMapper();
 
        parent::init();
 
@@ -1446,7 +1447,7 @@ class VerProjetosController extends MinC_Controller_Action_Abstract {
                             if($dados->tpSolicitacao == 'EN' || $dados->tpSolicitacao == 'EO' || $dados->tpSolicitacao == 'OR' || $dados->tpSolicitacao == 'PI'){
                                 $this->view->projetosENReconsideracao = $Projetos->buscaAreaSegmentoProjeto($dados->IdPRONAC);
 
-                                $this->view->comboareasculturaisReconsideracao = Agente_Model_ManterAgentesDAO::buscarAreasCulturais();
+                                $this->view->comboareasculturais = $mapperArea->fetchPairs('codigo',  'descricao');
                                 $this->view->combosegmentosculturaisReconsideracao = Segmentocultural::buscarSegmento($this->view->projetosENReconsideracao->cdArea);
 
                                 $parecer = new Parecer();
@@ -1484,7 +1485,7 @@ class VerProjetosController extends MinC_Controller_Action_Abstract {
                             if($dados->tpSolicitacao == 'EN' || $dados->tpSolicitacao == 'EO' || $dados->tpSolicitacao == 'OR' || $dados->tpSolicitacao == 'PI'){
                                 $this->view->projetosENRecurso = $Projetos->buscaAreaSegmentoProjeto($dados->IdPRONAC);
 
-                                $this->view->comboareasculturaisRecurso = Agente_Model_ManterAgentesDAO::buscarAreasCulturais();
+                                $this->view->comboareasculturais = $mapperArea->fetchPairs('codigo',  'descricao');
                                 $this->view->combosegmentosculturaisRecurso = Segmentocultural::buscarSegmento($this->view->projetosENRecurso->cdArea);
 
                                 $parecer = new Parecer();
