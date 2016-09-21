7d5b743ecabf5249e80adbf95959df85b3b812d0
Ruy Ferreira <ruyjfs@gmail.com>
2016-09-21 16:49:03 -0300

    [ruy.silva] - Tela Plano de divulgação e a tela de cadastro OK. - Dump da tabela verificacao.

---
+++ application/modules/proposta/models/DbTable/Verificacao.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class Proposta_Model_DbTable_Verificacao
+ *
+ * @name Proposta_Model_DbTable_Verificacao
+ * @package Modules/Agente
+ * @subpackage Models/DbTable
+ * @version $Id$
+ *
+ * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
+ * @since 21/09/2016
+ *
+ * @copyright © 2012 - Ministerio da Cultura - Todos os direitos reservados.
+ * @link http://salic.cultura.gov.br
+ */
+class Proposta_Model_DbTable_Verificacao extends MinC_Db_Table_Abstract{
+
+    protected $_schema = 'sac';
+    protected $_name  = 'verificacao';
+}
