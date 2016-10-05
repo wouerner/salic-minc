d690401890dca0b4dd08c60071c932995b628be5
Ruy Ferreira <ruyjfs@gmail.com>
2016-10-05 17:00:54 -0300

    [ruy.silva] - Correcao na tela Administrativo -> Cadastrar Proponente (SQL server). - Alteracao de locais de algumas models, tirando da default e passando pro modulo especifico de cada um.

--- application/modules/agente/views/scripts/agentes/inc/cabecalho.php
+++ application/modules/agente/views/scripts/agentes/inc/cabecalho.php
@@ -1,7 +1,7 @@
-<?php if((empty($this->qtdDirigentes)) and ($this->qtdDirigentes == 0) and ($this->dados[0]->TipoPessoa == 1)):?>
+<?php if((empty($this->qtdDirigentes)) and ($this->qtdDirigentes == 0) and ($this->dados[0]->tipopessoa == 1)):?>
     <center>
         <div class="msgALERT" style="width: 96%;">
-            <div style="float: left;">Você deve cadastrar pelo menos um dirigente!</div>
+            <div style="float: left;">Voc&ecirc; deve cadastrar pelo menos um dirigente!</div>
         </div>
     </center>
 <?php endif;?>
@@ -9,28 +9,28 @@
 <table class="tabela">
     <tr>
         <td width="160" class="centro">
-            <?php if(($this->dados[0]->TipoPessoa) == 1): ?>
-            <b>CNPJ:</b> <?php echo Mascara::addMaskCNPJ($this->dados[0] ->CNPJCPF); ?>
+            <?php if(($this->dados[0]->tipopessoa) == 1): ?>
+            <b>CNPJ:</b> <?php echo Mascara::addMaskCNPJ($this->dados[0] ->cnpjcpf); ?>
             <?php else:?>
-            <b>CPF:</b> <?php echo Mascara::addMaskCPF($this->dados[0] ->CNPJCPF); ?>
+            <b>CPF:</b> <?php echo Mascara::addMaskCPF($this->dados[0] ->cnpjcpf); ?>
             <?php endif;?>
         </td>
-        <td width="250"><b>NOME:</b> <?php echo utf8_encode($this->dados[0] ->Nome); ?></td>
-        <td><b>VISÕES:</b>
+        <td width="250"><b>NOME:</b> <?php echo $this->dados[0] ->nome; ?></td>
+        <td><b>VIS&Otilde;ES:</b>
             <?php
                 $i = 0;
                 if($this->visoes):
                     foreach($this->visoes as $v)
                     {
                             if ( $i == 0 ):
-                                echo utf8_encode($v->Descricao);
+                                echo $v->descricao;
                             else:
-                                echo " | " .utf8_encode($v->Descricao);
+                                echo " | " .$v->descricao;
                             endif;
                             $i++;
                     }
                 else:
-                    echo 'O Agente não tem nenhuma visão!';
+                    echo 'O Agente n&atilde;o tem nenhuma vis&atilde;o!';
                 endif;
             ?>
         </td>
