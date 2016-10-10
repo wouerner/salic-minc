2e161b2983fe85b4d86dbb5df3e4c17bc0fd7475
Ruy Junior Ferreira Silva <ruy.silva@cultura.gov.br>
2016-10-10 17:11:57 -0300

    [ruy.silva] - Correcao da trigger ao cadastrar uma proposta cadastrar tambem uma movimentacao por padrao. - Correcao de alguns menus por causa da movimentacao.
    Former-commit-id: a044b21de6e0ac2b40c4b81939a379977ee87e3d

--- application/modules/proposta/controllers/ManterorcamentoController.php
+++ application/modules/proposta/controllers/ManterorcamentoController.php
@@ -47,9 +47,9 @@ class Proposta_ManterorcamentoController extends MinC_Controller_Action_Abstract
         if(!empty ($_REQUEST['idPreProjeto'])) {
             $this->idPreProjeto = $_REQUEST['idPreProjeto'];
             //VERIFICA SE A PROPOSTA ESTA COM O MINC
-            $Movimentacao = new Proposta_Model_DbTable_Movimentacao();
+            $Movimentacao = new Proposta_Model_DbTable_TbMovimentacao();
             $rsStatusAtual = $Movimentacao->buscarStatusAtualProposta($_REQUEST['idPreProjeto']);
-            $this->view->movimentacaoAtual = isset($rsStatusAtual->Movimentacao) ? $rsStatusAtual->Movimentacao : '';
+            $this->view->movimentacaoAtual = isset($rsStatusAtual['movimentacao']) ? $rsStatusAtual['movimentacao'] : '';
         }else {
             if($_REQUEST['idPreProjeto'] != '0'){
                 parent::message("Necess�rio informar o n�mero da proposta.", "/manterpropostaincentivofiscal/index", "ERROR");
