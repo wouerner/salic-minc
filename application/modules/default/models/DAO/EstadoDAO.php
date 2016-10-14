e23f44d33dc3f036f0aca53237ef08185cb413c9
Wouerner <12307444793@cultura.gov.br>
2016-10-14 14:48:20 -0300

    [FIXls]

--- application/modules/default/models/DAO/EstadoDAO.php
+++ application/modules/default/models/DAO/EstadoDAO.php
@@ -1,50 +1,13 @@
 <?php
 /**
  * Modelo Estado
- * @author Equipe RUP - Politec
  * @since 29/03/2010
- * @version 1.0
- * @package application
- * @subpackage application.models
- * @copyright � 2010 - Minist�rio da Cultura - Todos os direitos reservados.
- * @link http://www.cultura.gov.br
  */
 
-class EstadoDAO extends Zend_Db_Table
+class EstadoDAO extends MinC_Db_Table_Abstract
 {
-	protected $_name = 'uf'; // nome da tabela
-	protected $_schema = 'agentes'; // nome da tabela
-
-	/**
-	 * M�todo para buscar os estados
-	 * @access public
-	 * @param void
-	 * @return object $db->fetchAll($sql)
-	 */
-	public static function buscar($id = null)
-	{
-		$sql = "SELECT idUF AS id, Sigla AS descricao ";
-		$sql.= "FROM AGENTES.dbo.UF ";
-
-		if (!empty($id))
-		{
-			$sql.= "WHERE idUF = {$id} ";
-		}
-
-		$sql.= "ORDER BY Sigla";
-
-		try
-		{
-			$db = Zend_Db_Table::getDefaultAdapter();
-			$db->setFetchMode(Zend_DB::FETCH_OBJ);
-		}
-		catch (Zend_Exception_Db $e)
-		{
-			$this->view->message = "Erro ao buscar Estados: " . $e->getMessage();
-		}
-		//xd($sql);
-		return $db->fetchAll($sql);
-	} // fecha buscar()
+	protected $_name = 'uf';
+	protected $_schema = 'agentes';
 
     public function listar($id = null)
     {
@@ -54,8 +17,7 @@ class EstadoDAO extends Zend_Db_Table
         $sql = $db->select()
             ->from($this->_name, ['iduf AS id', 'sigla AS descricao'], $this->_schema);
 
-        if (!empty($id))
-        {
+        if (!empty($id)) {
             $sql->where('idUF = ?', $id);
         }
 
