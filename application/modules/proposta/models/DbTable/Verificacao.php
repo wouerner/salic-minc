a4fe6cf8dd6e05081483fc496fc58adc331756f7
05681802167 <guilherme.adler@cultura.gov.br>
2016-10-19 12:10:51 -0200

    conserto Custos Administrativos -> Novo  nível 2

--- application/modules/proposta/models/DbTable/Verificacao.php
+++ application/modules/proposta/models/DbTable/Verificacao.php
@@ -17,4 +17,33 @@ class Proposta_Model_DbTable_Verificacao extends MinC_Db_Table_Abstract{
 
     protected $_schema = 'sac';
     protected $_name  = 'verificacao';
+
+    public function buscarFonteRecurso() {
+//        $sql = "select Verificacao.idVerificacao, ltrim(Verificacao.Descricao) as VerificacaoDescricao
+//                from SAC.dbo.Verificacao as Verificacao
+//                inner join SAC.dbo.Tipo as Tipo
+//                on Verificacao.idTipo = Tipo.idTipo
+//                where Tipo.idTipo = 5";
+        $select = $this->select();
+        $select->setIntegrityCheck(false);
+        $select->from(
+            array('V' => $this->_name),
+            array(
+                'idverificacao',
+                $this->getExpressionTrim('v.descricao',  'verificacaodescricao'),
+            ),
+            $this->_schema
+            );
+        $select->joinInner(array('tipo'=>'Tipo'),
+            'V.idtipo = tipo.idtipo' ,
+            null,
+            $this->_schema
+        );
+        $select->where('tipo.idtipo = ?', '5');
+
+        $db= Zend_Db_Table::getDefaultAdapter();
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+
+        return $db->fetchAll($select);
+    }
 }
