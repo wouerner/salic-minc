354e7b4bd7ddf66ab654f97c568cc71bc9fc33b4
Ruy Ferreira <ruyjfs@gmail.com>
2016-10-19 17:23:08 -0200

    [ruy.silva] - Feito dump de algumas tabelas. - Alterado a senha do servico de email. - Alteracao em alguns metodos de agente.
    Former-commit-id: 0e0906ef853c4d208be7f324a706e42a49c3da3b

--- application/modules/agente/models/Agentes.php
+++ application/modules/agente/models/Agentes.php
@@ -24,4 +24,165 @@ class Agente_Model_Agentes extends MinC_Db_Model
     protected $_status;
     protected $_usuario;
 
+    /**
+     * @return mixed
+     */
+    public function getIdagente()
+    {
+        return $this->_idagente;
+    }
+
+    /**
+     * @param mixed $idagente
+     * @return Agente_Model_Agentes
+     */
+    public function setIdagente($idagente)
+    {
+        $this->_idagente = $idagente;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCnpjcpf()
+    {
+        return $this->_cnpjcpf;
+    }
+
+    /**
+     * @param mixed $cnpjcpf
+     * @return Agente_Model_Agentes
+     */
+    public function setCnpjcpf($cnpjcpf)
+    {
+        $this->_cnpjcpf = $cnpjcpf;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCnpjcpfsuperior()
+    {
+        return $this->_cnpjcpfsuperior;
+    }
+
+    /**
+     * @param mixed $cnpjcpfsuperior
+     * @return Agente_Model_Agentes
+     */
+    public function setCnpjcpfsuperior($cnpjcpfsuperior)
+    {
+        $this->_cnpjcpfsuperior = $cnpjcpfsuperior;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTipopessoa()
+    {
+        return $this->_tipopessoa;
+    }
+
+    /**
+     * @param mixed $tipopessoa
+     * @return Agente_Model_Agentes
+     */
+    public function setTipopessoa($tipopessoa)
+    {
+        $this->_tipopessoa = $tipopessoa;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtcadastro()
+    {
+        return $this->_dtcadastro;
+    }
+
+    /**
+     * @param mixed $dtcadastro
+     * @return Agente_Model_Agentes
+     */
+    public function setDtcadastro($dtcadastro)
+    {
+        $this->_dtcadastro = $dtcadastro;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtatualizacao()
+    {
+        return $this->_dtatualizacao;
+    }
+
+    /**
+     * @param mixed $dtatualizacao
+     * @return Agente_Model_Agentes
+     */
+    public function setDtatualizacao($dtatualizacao)
+    {
+        $this->_dtatualizacao = $dtatualizacao;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtvalidade()
+    {
+        return $this->_dtvalidade;
+    }
+
+    /**
+     * @param mixed $dtvalidade
+     * @return Agente_Model_Agentes
+     */
+    public function setDtvalidade($dtvalidade)
+    {
+        $this->_dtvalidade = $dtvalidade;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    /**
+     * @param mixed $status
+     * @return Agente_Model_Agentes
+     */
+    public function setStatus($status)
+    {
+        $this->_status = $status;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsuario()
+    {
+        return $this->_usuario;
+    }
+
+    /**
+     * @param mixed $usuario
+     * @return Agente_Model_Agentes
+     */
+    public function setUsuario($usuario)
+    {
+        $this->_usuario = $usuario;
+        return $this;
+    }
 }
