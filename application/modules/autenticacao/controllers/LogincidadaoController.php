73bc43c0bf982651024129b7a737288f96b51b05
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-10-24 11:41:35 -0200

    [add/update] Criação da classe abstrata 'AOAuth' que herda da classe 'Zend_Controller_Action' que tem como responsabilidade fornecer todos os recursos necessários para que uma nova abstração utilizando OAuth seja utilizada contendo métodos que já tratam o Flow necessário para a implementação do OAuth2 e redireciona para os métodos 'successAction' ou 'errorAction' que são métodos abstratos, obrigando a implementação deles quando as classes herdarem da classe 'MinC_Auth_Controller_AOAuth'. [ add / update ] Criação da classe 'Autenticacao_LogincidadaoController' que herda de 'MinC_Auth_Controller_AOAuth' e implementa os métodos 'sucessAction' e 'errorAction'

--- application/modules/autenticacao/controllers/LogincidadaoController.php
+++ application/modules/autenticacao/controllers/LogincidadaoController.php
@@ -5,81 +5,24 @@
  * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
  * @since 06/10/16 11:25
  */
-class Autenticacao_LogincidadaoController extends MinC_Controller_Action_Abstract
+class Autenticacao_LogincidadaoController extends MinC_Auth_Controller_AOAuth
 {
-    private $oauthConfig;
-
     /**
      * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
      * @return void
      */
-    public function init()
+    public function successAction()
     {
-        $this->oauthConfig = $this->obterConfiguracoesOPAuth();
-        Zend_Layout::startMvc(array('layout' => 'layout_login'));
-        parent::init();
+        $this->_helper->viewRenderer->setNoRender(true);
+        $this->_helper->flashMessenger->addMessage("Sucesso!");
+        $this->redirect("/principal");
     }
 
     /**
      * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
      * @return void
+     * @todo Implementar esse método
      */
-    public function indexAction()
-    {
-        $opauth = new Opauth($this->oauthConfig, false);
-        $opauth->run();
-    }
-
-    /**
-     * @return array
-     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
-     * @return mixed
-     */
-    private function obterConfiguracoesOPAuth()
-    {
-        $oauthConfig = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', "oauth");
-        $oauthConfigArray = $oauthConfig->toArray();
-        return $oauthConfigArray['config'];
-    }
-
-    /**
-     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
-     * @return void
-     */
-    public function oauth2Callback()
-    {
-        $objOputh = new Opauth($this->oauthConfig, false);
-        $response = null;
-
-        switch ($objOputh->env['callback_transport']) {
-            case 'session':
-                session_start();
-                $response = $_SESSION['opauth'];
-                unset($_SESSION['opauth']);
-                break;
-            case 'post':
-                $response = unserialize(base64_decode($_POST['opauth']));
-                break;
-            case 'get':
-                $response = unserialize(base64_decode($_GET['opauth']));
-                break;
-            default:
-                echo '<strong style="color: red;">Error: </strong>Unsupported callback_transport.' . "<br>\n";
-                break;
-        }
-
-        if (array_key_exists('error', $response)) {
-            echo '<strong style="color: red;">Authentication error: </strong> Opauth returns error auth response.' . "<br>\n";
-        } else {
-            if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid'])) {
-                echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.' . "<br>\n";
-            } elseif (!$objOputh->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason)) {
-                echo '<strong style="color: red;">Invalid auth response: </strong>' . $reason . ".<br>\n";
-            } else {
-                echo '<strong style="color: green;">OK: </strong>Auth response is validated.' . "<br>\n";
-            }
-        }
-
-        xd($response);
-    }
+    public function errorAction()
+    {}
 }
