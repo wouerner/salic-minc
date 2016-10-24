a2dfa16349d0d2387c993a522f18d6a6fcd11c74
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-10-24 11:45:31 -0200

    [ update ] Ajuste nos comentários da classe 'MinC_Auth_Adapter_LoginCidadao'

--- library/MinC/Auth/Adapter/LoginCidadao.php
+++ library/MinC/Auth/Adapter/LoginCidadao.php
@@ -1,12 +1,32 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: vinnyfs89
- * Date: 20/10/16
- * Time: 14:32
+ * Adapter para autentição 'LoginCidadao', utilizado para que o método 'authenticate' da Classe 'Zend_Auth' consiga receber como parâmetro um objeto que implemente a interface 'Zend_Auth_Adapter_Interface'
+ * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
+ * @since 20/10/16 14:32
  */
-class LoginCidadao
+class MinC_Auth_Adapter_LoginCidadao implements Zend_Auth_Adapter_Interface
 {
 
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Performs an authentication attempt
+     * @throws Zend_Auth_Adapter_Exception If authentication cannot be performed
+     * @return Zend_Auth_Result
+     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
+     */
+
+    public function authenticate()
+    {
+        $objSession = new Zend_Session_Namespace("opauth");
+
+        if(!isset($objSession->auth)) {
+            return new Zend_Auth_Result(Zend_Auth_Result::FAILURE, $_SESSION['opauth']);
+        }
+        return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $_SESSION['opauth']);
+    }
 }
