bf9ee0e1d171f701f58b3ab85cd97b630bd9069b
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-10-31 19:05:03 -0200

    [ update ] Remoção de mensagens no redirecionamento do método 'success' [ update ] Adição de mensagem e tipo de mensagem 'ALERT' para o método 'error' [ update ] Criação dos métodos 'cadastrarusuario' e 'validarCadastro' que tem como responsabilidade realizar o cadastro do usuário utilizando os dados recebidos da autenticação utilizando o protocolo OAuth em conjunto com validações necessárias.
    Former-commit-id: c8a60ebd65bc341d5ae6144748c59b9fb1dce182

--- application/modules/autenticacao/controllers/LogincidadaoController.php
+++ application/modules/autenticacao/controllers/LogincidadaoController.php
@@ -13,16 +13,91 @@ class Autenticacao_LogincidadaoController extends MinC_Auth_Controller_AOAuth
      */
     public function successAction()
     {
-        $this->_helper->viewRenderer->setNoRender(true);
-        $this->_helper->flashMessenger->addMessage("Sucesso!");
         $this->redirect("/principal");
     }
 
     /**
      * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
      * @return void
-     * @todo Implementar esse método
      */
     public function errorAction()
-    {}
+    {
+        parent::message("Não foi possível autenticar na aplicação.", "/autenticacao", "ALERT");
+    }
+
+    /**
+     * @access public
+     * @return void
+     * @author Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
+     */
+    public function cadastrarusuarioAction()
+    {
+       try {
+            if (!$_POST) {
+                throw new Exception("Tentativa de acesso inv&aacute;lido.");
+            }
+            $objPost = $this->getAllParams();
+            $cpf = Mascara::delMaskCNPJ(Mascara::delMaskCPF($objPost["cpf"]));
+            $senhaCriptografada = EncriptaSenhaDAO::encriptaSenha($cpf, $objPost["id"]);
+            $arrayDTNascimento = explode("T", $objPost["birthdate"]);
+            $dtNascimento = $arrayDTNascimento[0];
+
+            $dados = array(
+                "cpf" => $cpf,
+                "nome" => $objPost["full_name"],
+                "dtnascimento" => $dtNascimento,
+                "email" => $objPost["email"],
+                "senha" => $senhaCriptografada,
+                "dtcadastro" => date("Y-m-d"),
+                "situacao" => 3,
+                "dtsituacao" => date("Y-m-d"),
+                "id_login_cidadao" => $objPost["id"]
+            );
+           
+            $this->validarCadastro($dados);
+
+            $sgcAcesso = new Autenticacao_Model_Sgcacesso();
+            $pkSgcAcessoSave = $sgcAcesso->salvar($dados);
+
+            $objAgentes = new Agente_Model_DbTable_Agentes();
+            $buscarAgente = $objAgentes->buscar(array('cnpjcpf = ?' => $cpf));
+            $idAgenteProp = count($buscarAgente) > 0 ? $buscarAgente[0]->idagente : 0;
+            $objVisao = new Visao();
+            $buscarVisao = $objVisao->buscar(array('visao = ?' => 144, 'stativo = ?' => 'A', 'idagente = ?' => $idAgenteProp));
+
+            if (count($buscarVisao) > 0)  {
+                $tbVinculo = new Agente_Model_DbTable_TbVinculo();
+                $idResp = $sgcAcesso->buscar(array('Cpf = ?' => $pkSgcAcessoSave));
+                $dadosVinculo = array(
+                    'idAgenteProponente' => $idAgenteProp
+                    ,'dtVinculo' => new Zend_Db_Expr('GETDATE()')
+                    ,'siVinculo' => 2
+                    ,'idUsuarioResponsavel' => $idResp[0]->idusuario
+                );
+                $tbVinculo->inserir($dadosVinculo);
+            }
+
+            parent::message("Bem vindo!", "/principal", "CONFIRM");
+
+        } catch (Exception $objException) {
+            parent::message($objException->getMessage(), "/autenticacao", "ALERT");
+        }
+    }
+
+    /**
+     * @param array $dados
+     * @author Vinícius Feitosa da Silva <viniciusfesil@mail.com>
+     * @return void
+     */
+    private function validarCadastro($dados)
+    {
+        $objSgcAcesso = new Autenticacao_Model_Sgcacesso();
+        if (!$objSgcAcesso->hasCPFCadastrado($dados['cpf'])) {
+            throw new Exception('CNPJ ou CPF j&aacute; cadastrado.');
+        }
+
+        if(!$objSgcAcesso->hasEmailCadastrado($dados['email'])) {
+            throw new Exception('E-mail j&aacute; cadastrado');
+        }
+    }
 }
