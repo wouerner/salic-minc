25f385ed15699512451c2c2a56d9fdd5c97b7232
Cleber Santos <oclebersantos@gmail.com>
2016-10-31 14:22:41 -0200

    Corrigindo view principal endereco do agente

--- application/modules/agente/models/DbTable/EnderecoNacional.php
+++ application/modules/agente/models/DbTable/EnderecoNacional.php
@@ -40,4 +40,43 @@ class Agente_Model_DbTable_EnderecoNacional extends MinC_Db_Table_Abstract
      */
     protected $_primary = 'idendereco';
 
+    /**
+     * Metodo para buscar os enderecos do agente
+     *
+     * @access public
+     * @static
+     * @param integer $idAgente
+     * @return object
+     */
+    public function buscarEnderecos($idAgente = null)
+    {
+        $ve = [
+            've.descricao as tipoendereco',
+            've.idverificacao as codtipoendereco',
+        ];
+
+        $m = [
+            'm.descricao as municipio',
+            'm.idmunicipioibge as codmun',
+        ];
+
+        $u = [
+            'u.sigla as uf',
+            'u.iduf as coduf'
+        ];
+
+        $sql = $this->select()
+            ->setIntegrityCheck(false)
+            ->from(['e' => 'endereconacional'], $this->_getCols(), $this->_schema)
+            ->joinLeft(['ve' => 'verificacao'], 've.idverificacao = e.tipoendereco', $ve, $this->_schema)
+            ->joinLeft(['m' => 'municipios'], 'm.idmunicipioibge = e.cidade', $m, $this->_schema)
+            ->joinLeft(['u' => 'uf'], 'U.iduf = e.uf', $u, $this->_schema)
+            ->joinLeft(['vl' => 'verificacao'], 'vl.idverificacao = e.tipologradouro', ['vl.descricao as dstipologradouro'], $this->_schema)
+            ->where('e.idagente = ?', $idAgente)
+            ->order(['status DESC'])
+        ;
+
+        return $this->fetchAll($sql);
+    }
+
 }
