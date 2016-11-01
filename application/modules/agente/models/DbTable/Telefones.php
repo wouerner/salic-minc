378106d3ac2b267f9da0e97978056ae3bf07c202
Cleber Santos <oclebersantos@gmail.com>
2016-11-01 16:36:55 -0200

    [Fix] Bug manter agente telefone

--- application/modules/agente/models/DbTable/Telefones.php
+++ application/modules/agente/models/DbTable/Telefones.php
@@ -40,4 +40,58 @@ class Agente_Model_DbTable_Telefones extends MinC_Db_Table_Abstract
      */
     protected $_primary = 'idtelefone';
 
+
+
+    /**
+     * Metodo para buscar os telefones do agente
+     *
+     * @access public
+     * @static
+     * @param integer $idAgente
+     * @return object
+     */
+    public function buscarFones($idAgente = null)
+    {
+        $tblAgentes = new Agente_Model_DbTable_Agentes();
+        $db = Zend_Db_Table::getDefaultAdapter();
+
+        $tl = [
+            'tl.idtelefone',
+            'tl.tipotelefone',
+            'tl.numero',
+            'tl.divulgar',
+            new Zend_Db_Expr("
+                    CASE
+                    WHEN tl.tipotelefone = 22 or tl.tipotelefone = 24
+                    THEN 'Residencial'
+                    WHEN tl.tipotelefone = 23 or tl.tipotelefone = 25
+                    THEN 'Comercial'
+                    WHEN tl.tipotelefone = 26
+                    THEN 'Celular'
+                    WHEN tl.tipotelefone = 27
+                    THEN 'Fax'
+                    END as dstelefone
+            ")
+        ];
+
+        $ddd = [
+            'ddd.codigo as ddd',
+            'ddd.codigo as codigo',
+        ];
+
+        $sql = $db->select()
+            ->from(['tl' => $this->_name], $tl, $this->_schema)
+            ->join(['uf' => 'uf'], 'uf.iduf = tl.uf', ['uf.sigla as ufsigla'], $this->_schema)
+            ->join(['ag' => 'agentes'], 'ag.idagente = tl.idagente', ['ag.idagente'], $this->_schema)
+            ->joinLeft(['ddd' => 'ddd'], 'tl.ddd = ddd.codigo', $ddd, $this->_schema)
+        ;
+
+        if (!empty($idAgente)) { // busca de acordo com o id do agente
+            $sql->where('tl.idagente = ?', $idAgente);
+        }
+
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+        return $db->fetchAll($sql);
+    }
+
 }
