1e73a67c08dae034454e66809c4aadda1b1460e9
Cleber Santos <oclebersantos@gmail.com>
2016-11-09 15:19:15 -0200

    [FIX] Corrige Planilha orcamentaria

--- application/modules/proposta/models/DbTable/TbPlanilhaProposta.php
+++ application/modules/proposta/models/DbTable/TbPlanilhaProposta.php
@@ -163,17 +163,113 @@ class Proposta_Model_DbTable_TbPlanilhaProposta extends MinC_Db_Table_Abstract
             ->from($this->_name, '*' , $this->_schema)
             ->limit(1)
             ->order('idPlanilhaProposta DESC');
-//        $sql = "
-//			select
-//				top 1 *
-//			from
-//				SAC.dbo.tbPlanilhaProposta
-//			order by
-//				idPlanilhaProposta
-//			desc
-//        ";
 
         return $db->fetchAll($sql);
     }
 
+    public function editarPlanilhaProdutos($dados, $where) {
+        try {
+            $this->update($dados, $where);
+        }
+        catch(Zend_Exception $e) {
+            die("Erro:".$e->getMessage());
+        }
+    }
+
+    public function buscarDadosCustos($array = array()) {
+
+        $pp = array(
+                    'pp.idEtapa as idEtapa',
+                    'pp.idPlanilhaItem AS idItem',
+                    'pp.UfDespesa AS IdUf',
+                    'pp.MunicipioDespesa as Municipio',
+                    'pp.FonteRecurso as Recurso',
+                    'pp.Unidade as Unidade',
+                    'pp.Quantidade as Quantidade',
+                    'pp.Ocorrencia as Ocorrencia',
+                    'pp.ValorUnitario as ValorUnitario',
+                    'pp.QtdeDias as QtdDias',
+                    'CAST(pp.dsJustificativa AS TEXT) as Justificativa'
+        );
+
+        $sql = $this->select()
+                    ->setIntegrityCheck(false)
+                    ->from(array('pre' => 'preprojeto'), array('pre.idpreprojeto'), $this->_schema );
+
+        $sql->joinInner(array('pp' => 'tbPlanilhaProposta'), 'pre.idPreProjeto = pp.idProjeto', $pp, $this->_schema);
+
+        $sql->joinInner(array('ti' => 'tbPlanilhaItens'), 'ti.idPlanilhaItens = pp.idPlanilhaItem', array('ti.Descricao as DescricaoItem'), $this->_schema);
+
+        $sql->joinInner(array('uf' => 'Uf'), 'uf.CodUfIbge = pp.UfDespesa', array('uf.Descricao AS DescricaoUf'), $this->_schema);
+
+        $sql->joinInner(array('mun' => 'Municipios'), 'mun.idMunicipioIBGE = pp.MunicipioDespesa', array('mun.Descricao as DescricaoMunicipio,'), $this->getSchema('agentes'));
+
+        $sql->joinInner(array('pe' => 'tbPlanilhaEtapa'),'pp.idEtapa = pe.idPlanilhaEtapa', array('pe.Descricao as DescricaoEtapa'), $this->_schema);
+
+        $sql->joinLeft(array('rec' =>'Verificacao'), 'rec.idVerificacao = pp.FonteRecurso', array('rec.Descricao as DescricaoRecurso'), $this->_schema);
+
+        $sql->joinLeft(array('uni' => 'tbPlanilhaUnidade'), 'uni.idUnidade = pp.Unidade', array('uni.Descricao as DescricaoUnidade'), $this->_schema);
+
+        $sql->where('pre.idPreProjeto = ?', $array['idPreProjeto']);
+        $sql->where('pp.idEtapa = ?', $array['etapa']);
+        $sql->where('pp.idPlanilhaItem = ?', $array['item']);
+        $sql->where('pp.idPlanilhaProposta = ?', $array['idPlanilhaProposta']);
+
+
+//        $sql = "SELECT
+//                    pe.Descricao as DescricaoEtapa,
+//                    ti.Descricao as DescricaoItem,
+//                    uf.Descricao AS DescricaoUf,
+//                    mun.Descricao as DescricaoMunicipio,
+//                    uni.Descricao as DescricaoUnidade,
+//                    rec.Descricao as DescricaoRecurso,
+//                    pp.idEtapa as idEtapa,
+//                    pp.idPlanilhaItem AS idItem,
+//                    pp.UfDespesa AS IdUf,
+//                    pp.MunicipioDespesa as Municipio,
+//                    pp.FonteRecurso as Recurso,
+//                    pp.Unidade as Unidade,
+//                    pp.Quantidade as Quantidade,
+//                    pp.Ocorrencia as Ocorrencia,
+//                    pp.ValorUnitario as ValorUnitario,
+//                    pp.QtdeDias as QtdDias,
+//                    CAST(pp.dsJustificativa AS TEXT) as Justificativa
+//                  FROM SAC.dbo.PreProjeto AS pre
+//                        INNER JOIN SAC.dbo.tbPlanilhaProposta pp ON pre.idPreProjeto = pp.idProjeto
+//                        INNER JOIN SAC.dbo.tbPlanilhaItens ti ON ti.idPlanilhaItens = pp.idPlanilhaItem
+//                        INNER JOIN SAC.dbo.Uf AS uf ON uf.CodUfIbge = pp.UfDespesa
+//                        INNER JOIN AGENTES.dbo.Municipios mun ON mun.idMunicipioIBGE = pp.MunicipioDespesa
+//                        INNER JOIN SAC.dbo.tbPlanilhaEtapa pe ON pp.idEtapa = pe.idPlanilhaEtapa
+//                        LEFT JOIN SAC.dbo.Verificacao rec ON rec.idVerificacao = pp.FonteRecurso
+//                        LEFT JOIN SAC.dbo.tbPlanilhaUnidade uni ON uni.idUnidade = pp.Unidade
+//                WHERE (pre.idPreProjeto = {$array['idPreProjeto']} and  pp.idEtapa = {$array['etapa']} and pp.idPlanilhaItem = {$array['item']} )
+//                       and pp.idPlanilhaProposta = {$array['idPlanilhaProposta']}";
+
+
+        $db= Zend_Db_Table::getDefaultAdapter();
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+
+        //xd($sql);
+        return $db->fetchAll($sql);
+    }
+
+    public function buscarDadosCadastrarCustos($idPreProjeto) {
+
+        $select = $this->select();
+        $select->setIntegrityCheck(false);
+        $select->from(
+            array('tpp'=>$this->_name),
+            array(
+                'idproposta'=>'tpp.idprojeto',
+            ),
+            $this->_schema
+        );
+        $select->where('tpp.idprojeto = ?',$idPreProjeto);
+
+        $db= Zend_Db_Table::getDefaultAdapter();
+        $db->setFetchMode(Zend_DB::FETCH_OBJ);
+
+        return $db->fetchRow($select);
+
+    }
 }
