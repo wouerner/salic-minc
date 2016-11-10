b4ef1a44fe61f47204f980910f07681575fe5a9e
Ruy Ferreira <ruyjfs@gmail.com>
2016-11-10 17:30:51 -0200

    [ruy.silva] - Cadastro de proponente alguns ajustes.
    Former-commit-id: e51f4694dcf94d3a9bf3037b9a79a565987916b6

--- sql/PostgreSQL/data/sac_tipo.sql
+++ sql/PostgreSQL/data/sac_tipo.sql
@@ -1,22 +1,50 @@
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Peça de Divulgação', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Veiculo de Divulgação', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Posição da Logomarca', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Movimentação do Projeto', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Fonte de Recurso', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Texto do Email', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Edifício', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Diligencia', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Despacho', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Recurso', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Solicitação de Alteração', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Edital', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Modalidade do Edital', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Demanda', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Fundo', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Linguagem', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Módulo', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Devolução de Recursos Captados', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Termo Decisão', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de comprovação', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Readequação', 1);
-INSERT INTO sac.tipo (descricao, stestado) VALUES ('Tipo de Ocorrência de Abertura de Conta', 1);
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+1,'Nome');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+2,'Endereço');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+3,'Telefone');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+4,'Internet');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+5,'Documento');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+6,'Atributo do Documento');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+7,'Direito');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+8,'Esfera');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+9,'Poder');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+10,'Administração');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+11,'Caracterista do Agente');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+13,'Tipo Logradouro');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+14,'Perfil');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+16,'Visão');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+17,'Certidão Negativa');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+18,'Lista de documentos de pessoa física');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+19,'Lista de documentos de pessoa jurídica');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+20,'Lista de documentos de pessoa física e jurídica');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+21,'Escolaridade');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+22,'Vínculo Empregatício');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+23,'Faixa Salarial');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+24,'Área de Atuação');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+25,'Nível do Parecerista');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+26,'Nome de Representante Legal');
+INSERT INTO agentes.Tipo (idTipo,Descricao) VALUES (
+27,'Cargo em Comissão');
