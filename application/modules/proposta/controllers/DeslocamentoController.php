c7f845d64ccdb9eb4dca0b5697b7cda48312182b
Wouerner <12307444793@cultura.gov.br>
2016-11-23 16:33:27 -0200

    [FIX] Erro no deslocamento

--- application/modules/proposta/controllers/DeslocamentoController.php
+++ application/modules/proposta/controllers/DeslocamentoController.php
@@ -84,6 +84,7 @@ class Proposta_DeslocamentoController extends MinC_Controller_Action_Abstract {
         if (empty($_GET['verifica'])) {
             $this->_helper->layout->disableLayout();
         }
+
         if($_GET) {
             $id = null;
 
@@ -108,9 +109,9 @@ class Proposta_DeslocamentoController extends MinC_Controller_Action_Abstract {
                 }
 
                 $mapperMunicipio = new Agente_Model_MunicipiosMapper();
-                $this->view->combocidadesO = $mapperMunicipio->fetchPairs('idmunicipioibge' , 'descricao', array('idufibge' => $idUFO));
+                $this->view->combocidadesO = $mapperMunicipio->fetchPairs('idMunicipioIBGE' , 'Descricao', array('idufibge' => $idUFO));
                 //$this->view->combocidadesO = Cidade::buscar($idUFO);
-                $this->view->combocidadesD = $mapperMunicipio->fetchPairs('idmunicipioibge' , 'descricao', array('idufibge' => $idUFD));
+                $this->view->combocidadesD = $mapperMunicipio->fetchPairs('idMunicipioIBGE' , 'Descricao', array('idufibge' => $idUFD));
 
                 $this->view->idPaisO 	= $idPaisO;
                 $this->view->idPaisD 	= $idPaisD;
