60dac8c728750692979869a0bfbe5f1bbc8eee32
Wouerner <12307444793@cultura.gov.br>
2016-11-24 15:00:36 -0200

    [FIX] MoveimentacaoAtual
    Former-commit-id: ba0289f833ca01285693e6cb761d59d5532a3d8f

--- application/modules/proposta/controllers/ManterpropostaeditalController.php
+++ application/modules/proposta/controllers/ManterpropostaeditalController.php
@@ -693,7 +693,7 @@ class Proposta_ManterpropostaeditalController extends MinC_Controller_Action_Abs
         $tblPreProjeto = new Proposta_Model_DbTable_PreProjeto();
         $rsPreProjeto = $tblPreProjeto->find($idPreProjeto)->current();
         //altera Estado da proposta
-        $rsPreProjeto->stestado = 0;
+        $rsPreProjeto->stEstado = 0;
 
         if ($rsPreProjeto->save()) {
             parent::message("Exclus&atilde;o realizada com sucesso!", "/proposta/manterpropostaincentivofiscal/listar-propostas", "CONFIRM");
