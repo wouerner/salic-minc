ca5b00d15f80bcf862bc41e0c609e0191774b07d
Wouerner <12307444793@cultura.gov.br>
2016-11-28 18:44:41 -0200

    [FIX] Adicionando scCorp ao metodo abstract para ignorar na conexao
    Former-commit-id: fe58679dd08bfd7ebc40e5bd0c2ca06bcaf52a0d

--- application/modules/default/models/spSelecionarParecerista.php
+++ application/modules/default/models/spSelecionarParecerista.php
@@ -5,15 +5,26 @@
  *
  */
 class spSelecionarParecerista extends MinC_Db_Table_Abstract {
-        
+
     protected $_banco = 'SAC';
     protected $_name  = 'spSelecionarParecerista';
 
-    public function exec($idOrgao, $idArea, $idSegmento, $vlProduto){
-        $sql = "exec ".$this->_banco.".dbo.".$this->_name." $idOrgao, '$idArea', '$idSegmento', $vlProduto";
-        $db = Zend_Registry :: get('db');
+    /**
+     * exec
+     *
+     * @param mixed $idOrgao
+     * @param mixed $idArea
+     * @param mixed $idSegmento
+     * @param mixed $vlProduto
+     * @access public
+     * @return void
+     * @todo remover metodo e passar para Zend_DB
+     */
+    public function exec($idOrgao, $idArea, $idSegmento, $vlProduto)
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $sql = "exec sac.dbo.".$this->_name." $idOrgao, '$idArea', '$idSegmento', $vlProduto";
         $db->setFetchMode(Zend_DB :: FETCH_OBJ);
         return $db->fetchAll($sql);
     }
 }
-?>
