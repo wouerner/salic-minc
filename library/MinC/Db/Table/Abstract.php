5343cfb07686299ee8b74294e6129021c7a0e0f8
Ruy Ferreira <ruyjfs@gmail.com>
2016-12-05 21:41:40 -0200

    [ruy.silva] - Implementacao da tela de mensagem. - Criacao de 3 telas diferentes para exemplo cada um com um layout diferente.  -- Normal  -- Bootstrap  -- Material Designe. - Alteracao na classe de servico para utilizar as configuracoes do application.ini conforme o ambiente.
    Former-commit-id: 53bd72b60dd6bfc3d7bdb24a87f449bf90583040

--- library/MinC/Db/Table/Abstract.php
+++ library/MinC/Db/Table/Abstract.php
@@ -59,8 +59,7 @@ abstract class MinC_Db_Table_Abstract extends Zend_Db_Table_Abstract
 
                 $db = Zend_Db_Table::getDefaultAdapter();
                 $arrConfig = $db->getConfig();
-
-                $strDb = str_replace(array('.dbo', '.scdne'), '', $this->_schema);
+                $strDb = str_replace(array('.dbo', '.scdne', '.scsac'), '', $this->_schema);
                 $strDb = str_replace('.sccorp', '', strtolower($strDb));
 
                 $arrConfig['dbname'] = strtoupper($strDb);
@@ -95,6 +94,8 @@ abstract class MinC_Db_Table_Abstract extends Zend_Db_Table_Abstract
             $this->_schema = str_replace('.scdne', '', $this->_schema);
         } else if (is_int(strpos($this->_schema, 'scCorp'))) {
             $this->_schema = str_replace('.scCorp', '', $this->_schema);
+        } else if (is_int(strpos($this->_schema, 'scsac'))) {
+            $this->_schema = str_replace('.scsac', '', $this->_schema);
         }
     }
 
