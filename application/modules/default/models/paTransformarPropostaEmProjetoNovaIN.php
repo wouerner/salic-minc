6958f51a4f812ec0cb8001f51b238b6263020e29
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-12-07 15:22:55 -0200

    [ ADD ] Adição de classe e mudança de referência para SP
    Former-commit-id: f5b112b4a2aa0dba47d16edfee10c6aa3824f213

---
+++ application/modules/default/models/paTransformarPropostaEmProjetoNovaIN.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of paTransformarPropostaEmProjetoNovaIN
+ */
+class paTransformarPropostaEmProjetoNovaIN extends MinC_Db_Table_Abstract {
+
+    protected $_banco = 'SAC';
+    protected $_name  = 'paTransformarPropostaEmProjetoNovaIN';
+
+    #public function execSP($idProposta, $CNPJCPF, $idOrgao, $idUsuario){
+    /**
+     * @author Alysson Vicu�a de Oliveira
+     * Chamada da Procedure respons�vel por Transformar uma Proposta em Projeto dentro do Salic
+     * @param $idProposta
+     * @param $CNPJCPF
+     * @param $idOrgao
+     * @param $idUsuario
+     * @param $nrProcesso
+     * @return string|Zend_Db_Statement_Interface
+     */
+    #public function execSP($idProposta, $CNPJCPF, $idOrgao, $idUsuario){
+    public function execSP($idProposta, $CNPJCPF, $idOrgao, $idUsuario, $nrProcesso){
+        try{
+            #$rodar = "exec " . $this->_banco .".dbo.". $this->_name . ' ' . $idProposta .',"'. $CNPJCPF.'",'. $idOrgao.','. $idUsuario;
+            $rodar = "exec " . $this->_banco .".dbo.". $this->_name . ' ' . $idProposta .',"'. $CNPJCPF.'",'. $idOrgao.','. $idUsuario.',"'. $nrProcesso . '"';
+            #xd( $rodar);
+
+            return  $this->getAdapter()->query($rodar);
+        }
+        catch(Zend_Exception $e){
+            return $e->getMessage();
+        }
+    }
+}
+?>
