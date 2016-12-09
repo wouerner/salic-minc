e22853bc8ee595134b8a4b1e578b3f9d9b3beaae
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-12-09 16:46:41 -0200

    [ update ] Alteração de campo "Justificativa" para Editor Rico. [ ADD ] Criação de plug-in chamado de "editorRico.js" para EDITOR DE TEXTO RICO que tem seu comportamento parametrizável. Exemplo de seu uso :  jQuery(function ($) {      $("#observacao").editorRico({ altura: 200 });  });
    Former-commit-id: f1aebbe762360666b5fbecd4a23d55b06adfca68

---
+++ public/js/jquery/plugins/editorRico.js
@@ -0,0 +1,49 @@
+jQuery.fn.editorRico = function (options) {
+
+    if(!options) {
+        options = {};
+    }
+
+    var minchar = (options.minchar) ? options.minchar : -1;
+    var maxchar = (options.maxchar) ? options.maxchar : 1000;
+    var isLimitarCarateres = (typeof  options.isLimitarCarateres  !== 'undefined') ? options.isLimitarCarateres : true;
+    var isDesabilitarEdicao = (typeof  options.isDesabilitarEdicao  !== 'undefined') ? options.isDesabilitarEdicao : false;
+    var idElemento = $(this).attr('id');
+    var altura = (options.altura) ? options.altura : 500;
+
+    function ContarCharacteres() {
+        var body = tinymce.get(idElemento).getBody();
+        var content = tinymce.trim(body.innerText || body.textContent);
+        return content.length;
+    }
+
+    function ExecucaoDaFuncaoLimiterPag() {
+        var countChars = ContarCharacteres(idElemento);
+        $("#contadorRico" + idElemento).html("Caracteres: " + countChars + "/" + maxchar);
+        $("#contadorRico" + idElemento).css('color', 'black');
+        if ((countChars > maxchar) || (countChars <= minchar)) {
+            $("#contadorRico" + idElemento).css('color', 'red');
+        }
+    }
+
+    tinymce.init({
+        plugins: "paste,textcolor",
+        language: "pt_BR",
+        paste_as_text: true,
+        selector: '#' + idElemento,
+        height: altura,
+        toolbar: "bold,italic,underline,color,forecolor backcolor,fontsizeselect",
+        menubar: "",
+        readonly : isDesabilitarEdicao,
+        setup: function (ed) {
+            if (isLimitarCarateres) {
+                ed.on('init', function (e) {
+                    $("#" + idElemento).after("<div id='contadorRico" + idElemento + "'></div>");
+                    ExecucaoDaFuncaoLimiterPag('observacao');
+                }).on('keyup', function (e) {
+                    ExecucaoDaFuncaoLimiterPag('observacao');
+                });
+            }
+        }
+    });
+};
