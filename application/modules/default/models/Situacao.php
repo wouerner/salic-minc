5611231d31931ffe7ebca6c9f35e0d788af67e13
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2016-12-12 17:01:41 -0200

    [FIX] consertada a listagem de analisar prestação de contas no perfil coordenador de prestação de contas
    Former-commit-id: 205ee8e0af3d8dc517c325754eae2c667eb8aa1f

--- application/modules/default/models/Situacao.php
+++ application/modules/default/models/Situacao.php
@@ -2,21 +2,22 @@
 class Situacao extends MinC_Db_Table_Abstract {
 
     protected $_banco = 'SAC';
-    protected $_name = 'dbo.Situacao';
+    protected $_name = 'Situacao';
 
     public function listasituacao($codigosituacao=array())
 	{
+
             $select = $this->select();
             $select->setIntegrityCheck(false);
             $select->from(
                         array('sit'=>$this->_name)
                       );
-            foreach($codigosituacao as $situacao)
+
+            foreach($codigosituacao as $campo => $situacao)
             {
-            	$select->orwhere('sit.Codigo = ?', $situacao);
+            	$select->orWhere($campo, $situacao);
             }
             return $this->fetchAll($select);
-	} // fecha m�todo listasituacao()
+	} 
         
 }
-?>
