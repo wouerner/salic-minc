3fc08509a14b571018cfc39a9d2977b76af3c47f
Cleber Santos <oclebersantos@gmail.com>
2016-12-19 18:41:24 -0200

    [update] Atualiza regras para custos vinculados
    Former-commit-id: 8e00e7e169086b96abf2156a70497c2c19f29dba

--- sql/SQLServer/dbchangelog/28-11-2016_atualizacoes_sprint1_novain.sql
+++ sql/SQLServer/dbchangelog/28-11-2016_atualizacoes_sprint1_novain.sql
@@ -6,9 +6,7 @@ WHERE idPlanilhaEtapa=5;
 -- Remove Not Null na coluna idPolicaoDaLogo
 ALTER TABLE sac.dbo.PlanoDistribuicaoProduto ALTER COLUMN idPosicaoDaLogo INT;
 
--- cria mais duas etapas
-INSERT INTO SAC.dbo.tbPlanilhaEtapa (Descricao, tpCusto, stEstado, tpGrupo) VALUES ('Pós-Produção', 'P', 1, 'A');
-INSERT INTO SAC.dbo.tbPlanilhaEtapa (Descricao, tpCusto, stEstado, tpGrupo) VALUES ('Custos Vinculados', 'A', 1, 'D');
+
 
 -- cria itens para a lista de execução imediata
 INSERT INTO SAC.dbo.Verificacao (idTipo, Descricao, stEstado) VALUES (23, 'Proposta normal', 1);
@@ -31,3 +29,48 @@ VALUES ('RESULTADO DA SELEÇÃO PÚBLICA','0',2,1,1);
 
 -- Cria novo campo para prorrogacao automática
 ALTER TABLE sac.dbo.PreProjeto ADD tpProrrogacao BIT DEFAULT 1 NULL;
+
+-- cria mais duas etapas para planilha orcamentaria
+INSERT INTO SAC.dbo.tbPlanilhaEtapa (Descricao, tpCusto, stEstado, tpGrupo) VALUES ('Pós-Produção', 'P', 1, 'A');
+INSERT INTO SAC.dbo.tbPlanilhaEtapa (Descricao, tpCusto, stEstado, tpGrupo) VALUES ('Custos Vinculados', 'A', 1, 'D');
+
+-- ajusta o cárdapio da etapa de custos vinculados no banco de dados
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40302, 51, 8, 8197, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40303, 51, 8, 8198, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40304, 51, 8, 8199, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40305, 1, 8, 8197, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40306, 1, 8, 8198, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40307, 1, 8, 8199, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40308, 19, 8, 8197, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40309, 19, 8, 8198, 236);
+INSERT INTO SAC.dbo.tbItensPlanilhaProduto
+(idItensPlanilhaProduto, idProduto, idPlanilhaEtapa, idPlanilhaItens, idUsuario)
+VALUES(40310, 19, 8, 8199, 236);
+
+
+-- cria novos itens
+INSERT INTO SAC.dbo.tbPlanilhaItens
+(idPlanilhaItens, Descricao, idUsuario)
+VALUES(8197, 'Custos de Administração', 236);
+INSERT INTO SAC.dbo.tbPlanilhaItens
+(idPlanilhaItens, Descricao, idUsuario)
+VALUES(8198, 'Custos de Divulgação', 236);
+INSERT INTO SAC.dbo.tbPlanilhaItens
+(idPlanilhaItens, Descricao, idUsuario)
+VALUES(8199, 'Controle e Auditoria', 236);
