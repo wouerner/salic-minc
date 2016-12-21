c6f693863bd515e70a7c3bdbfe2fe6491c3c2b33
wouerner <wouerner@gmail.com>
2016-12-21 14:47:55 -0200

    [FIX][NOVSIN]  Erro ao acessar menu lateral

--- application/modules/default/models/tbRecurso.php
+++ application/modules/default/models/tbRecurso.php
@@ -1,18 +1,11 @@
 <?php
 /**
- * DAO Recurso
- * @author emanuel.sampaio - Politec
  * @since 18/02/2011
- * @version 1.0
- * @package application
- * @subpackage application.model
- * @copyright � 2011 - Minist�rio da Cultura - Todos os direitos reservados.
  * @link http://www.cultura.gov.br
  */
 
 class tbRecurso extends MinC_Db_Table_Abstract
 {
-	protected $_banco  = "SAC";
 	protected $_schema = "SAC";
 	protected $_name   = "tbRecurso";
 
@@ -124,7 +117,7 @@ class tbRecurso extends MinC_Db_Table_Abstract
 	public function cadastrarDados($dados)
 	{
 		return $this->insert($dados);
-	} // fecha m�todo cadastrarDados()
+	}
 
 
 	/**
