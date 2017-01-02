8dfd05399c56efb3e1158141f39dfcf94d41ef11
Ruy Ferreira <ruyjfs@gmail.com>
2017-01-02 13:16:48 -0200

    [ruy.silva] - Implementando melhorias e ajustes no css para funcionar o validation com o material design. - Implementando automaticamente asterisco vermelho nas labels dos imputs com required. - Alteracao de pasta do jquery 3 e plugins criados pro salic. - Atualizacao na tela de login. - Alteracao na tela de cadastro, deixando com material design. - Alteracao na tela de esqueci minha senha deixando com material design. - Implementacao da nova versao do plugin jquery validation para ser utilizado com o jquery 3. - Implementacao de imagem do material como plano de fundo da tela principal.
    Former-commit-id: 9bd3001300a561b6d886d91512538e328d5c91f6

--- public/materialize/js/materialize-ready.js
+++ public/materialize/js/materialize-ready.js
@@ -11,11 +11,33 @@
     {
         $('.tooltipped').tooltip();
         $('.container.fade').fadeIn(1500);
+        Materialize.updateTextFields();
         elmFormsMaterialize = $('form.materialize');
         elmFormsMaterialize.find('select').material_select();
+        var arrObj = elmFormsMaterialize.find('[required=required]').filter(function(){
+            if ($(this).closest('.input-field').find('label').find('b.red-text').length > 0) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+        $(arrObj).each(function (){
+            var elm = $(this),
+                elmLabel = elm.closest('.input-field').find('label');
+            elmLabel.html(elmLabel.html() + ' <b class="red-text">*</b>')
+        });
+        // elmInputInvalid = $('input.invalid');
+        // $(elmInputInvalid).each(function(){
+        //     var elmInput = $(this),
+        //         ;
+        //
+        // });
+        // elmInputInvalid
+
         setTimeout(function () {
-            elmFormsMaterialize.find('[required=required]').closest('.input-field').find('input.select-dropdown').addClass('invalid');
-            elmFormsMaterialize.find('[required=required]').addClass('invalid');
+            // Materialize.updateTextFields();
+            // elmFormsMaterialize.find('[required=required]').closest('.input-field').find('input.select-dropdown').addClass('invalid');
+            // elmFormsMaterialize.find('[required=required]').addClass('invalid');
         }, 300);
     }
 }($3));
