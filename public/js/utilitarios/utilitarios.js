4ecb55c1b51d25bc073c4ce19c606fbf4879358b
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-01-06 11:54:03 -0200

    [ update ] Remoção de comentário colocado automaticamente pela IDE. [NOVAIN]

--- public/js/utilitarios/utilitarios.js
+++ public/js/utilitarios/utilitarios.js
@@ -1,7 +1,3 @@
-/**
- * @author Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
- * @since 06/01/2016 11:52
- */
 utilitarios = {
     marcarDesmarcarCheckBoxes: function (seletorCheckBoxes) {
         $(this).prop('checked', !$(this).prop('checked'));
