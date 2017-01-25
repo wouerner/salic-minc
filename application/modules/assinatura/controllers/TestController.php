10b900df065352fcb58d7930332c6c3fcf81cc61
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-01-25 15:08:06 -0200

    [ ADD ] Adição de "GenericController" sendo carregado por padrão no Bootstrap. [ ADD ] Adição de da classe "TestController" como POC para a classe 'GenericController' [ UPDATE ] Bootstrap.php > Adição do método "_initPath"

--- application/modules/assinatura/controllers/TestController.php
+++ application/modules/assinatura/controllers/TestController.php
@@ -1,6 +1,6 @@
 <?php
 
-class Proposta_TestController extends Proposta_GenericController {
+class Assinatura_TestController extends Assinatura_GenericController {
 
     public function indexAction () {
         xd("Teste >> M&oacute;dulo " .  ucfirst($this->moduleName));
