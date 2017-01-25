6b9bc83f908f8a3c864c61ca166a1d805eb904d6
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-01-25 15:03:36 -0200

    [ FIX ] Ajuste de nome da classe GenericController para Proposta_GenericController

--- application/modules/proposta/controllers/GenericController.php
+++ application/modules/proposta/controllers/GenericController.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class GenericController extends MinC_Controller_Action_Abstract {
+abstract class Proposta_GenericController extends MinC_Controller_Action_Abstract {
 
     public function init()
     {
