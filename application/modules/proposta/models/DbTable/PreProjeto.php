36a7611e281cb2f82b051f5196d00e2b8e04a826
Cleber Santos <oclebersantos@gmail.com>
2017-01-31 10:18:33 -0200

    [update][novain] Permitindo  nome da proposta para alteracao do projeto

--- application/modules/proposta/models/DbTable/PreProjeto.php
+++ application/modules/proposta/models/DbTable/PreProjeto.php
@@ -206,7 +206,7 @@ class Proposta_Model_DbTable_PreProjeto extends MinC_Db_Table_Abstract
         {
             $slct->where($coluna, $valor);
         }
-        $slct->where(new Zend_Db_Expr("NOT EXISTS(select 1 from " . $this->getSchema('sac') . ".projetos pr where a.idPreProjeto = pr.idProjeto and pr.Situacao != 'E09')")); //@todo alterar quando Rômulo enviar a situacao correta
+        $slct->where(new Zend_Db_Expr("NOT EXISTS(select 1 from " . $this->getSchema('sac') . ".projetos pr where a.idPreProjeto = pr.idProjeto and pr.Situacao != 'E90')")); //@todo alterar quando Rômulo enviar a situacao correta
 
         $slct->order($order);
 
