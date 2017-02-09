4c4ed8a2ac793e8c3f68623e6a68ee7f57190330
wouerner <wouerner@gmail.com>
2017-02-09 16:05:20 -0200

    [NOVAIN]  Erros ao executar SP pachecklistsolicitarprorrogacaoprazo
    Former-commit-id: b68ac2b777780fd6f2f56d582128030bb03d6e1c

--- application/modules/default/models/paChecklistSolicitacaoProrrogacaoPrazo.php
+++ application/modules/default/models/paChecklistSolicitacaoProrrogacaoPrazo.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -9,12 +9,13 @@
  * @author Jefferson Alessandro
  */
 class paChecklistSolicitacaoProrrogacaoPrazo extends MinC_Db_Table_Abstract {
-        
+
     protected $_banco = 'SAC';
+    protected $_schema = 'SAC';
     protected $_name  = 'paChecklistSolicitacaoProrrogacaoPrazo';
 
     public function checkSolicitacao($idPronac, $dtInicio, $dtFinal, $acao){
-        $sql = "exec ".$this->_banco.".dbo.".$this->_name." $idPronac, '$dtInicio', '$dtFinal', '$acao' ";
+        $sql = "exec ".$this->_schema.'.'.$this->_name." $idPronac, '$dtInicio', '$dtFinal', '$acao' ";
         $db = Zend_Db_Table::getDefaultAdapter();
         $db->setFetchMode(Zend_DB :: FETCH_OBJ);
         return $db->fetchAll($sql);
