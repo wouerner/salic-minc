05dc5ad81fe82c44b3eef40730bf9b905b4eef82
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-02-16 15:15:49 -0200

    [ UPDATE  / REMOVE ] Inclusão do script de criação da tabela 'tbDispositivoMovel' feito no dia 14/02/2017.
    Former-commit-id: f9b61f271ef5e4c5dac40e3cbd0e718250910bc0

--- sql/SQLServer/dbchangelog/2017.scripts.sql
+++ sql/SQLServer/dbchangelog/2017.scripts.sql
@@ -213,3 +213,13 @@ ALTER TABLE sac.dbo.TbAssinatura ADD idDocumentoAssinatura INT NOT NULL;
 ALTER TABLE sac.dbo.TbAssinatura
 ADD CONSTRAINT TbAssinatura_tbDocumentoAssinatura_idDocumentoAssinatura_fk
 FOREIGN KEY (idDocumentoAssinatura) REFERENCES tbDocumentoAssinatura (idDocumentoAssinatura);
+
+CREATE TABLE SAC.dbo.tbDispositivoMovel (
+	idDispositivoMovel int NOT NULL IDENTITY(1,1),
+	idRegistration varchar(255) NOT NULL,
+	dtRegistration datetime NOT NULL DEFAULT '(getdate())',
+	nrCPF char(11),
+	dtAcesso datetime,
+	CONSTRAINT PK_idDispositivoMovel PRIMARY KEY (idDispositivoMovel)
+)
+
