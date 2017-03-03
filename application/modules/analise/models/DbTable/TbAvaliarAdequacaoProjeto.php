2e8430b130082370217624e390327b73bda66c50
Cleber Santos <oclebersantos@gmail.com>
2017-03-03 19:13:51 -0300

    [update][novain] Registrando corretamente os valores na tabela tbAvaliarAdequacaoProjeto ao avaliar adequacao
    Former-commit-id: 692d34853bb9f78bfe32cacd5b7f82b48f8dd4a2

--- application/modules/analise/models/DbTable/TbAvaliarAdequacaoProjeto.php
+++ application/modules/analise/models/DbTable/TbAvaliarAdequacaoProjeto.php
@@ -6,12 +6,22 @@ class Analise_Model_DbTable_TbAvaliarAdequacaoProjeto extends MinC_Db_Table_Abst
     protected $_name = 'tbAvaliarAdequacaoProjeto';
     protected $_primary = 'idAvaliarAdequacaoProjeto';
 
-    public function inserirAvaliacao($idPronac, $orgaoUsuario)
+
+    public function buscarUltimaAvaliacao($idPronac)
     {
+        $avaliacao = $this->buscar(array('idPronac = ?' => $idPronac), array('idAvaliarAdequacaoProjeto DESC'), 1)->toArray();
+        return $avaliacao[0];
+    }
+
+    public function inserirAvaliacao($idPronac, $orgaoUsuario, $idTecnico = null)
+    {
+        if (empty($idTecnico))
+            $idTecnico = new Zend_Db_Expr('sac.dbo.fnPegarTecnico(110, ' . $orgaoUsuario . ' ,1)');
+
         $dados = array(
             'idPronac' => $idPronac,
             'dtEncaminhamento' => new Zend_Db_Expr('GETDATE()'),
-            'idTecnico' => new Zend_Db_Expr('sac.dbo.fnPegarTecnico(110, ' . $orgaoUsuario . ' ,1)'),
+            'idTecnico' => $idTecnico,
             'dtAvaliacao' => null,
             'dsAvaliacao' => null,
             'siEncaminhamento' => 1,
@@ -20,6 +30,47 @@ class Analise_Model_DbTable_TbAvaliarAdequacaoProjeto extends MinC_Db_Table_Abst
         );
 
         return $this->insert($dados);
+    }
+
+    public function atualizarAvaliacaoNegativa($idPronac, $idTecnico, $avaliacao)
+    {
+        $dados = array(
+            'dtAvaliacao' => new Zend_Db_Expr('GETDATE()'),
+            'dsAvaliacao' => $avaliacao,
+            'siEncaminhamento' => 16,
+            'stAvaliacao' => 2,
+            'stEstado' => 0
+        );
+
+        $where = array(
+            'idPronac = ?' => $idPronac,
+            'stEstado = ?' => 1,
+            'siEncaminhamento = ?' => 1,
+            'idTecnico = ?' => $idTecnico
+        );
+
+        return $this->update($dados, $where);
+    }
+
+
+    public function atualizarAvaliacaoPositiva($idPronac, $idTecnico, $avaliacao)
+    {
+        $dados = array(
+            'dtAvaliacao' => new Zend_Db_Expr('GETDATE()'),
+            'dsAvaliacao' => $avaliacao,
+            'siEncaminhamento' => 15,
+            'stAvaliacao' => 1,
+            'stEstado' => 0
+        );
+
+        $where = array(
+            'idPronac = ?' => $idPronac,
+            'stEstado = ?' => 1,
+            'siEncaminhamento = ?' => 1,
+            'idTecnico = ?' => $idTecnico
+        );
+
+        return $this->update($dados, $where);
     }
 
 }
