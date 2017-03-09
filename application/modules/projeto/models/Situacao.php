d0eab8dc7d001083735839309f7e6be4ba40ccdd
Cleber Santos <oclebersantos@gmail.com>
2017-03-09 15:14:46 -0300

    [add][novain] Nova model para inserir as constantes das Situações do Projeto, melhorando a legibilidade das situações no código. By Cleber e Wouerner

---
+++ application/modules/projeto/models/Situacao.php
@@ -0,0 +1,13 @@
+<?php
+
+class Projeto_Model_Situacao extends MinC_Db_Table_Abstract
+{
+    protected $_name = 'Situacao';
+    protected $_schema = 'sac';
+    protected $_primary = 'Codigo';
+
+    const PROPOSTA_TRANSFORMADA_EM_PROJETO = 'B01';
+    const ENCAMINHADO_PARA_ANALISE_TECNICA = 'B11';
+    const AGUARDANDO_ELABORACAO_DE_PROTARIA_DE_PRORROGACAO = 'D22';
+
+}
