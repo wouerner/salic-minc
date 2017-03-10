878c1a7a981a0bc045d6ae3f51e1ad23a4c83fb2
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-03-10 15:08:30 -0300

    [ FIX ] Correção de tipo de dados Object p/ Array para método getIdUsuario() Autores: Fernão Vinícius Wourner
    Former-commit-id: cece50c0958b75482c417b5da306048d7be4b463

--- application/modules/default/controllers/MantercontabancariaController.php
+++ application/modules/default/controllers/MantercontabancariaController.php
@@ -157,7 +157,7 @@ class MantercontabancariaController extends MinC_Controller_Action_Abstract {
         
         if(count($AgenciaDados) > 0){
             
-            //INSERE OS DADOS NA TABELA DE HISTÓRICO - SAC.dbo.tbHistoricoExclusaoConta
+            //INSERE OS DADOS NA TABELA DE HISTï¿½RICO - SAC.dbo.tbHistoricoExclusaoConta
             $dadosInsert = array(
                 'idContaBancaria' => $resp->IdContaBancaria,
                 'Banco' => $resp->Banco,
@@ -166,7 +166,7 @@ class MantercontabancariaController extends MinC_Controller_Action_Abstract {
                     'ContaLivre' => $resp->ContaLivre,
                 'DtExclusao' => new Zend_Db_Expr('GETDATE()'),
                 'Motivo' => $this->_request->getParam('justificativa'),
-                'idUsuario' => $idagente->usu_codigo
+                'idUsuario' => $idagente['usu_codigo']
             );
             
             $id = $he->inserir($dadosInsert);
