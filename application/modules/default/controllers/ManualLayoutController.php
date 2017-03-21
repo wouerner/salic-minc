3c828e26c7050aba6a6fd35d53069cb9f671bf56
Cleber Santos <oclebersantos@gmail.com>
2017-03-21 15:58:54 -0300

    [fix] Menu lateral no Manual de layout
    Former-commit-id: 31ade86a2e7e1cbdede33a8b5c0f239062fd78d8

--- application/modules/default/controllers/ManualLayoutController.php
+++ application/modules/default/controllers/ManualLayoutController.php
@@ -22,7 +22,7 @@ class ManualLayoutController extends MinC_Controller_Action_Abstract
 	public function init()
 	{
 		// configurações do layout padrão
-		Zend_Layout::startMvc(array('layout' => 'layout_login'));
+//		Zend_Layout::startMvc(array('layout' => 'layout_login'));
 
 		parent::init(); // chama o init() do pai GenericControllerNew
 	} // fecha método init()
