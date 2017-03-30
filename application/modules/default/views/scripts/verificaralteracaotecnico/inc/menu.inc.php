594af224826a4b42d8843fa8e174ab63201a12c6
Cleber Santos <oclebersantos@gmail.com>
2017-03-30 17:24:59 -0300

    [fix] Corrigindo chamada da Controller Diligenciar no modulo de propostas

--- application/modules/default/views/scripts/verificaralteracaotecnico/inc/menu.inc.php
+++ application/modules/default/views/scripts/verificaralteracaotecnico/inc/menu.inc.php
@@ -53,7 +53,7 @@ function carregaPagina(){
 <div id="menuContexto" style="margin-bottom: 15px;">
     <div class="top"></div>
     <div id="qm0" class="qmmc">
-    	<a class="no_seta last" onclick="redirecionar('<?php echo $this->url(array('controller' => 'diligenciar', 'action' => 'listardiligenciaanalista')); ?>?idPronac=<?php echo $this->dados->IdPRONAC; ?>&tpDiligencia=171&situacao=<?php echo $this->verificarSituacaoProjeto($this->dados->IdPRONAC); ?>');" href="#" title="Ir para Projetos Diligenciados">Diligenciar</a>
+    	<a class="no_seta last" onclick="redirecionar('<?php echo $this->url(array('module' => 'proposta', 'controller' => 'diligenciar', 'action' => 'listardiligenciaanalista')); ?>?idPronac=<?php echo $this->dados->IdPRONAC; ?>&tpDiligencia=171&situacao=<?php echo $this->verificarSituacaoProjeto($this->dados->IdPRONAC); ?>');" href="#" title="Ir para Projetos Diligenciados">Diligenciar</a>
     </div>
     <div class="bottom"></div>
     <div id="alertar"></div>
