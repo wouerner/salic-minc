248506646679c4a1d2e0c489ee45130bf69d2b14
Fernão Lopes <fernao@riseup.net>
2017-04-03 13:58:31 -0300

    [FIX] Atualizando models de SPs e PAs

--- application/modules/default/models/spAtivarPlanilhaOrcamentaria.php
+++ application/modules/default/models/spAtivarPlanilhaOrcamentaria.php
@@ -10,7 +10,7 @@ class spAtivarPlanilhaOrcamentaria extends MinC_Db_Table_Abstract {
     protected $_name  = 'spAtivarPlanilhaOrcamentaria';
 
     public function exec($idPronac){
-      $sql = "exec ".$this->_banco.".dbo.".$this->_name." $idPronac";
+      $sql = "exec ".$this->_banco.".".$this->_name." $idPronac";
       return $this->getAdapter()->query($sql);
     }
 }
