73ce015aa196e4559b4242e8e03d9e342f5d27a7
Fernão Lopes <fernao@riseup.net>
2017-04-03 13:58:31 -0300

    [FIX] Atualizando models de SPs e PAs
    Former-commit-id: 3b49c0e48bbf68aa1ac9004888cb3c00db30924a

--- application/modules/default/models/spTipoDeReadequacaoOrcamentaria.php
+++ application/modules/default/models/spTipoDeReadequacaoOrcamentaria.php
@@ -14,7 +14,7 @@ class spTipoDeReadequacaoOrcamentaria extends MinC_Db_Table_Abstract {
      * @return array
      */
     public function exec($idPronac){
-      $sql = "exec ".$this->_banco.".dbo.".$this->_name." $idPronac";
+      $sql = "exec ".$this->_banco.".".$this->_name." $idPronac";
 
       //Foi realizado uma alteracao na Store Procedure pra trazer mais valores - 26/02/2016
       return $this->getAdapter()->fetchAll($sql);
