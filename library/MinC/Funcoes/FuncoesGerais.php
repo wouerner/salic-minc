413a6decbd18fd26998a784912c05b153cc01146
wouerner <wouerner@gmail.com>
2017-04-07 10:42:26 -0300

    [FIX] ajuste nas migalhas do sistema

--- library/MinC/Funcoes/FuncoesGerais.php
+++ library/MinC/Funcoes/FuncoesGerais.php
@@ -221,7 +221,6 @@ function gerarBreadCrumb($links = array()) {
 
         $contador = 0;
         if ($qtdLinks > 0) {
-
             foreach ($links as $link) {
                 foreach ($link as $nomeLink => $val) {
                     $contador++;
@@ -234,7 +233,11 @@ function gerarBreadCrumb($links = array()) {
                             if(isset($val['module']) && !empty($val['module'])) {
                                 $arrayLink['module'] = $val['module'];
                             }
+
+                            // @todo: não é possivel nesse momento otimizar essa rotina.
                             $url = $router->assemble($arrayLink);
+                            $url = explode('/', $url);
+                            $url = ('/'.$url[1] . '/' . $url[2] .'/'.$url[3]);
                         }
                         $guia .= "<li><a href='" . $url . "' title='{$nomeLink}'>" . $nomeLink . "</a></li>";
                     }
