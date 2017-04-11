6b7c6678dd81b7995c254bceed6f731c6834eed6
wouerner <wouerner@gmail.com>
2017-04-11 16:49:08 -0300

    [FIX] Ajuste layout do menu de diligencias.

--- application/modules/proposta/views/scripts/diligenciar/inc/menu.inc.php
+++ application/modules/proposta/views/scripts/diligenciar/inc/menu.inc.php
@@ -1,37 +1,31 @@
-<!-- ========== IN?CIO MENU ========== --> 
-<script language="javascript" type="text/javascript" src="<?php echo $this->baseUrl(); ?>/public/scripts/quickmenu.js"></script> 
-<div id="menu"> 
- 
-    <!-- inￜcio: conteￜdo principal #container --> 
-    <div id="container"> 
- 
-        <!-- inￜcio: navegaￜￜo local  --> 
-        <script type="text/javascript"> 
+<script language="javascript" type="text/javascript" src="<?php echo $this->baseUrl(); ?>/public/scripts/quickmenu.js"></script>
+
+<div class="col s2" id="menu">
+    <div id="container">
+        <script type="text/javascript">
             function layout_fluido() {
-                var janela = $(window).width(); 
-                var fluidNavGlobal = janela - 245; 
-                var fluidConteudo = janela - 253; 
-                var fluidTitulo = janela - 252; 
-                var fluidRodape = janela - 19; 
-                $("#navglobal").css("width",fluidNavGlobal); 
-                $("#conteudo").css("width",fluidConteudo); 
-                $("#titulo").css("width",fluidTitulo); 
-                $("#rodapeConteudo").css("width",fluidConteudo); 
-                $("#rodape").css("width",fluidRodape); 
-                $("div#rodapeConteudo").attr("id", "rodapeConteudo_com_menu"); 
-            } 
-        </script> 
- 
-        <style type="text/css"> 
-            .sanfonaDiv { 
-                clear: both; 
-                display: none; 
-            } 
-        </style> 
-        <div id="corfirma" title="Confirmacao" style='display:none;'></div> 
+                var janela = $(window).width();
+                var fluidNavGlobal = janela - 245;
+                var fluidConteudo = janela - 253;
+                var fluidTitulo = janela - 252;
+                var fluidRodape = janela - 19;
+                $("#navglobal").css("width",fluidNavGlobal);
+                $("#conteudo").css("width",fluidConteudo);
+                $("#titulo").css("width",fluidTitulo);
+                $("#rodapeConteudo").css("width",fluidConteudo);
+                $("#rodape").css("width",fluidRodape);
+                $("div#rodapeConteudo").attr("id", "rodapeConteudo_com_menu");
+            }
+        </script>
+        <style type="text/css">
+            .sanfonaDiv {
+                clear: both;
+                display: none;
+            }
+        </style>
+        <div id="corfirma" title="Confirmacao" style='display:none;'></div>
         <div id="ok" title="Confirmacao" style='display:none;'></div>
         <?php
-
             $get = Zend_Registry::get("get");
             $pronac = null;
             $projeto = null;
@@ -44,7 +38,7 @@
                 $codPronac .= $get->idPronac;
                 $pronac = $get->idPronac;
             }
-            
+
             //define id do PreProjeto que sera passado as outras implementacoes
             $codProjeto = "?idPreProjeto=";
             if(isset($this->idPreProjeto)){
@@ -55,30 +49,21 @@
                 $projeto = $get->idPreProjeto;
             }
         ?>
-        <div id="menuContexto">
-            <div class="top"></div>
-            <div id="qm0" class="qmmc">
+        <div class="collection">
                 <?php if(!empty($pronac)):?>
-                <a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'consultardadosprojeto', 'action' => 'index')); ?><?php echo $codPronac;?>">Dados do Projeto</a>
+                    <li class="collection-item"><a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'consultardadosprojeto', 'action' => 'index')); ?><?php echo $codPronac;?>">Dados do Projeto</a></li>
                 <?php endif;?>
                 <?php if(!empty($projeto)):?>
                     <?php if(isset($_GET['edital'])):?>
-                    <a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'manterpropostaedital', 'action' => 'dadospropostaedital')); ?><?php echo $codProjeto;?>">Dados da Proposta</a>
+                        <li class="collection-item"><a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'manterpropostaedital', 'action' => 'dadospropostaedital')); ?><?php echo $codProjeto;?>">Dados da Proposta</a></li>
                     <?php else:?>
-                    <a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'manterpropostaincentivofiscal', 'action' => 'editar')); ?><?php echo $codProjeto;?>">Dados da Proposta</a>
+                        <li class="collection-item"><a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'manterpropostaincentivofiscal', 'action' => 'editar')); ?><?php echo $codProjeto;?>">Dados da Proposta</a></li>
                     <?php endif;?>
                 <?php endif;?>
-                <?php
-                    if(isset($this->menumsg)){
-                ?>
-                <div class="sanfonaDiv"></div>
-                <a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'mantermensagens', 'action' => 'consultarmensagem')); ?>/idpronac/<?php echo $this->idPronac; ?>">Mensagens</a>
+                <?php if(isset($this->menumsg)){ ?>
+                    <div class="sanfonaDiv"></div>
+                        <li class="collection-item"><a class="no_seta" href="<?php echo $this->url(array('module'=> 'default', 'controller' => 'mantermensagens', 'action' => 'consultarmensagem')); ?>/idpronac/<?php echo $this->idPronac; ?>">Mensagens</a></li>
                 <?php } ?>
-            </div>
-            <div class="bottom"></div>
-            <div id="space_menu"></div>
-        <!-- final: navegaￜￜo local -->
         </div>
-    </div> 
-</div> 
-<!-- ========== FIM MENU ========== -->
+    </div>
+</div>
