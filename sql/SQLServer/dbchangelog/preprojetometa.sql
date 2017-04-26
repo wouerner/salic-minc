d86b3606cd82c03cd6d8144c3b836ef7ab74bb2d
wouerner <wouerner@gmail.com>
2017-04-26 11:35:30 -0300

    [CLEAN] removendo aquivos de video e removendo pasta sql
    Former-commit-id: d9668eeeb546e23c766872862a9911fcc24cd424

--- sql/SQLServer/dbchangelog/preprojetometa.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sac.dbo.PreProjetoMeta (
-  idPreProjetoMeta INT NOT NULL IDENTITY,
-  idPreProjeto int NOT NULL,
-  metaKey varchar(250) NOT NULL,
-  metaValue varchar(2500),
-  CONSTRAINT PreProjetoMeta_PK PRIMARY KEY (idPreProjetoMeta),
-  CONSTRAINT PreProjetoMeta_PreProjeto_FK FOREIGN KEY (idPreProjeto) REFERENCES sac.dbo.PreProjeto(idPreProjeto)
-);
