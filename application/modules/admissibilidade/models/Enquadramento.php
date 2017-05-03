b23b9771842d42ef5f67b1fed50f8b18998f5669
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-05-03 15:14:08 -0300

    [ REFACTOR ] Alteração de estrutura de pastas e nomes de classes [ UPDATE ] Modificação de localização do método 'obterProjetosEncaminhadosParaAssinatura' que agora foi para a classe "Assinatura_Model_DbTable_TbDocumentoAssinatura"

--- application/modules/admissibilidade/models/Enquadramento.php
+++ application/modules/admissibilidade/models/Enquadramento.php
@@ -225,78 +225,7 @@ class Admissibilidade_Model_Enquadramento extends MinC_Db_Table_Abstract
         return $this->_db->fetchAll($select);
     }
 
-    public function obterProjetosEncaminhadosParaAssinatura($codOrgao = null, $ordenacao = array())
-    {
-        $query = $this->select();
-        $query->setIntegrityCheck(false);
 
-        $queryPlanilhaOrcamentaria = $this->select();
-        $queryPlanilhaOrcamentaria->setIntegrityCheck(false);
-        $queryPlanilhaOrcamentaria->from('tbPlanilhaAprovacao',
-            array(
-                "vlAprovado" => New Zend_Db_Expr(
-                    "tbPlanilhaAprovacao.vlUnitario * tbPlanilhaAprovacao.qtItem * tbPlanilhaAprovacao.nrOcorrencia"
-                )
-            ), $this->_schema
-        );
-        $queryPlanilhaOrcamentaria->where("tbPlanilhaAprovacao.IdPRONAC = projetos.IdPRONAC");
-
-        $query->from(
-            array("Projetos" => "Projetos"),
-            array(
-                'pronac' => New Zend_Db_Expr('Projetos.AnoProjeto + Projetos.Sequencial'),
-                'Projetos.nomeProjeto',
-                'Projetos.IdPRONAC',
-                'Projetos.CgcCpf',
-                'Projetos.Area as cdarea',
-                'Projetos.ResumoProjeto',
-                'Projetos.UfProjeto',
-                'Projetos.DtInicioExecucao',
-                'Projetos.DtFimExecucao',
-                'Projetos.Situacao',
-                'Projetos.DtSituacao',
-                'Projetos.Orgao',
-                'dias' => 'DATEDIFF(DAY, projetos.DtSituacao, GETDATE())',
-                '(' . $queryPlanilhaOrcamentaria->assemble() . ') as vlAprovado'
-            ),
-            $this->_schema
-        );
-
-        $query->joinInner(
-            array('Area' => 'Area'),
-            "Area.Codigo = Projetos.Area",
-            "Area.Descricao as area",
-            $this->_schema
-        );
-
-        $query->joinInner(
-            array('Segmento' => 'Segmento'),
-            "Segmento.Codigo = Projetos.Segmento",
-            array(
-                "Segmento.Descricao as segmento",
-                "Segmento.tp_enquadramento"
-            ),
-            $this->_schema
-        );
-
-        $query->joinInner(
-            array('tbDocumentoAssinatura' => 'tbDocumentoAssinatura'),
-            "tbDocumentoAssinatura.IdPRONAC = Projetos.IdPRONAC",
-            array(
-                "tbDocumentoAssinatura.idTipoDoAtoAdministrativo"
-            ),
-            $this->_schema
-        );
-
-        if ($codOrgao) {
-            $query->where("Projetos.Orgao = ?", $codOrgao);
-        }
-
-        $query->where("Projetos.Situacao in (?)", array('B04'));
-        $query->order($ordenacao);
-//xd($query->assemble());
-        return $this->_db->fetchAll($query);
-    }
 
 
     public function verificarDesistenciaRecursal($idPronac)
--- application/modules/assinatura/models/DbTable/TbDocumentoAssinatura.php
+++ application/modules/assinatura/models/DbTable/TbDocumentoAssinatura.php
@@ -23,4 +23,78 @@ class Assinatura_Model_DbTable_TbDocumentoAssinatura extends MinC_Db_Table_Abstr
         }
     }
 
+    public function obterProjetosEncaminhadosParaAssinatura($codOrgao = null, $ordenacao = array())
+    {
+        $query = $this->select();
+        $query->setIntegrityCheck(false);
+
+        $queryPlanilhaOrcamentaria = $this->select();
+        $queryPlanilhaOrcamentaria->setIntegrityCheck(false);
+        $queryPlanilhaOrcamentaria->from('tbPlanilhaAprovacao',
+            array(
+                "vlAprovado" => New Zend_Db_Expr(
+                    "tbPlanilhaAprovacao.vlUnitario * tbPlanilhaAprovacao.qtItem * tbPlanilhaAprovacao.nrOcorrencia"
+                )
+            ), $this->_schema
+        );
+        $queryPlanilhaOrcamentaria->where("tbPlanilhaAprovacao.IdPRONAC = projetos.IdPRONAC");
+
+        $query->from(
+            array("Projetos" => "Projetos"),
+            array(
+                'pronac' => New Zend_Db_Expr('Projetos.AnoProjeto + Projetos.Sequencial'),
+                'Projetos.nomeProjeto',
+                'Projetos.IdPRONAC',
+                'Projetos.CgcCpf',
+                'Projetos.Area as cdarea',
+                'Projetos.ResumoProjeto',
+                'Projetos.UfProjeto',
+                'Projetos.DtInicioExecucao',
+                'Projetos.DtFimExecucao',
+                'Projetos.Situacao',
+                'Projetos.DtSituacao',
+                'Projetos.Orgao',
+                'dias' => 'DATEDIFF(DAY, projetos.DtSituacao, GETDATE())',
+                '(' . $queryPlanilhaOrcamentaria->assemble() . ') as vlAprovado'
+            ),
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('Area' => 'Area'),
+            "Area.Codigo = Projetos.Area",
+            "Area.Descricao as area",
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('Segmento' => 'Segmento'),
+            "Segmento.Codigo = Projetos.Segmento",
+            array(
+                "Segmento.Descricao as segmento",
+                "Segmento.tp_enquadramento"
+            ),
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('tbDocumentoAssinatura' => 'tbDocumentoAssinatura'),
+            "tbDocumentoAssinatura.IdPRONAC = Projetos.IdPRONAC",
+            array(
+                "tbDocumentoAssinatura.idTipoDoAtoAdministrativo"
+            ),
+            $this->_schema
+        );
+
+        if ($codOrgao) {
+            $query->where("Projetos.Orgao = ?", $codOrgao);
+        }
+
+        $query->where("tbDocumentoAssinatura.Situacao = ?", Assinatura_Model_TbDocumentoAssinatura::CD_SITUACAO_ABERTO);
+        $query->where("Projetos.Situacao in (?)", array('B04'));
+        $query->order($ordenacao);
+//xd($query->assemble());
+        return $this->_db->fetchAll($query);
+    }
+
 }
