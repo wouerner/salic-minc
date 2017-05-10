83dda18b9991019e3709494a12169a2447d53334
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-05-09 21:00:02 -0300

    [ UPDATE ] Adição de classificação na gerencia de assinaturas de acordo com o Tipo do Ato administrativo

--- application/modules/assinatura/models/DbTable/TbDocumentoAssinatura.php
+++ application/modules/assinatura/models/DbTable/TbDocumentoAssinatura.php
@@ -107,4 +107,80 @@ class Assinatura_Model_DbTable_TbDocumentoAssinatura extends MinC_Db_Table_Abstr
         return $this->_db->fetchAll($query);
     }
 
+    public function obterProjetosComAssinaturasAbertas($codOrgao = null, $ordenacao = array())
+    {
+        $query = $this->select();
+        $query->setIntegrityCheck(false);
+
+        $query->from(
+            array("Projetos" => "Projetos"),
+            array(
+                'pronac' => New Zend_Db_Expr('Projetos.AnoProjeto + Projetos.Sequencial'),
+                'Projetos.nomeProjeto',
+                'Projetos.IdPRONAC',
+                'Projetos.CgcCpf',
+                'Projetos.Area as cdarea',
+                'Projetos.ResumoProjeto',
+                'Projetos.UfProjeto',
+                'Projetos.DtInicioExecucao',
+                'Projetos.DtFimExecucao',
+                'Projetos.Situacao',
+                'Projetos.DtSituacao',
+                'Projetos.Orgao',
+                'tbDocumentoAssinatura.cdSituacao'
+            ),
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('Area' => 'Area'),
+            "Area.Codigo = Projetos.Area",
+            "Area.Descricao as area",
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('Segmento' => 'Segmento'),
+            "Segmento.Codigo = Projetos.Segmento",
+            array(
+                "Segmento.Descricao as segmento",
+                "Segmento.tp_enquadramento"
+            ),
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('tbDocumentoAssinatura' => 'tbDocumentoAssinatura'),
+            "tbDocumentoAssinatura.IdPRONAC = Projetos.IdPRONAC",
+            array(
+                "tbDocumentoAssinatura.idTipoDoAtoAdministrativo"
+            ),
+            $this->_schema
+        );
+
+        $query->joinInner(
+            array('Verificacao' => 'Verificacao'),
+            "Verificacao.idVerificacao = tbDocumentoAssinatura.idTipoDoAtoAdministrativo",
+            'Verificacao.Descricao as tipoDoAtoAdministrativo',
+            $this->_schema
+        );
+
+        $query->joinLeft(
+            array('Enquadramento' => 'Enquadramento'),
+            "Enquadramento.IdPRONAC = Projetos.IdPRONAC
+             AND Enquadramento.AnoProjeto = Projetos.AnoProjeto
+             AND Enquadramento.Sequencial = Projetos.Sequencial",
+            array("Enquadramento.IdEnquadramento"),
+            $this->_schema
+        );
+
+        if ($codOrgao) {
+            $query->where("Projetos.Orgao = ?", $codOrgao);
+        }
+
+        $query->where("tbDocumentoAssinatura.cdSituacao = ?", Assinatura_Model_TbDocumentoAssinatura::CD_SITUACAO_DISPONIVEL_PARA_ASSINATURA);
+        $query->order($ordenacao);
+
+        return $this->_db->fetchAll($query);
+    }
 }
