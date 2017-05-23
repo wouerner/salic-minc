ce7d80b82bf02cb1732578e4c5b264282e6fb214
wouerner <wouerner@gmail.com>
2017-05-23 17:39:21 -0300

    * Fix: [Movimentação de Conta] Ajusta na SP de execução.

--- application/modules/default/models/spValidarDepositoIdentificado.php
+++ application/modules/default/models/spValidarDepositoIdentificado.php
@@ -1,38 +1,22 @@
 <?php
-/**
- * DAO spValidarDepositoIdentificado
- * @author Jefferson Alessandro
- * @since 22/04/2013
- * @version 1.0
- * @package application
- * @subpackage application.model
- * @copyright � 2011 - Minist�rio da Cultura - Todos os direitos reservados.
- * @link http://www.cultura.gov.br
- */
-
-class spValidarDepositoIdentificado extends MinC_Db_Table_Abstract {
-
-    /* dados da tabela */
-    protected $_banco   = "SAC";
-    protected $_schema  = "dbo";
+class spValidarDepositoIdentificado extends MinC_Db_Table_Abstract 
+{
+    protected $_schema  = "SAC.dbo";
     protected $_name    = "spValidarDepositoIdentificado";
 
     /**
-     * M�todo para executar a SP de movimenta��o banc�ria.
-     * A mesma verifica se as inconsist�ncias foram corrigidas.
-     * @access public
-     * @param void
-     * @return bool
+     * Método para executar a SP de movimentação bancária.
+     * A mesma verifica se as inconsistências foram corrigidas.
      */
     public function verificarInconsistencias($idUsuarioLogado) {
+
         try {
             // executa a sp
-            $executar = "EXEC " . $this->_banco . "." . $this->_name . " ".$idUsuarioLogado;
+            $executar = 'EXEC SAC.dbo.spvalidardepositoidentificado' . ' '.$idUsuarioLogado;
             return $this->getAdapter()->query($executar);
         }
         catch (Zend_Exception $e) {
             return $e->getMessage();
         }
-    } // fecha m�todo verificarInconsistencias()
-
-} // fecha class
+    }
+}
