052d1bc8c738013f5ab3c8a757e2152e9bb3fdf8
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-06-26 16:20:56 -0300

    [ FIX ] Assinatura  : Criação de SQL para alterar registros já existentes e com documentos abertos pra assinatura para estarem com situação ativa." :
    Former-commit-id: b76c34d7e662529b2e4430102f865907725cec77

--- sql/SQLServer/dbchangelog/Assinatura.scripts.sql
+++ sql/SQLServer/dbchangelog/Assinatura.scripts.sql
@@ -23,3 +23,10 @@ ALTER TABLE sac.dbo.TbAssinatura DROP COLUMN idAtoDeGestao;
  * Adição da coluna "idAtoDeGestao" na tabela "sac.dbo.tbDocumentoAssinatura"
  */
 ALTER TABLE sac.dbo.tbDocumentoAssinatura ADD idAtoDeGestao INT NULL;
+
+
+
+---------------------------------
+-- > Criação de SQL para alterar registros já existentes e com documentos abertos pra assinatura para estarem com situação ativa." ---
+---------------------------------
+update sac.dbo.tbDocumentoAssinatura set stEstado = 1 where cdSituacao = 1
