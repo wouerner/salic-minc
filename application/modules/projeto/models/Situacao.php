3ab1ff47147b94db2b12d1a0241ae907bd789291
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-06-29 19:10:39 -0300

    [UPDATE] Admissibilidade : Alteração da situação de destino do projeto quando devolvido. [UPDATE] Projetos/Models/Situacao : Inclusão de constante para situação "B05".

--- application/modules/projeto/models/Situacao.php
+++ application/modules/projeto/models/Situacao.php
@@ -7,6 +7,7 @@ class Projeto_Model_Situacao extends MinC_Db_Table_Abstract
     protected $_primary = 'Codigo';
 
     const PROPOSTA_TRANSFORMADA_EM_PROJETO = 'B01';
+    const PROPOSTA_DEVOLVIDA_PARA_ENQUADRAMENTO = 'B05';
     const ENCAMINHADO_PARA_ANALISE_TECNICA = 'B11';
     const PROJETO_ADEQUADO_A_REALIDADE_DE_EXECUCAO = 'B20';
     const AGUARDANDO_ELABORACAO_DE_PORTARIA_DE_PRORROGACAO = 'D22';
