86473b6e49395787d92f6ed1fcf50262ef6cc75c
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-06-30 14:50:40 -0300

    [UPDATE] Default / Models / Orgaos : Inclusão de novas contantes de Orgaos.

--- application/modules/default/models/tbVerificaProjeto.php
+++ application/modules/default/models/tbVerificaProjeto.php
@@ -1,42 +1,32 @@
 <?php
-/**
- * Description of tbVerificaProjeto
- *
- * @author tiago
- */
-class tbVerificaProjeto extends MinC_Db_Table_Abstract{
+
+class tbVerificaProjeto extends MinC_Db_Table_Abstract
+{
 
     protected $_banco = 'SAC';
     protected $_name = 'tbVerificaProjeto';
-    protected $_schema  = 'SAC';
+    protected $_schema = 'SAC';
 
     public function salvar($dados)
     {
-
         //INSTANCIANDO UM OBJETO DE ACESSO AOS DADOS DA TABELA
         $tmpTblVerificaProjeto = new tbVerificaProjeto();
 
-
-
-        if(isset($dados['idVerificaProjeto'])){
+        if (isset($dados['idVerificaProjeto'])) {
             $tmpRsVerificaProjeto = $tmpTblVerificaProjeto->find($dados['idVerificaProjeto'])->current();
-        }else{
+        } else {
             $tmpRsVerificaProjeto = $tmpTblVerificaProjeto->createRow();
         }
 
         //ATRIBUINDO VALORES AOS CAMPOS QUE FORAM PASSADOS
-        if(isset($dados['IdPRONAC'])){ $tmpRsVerificaProjeto->IdPRONAC = $dados['IdPRONAC']; }
-
-
-        echo "<pre>";
-
-
-        //SALVANDO O OBJETO CRIADO
+        if (isset($dados['IdPRONAC'])) {
+            $tmpRsVerificaProjeto->IdPRONAC = $dados['IdPRONAC'];
+        }
         $id = $tmpRsVerificaProjeto->save();
 
-        if($id){
+        if ($id) {
             return $id;
-        }else{
+        } else {
             return false;
         }
     }
