83acc2ed4c31d3afd833de074d2bdee65fc5380a
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-07-06 17:36:15 -0300

    [ADD] Parecer / Assinatura : Adição de estrutura básica para assinatura do parecer.
    Autores:
    Fernão Lopes
    Vinícius Feitosa da Silva
    
    Former-commit-id: 169225a96317bf17f1a06952948a3537da09853e

---
+++ application/modules/parecer/controllers/IndexController.php
@@ -0,0 +1,77 @@
+<?php
+
+class Parecer_IndexController extends MinC_Controller_Action_Abstract implements MinC_Assinatura_Controller_IDocumentoAssinaturaController
+{
+    private $idPronac;
+    /**
+     * @var MinC_Assinatura_Documento_IDocumentoAssinatura $servicoDocumentoAssinatura
+     */
+    private $servicoDocumentoAssinatura;
+
+    public function init()
+    {
+        parent::perfil();
+        parent::init();
+        $this->auth = Zend_Auth::getInstance();
+        $this->grupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
+    }
+
+    /**
+     * @return Parecer_DocumentoAssinaturaController
+     */
+    function obterServicoDocumentoAssinatura()
+    {
+        if(!isset($this->servicoDocumentoAssinatura)) {
+            require_once __DIR__ . DIRECTORY_SEPARATOR . "DocumentoAssinatura.php";
+            $this->servicoDocumentoAssinatura = new Parecer_DocumentoAssinaturaController($this->getRequest()->getPost());
+        }
+        return $this->servicoDocumentoAssinatura;
+    }
+
+    public function indexAction()
+    {
+        $this->redirect("/{$this->moduleName}/index/encaminhar-assinatura");
+    }
+
+    public function encaminharAssinaturaAction() {
+        try {
+            $get = $this->getRequest()->getParams();
+            $post = $this->getRequest()->getPost();
+            $servicoDocumentoAssinatura = $this->obterServicoDocumentoAssinatura();
+
+            if (isset($get['IdPRONAC']) && !empty($get['IdPRONAC']) && $get['encaminhar'] == 'true') {
+                $servicoDocumentoAssinatura->idPronac = $get['IdPRONAC'];
+                $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
+                parent::message('Projeto encaminhado com sucesso.', '/{$this->moduleName}/index/encaminhar-assinatura', 'CONFIRM');
+            } elseif(isset($post['IdPRONAC']) && is_array($post['IdPRONAC']) && count($post['IdPRONAC']) > 0) {
+                foreach($post['IdPRONAC'] as $idPronac) {
+                    $servicoDocumentoAssinatura->idPronac = $idPronac;
+                    $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
+                }
+                parent::message('Projetos encaminhados com sucesso.', '/{$this->moduleName}/index/encaminhar-assinatura', 'CONFIRM');
+            }
+            $this->carregarListaEncaminhamentoAssinatura();
+        } catch (Exception $objException) {
+            parent::message($objException->getMessage(), '/{$this->moduleName}/index/encaminhar-assinatura');
+        }
+    }
+
+    private function carregarListaEncaminhamentoAssinatura() {
+        /**
+        $this->view->idUsuarioLogado = $this->auth->getIdentity()->usu_codigo;
+        $enquadramento = new Admissibilidade_Model_Enquadramento();
+
+        $this->view->dados = array();
+        $ordenacao = array("dias desc");
+        $dados = $enquadramento->obterProjetosEnquadradosParaAssinatura($this->grupoAtivo->codOrgao, $ordenacao);
+
+        foreach ($dados as $dado) {
+            $dado->desistenciaRecursal = $enquadramento->verificarDesistenciaRecursal($dado->IdPRONAC);
+            $this->view->dados[] = $dado;
+        }
+
+        $this->view->codGrupo = $this->grupoAtivo->codGrupo;
+        $this->view->codOrgao = $this->grupoAtivo->codOrgao;
+        */
+    }
+}
