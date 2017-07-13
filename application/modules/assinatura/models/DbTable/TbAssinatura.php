c315e1304552d3c2ee9c9d2aced2adc3f19625f9
Fernão Lopes <fernao@riseup.net>
2017-07-13 18:58:31 -0300

    [NOVO] Assinatura/TbAssinatura: adicionando constante para tipo_ato_analise_inicial
    Former-commit-id: ce8408ac45ec2df49d6c2717e91ff505b964f1f6

--- application/modules/assinatura/models/DbTable/TbAssinatura.php
+++ application/modules/assinatura/models/DbTable/TbAssinatura.php
@@ -7,6 +7,7 @@ class Assinatura_Model_DbTable_TbAssinatura extends MinC_Db_Table_Abstract
     protected $_primary = 'idAssinatura';
 
     const TIPO_ATO_ENQUADRAMENTO = 626;
+    const TIPO_ATO_ANALISE_INICIAL = 630;
 
     public function obterAssinaturas($idPronac, $idTipoDoAtoAdministrativo)
     {
