558978d0a8b54d5d838ab4057758c276cd9da06c
Fernão Lopes <fernao@riseup.net>
2017-07-14 19:10:31 -0300

    [FIX] Analiseprojetoparecer/análise inicial: habilitando botões de 1) gerar documento para assinatura, 2) visualizar documento para assinatura e 3) assinar documento

---
+++ application/modules/default/views/helpers/IsProjetoDisponivelParaAssinatura.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Helper para verificar se projeto está disponível para assinatura
+ */
+
+class Zend_View_Helper_IsProjetoDisponivelParaAssinatura
+{
+	/**
+	 * Método para verificar se o projeto está disponível para assinatura
+	 * @access public
+	 * @param integer $idPronac
+	 * @param integer $idTipoDoAtoAdministrativo
+	 * @return string
+	 */
+	public function IsProjetoDisponivelParaAssinatura($idPronac, $idTipoDoAtoAdministrativo)
+	{
+        $objModelDocumentoAssinatura = new Assinatura_Model_DbTable_TbDocumentoAssinatura();
+        $isProjetoDisponivelParaAssinatura = $objModelDocumentoAssinatura->isProjetoDisponivelParaAssinatura(
+            $idPronac,
+            $idTipoDoAtoAdministrativo
+        );
+        
+        return $isProjetoDisponivelParaAssinatura;
+    }
+}
