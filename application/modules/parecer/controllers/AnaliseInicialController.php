bb022fe70d29efe27dd5de31d1dd0b6679e917ec
Fernão Lopes <fernao@riseup.net>
2017-07-21 19:47:17 -0300

    [FIX] Parecer/analisar-projeto: movendo métodos e views de default/analisarprojetoparecer para parecer/analise-inicial
    Former-commit-id: 39a6ab552217a353204dd603990326f79b16d07e

---
+++ application/modules/parecer/controllers/AnaliseInicialController.php
@@ -0,0 +1,78 @@
+<?php
+
+class Parecer_AnaliseInicialController extends MinC_Controller_Action_Abstract implements MinC_Assinatura_Controller_IDocumentoAssinaturaController
+{
+    private $idPronac;
+
+    private function validarPerfis() {
+        $auth = Zend_Auth::getInstance();
+
+        $PermissoesGrupo = array();
+        $PermissoesGrupo[] = Autenticacao_Model_Grupos::PARECERISTA;
+        
+        isset($auth->getIdentity()->usu_codigo) ? parent::perfil(1, $PermissoesGrupo) : parent::perfil(4, $PermissoesGrupo);
+    }
+
+    public function init()
+    {
+        parent::perfil();
+        parent::init();
+        $this->auth = Zend_Auth::getInstance();
+        $this->grupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
+    }
+
+    public function gerenciarAssinaturasAction()
+    {
+        $this->redirect("/{$this->moduleName}/index");
+    }
+
+    public function encaminharAssinaturaAction()
+    {
+    }
+
+    function obterServicoDocumentoAssinatura()
+    {
+    }
+    
+    public function indexAction()
+    {
+        $this->validarPerfis();
+        
+        $auth = Zend_Auth::getInstance();
+        $idusuario = $auth->getIdentity()->usu_codigo;
+
+        $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
+        $idOrgao = $GrupoAtivo->codOrgao; //  ¿rg¿o ativo na sess¿o
+
+        $UsuarioDAO = new Autenticacao_Model_Usuario();
+        $agente = $UsuarioDAO->getIdUsuario($idusuario);
+        $idAgenteParecerista = $agente['idagente'];
+
+        $situacao = $this->_request->getParam('situacao');
+        
+        $projeto = new Projetos();
+        $resp = $projeto->buscaProjetosProdutosParaAnalise(
+            array(
+                'distribuirParecer.idAgenteParecerista = ?' => $idAgenteParecerista,
+                'distribuirParecer.idOrgao = ?' => $idOrgao,
+            )
+        );
+
+        $this->idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
+        $objTbAtoAdministrativo = new Assinatura_Model_DbTable_TbAtoAdministrativo();
+        $this->view->quantidadeMinimaAssinaturas = $objTbAtoAdministrativo->obterQuantidadeMinimaAssinaturas($this->idTipoDoAtoAdministrativo, $idOrgao);
+        $this->view->idTipoDoAtoAdministrativo = $this->idTipoDoAtoAdministrativo;
+        $this->view->idPerfilDoAssinante = $GrupoAtivo->codGrupo;
+        
+        Zend_Paginator::setDefaultScrollingStyle('Sliding');
+        Zend_View_Helper_PaginationControl::setDefaultViewPartial('paginacao/paginacao.phtml');
+        $paginator = Zend_Paginator::factory($resp); // dados a serem paginados
+        $currentPage = $this->_getParam('page', 1);
+        $paginator->setCurrentPageNumber($currentPage)->setItemCountPerPage(10); // 10 por p¿gina
+        
+        $this->view->qtdRegistro = count($resp);
+        $this->view->situacao = $situacao;
+        $this->view->buscar = $paginator;
+    }
+
+}
--- application/modules/parecer/controllers/IndexController.php
+++ application/modules/parecer/controllers/IndexController.php
@@ -30,80 +30,16 @@ class Parecer_IndexController extends MinC_Controller_Action_Abstract implements
 
     public function indexAction()
     {
-        $this->redirect("/{$this->moduleName}/index/analisar-projeto-parecer");
+        $this->redirect("/{$this->moduleName}/index/encaminhar-assinatura");
     }
 
     public function gerenciarAssinaturasAction()
     {
-        $this->redirect("/{$this->moduleName}/index/analisar-projeto-parecer");
+        $this->redirect("/{$this->moduleName}/index/encaminhar-assinatura");
     }
 
 
     public function encaminharAssinaturaAction() {
-        try {
-            $get = $this->getRequest()->getParams();
-            $post = $this->getRequest()->getPost();
-            $servicoDocumentoAssinatura = $this->obterServicoDocumentoAssinatura();
-            
-            if (isset($get['IdPRONAC']) && !empty($get['IdPRONAC']) && $get['encaminhar'] == 'true') {
-                $servicoDocumentoAssinatura->idPronac = $get['IdPRONAC'];
-                $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
-                
-                $idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
-               $this->redirect("/assinatura/index/visualizar-projeto/?IdPRONAC=" . $get['IdPRONAC'] . "&idTipoDoAtoAdministrativo=" . $idTipoDoAtoAdministrativo);
-            } elseif(isset($post['IdPRONAC']) && is_array($post['IdPRONAC']) && count($post['IdPRONAC']) > 0) {
-                // ainda nao implementado o encaminhamento de vários para pareceres
-                /*
-                foreach($post['IdPRONAC'] as $idPronac) {
-                    $servicoDocumentoAssinatura->idPronac = $idPronac;
-                    $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
-                }
-                
-                parent::message('Projetos encaminhados com sucesso.', '/default/analisarprojetoparecer/index', 'CONFIRM');
-                */
-           }
-            $this->carregarListaEncaminhamentoAssinatura();
-        } catch (Exception $objException) {
-            parent::message($objException->getMessage(), '/{$this->moduleName}/index/encaminhar-assinatura');
-        }
     }
 
-    
-    public function analisarProjetoParecerAction() {
-        $auth = Zend_Auth::getInstance();
-        $idusuario = $auth->getIdentity()->usu_codigo;
-
-        $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
-        $idOrgao = $GrupoAtivo->codOrgao; //  ¿rg¿o ativo na sess¿o
-
-        $UsuarioDAO = new Autenticacao_Model_Usuario();
-        $agente = $UsuarioDAO->getIdUsuario($idusuario);
-        $idAgenteParecerista = $agente['idagente'];
-
-        $situacao = $this->_request->getParam('situacao');
-        
-        $projeto = new Projetos();
-        $resp = $projeto->buscaProjetosProdutosParaAnalise(
-            array(
-                'distribuirParecer.idAgenteParecerista = ?' => $idAgenteParecerista,
-                'distribuirParecer.idOrgao = ?' => $idOrgao,
-            )
-        );
-
-        $this->idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
-        $objTbAtoAdministrativo = new Assinatura_Model_DbTable_TbAtoAdministrativo();
-        $this->view->quantidadeMinimaAssinaturas = $objTbAtoAdministrativo->obterQuantidadeMinimaAssinaturas($this->idTipoDoAtoAdministrativo, $idOrgao);
-        $this->view->idTipoDoAtoAdministrativo = $this->idTipoDoAtoAdministrativo;
-        $this->view->idPerfilDoAssinante = $GrupoAtivo->codGrupo;
-        
-        Zend_Paginator::setDefaultScrollingStyle('Sliding');
-        Zend_View_Helper_PaginationControl::setDefaultViewPartial('paginacao/paginacao.phtml');
-        $paginator = Zend_Paginator::factory($resp); // dados a serem paginados
-        $currentPage = $this->_getParam('page', 1);
-        $paginator->setCurrentPageNumber($currentPage)->setItemCountPerPage(10); // 10 por p¿gina
-        
-        $this->view->qtdRegistro = count($resp);
-        $this->view->situacao = $situacao;
-        $this->view->buscar = $paginator;
-    }
 }
