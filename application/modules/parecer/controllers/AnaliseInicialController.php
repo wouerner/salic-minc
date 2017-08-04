70c18eb66b93e57567157da0a20b8007758a1ff6
Fernão Lopes <fernao@riseup.net>
2017-08-04 18:07:15 -0300

    [REFACTOR] Parecer/Análise inicial: movendo métodos de parecer/index para parecer/analise-inicial e removendo arquivos desnecessários

--- application/modules/parecer/controllers/AnaliseInicialController.php
+++ application/modules/parecer/controllers/AnaliseInicialController.php
@@ -28,10 +28,49 @@ class Parecer_AnaliseInicialController extends MinC_Controller_Action_Abstract i
 
     public function encaminharAssinaturaAction()
     {
+        try {
+            $get = $this->getRequest()->getParams();
+            $post = $this->getRequest()->getPost();
+            $servicoDocumentoAssinatura = $this->obterServicoDocumentoAssinatura();
+            
+            if (isset($get['IdPRONAC']) && !empty($get['IdPRONAC']) && $get['encaminhar'] == 'true') {
+                $servicoDocumentoAssinatura->idPronac = $get['IdPRONAC'];
+                $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
+                
+                $idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
+                $idDocumentoAssinatura = $this->getIdDocumentoAssinatura($get['IdPRONAC'], $idTipoDoAtoAdministrativo);
+                
+                $this->redirect("/assinatura/index/visualizar-projeto/?idDocumentoAssinatura=" . $idDocumentoAssinatura . "&origin=" . $get['origin']);
+            } elseif(isset($post['IdPRONAC']) && is_array($post['IdPRONAC']) && count($post['IdPRONAC']) > 0) {
+                // ainda nao implementado o encaminhamento de vários para pareceres
+            }
+        } catch (Exception $objException) {
+            parent::message($objException->getMessage(), "/{$this->moduleName}/analise-inicial/index");
+        }   
+        
     }
 
     function obterServicoDocumentoAssinatura()
     {
+        if(!isset($this->servicoDocumentoAssinatura)) {
+            require_once __DIR__ . DIRECTORY_SEPARATOR . "AnaliseInicialDocumentoAssinaturaController.php";
+            $this->servicoDocumentoAssinatura = new Parecer_AnaliseInicialDocumentoAssinaturaController($this->getRequest()->getPost());
+        }
+        return $this->servicoDocumentoAssinatura;        
+    }
+
+    private function getIdDocumentoAssinatura($idPronac, $idTipoDoAtoAdministrativo)
+    {
+        $objDocumentoAssinatura = new Assinatura_Model_DbTable_TbDocumentoAssinatura();
+        
+        $where = array();
+        $where['IdPRONAC = ?'] = $idPronac;
+        $where['idTipoDoAtoAdministrativo = ?'] = $idTipoDoAtoAdministrativo;
+        $where['stEstado = ?'] = 1;
+        
+        $result = $objDocumentoAssinatura->buscar($where);
+        
+        return $result[0]['idDocumentoAssinatura'];
     }
     
     public function indexAction()
--- application/modules/parecer/controllers/IndexController.php
+++ application/modules/parecer/controllers/IndexController.php
@@ -21,16 +21,18 @@ class Parecer_IndexController extends MinC_Controller_Action_Abstract implements
      */
     function obterServicoDocumentoAssinatura()
     {
-        if(!isset($this->servicoDocumentoAssinatura)) {
-            require_once __DIR__ . DIRECTORY_SEPARATOR . "DocumentoAssinatura.php";
-            $this->servicoDocumentoAssinatura = new Parecer_DocumentoAssinaturaController($this->getRequest()->getPost());
-        }
-        return $this->servicoDocumentoAssinatura;
     }
 
     public function indexAction()
     {
-        $this->redirect("/{$this->moduleName}/analise-inicial");
+        switch ($this->grupoAtivo->codGrupo) {
+        case Autenticacao_Model_Grupos::PARECERISTA:
+            $this->redirect("/{$this->moduleName}/analise-inicial");
+            break;
+        case Autenticacao_Model_Grupos::COORDENADOR_DE_PARECERISTA:
+            $this->redirect("/{$this->moduleName}/gerenciar-parecer/index");
+            break;
+        }               
     }
 
     public function gerenciarAssinaturasAction()
@@ -47,38 +49,5 @@ class Parecer_IndexController extends MinC_Controller_Action_Abstract implements
 
 
     public function encaminharAssinaturaAction() {
-        try {
-            $get = $this->getRequest()->getParams();
-            $post = $this->getRequest()->getPost();
-            $servicoDocumentoAssinatura = $this->obterServicoDocumentoAssinatura();
-            
-            if (isset($get['IdPRONAC']) && !empty($get['IdPRONAC']) && $get['encaminhar'] == 'true') {
-                $servicoDocumentoAssinatura->idPronac = $get['IdPRONAC'];
-                $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
-                
-                $idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
-                $idDocumentoAssinatura = $this->getIdDocumentoAssinatura($get['IdPRONAC'], $idTipoDoAtoAdministrativo);
-                
-                $this->redirect("/assinatura/index/visualizar-projeto/?idDocumentoAssinatura=" . $idDocumentoAssinatura . "&origin=" . $get['origin']);
-            } elseif(isset($post['IdPRONAC']) && is_array($post['IdPRONAC']) && count($post['IdPRONAC']) > 0) {
-                // ainda nao implementado o encaminhamento de vários para pareceres
-            }
-        } catch (Exception $objException) {
-            parent::message($objException->getMessage(), "/{$this->moduleName}/index/encaminhar-assinatura");
-        }   
-    }
-
-    private function getIdDocumentoAssinatura($idPronac, $idTipoDoAtoAdministrativo)
-    {
-        $objDocumentoAssinatura = new Assinatura_Model_DbTable_TbDocumentoAssinatura();
-        
-        $where = array();
-        $where['IdPRONAC = ?'] = $idPronac;
-        $where['idTipoDoAtoAdministrativo = ?'] = $idTipoDoAtoAdministrativo;
-        $where['stEstado = ?'] = 1;
-        
-        $result = $objDocumentoAssinatura->buscar($where);
-        
-        return $result[0]['idDocumentoAssinatura'];
     }
 }
