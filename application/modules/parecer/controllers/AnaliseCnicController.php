53bfe8a2f91d594df179921f6c8c19acd7392fd1
Fernão Lopes <fernao@riseup.net>
2017-08-04 10:54:29 -0300

    [ADD] Parecer/Análise CNIC: Adicionando assinatura (incompleto)

--- application/modules/parecer/controllers/AnaliseCnicController.php
+++ application/modules/parecer/controllers/AnaliseCnicController.php
@@ -21,6 +21,7 @@ class Parecer_AnaliseCnicController extends MinC_Controller_Action_Abstract impl
         $this->grupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
     }
 
+    
     public function gerenciarAssinaturasAction()
     {
         $this->redirect("/{$this->moduleName}/index");
@@ -28,60 +29,63 @@ class Parecer_AnaliseCnicController extends MinC_Controller_Action_Abstract impl
 
     public function encaminharAssinaturaAction()
     {
+        try {
+            $get = $this->getRequest()->getParams();
+            $post = $this->getRequest()->getPost();
+            $servicoDocumentoAssinatura = $this->obterServicoDocumentoAssinatura();
+            
+            if (isset($get['IdPRONAC']) && !empty($get['IdPRONAC']) && $get['encaminhar'] == 'true') {
+                $servicoDocumentoAssinatura->idPronac = $get['IdPRONAC'];
+                $servicoDocumentoAssinatura->encaminharProjetoParaAssinatura();
+                
+                $idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_CNIC;
+                $idDocumentoAssinatura = $this->getIdDocumentoAssinatura($get['IdPRONAC'], $idTipoDoAtoAdministrativo);
+                
+                $this->redirect("/assinatura/index/visualizar-projeto/?idDocumentoAssinatura=" . $idDocumentoAssinatura . "&origin=" . $get['origin']);
+            } elseif(isset($post['IdPRONAC']) && is_array($post['IdPRONAC']) && count($post['IdPRONAC']) > 0) {
+                // ainda nao implementado o encaminhamento de vários para pareceres
+            }
+        } catch (Exception $objException) {
+            parent::message($objException->getMessage(), "/{$this->moduleName}/analise-cnic/encaminhar-assinatura");
+        }
     }
 
+    private function getIdDocumentoAssinatura($idPronac, $idTipoDoAtoAdministrativo)
+    {
+        $objDocumentoAssinatura = new Assinatura_Model_DbTable_TbDocumentoAssinatura();
+        
+        $where = array();
+        $where['IdPRONAC = ?'] = $idPronac;
+        $where['idTipoDoAtoAdministrativo = ?'] = $idTipoDoAtoAdministrativo;
+        $where['stEstado = ?'] = 1;
+        
+        $result = $objDocumentoAssinatura->buscar($where);
+        
+        return $result[0]['idDocumentoAssinatura'];
+    }
+    
+    /**
+     * @return Parecer_AnaliseCnicDocumentoAssinaturaController
+     */
     function obterServicoDocumentoAssinatura()
     {
+        if(!isset($this->servicoDocumentoAssinatura)) {
+            require_once __DIR__ . DIRECTORY_SEPARATOR . "DocumentoAssinatura.php";
+            $this->servicoDocumentoAssinatura = new Parecer_DocumentoAssinaturaController($this->getRequest()->getPost());
+        }
+        return $this->servicoDocumentoAssinatura;
     }
     
     public function indexAction()
     {
-        $this->validarPerfis();
-        
-        $auth = Zend_Auth::getInstance();
-        $idusuario = $auth->getIdentity()->usu_codigo;
-
-        $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo');
-        $idOrgao = $GrupoAtivo->codOrgao; //  ¿rg¿o ativo na sess¿o
-
-        $UsuarioDAO = new Autenticacao_Model_Usuario();
-        $agente = $UsuarioDAO->getIdUsuario($idusuario);
-        $idAgenteParecerista = $agente['idagente'];
-
-        $situacao = $this->_request->getParam('situacao');
-        
-        $projeto = new Projetos();
-        $resp = $projeto->buscaProjetosProdutosParaAnalise(
-            array(
-                'distribuirParecer.idAgenteParecerista = ?' => $idAgenteParecerista,
-                'distribuirParecer.idOrgao = ?' => $idOrgao,
-            )
-        );
-
-        $this->idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_INICIAL;
-        $objTbAtoAdministrativo = new Assinatura_Model_DbTable_TbAtoAdministrativo();
-        $this->view->quantidadeMinimaAssinaturas = $objTbAtoAdministrativo->obterQuantidadeMinimaAssinaturas(
-            $this->idTipoDoAtoAdministrativo,
-            $this->auth->getIdentity()->usu_org_max_superior
-        );
-        $this->view->idTipoDoAtoAdministrativo = $this->idTipoDoAtoAdministrativo;
-        $this->view->idPerfilDoAssinante = $GrupoAtivo->codGrupo;
-        
-        Zend_Paginator::setDefaultScrollingStyle('Sliding');
-        Zend_View_Helper_PaginationControl::setDefaultViewPartial('paginacao/paginacao.phtml');
-        $paginator = Zend_Paginator::factory($resp); // dados a serem paginados
-        $currentPage = $this->_getParam('page', 1);
-        $paginator->setCurrentPageNumber($currentPage)->setItemCountPerPage(10); // 10 por p¿gina
-        
-        $this->view->qtdRegistro = count($resp);
-        $this->view->situacao = $situacao;
-        $this->view->buscar = $paginator;
+        $this->redirect("/areadetrabalho");
     }
 
 
     public function emitirparecerAction()
     {
         $idPronac = $this->_request->getParam("idpronac");
+        $this->view->idTipoDoAtoAdministrativo = Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_ANALISE_CNIC;
         
         $projetos = new Projetos();
         if (!$projetos->VerificarIN2017($idPronac)) {
@@ -628,7 +632,9 @@ class Parecer_AnaliseCnicController extends MinC_Controller_Action_Abstract impl
                 
                 if (!$projetos->VerificarIN2017($idpronac)) {
                     $this->validacao50($idpronac, $projetoAtual);
-                }                
+                } else {
+                    $this->view->nrparecerfavoravel = 1;
+                }
                 
                 $produtos = RealizarAnaliseProjetoDAO::analiseDeConteudo($idpronac, $tpPlanilha);
                 $this->view->ResultRealizarAnaliseProjeto = RealizarAnaliseProjetoDAO::analiseparecerConsolidado($idpronac);
