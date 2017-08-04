66c243c5b5ad0e4d17fe580da7baba2c056ffb32
Cleber Santos <oclebersantos@gmail.com>
2017-08-04 18:40:42 -0300

    [UPDATE] SOLICITACAO PROPONENTE: ajustando modulo de arquivo conform trigger do banco
    Former-commit-id: 4cf648bbd145737b9914f529989829ef5ee07053

--- application/modules/arquivo/models/TbArquivoMapper.php
+++ application/modules/arquivo/models/TbArquivoMapper.php
@@ -35,12 +35,30 @@ class Arquivo_Model_TbArquivoMapper extends MinC_Db_Mapper
 
     }
 
-    public function saveCustom($arrPost, Zend_File_Transfer $file)
+    /**
+     *
+     * $arquivoData[
+     *   'dsDocumento'       => 'Descrição do arquivo',
+     *   'idTipoDocumento'   => 27
+     *   );
+     * ]
+     * @param $arrPost
+     * @param Zend_File_Transfer $file
+     * @param $formats
+     * @param $maxSizeFile
+     * @return int
+     */
+    public function saveCustom($arrData, Zend_File_Transfer $file, $formats = array(), $maxSizeFile = 10485760)
     {
-        $idArquivo = false;
+
+        $idArquivo = 0;
+
         $files = $file->getFileInfo();
 
-        if ($file->isUploaded()) {
+        try {
+
+            if (!$file->isUploaded())
+                throw new Exception("Falha ao anexar arquivo! Verifique o documento e tente novamente!");
 
             $arquivoNome = $files['arquivo']['name']; # nome
             $arquivoTemp = $files['arquivo']['tmp_name']; # nome temporario
@@ -53,72 +71,84 @@ class Arquivo_Model_TbArquivoMapper extends MinC_Db_Mapper
                 $arquivoHash = Upload::setHash($arquivoTemp); # hash
             }
 
-            # Tamanho do arquivo: 10MB
-            if ($arquivoTamanho > 10485760) {
-                $this->setMessage('O arquivo não pode ser maior do que 10MB!');
-            } else {
-
-                $dadosTbArquivo = [
-                    'idArquivo' => '',
-                    'nmArquivo' => $arquivoNome,
-                    'sgExtensao' => $arquivoExtensao,
-                    'nrTamanho' => $arquivoTamanho,
-                    'dtEnvio' => GETDATE(),
-                    'stAtivo' => 'I', // @todo verificar se eh isso mesmo
-                    'dsHash' => $arquivoHash
-                ];
-
-                $dadosTbArquivoImagem = [
-                    'idArquivo' => 'tbArquivo.idArquivo = tbArquivoImagem.idArquivo',
-                    'biArquivo' => $arquivoBinario
-                ];
-
-                $dadosTbDocumento = [
-                    'c.idDocumento' => '',
-                    'c.idTipoDocumento' => '',
-                    'c.dsDocumento' => '',
-                    'idArquivo' => 'tbArquivo.idArquivo = tbDocumento.idArquivo',
-                    'dtEmissaoDocumento' => '',
-                    'dtValidadeDocumento' => '',
-                    'idTipoEventoOrigem' => '',
-                    'nmTitulo' => '',
-                    'nrDocumento' => ''
-                ];
-
-
-                $tableTbArquivo = $this;
-//                $tableTbArquivoImagem = new Arquivo_Model_TbArquivoImagem();
-//                $tableTbDocumento = new Arquivo_Model_TbDocumento();
-
-                $modelTbArquivo = new Arquivo_Model_TbArquivo();
-
-                $tbArquivoImagem = new Arquivo_Model_DbTable_TbArquivoImagem();
-                $tbDocumento = new Arquivo_Model_DbTable_TbDocumento();
-
-                try {
-
-                    //$dadosTbArquivo['imdocumento'] = new Zend_Db_Expr("CONVERT(varbinary(MAX), {$arquivoBinario})");
-
-                    # iniciar transacao
-                    $modelTbArquivo->setOptions($dadosTbArquivo);
-                    $idArquivo = $tableTbArquivo->save($modelTbArquivo);
-
-                    $dadosTbArquivoImagem['idArquivo'] = $idArquivo;
-                    $dadosTbDocumento['idArquivo'] = $idArquivo;
-
-                    $tbArquivoImagem->insert($dadosTbArquivoImagem);
-                    $tbDocumento->insert($dadosTbDocumento);
-
-                    # commit
-
-                } catch (Exception $e) {
-                    #rollback
-                    $this->setMessage($e->getMessage());
-                }
+            if (!empty($formats) && !in_array(strtolower($arquivoExtensao), $formats)) {
+                $extensoesPermitidas = implode(", ", $formats);
+                throw new Exception("Erro! Extens&otilde;es permitidas {$extensoesPermitidas}!");
             }
-        } else {
-            $this->setMessage('Falha ao anexar arquivo! O tamanho m&aacute;ximo permitido &egrave; de 10MB.');
 
+            if ($arquivoTamanho > $maxSizeFile) {
+                throw new Exception("O arquivo n&atilde;o pode ser maior do que 10MB!");
+            }
+
+            $dadosTbArquivo = [
+                'idArquivo' => '',
+                'nmArquivo' => $arquivoNome,
+                'sgExtensao' => $arquivoExtensao,
+                'nrTamanho' => $arquivoTamanho,
+                'dtEnvio' => GETDATE(),
+                'stAtivo' => 'I', // @todo verificar se eh isso mesmo
+                'dsHash' => $arquivoHash
+            ];
+
+            $dadosTbArquivoImagem = [
+                'idArquivo' => 'tbArquivo.idArquivo = tbArquivoImagem.idArquivo',
+                'biArquivo' => $arquivoBinario
+            ];
+
+            $dadosTbDocumento = [
+                'c.idDocumento' => '',
+                'c.idTipoDocumento' => $arrData['idTipoDocumento'],
+                'c.dsDocumento' => $arrData['dsDocumento'],
+                'idArquivo' => $idArquivo,
+            ];
+//
+//            $tableTbArquivo = $this;
+//
+//            $objArquivo = new Arquivo_Model_TbArquivo();
+//            $objArquivo->setNmArquivo($arquivoNome);
+//            $objArquivo->setSgExtensao($arquivoExtensao);
+//            $objArquivo->setNrTamanho($arquivoTamanho);
+//            $objArquivo->setDtEnvio(GETDATE());
+//            $objArquivo->setStAtivo('I');
+//            $objArquivo->setDsHash($arquivoHash);
+//
+//            $objArquivoImagem = new Arquivo_Model_TbArquivoImagem();
+//
+////            $objArquivoImagem->setIdArquivo($idArquivo);
+////            $objArquivoImagem->setBiArquivo($arquivoBinario);
+//
+//            $objDocumento = new Arquivo_Model_TbDocumento();
+
+
+
+//            $objDocumento->setIdArquivo($idArquivo);
+//            $objDocumento->setIdTipoDocumento($arrData['idTipoDocumento']);
+//            $objDocumento->setDsDocumento($arrData['dsDocumento']);
+
+
+            $modelTbArquivo = new Arquivo_Model_TbArquivo();
+
+            $tableTbArquivo = $this;
+            $tbArquivoImagem = new Arquivo_Model_DbTable_TbArquivoImagem();
+            $tbDocumento = new Arquivo_Model_DbTable_TbDocumento();
+
+            //$dadosTbArquivo['imdocumento'] = new Zend_Db_Expr("CONVERT(varbinary(MAX), {$arquivoBinario})");
+
+            # iniciar transacao
+            $modelTbArquivo->setOptions($dadosTbArquivo);
+            $idArquivo = $tableTbArquivo->save($modelTbArquivo);
+
+            $dadosTbArquivoImagem['idArquivo'] = $idArquivo;
+            $dadosTbDocumento['idArquivo'] = $idArquivo;
+
+            $tbArquivoImagem->insert($dadosTbArquivoImagem);
+            $tbDocumento->insert($dadosTbDocumento);
+
+            # commit
+
+        } catch (Exception $e) {
+            #rollback
+            $this->setMessage($e->getMessage());
         }
 
         return $idArquivo;
