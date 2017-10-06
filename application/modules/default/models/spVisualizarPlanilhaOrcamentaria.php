4608fec5736f84781322e04551d2d473bb89a1dd
Fernão Lopes <fernao@riseup.net>
2017-10-06 17:43:14 -0300

    [ADD] Remanejamento 50%: nova consulta para visualizar dados de parecer

---
+++ application/modules/default/models/spVisualizarPlanilhaOrcamentaria.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * spVisualizarPlanilhaOrcamentaria
+ *
+ * @uses GenericModel
+ * @author
+ */
+class spVisualizarPlanilhaOrcamentaria extends MinC_Db_Table_Abstract {
+
+    protected $_schema = 'sac';
+    protected $_name  = 'spVisualizarPlanilhaOrcamentaria';
+
+    public function exec($idPronac)
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $sql = "exec ".$this->_banco.".".$this->_name." $idPronac";
+        $db->setFetchMode(Zend_DB :: FETCH_OBJ);
+        return $db->fetchAll($sql);        
+    }        
+}
