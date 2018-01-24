fbcdf06882a3c8a67d77edbf00d8f3a452c86a25
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2018-01-24 16:09:32 -0200

    [UPDATE] Criacao das entidades Model, DbTable e Mapper para SugestaoEnquadramento

---
+++ application/modules/admissibilidade/models/DbTable/SugestaoEnquadramento.php
@@ -0,0 +1,8 @@
+<?php
+
+class Admissibilidade_Model_DbTable_SugestaoEnquadramento extends MinC_Db_Table_Abstract
+{
+    protected $_name = "sugestao_enquadramento";
+    protected $_schema = "sac";
+    protected $_primary = "id_sugestao_enquadramento";
+}
