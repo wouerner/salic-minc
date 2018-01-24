c877caa2d98e830cbbdf2c9b90731c31784f25cf
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-24 19:55:12 -0200

    [FIX] Correcao de carregamento lista de propostas para avaliacao #298

--- application/modules/admissibilidade/models/DbTable/SugestaoEnquadramento.php
+++ application/modules/admissibilidade/models/DbTable/SugestaoEnquadramento.php
@@ -6,18 +6,22 @@ class Admissibilidade_Model_DbTable_SugestaoEnquadramento extends MinC_Db_Table_
     protected $_schema = "sac";
     protected $_primary = "id_sugestao_enquadramento";
 
-    public function obterHistoricoEnquadramento($id_preprojeto) {
-
+    public function obterHistoricoEnquadramento($id_preprojeto)
+    {
         return $this->findBy(['id_preprojeto' => $id_preprojeto]);
     }
 
-    public function isPropostaEnquadrada($id_preprojeto, $id_orgao, $id_perfil_usuario){
-
-        $resultado = $this->findBy(['id_preprojeto' => $id_preprojeto, 'id_orgao' => $id_orgao, 'id_perfil_usuario' => $id_perfil_usuario]);
-        if(count($resultado) > 0){
+    public function isPropostaEnquadrada($id_preprojeto, $id_orgao, $id_perfil_usuario)
+    {
+        $resultado = $this->findBy(
+            [
+                'id_preprojeto' => $id_preprojeto,
+                'id_orgao' => $id_orgao,
+                'id_perfil_usuario' => $id_perfil_usuario
+            ]
+        );
+        if (count($resultado) > 0) {
             return true;
         }
-
-
     }
 }
