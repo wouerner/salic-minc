5d9235bfe49a204c3c6d5350b74e13599da04692
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-30 15:01:20 -0200

    [FIX] Correção de nome da entidade Grupos #296

--- application/modules/autenticacao/models/Grupos.php
+++ application/modules/autenticacao/models/Grupos.php
@@ -2,7 +2,7 @@
 
 class Autenticacao_Model_Grupos extends MinC_Db_Table_Abstract
 {
-    protected $_name = 'usuarios';
+    protected $_name = 'Grupos';
     protected $_schema = 'tabelas';
     protected $_primary = 'gru_codigo';
 
