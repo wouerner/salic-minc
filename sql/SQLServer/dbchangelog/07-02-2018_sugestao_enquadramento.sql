16cdb4c434db46999a2963c94c85c157dff4190b
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-02-07 15:28:19 -0200

    [ADD] Adicionar a coluna id_orgao_superior e ajuste para dados antigos. #349

--- sql/SQLServer/dbchangelog/07-02-2018_sugestao_enquadramento.sql
+++ sql/SQLServer/dbchangelog/07-02-2018_sugestao_enquadramento.sql
@@ -1,1 +1,13 @@
+-- Adição da coluna 'id_orgao_superior' na tabela 'sac.dbo.sugestao_enquadramento'
 ALTER TABLE sac.dbo.sugestao_enquadramento ADD id_orgao_superior INT NULL;
+
+-- Ajuste de dados antigos da tabela 'sac.dbo.sugestao_enquadramento'
+update enquadramento set id_orgao_superior = tabela_temporaria.id_orgao_superior
+  from sac.dbo.sugestao_enquadramento enquadramento
+ inner join (select idPreProjeto as id_preprojeto,
+                    case AreaAbrangencia when '0' then '251'
+                    else '160' end as id_orgao_superior
+             from sac.dbo.PreProjeto) tabela_temporaria on enquadramento.id_preprojeto = tabela_temporaria.id_preprojeto
+ where enquadramento.id_orgao_superior is null
+
+ALTER TABLE sac.dbo.sugestao_enquadramento ALTER COLUMN id_orgao_superior INT NOT NULL
