136e0d86f42e8c1b582487ab45efe2bd04d5a61e
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-02-08 17:53:17 -0200

    [ADD] Tratamento para permitir encaminhar mais de uma vez para o mesmo perfil. #352

--- sql/SQLServer/dbchangelog/08-02-2018_sugestao_enquadramento.sql
+++ sql/SQLServer/dbchangelog/08-02-2018_sugestao_enquadramento.sql
@@ -1,9 +1,9 @@
--- Adição da coluna 'ultimo_enquadramento' na tabela 'sac.dbo.sugestao_enquadramento'
-ALTER TABLE sac.dbo.sugestao_enquadramento ADD ultimo_enquadramento BIT DEFAULT 0 NULL
+-- Adição da coluna 'ultima_sugestao' na tabela 'sac.dbo.sugestao_enquadramento'
+ALTER TABLE sac.dbo.sugestao_enquadramento ADD ultima_sugestao BIT DEFAULT 0 NULL
 
 -- Ajuste de dados antigos da tabela 'sac.dbo.sugestao_enquadramento'
 
-update enquadramento set ultimo_enquadramento = 1
+update enquadramento set ultima_sugestao = 1
 --select count(*)
 from sac.dbo.sugestao_enquadramento enquadramento
   inner join (select max(data_avaliacao) as data_avaliacao, id_preprojeto
@@ -12,13 +12,13 @@ from sac.dbo.sugestao_enquadramento enquadramento
              ) tabela_temporaria
     on enquadramento.id_preprojeto = tabela_temporaria.id_preprojeto
     and enquadramento.data_avaliacao = tabela_temporaria.data_avaliacao
-where enquadramento.ultimo_enquadramento is null
+where enquadramento.ultima_sugestao is null
 
-update enquadramento set ultimo_enquadramento = 0
+update enquadramento set ultima_sugestao = 0
 from sac.dbo.sugestao_enquadramento enquadramento
-where enquadramento.ultimo_enquadramento is null
+where enquadramento.ultima_sugestao is null
 --COMMIT TRANSACTION ;
 --ROLLBACK TRANSACTION ;
 
 -- Definindo coluna id_orgao_superior como obrigatório
-ALTER TABLE sac.dbo.sugestao_enquadramento ALTER COLUMN ultimo_enquadramento BIT NOT NULL;
+ALTER TABLE sac.dbo.sugestao_enquadramento ALTER COLUMN ultima_sugestao BIT NOT NULL;
