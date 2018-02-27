d52b0193108f79458e2845581366fc527d57ce23
Fernão Lopes <fernao@riseup.net>
2018-02-27 11:45:26 -0300

    [UPDATE] Testes: adicionando novos testes da admissibilidade e análise

--- tests/application/modules/assinatura/controllers/AssinaturaControllerTest.php
+++ tests/application/modules/assinatura/controllers/AssinaturaControllerTest.php
@@ -2,7 +2,22 @@
 
 class AssinaturaControllerTest extends MinC_Test_ControllerActionTestCase
 {
+     public function setUp()
+     {
+         parent::setUp();
 
+         $this->idPronac = '209649';
+         
+         $this->autenticar();
+
+        $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_GERAL_ADMISSIBILIDADE, Orgaos::ORGAO_SEFIC_DIC);
+
+        //reset para garantir respostas.
+        $this->resetRequest()
+            ->resetResponse();
+     }
+    
+    
     /**
      * TestGerenciarAssinatura
      *
@@ -11,14 +26,6 @@ class AssinaturaControllerTest extends MinC_Test_ControllerActionTestCase
      */
     public function testGerenciarAssinaturas()
     {
-        $this->autenticar();
-
-        $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_GERAL_ADMISSIBILIDADE, Orgaos::ORGAO_SEFIC_DIC);
-
-        //reset para garantir respostas.
-        $this->resetRequest()
-            ->resetResponse();
-
         $this->dispatch('/assinatura/index/gerenciar-assinaturas');
         $this->assertUrl('assinatura','index', 'gerenciar-assinaturas');
     }
@@ -31,14 +38,6 @@ class AssinaturaControllerTest extends MinC_Test_ControllerActionTestCase
      */
     public function testVisualizarAssinaturas()
     {
-        $this->autenticar();
-
-        $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_GERAL_ADMISSIBILIDADE, Orgaos::ORGAO_SEFIC_DIC);
-
-        //reset para garantir respostas.
-        $this->resetRequest()
-            ->resetResponse();
-
         $this->dispatch('/assinatura/index/visualizar-assinaturas');
         $this->assertUrl('assinatura','index', 'visualizar-assinaturas');
     }
@@ -51,15 +50,31 @@ class AssinaturaControllerTest extends MinC_Test_ControllerActionTestCase
      */
     public function testVisualizarProjeto()
     {
-        $this->autenticar();
-
-        $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_GERAL_ADMISSIBILIDADE, Orgaos::ORGAO_SEFIC_DIC);
-
-        //reset para garantir respostas.
-        $this->resetRequest()
-            ->resetResponse();
-
         $this->dispatch('/assinatura/index/visualizar-projeto?idDocumentoAssinatura=5842');
         $this->assertUrl('assinatura','index', 'visualizar-projeto');
     }
+
+    /**
+     * TestVisualizarAssinatura
+     *
+     * @access public
+     * @return void
+     */
+    public function testVisualizarDocumentosAssinaturaAjax()
+    {
+        $this->dispatch('/assinatura/index/visualizar-documentos-assinatura-ajax/idPronac/' . $this->idPronac);
+        $this->assertUrl('assinatura','index', 'visualizar-documentos-assinatura-ajax');
+    }
+
+    /**
+     * TestVisualizarDocumentoAssinaod
+     *
+     * @access public
+     * @return void
+     */
+    public function testVisualizarDocumentoAssinado()
+    {
+        $this->dispatch('/assinatura/index/visualizar-documento-assinado/idPronac/' . $this->idPronac . '?idDocumentoAssinatura=32');
+        $this->assertUrl('assinatura','index', 'visualizar-documento-assinado');
+    }    
 }
