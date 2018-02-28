0230a1e3c314e150bf870b60e8c0c98eebce07fb
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-02-28 16:51:21 -0300

    [UPDATE] Testes : Ajuste de padrão de testes para classe Admissibilidade.  #353

--- tests/application/modules/admissibilidade/controllers/AdmissibilidadeControllerTest.php
+++ tests/application/modules/admissibilidade/controllers/AdmissibilidadeControllerTest.php
@@ -2,123 +2,75 @@
 
 class AdmissibilidadeControllerTest extends MinC_Test_ControllerActionTestCase
 {
-     public function setUp()
-     {
+    public function setUp()
+    {
         parent::setUp();
 
         $this->idPreProjeto = 276034;
-        
+
         $this->autenticar();
 
         $this->resetRequest()
             ->resetResponse();
 
         $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_ADMISSIBILIDADE, Orgaos::ORGAO_GEAAP_SUAPI_DIAAPI);
-        
+
         $this->resetRequest()
             ->resetResponse();
-     }
-    
-    
-    /**
-     * TestAdmissibilidadeAvaliacao
-     *
-     * @access public
-     * @return void
-     */
-    public function testAdmissibilidadeAvaliacao()
+    }
+
+
+    public function testAdmissibilidadeAvaliacaoAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/listar-propostas');
-        $this->assertUrl('admissibilidade','admissibilidade', 'listar-propostas');
-        
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'listar-propostas');
+
         $this->assertQuery('div.container-fluid div');
     }
 
-    /**
-     * TestExibirpropostacultural
-     *
-     * @access public
-     * @return void
-     */
-    public function testExibirpropostacultural()
+    public function testExibirpropostaculturalAction()
     {
-        $this->dispatch('/admissibilidade/admissibilidade/exibirpropostacultural?idPreProjeto='. $this->idPreProjeto);
-        $this->assertUrl('admissibilidade','admissibilidade', 'exibirpropostacultural');
-        
+        $this->dispatch('/admissibilidade/admissibilidade/exibirpropostacultural?idPreProjeto=' . $this->idPreProjeto);
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'exibirpropostacultural');
+
         $this->assertQuery('div .exibir-proposta-cultural');
     }
 
-    /**
-     * TestAlterarunidadedeanaliseproposta
-     *
-     * @access public
-     * @return void
-     */
-    public function testAlterarunidadedeanaliseproposta()
+    public function testAlterarunidadedeanalisepropostaAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/alterarunianalisepropostaconsulta');
-        $this->assertUrl('admissibilidade','admissibilidade', 'alterarunianalisepropostaconsulta');
-        
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'alterarunianalisepropostaconsulta');
+
         $this->assertQuery('form table.tabela');
     }
 
-    /**
-     * TestMensagemAction
-     *
-     * @access public
-     * @return void
-     */
     public function testMensagemAction()
     {
         $this->dispatch('/admissibilidade/mensagem' . '?idPronac=' . $this->idPronac);
-        $this->assertUrl('admissibilidade','mensagem', 'index');
+        $this->assertUrl('admissibilidade', 'mensagem', 'index');
     }
 
-    /**
-     * TestDesarquivarPropostaAction
-     *
-     * @access public
-     * @return void
-     */
     public function testDesarquivarPropostaAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/desarquivarpropostas');
-        $this->assertUrl('admissibilidade','admissibilidade', 'desarquivarpropostas');
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'desarquivarpropostas');
     }
 
-    /**
-     * TestRedistribuiranaliseAction
-     *
-     * @access public
-     * @return void
-     */
     public function testRedistribuiranaliseAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/redistribuiranalise');
-        $this->assertUrl('admissibilidade','admissibilidade', 'redistribuiranalise');
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'redistribuiranalise');
     }
 
-    /**
-     * TestGerenciaranalistasAction
-     *
-     * @access public
-     * @return void
-     */
     public function testGerenciaranalistasAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/gerenciaranalistas');
-        $this->assertUrl('admissibilidade','admissibilidade', 'gerenciaranalistas');
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'gerenciaranalistas');
     }
 
-    /**
-     * TestGerenciaranalistasLista
-     *
-     * @access public
-     * @return void
-     */
     public function testGerenciaranalistaAction()
     {
         $this->dispatch('/admissibilidade/admissibilidade/gerenciaranalista?usu_cod=6927&usu_orgao=262&gru_codigo=92');
-        $this->assertUrl('admissibilidade','admissibilidade', 'gerenciaranalista');
+        $this->assertUrl('admissibilidade', 'admissibilidade', 'gerenciaranalista');
     }
 }
