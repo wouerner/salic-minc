710f8dbc7cfcd017506120a53cf1f9e0d498c3c4
Fernão Lopes <fernao@riseup.net>
2018-03-06 18:16:34 -0300

    [ADD] Readequações: adicionando testes na controller

---
+++ tests/application/modules/default/controllers/ReadequacoesControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * ReadequacoesControllerTest
+ *
+ * @package
+ * @author
+ */
+class ReadequacoesControllerTest extends MinC_Test_ControllerActionTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->idPronac = '209649';
+        $this->hashPronac = '501eac548e7d4fa987034573abc6e179MjA4OTQ3ZUA3NWVmUiEzNDUwb3RT';
+        
+        $this->autenticar();
+        
+        $this->resetRequest()
+            ->resetResponse();
+        
+        $this->perfilParaProponente();
+        
+        $this->resetRequest()
+            ->resetResponse();
+        
+    }
+
+    /**
+     * TestPlanilhaOrcamentariaAction
+     *
+     * @access public
+     * @return void
+     */    
+    public function testPlanilhaOrcamentariaAction()
+    {
+        $this->dispatch('/readequacoes/planilha-orcamentaria?idPronac=' . $this->hashPronac);
+        $this->assertUrl('default','readequacoes', 'planilha-orcamentaria');
+    }
+}
