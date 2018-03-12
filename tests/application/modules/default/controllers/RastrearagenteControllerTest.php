d6f47abaf0891139ec758559ec0d860b7fbfd187
isaiassm <isaias1113@outlook.com>
2018-03-12 10:30:41 -0300

    [ADD] Criando controller de teste Localizacao Fisica, GRUPO: coordenador de acompanhamento ORGÃO: SEFIC..SACAV

---
+++ tests/application/modules/default/controllers/RastrearagenteControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Projeto_RastrearagenteControllerTest
+ *
+ * @package
+ * @author isaiassm <isaias1113@outlook.com>
+ */
+class RastrearagenteControllerTest extends MinC_Test_ControllerActionTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->idPronac = '209649';
+
+        $this->hashIdPronac = "501eac548e7d4fa987034573abc6e179MjAxNzEzZUA3NWVmUiEzNDUwb3RT";
+
+        $this->autenticar();
+
+        $this->resetRequest()
+            ->resetResponse();
+
+        $this->alterarPerfil(Autenticacao_Model_Grupos::COORDENADOR_ACOMPANHAMENTO, Orgaos::ORGAO_GEAR_SACAV);
+
+        $this->resetRequest()
+            ->resetResponse();
+    }
+    public function testindexAction()
+    {
+        $this->dispatch('/rastrearagente?idPronac=' . $this->idPronac);
+        $this->assertUrl('default', 'rastrearagente', 'index');
+    }
+}
