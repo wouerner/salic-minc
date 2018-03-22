698f02eafbc19ab1e95b7c7184c60eee41c1d33d
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2018-03-22 15:50:52 -0300

    [ADD] Criação de Controller Default para recurso de propostas.  #639

---
+++ application/modules/recurso/controllers/RecursoPropostaController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Recurso_RecursoPropostaController extends MinC_Controller_Action_Abstract
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+
+    }
+}
