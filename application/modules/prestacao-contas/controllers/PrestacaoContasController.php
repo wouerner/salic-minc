f8241c81aa37b6146a405854f7a9d6b10c03ca40
Wouerner Brandão <wouerner@protonmail.com>
2018-04-06 16:33:16 -0300

    [MERGE] Amostragem: adicionando funções de amostragem.

--- application/modules/prestacao-contas/controllers/PrestacaoContasController.php
+++ application/modules/prestacao-contas/controllers/PrestacaoContasController.php
@@ -30,4 +30,71 @@ class PrestacaoContas_PrestacaoContasController extends MinC_Controller_Action_A
     {
     }
 
+    public function tipoAvaliacaoAction()
+    {
+        $idPronac = $this->_request->getParam("idPronac");
+        if (!$idPronac) {
+           throw new Exception('Não existe idPronac');
+        }
+        $informacoes = new PrestacaoContas_Model_vwInformacoesConsolidadasParaAvaliacaoFinanceira();
+        $informacoes = $informacoes->informacoes($idPronac);
+        $this->view->idPronac = $this->_request->getParam("idPronac");
+        $this->view->informacoes = $informacoes->current();
+    }
+
+    /* @todo: adicionar função de salvar o tipo de amostragem*/
+    public function tipoAvaliacaoSalvarAction()
+    {
+        $idPronac = $this->_request->getParam("idPronac");
+        $avaliacao = $this->_request->getParam("avaliacao");
+        if (!$idPronac) {
+           throw new Exception('Não existe idPronac');
+        }
+        if (!$avaliacao) {
+           throw new Exception('Não existe avaliacao');
+        }
+        if ($avaliacao == "todos") {
+            $this->redirect('/realizarprestacaodecontas/planilhaorcamentaria/idPronac/' . $idPronac );
+        }
+        $this->redirect('/prestacao-contas/prestacao-contas/amostragem/idPronac/' . $idPronac . '/tipoAvaliacao/' . $avaliacao);
+    }
+
+    public function amostragemAction()
+    {
+        $idPronac = $this->_request->getParam("idPronac");
+        $tipoAvaliacao = $this->_request->getParam("tipoAvaliacao");
+        if (!$idPronac) {
+           throw new Exception('Não existe idPronac');
+        }
+        if (!$tipoAvaliacao) {
+           throw new Exception('Não existe tipoAvaliacao');
+        }
+        $idPronac = $this->_request->getParam("idPronac");
+        $comprovantes = new PrestacaoContas_Model_spComprovantes();
+        $comprovantes = $comprovantes->exec($idPronac, $tipoAvaliacao);
+        $this->view->idPronac = $this->_request->getParam("idPronac");
+        $this->view->comprovantes = $comprovantes;
+    }
+
+    public function salvarAnaliseAction()
+    {
+        $idPronac = $this->_request->getParam("idPronac");
+        $idComprovantePagamento = $this->_request->getParam("idComprovantePagamento");
+        $situacao = $this->_request->getParam("situacao");
+        $justificativa = $this->_request->getParam("justificativa");
+        if (!$idComprovantePagamento || !$situacao) {
+           throw new Exception('Faltando dados');
+        }
+        $tblComprovantePag = new ComprovantePagamentoxPlanilhaAprovacao();
+        $rsComprovantePag = $tblComprovantePag
+            ->buscar(['idComprovantePagamento = ?' => $idComprovantePagamento])
+            ->current();
+        $rsComprovantePag->dtValidacao = date('Y/m/d H:i:s');
+        $rsComprovantePag->dsJustificativa = isset($justificativa) ? $justificativa : null;
+        $rsComprovantePag->stItemAvaliado = $situacao;
+        $rsComprovantePag->save();
+        $this->_helper->json(['idComprovantePagamento' => $idComprovantePagamento]);
+        /* $this->redirect('/prestacao-contas/prestacao-contas/amostragem/idPronac/' . $idPronac . '/tipoAvaliacao/90'); */
+    }
+
 }
