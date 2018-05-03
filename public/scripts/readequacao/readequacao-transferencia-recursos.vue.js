6454db3ffd8bbf3a35dcb8b6d54d830e627b6db4
Fernão Lopes <fernao@riseup.net>
2018-05-03 19:19:39 -0300

    [UPDATE] Readequação/transf recursos: adicionando validação e condicionais

--- public/scripts/readequacao/readequacao-transferencia-recursos.vue.js
+++ public/scripts/readequacao/readequacao-transferencia-recursos.vue.js
@@ -2,55 +2,80 @@ new Vue({
     el: '#vue-container',
     data: {
 	projeto: {
-	},
-	readequacao: {
-	    justificativa: '',
-	    idReadequacao: null
-	},
-	projetoRecebedor: {
 	    idPronac: null,
 	    nomeProjeto: '',
 	    tipoTransferencia: '',
 	    valorRecebido: 0.00
 	},
+	readequacao:  {
+	    justificativa: '',
+	    idReadequacao: null
+	},
+	projetoRecebedor: {},
 	projetosRecebedores: []
     },
     created: function() {
 	this.obterDadosProjeto();
-	this.obterDadosReadequacao();
+	//this.obterDadosReadequacao();
     },
     mounted: function() {
     },
     methods: {
 	incluirRecebedor: function() {
-	    if (
-		this.projetosRecebedores.idPronac != '' &&
-		    this.projetosRecebedores.nomeProjeto != '' &&
-		    this.projetosRecebedores.tipoTransferencia != ''
+	    if (this.projetoRecebedor.idPronac == '' ||
+		this.projetoRecebedor.idPronac == undefined
 	    ) {
-		this.projetosRecebedores.push(
-		    this.projetoRecebedor
-		);
-		this.projetoRecebedor = {
-		    idPronac: null,
-		    nomeProjeto: '',
-		    tipoTransferencia: '',
-		    valorRecebido: 0.00
-		};
-		// persistir dado ajax
-	    } else {
-		this.mensagemAlerta("\xC9 obrigat\xF3rio informar todos os dados");
-                this.$refs.dsProduto.focus();
+		this.mensagemAlerta("\xC9 obrigat\xF3rio informar o projeto recebedor!");
+		this.$refs.projetoRecebedorIdPronac.focus();
+                return;		
+	    }
+	    if (this.projetoRecebedor.tipoTransferencia == '' ||
+		this.projetoRecebedor.tipoTransferencia == undefined
+	    ) {
+		this.mensagemAlerta("\xC9 obrigat\xF3rio informar o tipo da transfer\xEAncia!");
+		this.$refs.projetoRecebedorTipoTransferencia.focus();
+                return;		
+	    }
+	    if (this.projetoRecebedor.valorRecebido == '' ||
+		this.projetoRecebedor.valorRecebido == undefined
+	    ) {
+		this.mensagemAlerta("\xC9 obrigat\xF3rio informar o valor a ser recebido pelo projeto!");
+		this.$refs.projetoRecebedorValorRecebido.$refs.input.focus();		
                 return;
 	    }
+	    
+	    var valorRecebido = parseFloat(this.projetoRecebedor.valorRecebido.replace(",", "."));
+	    valorRecebido = valorRecebido.toFixed(2);
+	    var somaTransferencia = parseFloat(this.projetoRecebedor.valorRecebido.replace(",", ".")) + parseFloat(this.totalRecebido);
+	    somaTransferencia = somaTransferencia.toFixed(2);
+	    var saldoDisponivel = parseFloat(this.projeto.saldoDisponivel);
+	    saldoDisponivel = saldoDisponivel.toFixed(2)
+	    
+	    if (parseFloat(somaTransferencia) > parseFloat(this.projeto.saldoDisponivel)) {
+		this.mensagemAlerta("Voc\xEA ultrapassou o saldo dispon\xEDvel para transfer\xEAncia, que \xE9 de R$ " + saldoDisponivel + "!");
+		this.$refs.projetoRecebedorValorRecebido.$refs.input.focus();
+                return;
+	    }
+	    this.projetosRecebedores.push(
+		this.projetoRecebedor
+	    );
+	    this.projetoRecebedor = {};
+	    // persistir dado ajax
 	},
 	excluirRecebedor: function(id) {
 	    Vue.delete(this.projetosRecebedores, id);
 	    // remover dado ajax
 	},
 	salvarReadequacao: function() {
-	    this.readequacao.idReadequacao = 1234;
-	    // adicionar via ajax
+	    if (this.readequacao.justificativa.length > 1) {
+		this.obterDadosReadequacao();
+		// adicionar via ajax
+		
+	    } else {
+		this.mensagemAlerta("\xC9 obrigat\xF3rio preencher a justificativa da readequa\xE7\xE3o!");
+		this.$refs.readequacaoJustificativa.focus();		     
+		return;
+	    }
 	},
 	finalizarReadequacao: function() {
 	},
@@ -60,35 +85,55 @@ new Vue({
 		pronac: 164783,
 		idPronac: 289999,
 		nomeProjeto: 'Expedição gastronimica',
-		valorComprovar: 1234,
-		saldoDisponivel: 1234
+		valorComprovar: 1234.00,
+		saldoDisponivel: 1234.00
 	    };
 	},
-	obterDadosReadequacao: function() {
-	    this.readequacao = {};
+	obterDadosReadequacao: function(idPronac) {    
+	    this.readequacao = {
+		idReadequacao: 2345,
+		idTipoReadequacao: 23
+	    };
 	}
     },
     mixins: [utils],
     computed: {
 	totalRecebido: function() {
 	    return this.projetosRecebedores.reduce(function (total, projeto) {
-                return total + parseFloat(projeto.valorRecebido);
+                var resultado = parseFloat(total) + parseFloat(projeto.valorRecebido);
+		return resultado.toFixed(2);
             }, 0);
 	},
 	saldoDisponivel: function() {
-	    return this.projeto.valorComprovar - this.totalRecebido;
+	    var saldo = parseFloat(this.projeto.valorComprovar) - parseFloat(this.totalRecebido);
+	    return saldo.toFixed(2);
 	},
 	disponivelAdicionarRecebedores: function() {
+	    console.log(this.readequacao);
 	    if (this.readequacao.idReadequacao != null) {
 		return true;
 	    } else {
 		return false;
 	    }
-	},	 
+	},
 	disponivelFinalizar: function() {
 	    if (this.projetosRecebedores.length > 0 &&
 		this.totalRecebido <= this.projeto.saldoDisponivel
-	       ) {
+	    ) {
+		return true;
+	    } else {
+		return false;
+	    }
+	},
+	disponivelAdicionarRecebedor: function() {
+	    if (this.totalRecebido == this.projeto.saldoDisponivel) {
+		return false;
+	    } else {
+		return true;
+	    }
+	},
+	exibeProjetosRecebedores: function() {
+	    if (this.projetosRecebedores.length > 0) {
 		return true;
 	    } else {
 		return false;
