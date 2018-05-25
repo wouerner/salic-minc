2e106669fbdb9373567c93d3d85b4fbc0db17bad
Fernão Lopes <fernao@riseup.net>
2018-05-25 17:33:30 -0300

    [UPDATE] Readequação/transf.rec: implementando regra de liberação de link #782

--- application/modules/default/models/fnLiberarLinks.php
+++ application/modules/default/models/fnLiberarLinks.php
@@ -126,13 +126,28 @@ class fnLiberarLinks extends MinC_Db_Table_Abstract
         $PercentualCaptado = $db->fetchRow($PercentualCaptado);
 
         $PercentualCaptado = ($PercentualCaptado->dado) ? $PercentualCaptado->dado : 0;
-
+        $Readequacao_Model_DbTable_TbReadequacao = new Readequacao_Model_DbTable_TbReadequacao();
+        
         if ($PercentualCaptado > 20) {
             $fnVlAcomprovar = new Zend_Db_Expr("SELECT sac.dbo.fnVlAComprovarProjeto($idPronac) AS vlAComprovar");
             $vlAComprovar = $db->fetchOne($fnVlAcomprovar);
             
             if ($vlAComprovar > 0) {
-                $ReadequacaoTransferenciaRecursos = 1;
+                $existeReadequacaoEmAndamento = $Readequacao_Model_DbTable_TbReadequacao->existeReadequacaoEmAndamento(
+                    $idPronac,
+                    Readequacao_Model_DbTable_TbReadequacao::TIPO_READEQUACAO_TRANSFERENCIA_RECURSOS
+                );
+                
+                $existeReadequacaoEmEdicao = $Readequacao_Model_DbTable_TbReadequacao->existeReadequacaoEmEdicao(
+                    $idPronac,
+                    Readequacao_Model_DbTable_TbReadequacao::TIPO_READEQUACAO_TRANSFERENCIA_RECURSOS
+                );
+                
+                if (!$existeReadequacaoEmAndamento
+                    || $existeReadequacaoEmEdicao
+                ) {
+                    $ReadequacaoTransferenciaRecursos = 1;
+                }
             }            
         }
         
@@ -318,7 +333,6 @@ class fnLiberarLinks extends MinC_Db_Table_Abstract
             $Marcas = 1;
 
             $objTbAtoAdministrativo = new Assinatura_Model_DbTable_TbAtoAdministrativo();
-            $Readequacao_Model_DbTable_TbReadequacao = new Readequacao_Model_DbTable_TbReadequacao();
             $existeReadequacaoEmAndamento = $Readequacao_Model_DbTable_TbReadequacao->existeReadequacaoEmAndamento($idPronac);
             $existeReadequacaoPlanilhaEmEdicao = $Readequacao_Model_DbTable_TbReadequacao->existeReadequacaoPlanilhaEmEdicao($idPronac);
             $existeReadequacaoParcialEmEdicao = $Readequacao_Model_DbTable_TbReadequacao->existeReadequacaoParcialEmEdicao($idPronac);
