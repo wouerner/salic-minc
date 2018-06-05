3258b28f021e54d244fbf6c156b048b1b87bb238
guiamado <amado.guigui@gmail.com>
2018-06-05 10:20:55 -0300

    [ADD] Adicionando nova Planilha de consulta dos dados de pagamento #844
    Co-authored-by: Marcos <marcosvanillle@gmail.com>

--- application/modules/prestacao-contas/controllers/PagamentoController.php
+++ application/modules/prestacao-contas/controllers/PagamentoController.php
@@ -28,6 +28,61 @@ class PrestacaoContas_PagamentoController extends MinC_Controller_Action_Abstrac
     {
         $idpronac = $this->_request->getParam('idpronac');
 
+        $this->view->idusuario = $this->idUsuario;
         $this->view->idpronac = $idpronac;
     }
-}
+
+    public function planilhaPagamentoAction()
+    {
+        $idpronac = (int)$this->_request->getParam('idpronac');
+        $this->view->idpronac = $idpronac;
+
+        $planilhaAprovacaoModel = new PlanilhaAprovacao();
+        $resposta = $planilhaAprovacaoModel->planilhaAprovada($this->view->idpronac);
+
+        $planilhaJSON = null;
+
+        foreach($resposta as $item) {
+            $planilhaJSON
+            [$item->cdProduto]
+            ['etapa']
+            [$item->cdEtapa]
+            ['UF']
+            [$item->cdUF]
+            ['cidade']
+            [$item->cdCidade]
+            ['item']
+            [$item->idPlanilhaItens] = [
+                'item' => utf8_encode($item->Item),
+                'varlorAprovado' => $item->vlAprovado,
+                'varlorComprovado' => $item->vlComprovado,
+                'comprovacaoValidada' => $item->ComprovacaoValidada,
+                'idPlanilhaAprovacao' => $item->idPlanilhaAprovacao,
+                'idPlanilhaItens' => $item->idPlanilhaItens,
+            ];
+
+            $planilhaJSON[$item->cdProduto] += [
+                'produto' => utf8_encode($item->Produto),
+                'cdProduto' => $item->cdProduto,
+            ];
+
+            $planilhaJSON[$item->cdProduto]['etapa'][$item->cdEtapa] += [
+                'etapa' => utf8_encode($item->Etapa),
+                'cdEtapa' =>  $item->cdEtapa
+            ];
+
+            $planilhaJSON[$item->cdProduto]['etapa'][$item->cdEtapa]['UF'][$item->cdUF] += [
+                'Uf' => $item->Uf,
+                'cdUF' => $item->cdUF
+            ];
+
+            $planilhaJSON[$item->cdProduto]['etapa'][$item->cdEtapa]['UF'][$item->cdUF]['cidade'][$item->cdCidade] += [
+                'cidade' => utf8_encode($item->Cidade),
+                'cdCidade' => $item->cdCidade
+            ];
+        }
+
+        $this->_helper->json($planilhaJSON);
+    }
+
+    }
