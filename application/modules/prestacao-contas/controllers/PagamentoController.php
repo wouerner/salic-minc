eb0bbb68d6195c82b869b7905eab9c4788095713
guiamado <amado.guigui@gmail.com>
2018-06-12 11:48:30 -0300

    [UPDATE] Alterando modo de title, botão de voltar e colocando nome do projeto #844

--- application/modules/prestacao-contas/controllers/PagamentoController.php
+++ application/modules/prestacao-contas/controllers/PagamentoController.php
@@ -36,9 +36,13 @@ class PrestacaoContas_PagamentoController extends MinC_Controller_Action_Abstrac
         foreach ($resposta as $item) {
             $vlComprovar = $item->vlAprovado - $item->vlComprovado;
             $vlTotalComprovar += $vlComprovar;
+            $nomeProjeto = $item->NomeProjeto;
+            $pronac = $item->Pronac;
         }
 
         $this->view->vlTotalComprovar = $vlTotalComprovar;
+        $this->view->pronac = $pronac;
+        $this->view->nomeProjeto = $nomeProjeto;
 
     }
 
