31ad527e23f18619171e7a45a45ef57c59b0f542
vinnyfs89 <viniciusfesil@gmail.com>
2018-06-21 18:56:42 -0300

    :zap: [UPDATE] Criação de Classes referentes a ações de assinatura para o Enquadramento. #943

---
+++ application/modules/admissibilidade/service/assinatura/acao/Assinar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Modules\Admissibilidade\Service\Assinatura\Acao;
+
+use MinC\Assinatura\Acao\IAcao;
+
+class Assinar implements IAcao, IAcaoAssinar
+{
+    public function executar(\MinC\Assinatura\Model\Assinatura $assinatura)
+    {
+
+    }
+}
