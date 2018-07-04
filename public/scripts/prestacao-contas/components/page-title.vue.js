2f42fb40f22d1174d7633ed9000e0581b63249a6
JRuan Herculano <ruannawe95@gmail.com>
2018-07-04 10:32:21 -0300

    [ ADD ] Renomeia título da página

--- public/scripts/prestacao-contas/components/page-title.vue.js
+++ public/scripts/prestacao-contas/components/page-title.vue.js
@@ -9,7 +9,7 @@ Vue.component('page-title', {
     <div class="page-title">
         <div class="row">
             <div class="col s12 m9 l10">
-                <h1>Presta&ccedil;&atilde;o de Contas: {{title}}</h1>
+                <h1>Presta&ccedil;&atilde;o de Contas: Am&aacute;lise</h1>
             </div>
             <div class="col s12 m3 l2 right-align">
                 <a href="javascript:voltar();" class="btn small grey lighten-3 grey-text z-depth-0 chat-toggle"><i class="material-icons">keyboard_return</i></a>
