e28d36c0dbe8c91d957160bf7905258f3d3d2287
vinnyfs89 <viniciusfesil@gmail.com>
2018-07-05 09:35:16 -0300

    :zap: [UPDATE] Adição de tratamento para que o botão de assinar só fique visível caso exista a possibilidade de assinar o Projeto. #896

--- library/MinC/Assinatura/Model/Assinatura.php
+++ library/MinC/Assinatura/Model/Assinatura.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace MinC\Assinatura\Model;
+use Mockery\Exception;
 
 /**
  * Class Assinatura
@@ -25,6 +26,36 @@ class Assinatura implements IModel
         $this->modeloTbAtoAdministrativo = new \Assinatura_Model_TbAtoAdministrativo($dadosModelo);
         $this->modeloTbDespacho = new \Proposta_Model_TbDespacho($dadosModelo);
         $this->modeloTbDocumentoAssinatura = new \Assinatura_Model_TbDocumentoAssinatura($dadosModelo);
+        if($this->modeloTbAssinatura->getIdPronac() && $this->modeloTbAtoAdministrativo->getIdTipoDoAto()) {
+            $this->carregarDocumentoDeAssinatura();
+        }
         $this->dbTableTbAssinatura = new \Assinatura_Model_DbTable_TbAssinatura();
+    }
+
+    public function definirModeloTbDocumentoAssinatura(array $dados)
+    {
+        $this->modeloTbDocumentoAssinatura = new \Assinatura_Model_TbDocumentoAssinatura($dados);
+        return $this;
+    }
+
+    public function carregarDocumentoDeAssinatura()
+    {
+        if(!$this->modeloTbAssinatura->getIdPronac()) {
+            throw new Exception("Identificador do Projeto n&atilde;o informado.");
+        }
+        if(!$this->modeloTbAtoAdministrativo->getIdTipoDoAto()) {
+            throw new Exception("Identificador do Tipo do Ato Administrativo n&atilde;o informado.");
+        }
+
+        $documentoAssinaturaDbTable = new \Assinatura_Model_DbTable_TbDocumentoAssinatura();
+        $documentoAssinatura = $documentoAssinaturaDbTable->findBy(
+            array(
+                'IdPRONAC' => $this->modeloTbAssinatura->getIdPronac(),
+                'idTipoDoAtoAdministrativo' => $this->modeloTbAtoAdministrativo->getIdTipoDoAto(),
+                'cdSituacao' => \Assinatura_Model_TbDocumentoAssinatura::CD_SITUACAO_DISPONIVEL_PARA_ASSINATURA,
+                'stEstado' => \Assinatura_Model_TbDocumentoAssinatura::ST_ESTADO_DOCUMENTO_ATIVO
+            )
+        );
+        $this->definirModeloTbDocumentoAssinatura($documentoAssinatura);
     }
 }
