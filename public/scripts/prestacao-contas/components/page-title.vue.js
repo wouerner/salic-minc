d22765d100a8429c2f1843bdc120ccbd0385d9e9
JRuan Herculano <ruannawe95@gmail.com>
2018-07-10 16:11:20 -0300

    [ ADD ] Cria computed para montar link de retorno

--- public/scripts/prestacao-contas/components/page-title.vue.js
+++ public/scripts/prestacao-contas/components/page-title.vue.js
@@ -12,15 +12,25 @@ Vue.component('page-title', {
                     <h1>Presta&ccedil;&atilde;o de Contas: An&aacute;lise</h1>
                 </div>
                 <div class="col s12 m3 l2 right-align">
-                    <a href="javascript:voltar();" class="btn small grey lighten-3 grey-text z-depth-0 chat-toggle"><i class="material-icons">keyboard_return</i></a>
+                    <a v-bind:href="goBackWithCollapseExpanded" class="btn small grey lighten-3 grey-text z-depth-0 chat-toggle"><i class="material-icons">keyboard_return</i></a>
                 </div>
             </div>
         </div>
     `,
-    watch:{
+    watch: {
         $route (to, from){
             console.log(to.meta.title);
             this.title = to.meta.title;
         }
-    }
-})
+    },
+    computed: {
+        goBackWithCollapseExpanded: function() {
+            const splitedUrl = window.location.href.split('/')
+            const idPronac = splitedUrl[7];
+            const hostname = window.location.hostname;
+            const url = 'http://' + hostname + '/prestacao-contas/realizar-prestacao-contas/index/idPronac/' + idPronac;
+
+            return url;
+        },
+    },
+});
