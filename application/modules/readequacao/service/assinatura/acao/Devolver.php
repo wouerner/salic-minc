9f458296cb0a97cb25ea037fb34fcbfc86bb121a
vinnyfs89 <viniciusfesil@gmail.com>
2018-07-12 22:28:06 -0300

    :zap: [UPDATE] Inclusão de ações de devolução após a assinatura para os 5 assinantes do tipo do ato administrativo 655. #899

--- application/modules/readequacao/service/assinatura/acao/Devolver.php
+++ application/modules/readequacao/service/assinatura/acao/Devolver.php
@@ -29,6 +29,8 @@ class Devolver implements IAcaoDevolver
             $this->tratarDevolucaoParaVinculadas($tbReadequacaoXParecer['idReadequacao']);
         } elseif ((int)$idTipoDoAtoAdministrativo == (int)\Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_PARECER_TECNICO_AJUSTE_DE_PROJETO) {
             $this->tratarDevolucaoDeAjustesDoProjeto($tbReadequacaoXParecer['idReadequacao']);
+        } elseif ((int)$idTipoDoAtoAdministrativo == (int)\Assinatura_Model_DbTable_TbAssinatura::TIPO_ATO_PARECER_TECNICO_READEQUACAO_PROJETOS_MINC) {
+            $this->tratarDevolucaoDeProjetosMinc($tbReadequacaoXParecer['idReadequacao']);
         }
     }
 
@@ -83,6 +85,52 @@ class Devolver implements IAcaoDevolver
         }
 
         if ($siEncaminhamento) {
+            $dados = ['siEncaminhamento' => $siEncaminhamento];
+            $where = "idReadequacao = {$idReadequacao}";
+            $tbReadequacao = new \Readequacao_Model_DbTable_TbReadequacao();
+            $tbReadequacao->update($dados, $where);
+        }
+    }
+
+    private function tratarDevolucaoDeProjetosMinc($idReadequacao)
+    {
+        $atoAdministrativo = $this->assinatura->modeloTbAtoAdministrativo;
+
+        $objTbProjetos = new Projeto_Model_DbTable_Projetos();
+        $objOrgaos = new \Orgaos();
+        $dadosProjeto = $objTbProjetos->findBy(array(
+            'IdPRONAC' => $this->assinatura->modeloTbAssinatura->getIdPronac()
+        ));
+        $dadosOrgaoSuperior = $objOrgaos->obterOrgaoSuperior($dadosProjeto['Orgao']);
+
+        switch ($atoAdministrativo->getIdPerfilDoAssinante()) {
+            case \Autenticacao_Model_Grupos::COORDENADOR_ACOMPANHAMENTO:
+                $siEncaminhamento = \Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_ENVIADO_ANALISE_TECNICA;
+                break;
+            case \Autenticacao_Model_Grupos::COORDENADOR_GERAL_ACOMPANHAMENTO:
+                $siEncaminhamento = \Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_SOLICITACAO_DEVOLVIDA_AO_COORDENADOR_PELO_COORDENADOR_GERAL;
+                break;
+            case \Autenticacao_Model_Grupos::DIRETOR_DEPARTAMENTO:
+                $siEncaminhamento = \Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_SOLICITACAO_DEVOLVIDA_AO_COORDENADOR_PELO_DIRETOR;
+                $orgaoDestino = \Orgaos::ORGAO_GEAAP_SUAPI_DIAAPI;
+                if ($dadosOrgaoSuperior['Codigo'] == \Orgaos::ORGAO_SUPERIOR_SAV) {
+                    $orgaoDestino = \Orgaos::ORGAO_SAV_DAP;
+                }
+                break;
+            case \Autenticacao_Model_Grupos::SECRETARIO:
+                $siEncaminhamento = \Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_SOLICITACAO_DEVOLVIDA_AO_COORDENADOR_PELO_SECRETARIO;
+                $orgaoDestino = \Orgaos::ORGAO_GEAAP_SUAPI_DIAAPI;
+                if ($dadosOrgaoSuperior['Codigo'] == \Orgaos::ORGAO_SUPERIOR_SAV) {
+                    $orgaoDestino = \Orgaos::ORGAO_SAV_DAP;
+                }
+                break;
+        }
+
+        if (isset($orgaoDestino)) {
+            $objTbProjetos->alterarOrgao($orgaoDestino, $this->assinatura->modeloTbAssinatura->getIdPronac());
+        }
+
+        if($siEncaminhamento) {
             $dados = ['siEncaminhamento' => $siEncaminhamento];
             $where = "idReadequacao = {$idReadequacao}";
             $tbReadequacao = new \Readequacao_Model_DbTable_TbReadequacao();
