f6f41dab79eea6b71ced89dbf45019aaaddf3848
Leonardo Volthier Barbosa Gomes <volthier@gmail.com>
2018-07-17 19:49:48 -0300

    [UPDATE] Retorno Json de uf e Cidades acrescentados a controler fornecedor

--- application/modules/prestacao-contas/controllers/FornecedorController.php
+++ application/modules/prestacao-contas/controllers/FornecedorController.php
@@ -4,7 +4,8 @@ class PrestacaoContas_FornecedorController extends MinC_Controller_Action_Abstra
 {
     public function init()
     {
-
+        Zend_Json::$useBuiltinEncoderDecoder = true;
+        $this->_helper->layout->disableLayout(); // desabilita o Zend_Layout
 
         parent::init();
     }
@@ -12,30 +13,30 @@ class PrestacaoContas_FornecedorController extends MinC_Controller_Action_Abstra
     public function indexAction()
     {
 
-
     }
 
-    public function cidadeAction()
-    {
-        Zend_Json::$useBuiltinEncoderDecoder = true;
-
-        $this->_helper->layout->disableLayout(); // desabilita o Zend_Layout
-
-        $id = $this->_request->getParam("id");
-
-        $cidade = new Cidade();
-
-        $dados = $this->view->combocidades = $cidade->buscar($id);
-
+    private function retornaJson($dados) {
         $data = [];
-
         foreach ($dados as $index => $dado) {
 
-                $data[$index]['id']= utf8_encode($dado->id);
-                $data[$index]['descricao'] = utf8_encode($dado->descricao);
+            $data[$index]['id']= utf8_encode($dado->id);
+            $data[$index]['descricao'] = utf8_encode($dado->descricao);
         };
 
-        $this->_helper->json($data);
+        return  $this->_helper->json($data);
+    }
+
+    public function ufAction() {
+        $estados = new Agente_Model_DbTable_UF();
+        $lista = $this->view->comboEstados = $estados->listar();
+        $this->retornaJson($lista);
+    }
+
+    public function cidadeAction() {
+        $id = $this->_request->getParam("id");
+        $cidade = new Cidade();
+        $dados = $this->view->combocidades = $cidade->buscar($id);
+        $this->retornaJson($dados);
 
     }
 }
