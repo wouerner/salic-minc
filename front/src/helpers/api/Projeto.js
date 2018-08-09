27c4294af753e9581b7e3482636b5059ce3580d7
fernao <fernao@riseup.net>
2018-08-09 20:21:08 -0300

    [UPDATE] Front/Vuex: ajustes nas rotas

--- front/src/helpers/api/Projeto.js
+++ front/src/helpers/api/Projeto.js
@@ -1,6 +1,6 @@
 import API from './base';
 
-const api = () => new API();
+const api = () => new API('http://localhost');
 
 export const buscaProjeto = (idPronac) => {
     const url = `/projeto/incentivo/obter-projeto-ajax/?idPronac=${idPronac}`;
--- front/src/helpers/api/base/index.js
+++ front/src/helpers/api/base/index.js
@@ -1,19 +1,24 @@
 import instance from './instance';
 
-const defaultConfig = () => ({
-    // eslint-disable-next-line
-    // headers: Object.assign({}, { 'Cookie': 'PHPSESSID=f35d28dcf98f13fded03d151ae088664' }),
-});
-
 export default class API {
-    constructor(config = {}) {
-        this.path = config.path;
-
+    constructor(path) {
+        this.path = path;
         this.axios = instance();
     }
 
-    // eslint-disable-next-line
-    get(url, resource = '', config = {}) {
-        return this.axios.get(url, Object.assign({}, defaultConfig(), config));
+    get(id = '') {
+        return this.axios.get(`${this.path}/${id}`);
+    }
+
+    post(data) {
+        return this.axios.post(this.path, data);
+    }
+
+    put(bodyFormData, id) {
+        return this.axios.post(`${this.path}/${id}`, bodyFormData);
+    }
+
+    delete(id) {
+        return this.axios.delete(`${this.path}/${id}`);
     }
 }
