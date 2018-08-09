da1227b7dd5a13eb0086a5eab9d0cdf8bee9c29f
Ruan Herculano <ruannawe95@gmail.com>
2018-08-09 02:39:06 -0300

    [ ADD ] Testes das actions setRegistroAtivo e removerRegistro

--- front/src/modules/foo/store/actions.spec.js
+++ front/src/modules/foo/store/actions.spec.js
@@ -123,4 +123,45 @@ describe('Foo actions', () => {
             expect(fooHelperAPI.atualizarRegistro).toHaveBeenCalled();
         });
     });
+
+    describe('removerRegistro', () => {
+        beforeEach(() => {
+            mockReponse = {};
+
+            commit = jest.fn();
+
+            MockAPI.setResponse(mockReponse);
+        });
+
+        afterEach(() => {
+            MockAPI.setResponse(null);
+        });
+
+        // test('it is commit to removerRegistro', (done) => {
+        //     const registro = mockReponse;
+        //     actions.removerRegistro({ commit }, registro);
+        //     done();
+        //     expect(commit).toHaveBeenCalledWith('ATUALIZAR_REGISTRO_TABELA', registro);
+        // });
+
+        test('it calls fooHelperAPI.removerRegistro', () => {
+            jest.spyOn(fooHelperAPI, 'removerRegistro');
+            const registro = mockReponse;
+            actions.removerRegistro({ commit }, registro);
+            expect(fooHelperAPI.removerRegistro).toHaveBeenCalled();
+        });
+    });
+
+    describe('setRegistroAtivo', () => {
+        beforeEach(() => {
+            commit = jest.fn();
+        });
+
+        test('it is commit to setRegistroAtivo', (done) => {
+            const registro = { Codigo: 1, DadoNr: 'Random String 1' };
+            actions.setRegistroAtivo({ commit }, registro);
+            done();
+            expect(commit).toHaveBeenCalledWith('SET_REGISTRO_ATIVO', registro);
+        });
+    });
 });
