b81eed02b588816d35db4e1204724020cfbd2be8
fernao <fernao@riseup.net>
2018-08-09 20:21:48 -0300

    [UPDATE] Front: ajuste de eslint - espaçamento

--- front/src/modules/readequacao/components/ReadequacaoFormulario.vue
+++ front/src/modules/readequacao/components/ReadequacaoFormulario.vue
@@ -91,163 +91,168 @@
   </div>
 </template>
 <script>
-
-import _ from 'lodash';
-import numeral from 'numeral';
-import { utils } from '@/mixins/utils';
+import _ from "lodash";
+import numeral from "numeral";
+import { utils } from "@/mixins/utils";
 
 // TODO: implementar usando slot para não ter que importar os módulos
-import ReadequacaoSaldoAplicacaoSaldo from '../SaldoAplicacao/components/ReadequacaoSaldoAplicacaoSaldo';
+import ReadequacaoSaldoAplicacaoSaldo from "../SaldoAplicacao/components/ReadequacaoSaldoAplicacaoSaldo";
 
 export default {
-    name: 'ReadequacaoFormulario',
+    name: "ReadequacaoFormulario",
     components: {
-	ReadequacaoSaldoAplicacaoSaldo,
+	ReadequacaoSaldoAplicacaoSaldo
     },
-    data: function () {
+    data: function() {
 	return {
-            readequacao: {
-                'idPronac': '',
-                'idReadequacao': '',
-                'justificativa': '',
-                'arquivo': '',
-                'idTipoReadequacao': '',
-                'dsSolicitacao': '',
-                'idDocumento': '',
-                'nomeArquivo': ''
-            },
+	    readequacao: {
+		idPronac: "",
+		idReadequacao: "",
+		justificativa: "",
+		arquivo: "",
+		idTipoReadequacao: "",
+		dsSolicitacao: "",
+		idDocumento: "",
+		nomeArquivo: ""
+	    },
 	    exibirInfo: false,
 	    minCaracteresJustificativa: 10,
-            arquivo: {
-                tamanhoMaximo: 500000,
-                tiposAceitos: ['pdf']
-            }
-        }
+	    arquivo: {
+		tamanhoMaximo: 500000,
+		tiposAceitos: ["pdf"]
+	    }
+	};
     },
     props: {
-        'idPronac': '',
-        'idTipoReadequacao': '',
-        'objReadequacao': {},
-        'disabled': false,
-        'componenteDsSolicitacao': ''
+	idPronac: "",
+	idTipoReadequacao: "",
+	objReadequacao: {},
+	disabled: false,
+	componenteDsSolicitacao: ""
     },
     mixins: [utils],
-    created: function () {
-        if (_.isEmpty(this.objReadequacao)) {
-            this.obterDadosReadequacao();
-        } else {
-            this.readequacao = this.objReadequacao;
-        }
+    mounted: function() {
+	if (!_.isEmpty(this.objReadequacao)) {
+	    this.readequacao = this.objReadequacao;
+	}
     },
     watch: {
-        objReadequacao: function (value) {
-	    if (typeof value.idReadequacao != 'undefined') {
+	objReadequacao: function(value) {
+	    if (typeof value.idReadequacao != "undefined") {
 		this.readequacao = value;
 	    }
-        }
+	}
     },
     methods: {
-        obterDadosReadequacao: function () {
-            let self = this;
-            $3.ajax({
-                type: "GET",
-                url: "/readequacao/readequacoes/obter-dados-readequacao",
-                data: {
-                    idTipoReadequacao: self.idTipoReadequacao,
-                    idPronac: self.idPronac
-                }
-            }).done(function (response) {
-                if (response.readequacao != null) {
-                    self.readequacao = response.readequacao;
-                }
-            });
-        },
-        salvarReadequacao: function () {
-            if (this.readequacao.justificativa.length < this.minCaracteresJustificativa) {
-                this.mensagemAlerta("\xC9 obrigat\xF3rio preencher a justificativa da readequa\xE7\xE3o!");
-                this.$refs.readequacaoJustificativa.focus();
-                return;
-            }
-
-            this.$emit('eventoSalvarReadequacao', this.readequacao);
-        },
-        subirDocumento: function () {
-            let arquivo = $('#arquivo')[0].files[0],
-                self = this;
-
-            if (!this.validarDocumento(arquivo)) {
-                return;
-            }
-            var formData = new FormData();
-            formData.append('arquivo', arquivo);
-            formData.append('idPronac', self.idPronac);
-            formData.append('idTipoReadequacao', self.idTipoReadequacao);
-            formData.append('idDocumentoAtual', self.readequacao.idDocumento);
-	    if (self.readequacao.idReadequacao) {
-		formData.append('idReadequacao', self.readequacao.idReadequacao);
+	salvarReadequacao: function() {
+	    if (
+		this.readequacao.justificativa.length < this.minCaracteresJustificativa
+	    ) {
+		this.mensagemAlerta(
+		    "\xC9 obrigat\xF3rio preencher a justificativa da readequa\xE7\xE3o!"
+		);
+		this.$refs.readequacaoJustificativa.focus();
+		return;
 	    }
 	    
-            $3('#carregando-arquivo').fadeIn('slow');
-            $3.ajax(
-                Object.assign(
-                    {},
-                    {
-                        type: "POST",
-                        url: "/readequacao/readequacoes/salvar-documento/idPronac/" + self.idPronac,
-                        processData: false,
-                        contentType: false,
-                    },
-                    {
-                        data: formData,
-                    }
-                )
-            ).done(function (response) {
-                self.readequacao.idDocumento = response.documento.idDocumento;
-                self.readequacao.nomeArquivo = response.documento.nomeArquivo;
-                self.readequacao.idReadequacao = response.readequacao.idReadequacao;
-                self.$emit('eventoAtualizarReadequacao', self.readequacao);
-                $3('#carregando-arquivo').fadeOut('slow');
-            });
-        },
-        excluirDocumento: function () {
-            $3('#carregando-arquivo').fadeIn('slow');
-
-            let self = this;
-            $3.ajax({
-                type: "GET",
-                url: "/readequacao/readequacoes/excluir-documento",
-                data: {
-                    idDocumento: self.readequacao.idDocumento,
-                    idPronac: self.idPronac,
-                    idReadequacao: self.readequacao.idReadequacao
-                }
-            }).done(function (response) {
-                self.mensagemSucesso("Documento excluido com sucesso.");
-                self.readequacao.nomeArquivo = '';
-                self.readequacao.idDocumento = '';
-                self.$emit('eventoAtualizarReadequacao', self.readequacao);
-                $3('#carregando-arquivo').fadeOut('slow');
-            }).fail(function (response) {
-                self.mensagemAlerta(response.mensagem);
-                $3('#carregando-arquivo').fadeOut('slow');
-            });
-        },
-	validarDocumento: function (arquivo) {
-            if (!this.arquivo.tiposAceitos.includes(arquivo.name.split(".").pop().toLowerCase())) {
-                this.mensagemAlerta("Extens\xE3o de arquivo inv\xE1lida. Envie arquivos nos tipos: " + this.arquivo.tiposAceitos.join(','));
-                return;
-            }
-
-            if (arquivo.size > this.arquivo.tamanhoMaximo) {
-                this.mensagemAlerta("Arquivo ultrapassou o limite de " + this.arquivo.tamanhoMaximo);
-                return;
-            }
-            return true;
-        },
+	    this.$emit("eventoSalvarReadequacao", this.readequacao);
+	},
+	subirDocumento: function() {
+	    let arquivo = $("#arquivo")[0].files[0],
+		self = this;
+	    
+	    if (!this.validarDocumento(arquivo)) {
+		return;
+	    }
+	    var formData = new FormData();
+	    formData.append("arquivo", arquivo);
+	    formData.append("idPronac", self.idPronac);
+	    formData.append("idTipoReadequacao", self.idTipoReadequacao);
+	    formData.append("idDocumentoAtual", self.readequacao.idDocumento);
+	    if (self.readequacao.idReadequacao) {
+		formData.append("idReadequacao", self.readequacao.idReadequacao);
+	    }
+	    
+	    $3("#carregando-arquivo").fadeIn("slow");
+	    $3
+		.ajax(
+		    Object.assign(
+			{},
+			{
+			    type: "POST",
+			    url:
+			    "/readequacao/readequacoes/salvar-documento/idPronac/" +
+				self.idPronac,
+			    processData: false,
+			    contentType: false
+			},
+			{
+			    data: formData
+			}
+		    )
+		)
+		.done(function(response) {
+		    self.readequacao.idDocumento = response.documento.idDocumento;
+		    self.readequacao.nomeArquivo = response.documento.nomeArquivo;
+		    self.readequacao.idReadequacao = response.readequacao.idReadequacao;
+		    self.$emit("eventoAtualizarReadequacao", self.readequacao);
+		    $3("#carregando-arquivo").fadeOut("slow");
+		});
+	},
+	excluirDocumento: function() {
+	    $3("#carregando-arquivo").fadeIn("slow");
+	    
+	    let self = this;
+	    $3
+		.ajax({
+		    type: "GET",
+		    url: "/readequacao/readequacoes/excluir-documento",
+		    data: {
+			idDocumento: self.readequacao.idDocumento,
+			idPronac: self.idPronac,
+			idReadequacao: self.readequacao.idReadequacao
+		    }
+		})
+		.done(function(response) {
+		    self.mensagemSucesso("Documento excluido com sucesso.");
+		    self.readequacao.nomeArquivo = "";
+		    self.readequacao.idDocumento = "";
+		    self.$emit("eventoAtualizarReadequacao", self.readequacao);
+		    $3("#carregando-arquivo").fadeOut("slow");
+		})
+		.fail(function(response) {
+		    self.mensagemAlerta(response.mensagem);
+		    $3("#carregando-arquivo").fadeOut("slow");
+		});
+	},
+	validarDocumento: function(arquivo) {
+	    if (
+		!this.arquivo.tiposAceitos.includes(
+		    arquivo.name
+			.split(".")
+			.pop()
+			.toLowerCase()
+		)
+	    ) {
+		this.mensagemAlerta(
+		    "Extens\xE3o de arquivo inv\xE1lida. Envie arquivos nos tipos: " +
+			this.arquivo.tiposAceitos.join(",")
+		);
+		return;
+	    }
+	    
+	    if (arquivo.size > this.arquivo.tamanhoMaximo) {
+		this.mensagemAlerta(
+		    "Arquivo ultrapassou o limite de " + this.arquivo.tamanhoMaximo
+		);
+		return;
+	    }
+	    return true;
+	},
 	atualizarDsSolicitacao: function(valor) {
 	    this.readequacao.dsSolicitacao = valor;
-	    this.$emit('eventoAtualizarReadequacao', this.readequacao);
+	    this.$emit("eventoAtualizarReadequacao", this.readequacao);
 	}
     }
-}
+};
 </script>
