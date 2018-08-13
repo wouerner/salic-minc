479e9bb9107b849f9726df228f407bd54ec94029
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-13 14:15:13 -0300

    :zap: Criação do módulo de assinatura para Análise CNIC
    #1158
    
    Co-Authored-By: Pedro Philipe <pedrophilipeti@gmail.com>

---
+++ application/modules/parecer/service/assinatura/analise-cnic/acao/Finalizar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Modules\Parecer\Service\Assinatura\AnaliseCNIC;
+
+use MinC\Assinatura\Acao\IAcaoFinalizar;
+
+class Finalizar implements IAcaoFinalizar
+{
+    public function executar(\MinC\Assinatura\Model\Assinatura $assinatura)
+    {
+    }
+
+}
