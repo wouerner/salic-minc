81a5a5d6eb092f934e0a5df047dd08846b777df9
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-13 15:50:40 -0300

    :zap: Adição de ação para que seja executada ao finalizar a assinatura do projeto.
    #1158

--- application/modules/parecer/service/assinatura/analise-cnic/acao/Finalizar.php
+++ application/modules/parecer/service/assinatura/analise-cnic/acao/Finalizar.php
@@ -8,6 +8,15 @@ class Finalizar implements IAcaoFinalizar
 {
     public function executar(\MinC\Assinatura\Model\Assinatura $assinatura)
     {
+        $modeloTbAssinatura = $assinatura->modeloTbAssinatura;
+
+        $objProjetos = new \Projetos();
+        $objProjetos->alterarSituacao(
+            $modeloTbAssinatura->getIdPronac(),
+            null,
+            \Projeto_Model_Situacao::PROJETO_APRECIADO_PELA_CNIC,
+            'Projeto apreciado pelo Componente da Comiss&atilde;o na Reuni&atilde;o da CNIC'
+        );
     }
 
 }
