f1389809ebf8128a4da4fa39017c1bd000da9d7b
guiamado <amado.guigui@gmail.com>
2018-08-14 12:08:20 -0300

    [ADD] Adicionando componentes necessários para Planilha de Proposta #1177

---
+++ front/src/modules/proposta/components/slTextoSimples.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div v-if="texto && texto.length > 0" v-html="texto"></div>
+        <div v-else>Não informado(a)</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'slTextoSimples',
+    props: ['texto']
+}
+</script>
