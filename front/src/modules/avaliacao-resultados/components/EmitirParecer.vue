8de467efc8bc6beb8581fed4b71c06bf8bb90dfb
leonardo <volthier@gmail.com>
2018-08-15 19:46:09 -0300

    [FIX] select navegador chrome

--- front/src/modules/avaliacao-resultados/components/EmitirParecer.vue
+++ front/src/modules/avaliacao-resultados/components/EmitirParecer.vue
@@ -1,90 +1,67 @@
 <template>
-    <div>
- <v-layout wrap align-center>
-        <v-flex xs12 sm6 d-flex>
-          <v-select
-            :items="items"
-            label="Standard"
-          ></v-select>
-        </v-flex>
-  
-        <v-flex xs12 sm6 d-flex>
-          <v-select
-            :items="items"
-            box
-            label="Box style"
-          ></v-select>
-        </v-flex>
-  
-        <v-flex xs12 sm6 d-flex>
-          <v-select
-            :items="items"
-            label="Outline style"
-            outline
-          ></v-select>
-        </v-flex>
-  
-        <v-flex xs12 sm6 d-flex>
-          <v-select
-            :items="items"
-            label="Solo field"
-            solo
-          ></v-select>
-        </v-flex>
-      </v-layout>
-    </div>
+    <v-container fluid grid-list-xl>
+        <v-layout wrap align-center>
+            <v-flex xs12 sm12 d-flex>
+                <v-select height="20px"
+                    :items="items"
+                    box
+                    label="Box style"
+                ></v-select>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import ModalTemplate from '@/components/modal';
+    import { mapActions, mapGetters } from 'vuex';
+    import ModalTemplate from '@/components/modal';
 
-export default {
-    name: 'UpdateBar',
-    data() {
-        return {
-            currentRegistro: {
-                Codigo: '',
-                DadoNr: '',
+    export default {
+        name: 'UpdateBar',
+        data() {
+            return {
+                currentRegistro: {
+                    Codigo: '',
+                    DadoNr: '',
+                },
+                items: ['pedro', 'leo']
+            };
+        },
+        props: ['registroAtivo'],
+        components: {
+            ModalTemplate,
+        },
+        methods: {
+            ...mapActions({
+                atualizarRegistro: 'foo/atualizarRegistro',
+                setRegistroAtivo: 'foo/setRegistroAtivo',
+                modalOpen: 'modal/modalOpen',
+                modalClose: 'modal/modalClose',
+            }),
+            buildRegistro(event) {
+                const DadoNr = event.target.value;
+                this.currentRegistro.DadoNr = DadoNr;
+                this.currentRegistro.Codigo = this.registro.Codigo;
             },
-            items: ['pedro', 'leo']
-        };
-    },
-    props: ['registroAtivo'],
-    components: {
-        ModalTemplate,
-    },
-    methods: {
-        ...mapActions({
-            atualizarRegistro: 'foo/atualizarRegistro',
-            setRegistroAtivo: 'foo/setRegistroAtivo',
-            modalOpen: 'modal/modalOpen',
-            modalClose: 'modal/modalClose',
-        }),
-        buildRegistro(event) {
-            const DadoNr = event.target.value;
-            this.currentRegistro.DadoNr = DadoNr;
-            this.currentRegistro.Codigo = this.registro.Codigo;
+            checkChangesAndUpdate() {
+                if (this.currentRegistro !== this.registro) {
+                    this.atualizarRegistro(this.currentRegistro);
+                }
+            },
+            fecharModal() {
+                // eslint-disable-next-line
+                $3('#modalTemplate').modal('close');
+                this.modalClose();
+            },
+            alert() {
+                alert('teste');
+            },
         },
-        checkChangesAndUpdate() {
-            if (this.currentRegistro !== this.registro) {
-                this.atualizarRegistro(this.currentRegistro);
-            }
+        computed: {
+            ...mapGetters({
+                registro: 'foo/registro',
+                modalVisible: 'modal/default',
+            }),
         },
-        fecharModal() {
-            // eslint-disable-next-line
-            $3('#modalTemplate').modal('close');
-            this.modalClose();
-        },
-        alert() {
-            alert('teste');
-        },
-    },
-    computed: {
-        ...mapGetters({
-            registro: 'foo/registro',
-            modalVisible: 'modal/default',
-        }),
-    },
-};
+    };
 </script>
