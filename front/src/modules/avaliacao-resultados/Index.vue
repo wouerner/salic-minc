8bbbf5b73d91293b77371308ac1c940a9d89ab05
leonardo <7534286leo>
2018-08-15 10:51:31 -0300

    [FIX] Correção de layout!

--- front/src/modules/avaliacao-resultados/Index.vue
+++ front/src/modules/avaliacao-resultados/Index.vue
@@ -1,16 +1,17 @@
 <template>
     <div id="app">
-    <v-app>
-    <v-container fluid grid-list-xl>
-      <v-layout wrap align-center>
-        <router-view></router-view>
-        </v-container>
-      </v-app>
+        <v-app>
+            <v-container fluid grid-list-xl>
+                <v-layout wrap align-center>
+                    <router-view></router-view>
+                </v-layout>
+            </v-container>
+        </v-app>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'Index',
-};
+    export default {
+        name: 'Index',
+    };
 </script>
