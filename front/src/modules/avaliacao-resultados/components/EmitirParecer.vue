10eec6b32bc25fa8c19845e9a58671444cdfa91d
leonardo <volthier@gmail.com>
2018-08-17 16:56:18 -0300

    [ADD] Arranjo de layout efetuado!

--- front/src/modules/avaliacao-resultados/components/EmitirParecer.vue
+++ front/src/modules/avaliacao-resultados/components/EmitirParecer.vue
@@ -1,9 +1,8 @@
 <template>
-    <v-container fluid grid-list-xl>
+    <v-container grid-list-xl>
         <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
             <v-btn slot="activator" color="green" dark>Emitir Parecer</v-btn>
             <v-card>
-
                 <v-toolbar dark color="green">
                     <v-btn icon dark @click.native="dialog = false">
                         <v-icon>close</v-icon>
@@ -16,112 +15,109 @@
                     </v-toolbar-items>
                 </v-toolbar>
 
-                <v-list subheader>
-                    <v-container fluid grid-list-sm>
-                        <v-layout row wrap>
-                            <v-flex xs12 sm4 md2 offset-lg2>
-                                <v-subheader>PRONAC</v-subheader>
-                                <div>20213465653</div>
-                            </v-flex>
-                            <v-flex xs12 sm4 md2>
-                                <v-subheader>Nome do Projeto</v-subheader>
-                                <div>20213465653</div>
-                            </v-flex>
-                            <v-flex xs12 sm4 md2>
-                                <v-subheader>Proponente</v-subheader>
-                                <div>Sr. Juquinha Amaral</div>
-                            </v-flex>
-                            <v-flex xs12 sm4 md2>
-                                <v-subheader>CPF / CNPJ </v-subheader>
-                                <div>20213465653</div>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </v-list>
-
-                <v-divider></v-divider>
+                <v-container grid-list-sm>
+                    <v-layout row wrap>
+                        <v-flex xs12 sm12 md12>
+                            <p><b>Projeto:</b> 20213465653 - QUALQUER NOME</p>
+                        </v-flex>
+                        <v-flex xs12 sm12 md12>
+                            <p><b>Proponente:</b> 707.707.012-00 - Sr. Juquinha Amaral</p>
+                        </v-flex>
+                    </v-layout>
+                    <v-divider></v-divider>
+                </v-container>
 
                 <h4 class="text-sm-center">Quantidade de Comprovantes</h4>
-                <v-list>
-                    <v-container fluid grid-list-sm>
-                        <v-layout row wrap>
-                            <v-flex xs12 sm3 md2 >
-                                <div>
-                                    <h4 class="label text-sm-right">Total</h4>
-                                    <p class="text-sm-right">20213465653</p>
-                                </div>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
-                                <div>
-                                    <h4 class="label text-sm-right">Validos</h4>
-                                    <p class="text-sm-right">20213465653</p>
-                                </div>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
-                                <div>
-                                    <h4 class="label text-sm-right">Recusados</h4>
-                                    <p class="text-sm-right">Sr. Juquinha Amaral</p>
-                                </div>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
-                                <div>
-                                    <h4 class="label text-sm-right">Não Avaliados</h4>
-                                    <p class="text-sm-right">20213465653</p>
-                                </div>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </v-list>
-
-                <v-divider></v-divider>
-
-                <h4 class="text-sm-center">Valores Comprovados</h4>
-                <v-list>
-                    <v-container fluid grid-list-sm>
-                        <v-layout row wrap>
-                            <v-flex xs12 sm3 md2>
+                <v-container grid-list-sm>
+                    <v-layout row wrap>
+                        <v-flex xs10 sm3 md3 >
+                            <div>
                                 <h4 class="label text-sm-right">Total</h4>
                                 <p class="text-sm-right">20213465653</p>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
                                 <h4 class="label text-sm-right">Validos</h4>
                                 <p class="text-sm-right">20213465653</p>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
                                 <h4 class="label text-sm-right">Recusados</h4>
                                 <p class="text-sm-right">Sr. Juquinha Amaral</p>
-                            </v-flex>
-                            <v-flex xs12 sm3 md2>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
                                 <h4 class="label text-sm-right">Não Avaliados</h4>
                                 <p class="text-sm-right">20213465653</p>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </v-list>
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                    <v-divider></v-divider>
+                </v-container>
 
-                <v-layout wrap align-center>
-                    <v-flex xs10 sm10 d-flex offset-{xs1-md1}>
-                        <v-select height="20px"
-                                  :items="items"
-                                  box
-                                  label="Manifestação"
-                        ></v-select>
+                <h4 class="text-sm-center">Valores Comprovados</h4>
+                <v-container grid-list-sm>
+                    <v-layout row wrap>
+                        <v-flex xs10 sm3 md3 >
+                            <div>
+                                <h4 class="label text-sm-right">Total</h4>
+                                <p class="text-sm-right">20213465653</p>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
+                                <h4 class="label text-sm-right">Validos</h4>
+                                <p class="text-sm-right">20213465653</p>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
+                                <h4 class="label text-sm-right">Recusados</h4>
+                                <p class="text-sm-right">Sr. Juquinha Amaral</p>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm3 md3>
+                            <div>
+                                <h4 class="label text-sm-right">Não Avaliados</h4>
+                                <p class="text-sm-right">20213465653</p>
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                    <v-divider></v-divider>
+                </v-container>
+
+                <v-container grid-list>
+                    <v-layout wrap align-center>
+                        <v-flex>
+                            <v-select height="20px"
+                                      :items="items"
+                                      :item-value="value"
+                                      item-text
+                                      filter="item in items"
+                                      box
+                                      label="Manifestação"
+                            ></v-select>
+                        </v-flex>
+                    </v-layout>
+                    <v-flex>
+                        <v-textarea
+
+                            color="deep-purple"
+                            label="Bio"
+                            height="200px"
+                        ></v-textarea>
                     </v-flex>
-                </v-layout>
-
-                <v-textarea
-                    auto-grow="true"
-                    color="deep-purple"
-                    label="Bio"
-                ></v-textarea>
-
+                </v-container>
             </v-card>
         </v-dialog>
     </v-container>
 </template>
 
 <script>
-    import {mapActions, mapGetters} from 'vuex';
+    import { mapActions, mapGetters } from 'vuex';
     import ModalTemplate from '@/components/modal';
 
     export default {
@@ -135,7 +131,7 @@
                     Codigo: '',
                     DadoNr: '',
                 },
-                items: [{'Reprovação':'R'}, {'Aprovação':'A'},{'Aprovação com Ressalva':'P'}]
+                items: [{Reprovação:'R'}, {Aprovação:'A'},{Aprovação_com_Ressalva:'P'}]
             };
         },
         props: ['registroAtivo'],
@@ -176,12 +172,3 @@
         },
     };
 </script>
-
-<style scoped>
-    .label{
-        color: rgba(0,0,0,.54);
-        font-size: 14px;
-        font-weight: 500;
-        text-align-all: right;
-    }
-</style>
