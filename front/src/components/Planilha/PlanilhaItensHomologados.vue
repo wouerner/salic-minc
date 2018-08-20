6cc44658b2a1b247f1c99912a7f579ab43dcfd1b
Cleber Santos <oclebersantos@gmail.com>
2018-08-20 14:32:10 -0300

    [fix] dados projeto: ajuste valor unitario #1199

--- front/src/components/Planilha/PlanilhaItensHomologados.vue
+++ front/src/components/Planilha/PlanilhaItensHomologados.vue
@@ -29,8 +29,8 @@
                 <td class="center-align">{{row.QtdeDias}}</td>
                 <td class="center-align">{{row.Quantidade}}</td>
                 <td class="center-align">{{row.Ocorrencia}}</td>
-                <td class="right-align">{{ row.vlUnitario }}</td>
-                <td class="right-align">{{row.vlSolicitado  | formatarParaReal}}</td>
+                <td class="right-align">{{row.vlUnitario | formatarParaReal}}</td>
+                <td class="right-align">{{row.vlSolicitado | formatarParaReal}}</td>
                 <td class="right-align">{{row.vlSugerido  | formatarParaReal}}</td>
                 <td class="right-align">{{row.vlAprovado | formatarParaReal}}</td>
                 <td class="justify" width="15%" v-html="row.JustProponente"></td>
