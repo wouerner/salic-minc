d65f828a69734428df139b1ea174ad1e371211ae
Fernanda Lopes de Assis <fernandalassis21@gmail.com>
2018-08-21 14:48:32 -0300

    [UPDATE] Correção da formatação do cep

--- front/src/components/SalicFormatarCep.vue
+++ front/src/components/SalicFormatarCep.vue
@@ -25,7 +25,7 @@
             formatarCep(v) {
                 v = v.replace(/\D/g, '');
                 v = v.replace(/(\d{2})(\d)/, '$1.$2');
-                v = v.replace(/(\d{3})(\d)/, '$1.$2');
+                v = v.replace(/(\d{3})(\d)/, '$1-$2');
                 return v;
             },
         },
