4030da735af92907f66d63b0edb64b6fe419cc7c
leonardo <volthier@gmail.com>
2018-08-30 15:06:48 -0300

    [UPDATE] Dados consolidados carregados no template!

--- front/src/modules/avaliacaoResultados/components/EmitirParecer.vue
+++ front/src/modules/avaliacaoResultados/components/EmitirParecer.vue
@@ -5,7 +5,7 @@
                 <v-btn slot="activator" color="green" dark>Emitir Parecer</v-btn>
                 <v-card>
                     <v-toolbar dark color="green">
-                        <v-btn icon dark :href="redirectLink+idPronac">
+                        <v-btn icon dark :href="redirectLink">
                             <v-icon>close</v-icon>
                         </v-btn>
 
@@ -19,10 +19,10 @@
                     <v-container grid-list-sm>
                         <v-layout row wrap>
                             <v-flex xs12 sm12 md12>
-                                <p><b>Projeto:</b>  - </p>
+                                <p><b>Projeto:</b> {{consolidacao.items.IdPronac}} -  {{consolidacao.items.NomeProjeto}}</p>
                             </v-flex>
                             <v-flex xs12 sm12 md12>
-                                <p><b>Proponente:</b> 707.707.012-00 - Sr. Juquinha Amaral</p>
+                                <p><b>Proponente:</b> {{consolidacao.items.CnpjCpf}} - {{consolidacao.items.Proponente}}</p>
                             </v-flex>
                         </v-layout>
                         <v-divider></v-divider>
@@ -34,25 +34,25 @@
                             <v-flex xs10 sm3 md3 >
                                 <div>
                                     <h4 class="label text-sm-right">Total</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.qtTotalComprovante}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Validados</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.qtComprovantesValidadosProjeto}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Recusados</h4>
-                                    <p class="text-sm-right">Sr. Juquinha Amaral</p>
+                                    <p class="text-sm-right">{{consolidacao.items.qtComprovantesRecusadosProjeto}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Não Avaliados</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.qtComprovantesNaoAvaliados}}</p>
                                 </div>
                             </v-flex>
                         </v-layout>
@@ -65,25 +65,25 @@
                             <v-flex xs10 sm3 md3 >
                                 <div>
                                     <h4 class="label text-sm-right">Total</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.vlComprovadoProjeto}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Validados</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.vlComprovadoValidado}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Recusados</h4>
-                                    <p class="text-sm-right">Sr. Juquinha Amaral</p>
+                                    <p class="text-sm-right">{{consolidacao.items.vlComprovadoRecusado}}</p>
                                 </div>
                             </v-flex>
                             <v-flex xs10 sm3 md3>
                                 <div>
                                     <h4 class="label text-sm-right">Não Avaliados</h4>
-                                    <p class="text-sm-right">20213465653</p>
+                                    <p class="text-sm-right">{{consolidacao.items.vlNaoComprovado}}</p>
                                 </div>
                             </v-flex>
                         </v-layout>
@@ -138,7 +138,6 @@
                 redirectLink: '/prestacao-contas/realizar-prestacao-contas/index/idPronac/',
                 valid: false,
                 dialog: true,
-                data: '',
                 itemRules: [
                     v => !!v || 'Tipo de manifestação e obrigatório!'
                 ],
@@ -146,12 +145,8 @@
                     v => !!v || 'Parecer e obrigatório!',
                     v => v.length >= 10 || 'Parecer deve conter mais que 10 characters'
                 ],
-                currentRegistro: {
-                    Codigo: '',
-                    DadoNr: '',
-                },
                 parecer: '',
-                item:'',
+                item: '',
                 items: [{ id:  'R', text:'Reprovação' }, { id: 'A',text:'Aprovação' }, {id : 'P', text:'Aprovação com Ressalva'}],
             };
         },
@@ -179,14 +174,14 @@
         computed:
             {
                 ...mapGetters({
-                    registro: 'foo/registro',
                     modalVisible: 'modal/default',
+                    consolidacao: 'avaliacaoResultados/consolidacao',
                 }),
             },
         mounted()
         {
+            this.redirectLink = this.redirectLink + this.idPronac;
             this.getConsolidacao(this.idPronac);
-            console.info(this.avaliacaoResultados);
         },
     };
 </script>
