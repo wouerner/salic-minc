44492c84eb85303ef6f6cc48a741756862a620a8
ramonsales <ramon.silvasales@gmail.com>
2018-08-30 11:49:20 -0300

    [ADD]Adicionado o componente ConsolidacaoAnalise

---
+++ front/src/modules/avaliacaoResultados/components/ConsolidacaoAnalise.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import Modal from '@/components/modal';
+
+export default {
+    name: 'ConsolidacaoAnalise',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
