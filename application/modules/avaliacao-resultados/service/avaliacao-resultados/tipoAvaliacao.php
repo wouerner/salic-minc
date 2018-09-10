ab886e4ac71c5aefc7c8043c2baba77be68f10b1
leonardo <volthier@gmail.com>
2018-09-10 15:33:08 -0300

    [UPDATE] Emissão de parecer tecnico sendo enviada para a base de dados!

---
+++ application/modules/avaliacao-resultados/service/avaliacao-resultados/tipoAvaliacao.php
@@ -0,0 +1,22 @@
+
+<?php
+
+class TipoAvaliacao{
+    /**
+     * @var \Zend_Controller_Request_Abstract $request
+     */
+    private $request;
+
+    /**
+     * @var \Zend_Controller_Response_Abstract $response
+     */
+    private $response;
+
+
+    function __construct(\Zend_Controller_Request_Abstract $request, \Zend_Controller_Response_Abstract $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+}
