14cb90daf311b48b82c92406450c1b3ea306f9ee
Ruan Herculano <ruannawe95@gmail.com>
2018-09-12 11:44:20 -0300

    [ADD] Adicionando parte de detalhe da solicitacao #1318
    Co-authored-by: Guilherme <amado.silva133@gmail.com>

--- front/src/modules/proposta/visualizar/components/PropostaHistoricoSolicitacoes.vue
+++ front/src/modules/proposta/visualizar/components/PropostaHistoricoSolicitacoes.vue
@@ -2,80 +2,110 @@
     <div>
         <table>
             <thead>
-                <tr>
-                    <th>N&ordm;</th>
-                    <th>Proposta/Projeto</th>
-                    <th>Solicita&ccedil;&atilde;o</th>
-                    <th>Estado</th>
-                    <th>Dt. Solicita&ccedil;&atilde;o</th>
-                    <th>Dt. Resposta</th>
-                    <th>#</th>
-                </tr>
+            <tr>
+                <th>N&ordm;</th>
+                <th>Proposta/Projeto</th>
+                <th>Solicita&ccedil;&atilde;o</th>
+                <th>Estado</th>
+                <th>Dt. Solicita&ccedil;&atilde;o</th>
+                <th>Dt. Resposta</th>
+                <th>#</th>
+            </tr>
             </thead>
             <tbody v-for="(dado, index) in dados" :key="index">
-                <tr>
-                    <td>{{ dado.idProjeto }}</td>
-                    <td>{{ dado.NomeProjeto }}</td>
-                    <td>{{ dado.dsSolicitacao }}</td>
-                    <td>{{ dado.dsEncaminhamento }}</td>
-                    <td>{{ dado.dtSolicitacao }}</td>
-                    <td>{{ dado.dtResposta }}</td>
-                    <td>
-                        <div class="btn blue small white-text tooltipped" data-tooltip="Visualizar" @click="setActiveTab(index);">
-                            <i class="material-icons">visibility</i>
+            <tr>
+                <td>{{ dado.idProjeto }}</td>
+                <td>{{ dado.NomeProjeto }}</td>
+                <td>{{ dado.dsSolicitacao }}</td>
+                <td>{{ dado.dsEncaminhamento }}</td>
+                <td>{{ dado.dtSolicitacao }}</td>
+                <td>{{ dado.dtResposta }}</td>
+                <td>
+                    <div class="btn blue small white-text tooltipped" data-tooltip="Visualizar"
+                         @click="setActiveTab(index);">
+                        <i class="material-icons">visibility</i>
+                    </div>
+                </td>
+            </tr>
+            <tr v-if="activeTab === index">
+                <td colspan="7">
+                    <div>
+                        <div class="row">
+                            <div class="col s12">
+                                <b>N&ordm; da Proposta: </b>{{dado.idProjeto}}
+                            </div>
+                            <div class="col s12">
+                                <b>Proposta/Projeto: </b>{{dado.NomeProjeto}}
+                            </div>
                         </div>
-                    </td>
-                </tr>
-                <tr v-if="activeTab === index">
-                    <td colspan="7">
-                        Informaçao collapse
-                    </td>
-                </tr>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <h5>Solicita&ccedil;&atilde;o</h5>
+                            </div>
+                            <div class="col s12">
+                                {{dado.dsSolicitacao}}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <h5>Resposta</h5>
+                            </div>
+                            <div class="col s12" v-if="dado.dsResposta">
+                                {{dado.dsResposta}}
+                            </div>
+                            <div class="col s12" v-else>
+                                Sem resposta para esta Solicita&ccedil;&atilde;o.
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
 </template>
 <script>
 
-export default {
-    name: 'PropostaHistoricoSolicitacoes',
-    data() {
-        return {
-            dados: [],
-            activeTab: -1,
-        };
-    },
-    props: ['idpreprojeto'],
-    mounted() {
-        if (typeof this.idpreprojeto !== 'undefined') {
-            this.fetch(this.idpreprojeto);
+    export default {
+        name: 'PropostaHistoricoSolicitacoes',
+        data() {
+            return {
+                dados: [],
+                activeTab: -1,
+            };
+        },
+        props: ['idpreprojeto'],
+        mounted() {
+            if (typeof this.idpreprojeto !== 'undefined') {
+                this.fetch(this.idpreprojeto);
+            }
+        },
+        watch: {
+            idpreprojeto(value) {
+                this.fetch(value);
+            },
+        },
+        methods: {
+            setActiveTab(index) {
+                if (this.activeTab === index) {
+                    this.activeTab = -1;
+                } else {
+                    this.activeTab = index;
+                }
+            },
+            fetch(id) {
+                if (id) {
+                    const self = this;
+                    /* eslint-disable */
+                    $3.ajax({
+                        url: '/solicitacao/mensagem-rest/historico-solicitacoes/idPreProjeto/' + 282175
+                    })
+                        .done(function (response) {
+                            console.log(response.data);
+                            self.dados = response.data.items;
+                        });
+                }
+            },
         }
-    },
-    watch: {
-        idpreprojeto(value) {
-            this.fetch(value);
-        },
-    },
-    methods: {
-        setActiveTab(index) {
-            if (this.activeTab === index) {
-                this.activeTab = -1;
-            } else {
-                this.activeTab = index;
-            }
-        },
-        fetch(id) {
-            if (id) {
-                const self = this;
-                /* eslint-disable */
-                $3.ajax({
-                    url: '/solicitacao/mensagem-rest/historico-solicitacoes/idPreProjeto/' + 282175
-                }).done(function (response) {
-                    console.log(response.data);
-                    self.dados = response.data.items;
-                });
-            }
-        },
-    }
-};
+    };
 </script>
