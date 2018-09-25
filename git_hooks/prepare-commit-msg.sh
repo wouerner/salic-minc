ae4fa0b7d09f7b074089cb42bfaa5a5eec879b96
Pedro Philipe Alves de Oliveira <pedrophilipe.ti@gmail.com>
2018-09-25 17:00:14 -0300

    [ADD] Proteção de merge da branch hmg

---
+++ git_hooks/prepare-commit-msg.sh
@@ -0,0 +1,16 @@
+MERGE_INTO_HMG=$(cat .git/MERGE_MSG | grep "Merge branch 'hmg' into $(git branch | grep \* | cut -d ' ' -f2)")
+if [ -n MERGE_INTO_HMG ]; then
+        NC='\033[0m'
+        echo '
+        ██╗  ██╗███╗   ███╗ ██████╗██████╗
+        ██║  ██║████╗ ████║██╔════╝╚════██╗
+        ███████║██╔████╔██║██║  ███╗ ▄███╔╝
+        ██╔══██║██║╚██╔╝██║██║   ██║ ▀▀══╝
+        ██║  ██║██║ ╚═╝ ██║╚██████╔╝ ██╗
+        ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝  ╚═╝
+        '
+        echo "\033[0;31mVocê não pode usar o merge do hmg para sua branch!${NC}"
+        echo "\033[0;32mUse 'git merge --abort' para cancelar o merge!${NC}"
+  exit 1
+fi
+
