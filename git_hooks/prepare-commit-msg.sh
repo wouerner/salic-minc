61a54ed19cfb1432924ba171ddc18debd3dd4ab8
Pedro Philipe Alves de Oliveira <pedrophilipe.ti@gmail.com>
2018-09-26 10:07:18 -0300

    [FIX] Correção de hook

--- git_hooks/prepare-commit-msg.sh
+++ git_hooks/prepare-commit-msg.sh
@@ -1,8 +1,7 @@
 if [ -e .git/MERGE_MSG ]; then
-	MERGE_INTO_HMG=$(cat .git/MERGE_MSG | grep "Merge branch 'hmg' into $(git branch | grep \* | cut -d ' ' -f2)")
-	echo "Merge branch 'hmg' into $(git branch | grep \* | cut -d ' ' -f2)"
-
-	if [ -n MERGE_INTO_HMG ]; then
+	MERGE_INTO_HMG=$(cat .git/MERGE_MSG | grep "Merge branch 'hmg' into $(git branch | grep \* | cut -d ' ' -f2)" | wc -m)
+	echo ${MERGE_INTO_HMG}
+	if [[ $MERGE_INTO_HMG == "0" ]]; then
 		NC='\033[0m'
 		echo '
 		██╗  ██╗███╗   ███╗ ██████╗██████╗
@@ -12,7 +11,7 @@ if [ -e .git/MERGE_MSG ]; then
 		██║  ██║██║ ╚═╝ ██║╚██████╔╝ ██╗
 		╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝  ╚═╝
 		'
-		echo "\033[0;31mVocê não pode usar o merge do hmg para sua branch!${NC}"
+		echo "\033[0;31mVocê não pode usar o merge do${MERGE_INTO_HMG} hmg para sua branch!${NC}"
 		echo "\033[0;32mUse 'git merge --abort' para cancelar o merge!${NC}"
 	  exit 1
 	fi
