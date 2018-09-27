8eedd21fa41bd05e660cb63da7e8f9e5923badfc
Leonardo Volthier Barbosa Gomes <volthier@gmail.com>
2018-09-27 16:04:34 -0300

    [ADD] MODEL: Revisão Parecer avaliação de resultados!

---
+++ application/modules/avaliacao-resultados/models/tbRevisaoParecer.php
@@ -0,0 +1,127 @@
+<?php
+
+class AvaliacaoResultados_Model_tbRevisaoParecer extends MinC_Db_Model
+{
+
+    protected $_idRevisaoParecer;
+    protected $_idParecerAvaliacaoFinanceira;
+    protected $_idUsuario;
+    protected $_idGrupoAtivo;
+    protected $_dtRevisao;
+    protected $_siStattus;
+    protected $_dsRevisao;
+
+    /**
+     * @return mixed
+     */
+    public function getIdRevisaoParecer()
+    {
+        return $this->_idRevisaoParecer;
+    }
+
+    /**
+     * @param mixed $idRevisaoParecer
+     */
+    public function setIdRevisaoParecer($idRevisaoParecer)
+    {
+        $this->_idRevisaoParecer = $idRevisaoParecer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdParecerAvaliacaoFinanceira()
+    {
+        return $this->_idParecerAvaliacaoFinanceira;
+    }
+
+    /**
+     * @param mixed $idParecerAvaliacaoFinanceira
+     */
+    public function setIdParecerAvaliacaoFinanceira($idParecerAvaliacaoFinanceira)
+    {
+        $this->_idParecerAvaliacaoFinanceira = $idParecerAvaliacaoFinanceira;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuario()
+    {
+        return $this->_idUsuario;
+    }
+
+    /**
+     * @param mixed $idUsuario
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->_idUsuario = $idUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdGrupoAtivo()
+    {
+        return $this->_idGrupoAtivo;
+    }
+
+    /**
+     * @param mixed $idGrupoAtivo
+     */
+    public function setIdGrupoAtivo($idGrupoAtivo)
+    {
+        $this->_idGrupoAtivo = $idGrupoAtivo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtRevisao()
+    {
+        return $this->_dtRevisao;
+    }
+
+    /**
+     * @param mixed $dtRevisao
+     */
+    public function setDtRevisao($dtRevisao)
+    {
+        $this->_dtRevisao = $dtRevisao;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiStattus()
+    {
+        return $this->_siStattus;
+    }
+
+    /**
+     * @param mixed $siStattus
+     */
+    public function setSiStattus($siStattus)
+    {
+        $this->_siStattus = $siStattus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDsRevisao()
+    {
+        return $this->_dsRevisao;
+    }
+
+    /**
+     * @param mixed $dsRevisao
+     */
+    public function setDsRevisao($dsRevisao)
+    {
+        $this->_dsRevisao = $dsRevisao;
+    }
+
+}
+
