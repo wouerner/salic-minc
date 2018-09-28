918ffffbb9659e4cf79a44681139b0ce65fee489
guiamado <amado.guigui@gmail.com>
2018-09-28 16:53:47 -0300

    [ADD] Adicionando Estrutura Rest e Service para Certidoes Negativas #1456
    Co-authored-by: Paulo Henrique <paulotux.mendes@gmail.com>

---
+++ application/modules/projeto/service/certidoesNegativas/CertidoesNegativas.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Modules\CertidoesNegativas\Service\CertidoesNegativas;
+
+use Seguranca;
+
+class CertidoesNegativas
+{
+    /**
+     * @var \Zend_Controller_Request_Abstract $request
+     */
+    private $request;
+
+    /**
+     * @var \Zend_Controller_Response_Abstract $response
+     */
+    private $response;
+
+    function __construct($request, $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function buscaCertidoesNegativas()
+    {
+        $idPronac = $this->request->idPronac;
+
+        if (strlen($idPronac) > 7) {
+            $idPronac = \Seguranca::dencrypt($idPronac);
+        }
+
+        $Projetos = new \Projetos();
+        $rs = $Projetos->buscar(array('IdPRONAC = ?' => $idPronac))->current();
+
+        $sv = new \certidaoNegativa();
+        $resultado = $sv->buscarCertidaoNegativa($rs->CgcCpf);
+    }
+}
