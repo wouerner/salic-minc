69da72a3c2364995fe9d4a7b6b770eaa053387a6
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-01 18:48:40 -0300

    [FIX] Ajustando a tabela de Certidoes Negativas
    Co-authored-by: Guiamado <amado.silva133@gmail.com>

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/CertidoesNegativas.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/CertidoesNegativas.vue
@@ -1,44 +1,46 @@
 <template>
-    <div v-if="dados">
-        <table class="tabela">
-            <thead>
-                <tr class="destacar">
-                    <th>PRONAC</th>
-                    <th>Nome do Projeto</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ dados.informacoes['Pronac'] }}</td>
-                    <td>{{ dados.informacoes['NomeProjeto'] }}</td>
-                </tr>
-            </tbody>
-        </table>
-        <table class="tabela">
-            <thead>
-                <tr class="destacar">
-                    <th>CERTIDOES</th>
-                    <th>DATA DE EMISSAO</th>
-                    <th>DATA DE VALIDADE</th>
-                    <th>PRONAC</th>
-                    <th>SITUACAO</th>
-                </tr>
-            </thead>
-            <tbody v-for="(dado, index) in dados.certidoes" :key="index">
-                <tr>
-                    <td>{{ dado.dsCertidao }}</td>
-                    <td>{{ dado.DtEmissao }}</td>
-                    <td>{{ dado.DtValidade }}</td>
-                    <td>{{ dado.Pronac }}</td>
-                    <div v-if="dado.Situacao">
-                        <td>{{ dado.Situacao }}</td>  
-                    </div>
-                    <div v-else>
-                        Vencida.
-                    </div>
-                </tr>
-            </tbody>
-        </table>
+    <div id="conteudo">
+        <div v-if="dados">
+            <table class="tabela">
+                <thead>
+                    <tr class="destacar">
+                        <th>PRONAC</th>
+                        <th>Nome do Projeto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ dados.informacoes['Pronac'] }}</td>
+                        <td>{{ dados.informacoes['NomeProjeto'] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="tabela">
+                <thead>
+                    <tr class="destacar">
+                        <th>CERTIDOES</th>
+                        <th>DATA DE EMISSAO</th>
+                        <th>DATA DE VALIDADE</th>
+                        <th>PRONAC</th>
+                        <th>SITUACAO</th>
+                    </tr>
+                </thead>
+                <tbody v-for="(dado, index) in dados.certidoes" :key="index">
+                    <tr>
+                        <td>{{ dado.dsCertidao }}</td>
+                        <td>{{ dado.DtEmissao }}</td>
+                        <td>{{ dado.DtValidade }}</td>
+                        <td>{{ dado.Pronac }}</td>
+                        <td v-if="dado.Situacao" >
+                            {{ dado.Situacao }}
+                        </td>
+                        <td v-else>
+                            Vencida
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
