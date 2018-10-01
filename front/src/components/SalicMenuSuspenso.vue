4dd513bee9075a909054eae9049e1db05ac51083
Cleber Santos <oclebersantos@gmail.com>
2018-10-01 15:37:25 -0300

    [update] proposta: criando componente de menu suspenso #1400

---
+++ front/src/components/SalicMenuSuspenso.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="fixed-action-btn vertical">
+            <a class="btn-floating btn-large red">
+                <i class="large material-icons">add</i>
+            </a>
+            <ul>
+                <li>
+                    <a id="ir-para-o-topo" class="btn-floating yellow darken-1 tooltipped"
+                       data-tooltip="Ir para o topo"
+                       href='javascript:void(0)'>
+                        <i class="material-icons">arrow_upward</i>
+                    </a>
+                </li>
+                <li>
+                    <a id="ir-para-o-fim" class="btn-floating yellow darken-1 tooltipped"
+                       data-tooltip="Ir para o fim"
+                       href='javascript:void(0)'>
+                        <i class="material-icons">arrow_downward</i>
+                    </a>
+                </li>
+                <slot></slot>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'SalicMenuSuspenso',
+        /* eslint-disable */
+        mounted() {
+            this.irParaOTopo();
+            this.irParaOFim();
+        },
+        methods: {
+            irParaOTopo() {
+                if ($3("#ir-para-o-topo").length) {
+                    var scrollTrigger = 100, // px
+                        backToTop = function () {
+                            var scrollTop = $3(window).scrollTop();
+                            $3("#ir-para-o-topo")
+                                .parent()
+                                .hide();
+                            if (scrollTop > scrollTrigger) {
+                                $3("#ir-para-o-topo")
+                                    .parent()
+                                    .show();
+                            }
+                        };
+                    backToTop();
+
+                    $3(window).on("scroll", function () {
+                        backToTop();
+                    });
+
+                    $3("#ir-para-o-topo").on("click", function (e) {
+                        e.preventDefault();
+                        $3("html,body").animate(
+                            {
+                                scrollTop: 0,
+                            },
+                            700,
+                        );
+                    });
+                }
+            },
+            irParaOFim() {
+                if ($3("#ir-para-o-fim").length) {
+                    $3("#ir-para-o-fim").on("click", function (e) {
+                        e.preventDefault();
+                        $3("html,body").animate(
+                            {
+                                scrollTop: $(document).height(),
+                            },
+                            700,
+                        );
+                    });
+                }
+            },
+        },
+    };
+</script>
