8fb3e856f896df751110a363a95c3f9286cf7305
guiamado <amado.guigui@gmail.com>
2018-10-02 14:52:27 -0300

    [ADD] Adiciona Modelo Rest e Service Para Local Realização e Deslocamento #1456

---
+++ application/modules/projeto/service/local-realizacao-deslocamento/LocalRealizacaoDeslocamento.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Application\Modules\Projeto\Service\LocalRealizacaoDeslocamento;
+
+use Seguranca;
+
+class LocalRealizacaoDeslocamento
+{
+    /**
+     * @var \Zend_Controller_Request_Abstract $request
+     */
+    private $request;
+
+    /**
+     * @var \Zend_Controller_Response_Abstract $response
+     */
+    private $response;
+
+    function __construct($request, $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function buscaLocalRealizacaoDeslocamento()
+    {
+        $idPronac = $this->request->idPronac;
+
+        if (strlen($idPronac) > 7) {
+            $idPronac = \Seguranca::dencrypt($idPronac);
+        }
+
+        $Projetos = new \Projetos();
+        $result = $Projetos->buscar(array('IdPRONAC = ?' => $idPronac))->current();
+
+        if (!empty($idPronac)) {
+            $buscarLocalRealizacao = \RealizarAnaliseProjetoDAO::localrealizacao($idPronac);
+            $buscarDeslocamento = \RealizarAnaliseProjetoDAO::deslocamento($idPronac);
+        }
+
+        $resultArray = [];
+
+        $localRealizacoes = $this->montaArrayLocalRealizacao($buscarLocalRealizacao);
+        $deslocamento = $this->montaArrayDeslocamento($buscarDeslocamento);
+
+        $informacoes['Pronac'] = $result['AnoProjeto'] + $result['Sequencial'];
+        $informacoes['NomeProjeto'] = $result['NomeProjeto'];
+
+        $resultArray['localRealizacoes'] = $localRealizacoes;
+        $resultArray['Deslocamento'] = $deslocamento;
+        $resultArray['informacoes'] = $informacoes;
+
+        return $resultArray;
+    }
+
+    private function montaArrayLocalRealizacao($local) {
+        $localRealizacoes = [];
+
+        foreach ($local as $item) {
+            $descricao = html_entity_decode(utf8_encode($item->Descricao));
+            $uf = html_entity_decode(utf8_encode($item->UF));
+            $cidade = html_entity_decode(utf8_encode($item->Cidade));
+
+            $localRealizacoes = [
+                'Descricao' => $descricao,
+                'UF' => $uf,
+                'Cidade' => $cidade,
+            ];
+
+        }
+        return $localRealizacoes;
+    }
+
+    private function montaArrayDeslocamento($deslocamento) {
+        $deslocamentoResult = [];
+
+        foreach ($deslocamento as $item) {
+            $PaisOrigem = html_entity_decode(utf8_encode($item->PaisOrigem));
+            $UFOrigem = html_entity_decode(utf8_encode($item->UFOrigem));
+            $MunicipioOrigem = html_entity_decode(utf8_encode($item->MunicipioOrigem));
+            $PaisDestino = html_entity_decode(utf8_encode($item->PaisDestino));
+            $UFDestino = html_entity_decode(utf8_encode($item->UFDestino));
+            $MunicipioDestino = html_entity_decode(utf8_encode($item->MunicipioDestino));
+
+            $deslocamentoResult = [
+                'Qtde' => $item->Qtde,
+                'PaisOrigem' => $PaisOrigem,
+                'UFOrigem' => $UFOrigem,
+                'MunicipioOrigem' => $MunicipioOrigem,
+                'PaisDestino' => $PaisDestino,
+                'UFDestino' => $UFDestino,
+                'MunicipioDestino' => $MunicipioDestino,
+            ];
+
+        }
+        return $deslocamentoResult;
+    }
+}
