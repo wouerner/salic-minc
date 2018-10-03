9496ad47d2df7d95bee3c4ca7d9285b0d0611f1e
Ruan Herculano <ruannawe95@gmail.com>
2018-10-03 17:10:27 -0300

    [ REFACTOR ] limpando código desnecessário

--- application/modules/navegacao/service/perfil/Perfil.php
+++ application/modules/navegacao/service/perfil/Perfil.php
@@ -28,40 +28,18 @@ class Perfil
         $arrAuth = array_change_key_case((array)$objIdentity);
 
         $objModelUsuario = new \Autenticacao_Model_DbTable_Usuario(); // objeto usuario
-        $UsuarioAtivo = new \Zend_Session_Namespace('UsuarioAtivo'); // cria a sessao com o usuario ativo
-        $GrupoAtivo = new \Zend_Session_Namespace('GrupoAtivo'); // cria a sessao com o grupo ativo
+//        $UsuarioAtivo = new \Zend_Session_Namespace('UsuarioAtivo'); // cria a sessao com o usuario ativo
+//        $GrupoAtivo = new \Zend_Session_Namespace('GrupoAtivo'); // cria a sessao com o grupo ativo
 
-        // somente autenticacao zend
-        $resposta = [];
-        $from = base64_encode($this->request->getRequestUri());
-        if (0 == 0) {
+        $grupos = $objModelUsuario->buscarUnidades($arrAuth['usu_codigo'], 21)->toArray();
+//        $objAgente = $objModelUsuario->getIdUsuario($arrAuth['usu_codigo']);
+//        $idAgente = $objAgente['idagente'];
+//        $cpfLogado = $objAgente['usu_identificacao'];
 
-
-            // pega as unidades autorizadas, orgaos e grupos do usuario (pega todos os grupos)
-            if (isset($objIdentity->usu_codigo) && !empty($arrAuth['usu_codigo'])) {
-                $grupos = $objModelUsuario->buscarUnidades($arrAuth['usu_codigo'], 21);
-                $objAgente = $objModelUsuario->getIdUsuario($arrAuth['usu_codigo']);
-                $idAgente = $objAgente['idagente'];
-                $cpfLogado = $objAgente['usu_identificacao'];
-            } elseif (isset($objIdentity->auth) && isset($objIdentity->auth['uid'])) {
-
-                $this->tratarPerfilOAuth($objIdentity);
-            } else {
-                return $this->_helper->redirector->goToRoute(array('controller' => 'index', 'action' => 'logout', 'module' => 'autenticacao', 'from' => $from), null, true);
-            }
-//                $this->view->idAgente = $idAgente;
-//                $this->view->usuario = $objIdentity; // manda os dados do usuario para a visao
-//                $this->view->arrayGrupos = $grupos; // manda todos os grupos do usuario para a visao
-//                $this->view->grupoAtivo = $GrupoAtivo->codGrupo; // manda o grupo ativo do usuario para a vis?o
-//                $this->view->orgaoAtivo = $GrupoAtivo->codOrgao; // manda o orgao ativo do usuario para a vis?o
-
-            $resposta = $grupos->toArray();
-        }
-
-        array_walk($resposta, function (&$value) {
+        array_walk($grupos, function (&$value) {
             $value = array_map('utf8_encode', $value);
         });
 
-        return $resposta;
+        return $grupos;
     }
 }
