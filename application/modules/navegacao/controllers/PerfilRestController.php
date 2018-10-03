dd8a5bbcf85eb640e22c6bdc65e72b23e2e3c667
Ruan Herculano <ruannawe95@gmail.com>
2018-10-03 17:00:01 -0300

    [ FIX ] Trocando nome de classe
    Co-authored-by: Pedro <pedrophilipe.ti@gmail.com>

--- application/modules/navegacao/controllers/PerfilRestController.php
+++ application/modules/navegacao/controllers/PerfilRestController.php
@@ -1,10 +1,9 @@
 <?php
-//namespace Application\Modules\navegacao\controllers;
+
+use Application\Modules\Navegacao\Service\Perfil\Perfil as PerfilService;
 
 class Navegacao_PerfilRestController extends MinC_Controller_Rest_Abstract
 {
-    private $response;
-
     public function __construct(
         Zend_Controller_Request_Abstract $request,
         Zend_Controller_Response_Abstract $response,
@@ -31,46 +30,8 @@ class Navegacao_PerfilRestController extends MinC_Controller_Rest_Abstract
 
     public function indexAction()
     {
-        # Convertendo os objetos da sessao em array, transformando as chaves em minusculas.
-        $auth = Zend_Auth::getInstance();
-        $objIdentity = $auth->getIdentity();
-        $arrAuth = array_change_key_case((array)$objIdentity);
-
-        $objModelUsuario = new Autenticacao_Model_DbTable_Usuario(); // objeto usuario
-        $UsuarioAtivo = new Zend_Session_Namespace('UsuarioAtivo'); // cria a sessao com o usuario ativo
-        $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo'); // cria a sessao com o grupo ativo
-//        xd('AAAAAAAAAA', $UsuarioAtivo, $GrupoAtivo->codGrupo, $GrupoAtivo->codOrgao, $objIdentity, $idAgente);
-
-        // somente autenticacao zend
-        $resposta = [];
-        $from = base64_encode($this->getRequest()->getRequestUri());
-        if (0 == 0) {
-
-
-            // pega as unidades autorizadas, orgaos e grupos do usuario (pega todos os grupos)
-            if (isset($objIdentity->usu_codigo) && !empty($arrAuth['usu_codigo'])) {
-                $grupos = $objModelUsuario->buscarUnidades($arrAuth['usu_codigo'], 21);
-                $objAgente = $objModelUsuario->getIdUsuario($arrAuth['usu_codigo']);
-                $idAgente = $objAgente['idagente'];
-                $cpfLogado = $objAgente['usu_identificacao'];
-            } elseif (isset($objIdentity->auth) && isset($objIdentity->auth['uid'])) {
-
-                $this->tratarPerfilOAuth($objIdentity);
-            } else {
-                return $this->_helper->redirector->goToRoute(array('controller' => 'index', 'action' => 'logout', 'module' => 'autenticacao', 'from' => $from), null, true);
-            }
-//                $this->view->idAgente = $idAgente;
-//                $this->view->usuario = $objIdentity; // manda os dados do usuario para a visao
-//                $this->view->arrayGrupos = $grupos; // manda todos os grupos do usuario para a visao
-//                $this->view->grupoAtivo = $GrupoAtivo->codGrupo; // manda o grupo ativo do usuario para a vis?o
-//                $this->view->orgaoAtivo = $GrupoAtivo->codOrgao; // manda o orgao ativo do usuario para a vis?o
-
-            $resposta = $grupos->toArray();
-        }
-
-        array_walk($resposta, function (&$value) {
-            $value = array_map('utf8_encode', $value);
-        });
+        $perfilService = new PerfilService($this->getRequest(), $this->getResponse());
+        $resposta = $perfilService->buscarPerfisDisponoveis();
 
         $this->renderJsonResponse($resposta, 200);
     }
