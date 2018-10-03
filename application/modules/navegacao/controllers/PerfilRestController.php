b58842232c712acbd2b35736b405283253abfc4a
Ruan Herculano <ruannawe95@gmail.com>
2018-10-03 01:26:50 -0300

    [ REFACTOR ] Melhorando código legado

--- application/modules/navegacao/controllers/PerfilRestController.php
+++ application/modules/navegacao/controllers/PerfilRestController.php
@@ -39,57 +39,40 @@ class Navegacao_PerfilRestController extends MinC_Controller_Rest_Abstract
         $objModelUsuario = new Autenticacao_Model_DbTable_Usuario(); // objeto usuario
         $UsuarioAtivo = new Zend_Session_Namespace('UsuarioAtivo'); // cria a sessao com o usuario ativo
         $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo'); // cria a sessao com o grupo ativo
+//        xd('AAAAAAAAAA', $UsuarioAtivo, $GrupoAtivo->codGrupo, $GrupoAtivo->codOrgao, $objIdentity, $idAgente);
+
         // somente autenticacao zend
-        $perfisDisponiveis = [];
+        $resposta = [];
         $from = base64_encode($this->getRequest()->getRequestUri());
         if (0 == 0) {
-            if ($auth->hasIdentity()) // caso o usuario esteja autenticado
-            {
 
-                // pega as unidades autorizadas, orgaos e grupos do usuario (pega todos os grupos)
-                if (isset($objIdentity->usu_codigo) && !empty($arrAuth['usu_codigo'])) {
-                    $grupos = $objModelUsuario->buscarUnidades($arrAuth['usu_codigo'], 21);
-                    $objAgente = $objModelUsuario->getIdUsuario($arrAuth['usu_codigo']);
-                    $idAgente = $objAgente['idagente'];
-                    $cpfLogado = $objAgente['usu_identificacao'];
-                } elseif (isset($objIdentity->auth) && isset($objIdentity->auth['uid'])) {
 
-                    $this->tratarPerfilOAuth($objIdentity);
-                } else {
-                    return $this->_helper->redirector->goToRoute(array('controller' => 'index', 'action' => 'logout', 'module' => 'autenticacao', 'from' => $from), null, true);
-                }
+            // pega as unidades autorizadas, orgaos e grupos do usuario (pega todos os grupos)
+            if (isset($objIdentity->usu_codigo) && !empty($arrAuth['usu_codigo'])) {
+                $grupos = $objModelUsuario->buscarUnidades($arrAuth['usu_codigo'], 21);
+                $objAgente = $objModelUsuario->getIdUsuario($arrAuth['usu_codigo']);
+                $idAgente = $objAgente['idagente'];
+                $cpfLogado = $objAgente['usu_identificacao'];
+            } elseif (isset($objIdentity->auth) && isset($objIdentity->auth['uid'])) {
 
-                // manda os dados para a visao
+                $this->tratarPerfilOAuth($objIdentity);
+            } else {
+                return $this->_helper->redirector->goToRoute(array('controller' => 'index', 'action' => 'logout', 'module' => 'autenticacao', 'from' => $from), null, true);
+            }
 //                $this->view->idAgente = $idAgente;
 //                $this->view->usuario = $objIdentity; // manda os dados do usuario para a visao
 //                $this->view->arrayGrupos = $grupos; // manda todos os grupos do usuario para a visao
 //                $this->view->grupoAtivo = $GrupoAtivo->codGrupo; // manda o grupo ativo do usuario para a vis?o
 //                $this->view->orgaoAtivo = $GrupoAtivo->codOrgao; // manda o orgao ativo do usuario para a vis?o
 
-                $perfisDisponiveis = $grupos->toArray();
-
-//                return $perfisDisponiveis;
-            } else {
-
-                return $this->_helper->redirector->goToRoute(
-                    array(
-                        'controller' => 'index',
-                        'action' => 'logout',
-                        'module' => 'autenticacao',
-                        'from' => $from
-                    )
-                    , null
-                    , true
-                );
-            }
+            $resposta = $grupos->toArray();
         }
 
-        array_walk($perfisDisponiveis, function (&$value) {
+        array_walk($resposta, function (&$value) {
             $value = array_map('utf8_encode', $value);
         });
 
-//        xd($perfisDisponiveis);
-        $this->renderJsonResponse($perfisDisponiveis, 200);
+        $this->renderJsonResponse($resposta, 200);
     }
 
     public function getAction()
