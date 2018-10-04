f830cdeea37c8757477e10d4301f244dab744bbb
ramonsales <ramon.silvasales@gmail.com>
2018-10-04 17:12:44 -0300

    [UPDATE] Transformando o componente Consolidação Análise em uma pop-up

--- front/src/modules/avaliacaoResultados/components/ConsolidacaoAnalise.vue
+++ front/src/modules/avaliacaoResultados/components/ConsolidacaoAnalise.vue
@@ -1,26 +1,38 @@
 <template>
-    <div>
-        <v-expansion-panel>
-            <v-expansion-panel-content>
-                <div slot="header"></div>
-                <v-card>
-                    <v-card-text>
-                        <table width="100%">
-                            <tr>
-                                <th>
-                                </th>
-                            </tr>
-                            <tr>
-                                <td>
-                                </td>
-                            </tr>
+    <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+    >
+        <v-btn slot="activator" color="success">CONSOLIDAÇÃO</v-btn>
+        <v-card>
+            <h2>{{ nomeProjeto }}</h2>
+            <div>
+                <v-expansion-panel class="mt-2">
+                    <v-expansion-panel-content>
+                        <div slot="header"></div>
+                        <v-card>
+                            <v-card-text>
+                                <table>
+                                    <tr>
+                                        <th>
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                        </td>
+                                    </tr>
 
-                        </table>
-                    </v-card-text>
-                </v-card>
-            </v-expansion-panel-content>
-        </v-expansion-panel>
-    </div>
+                                </table>
+                            </v-card-text>
+                        </v-card>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </div>
+            <v-btn color="red" @click="dialog = false" flat block>FECHAR</v-btn>
+        </v-card>
+
+    </v-dialog>
 </template>
 
 <script>
@@ -29,8 +41,12 @@ export default {
     name: 'ConsolidacaoAnalise',
     data() {
         return {
-
+            dialog: false,
         };
+    },
+    props: {
+        idPronac: Number,
+        nomeProjeto: String,
     },
 };
 </script>
