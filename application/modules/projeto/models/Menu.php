5b7a1c22a8a05c565bc8a59acfab7e311e6acdd0
guiamado <amado.guigui@gmail.com>
2018-10-09 11:27:10 -0300

    [FIX] Providencia Tomada: Corrige Rota #1456

--- application/modules/projeto/models/Menu.php
+++ application/modules/projeto/models/Menu.php
@@ -176,7 +176,7 @@ class Projeto_Model_Menu extends MinC_Db_Table_Abstract
         $menu['outrasinformacoes']['submenu'][] = [
             'label' => 'Provid&ecirc;ncia tomada',
             'title' => 'Ir para Provid&ecirc;ncia tomada',
-            'link' => "/projeto/#/{$idPronacHash}/providencia-tomadas",
+            'link' => "/projeto/#/{$idPronacHash}/providencia-tomada",
             'ajax' => false,
             'grupo' => []
         ];
@@ -343,7 +343,7 @@ class Projeto_Model_Menu extends MinC_Db_Table_Abstract
         $menu['outrasinformacoes']['submenu'][] = [
             'label' => 'Provid&ecirc;ncia tomada',
             'title' => 'Ir para Provid&ecirc;ncia tomada',
-            'link' => "/projeto/#/{$idPronacHash}/providencia-tomadas",
+            'link' => "/projeto/#/{$idPronacHash}/providencia-tomada",
             'ajax' => false,
             'grupo' => []
         ];
