6f2224938125c1cc3bf20708a47f020d6b65364f
fernao <fernao@riseup.net>
2018-10-11 17:50:24 -0300

    [UPDATE] Readequação / assinatura: implementa regra para finalização da assinatura

--- application/modules/readequacao/service/assinatura/acao/Finalizar.php
+++ application/modules/readequacao/service/assinatura/acao/Finalizar.php
@@ -8,5 +8,20 @@ class Finalizar implements IAcaoFinalizar
 {
     public function executar(\MinC\Assinatura\Model\Assinatura $assinatura)
     {
+        $tbReadequacaoXParecerDbTable = new \Readequacao_Model_DbTable_TbReadequacaoXParecer();		
+        $tbReadequacaoXParecer = $tbReadequacaoXParecerDbTable->findBy([
+            'idParecer' => $assinatura->modeloTbDocumentoAssinatura->getIdAtoDeGestao()
+        ]);
+
+        $idReadequacao = $tbReadequacaoXParecer['idReadequacao'];
+
+        $dados = [
+            'stEstado' => (int)\Readequacao_Model_DbTable_TbReadequacao::ST_ESTADO_FINALIZADO,
+            'siEncaminhamento' => (int)\Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_CHECKLIST_PUBLICACAO
+        ];
+        
+        $where = "idReadequacao = {$idReadequacao}";	
+        $tbReadequacao = new \Readequacao_Model_DbTable_TbReadequacao();
+        $tbReadequacao->update($dados, $where);       
     }
 }
