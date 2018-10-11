11b061bec97ccbf9540e2e9b7e6cfe3dbe1861fc
Ruan Herculano <ruannawe95@gmail.com>
2018-10-11 14:09:40 -0300

    [ ADD ] Logo do salic redirecionando pra home

--- front/src/components/menu-superior/index.vue
+++ front/src/components/menu-superior/index.vue
@@ -1,84 +1,14 @@
 <template>
     <div>
-        <div v-if="dados">
+        <div class="teste" v-if="dados">
+
             <v-toolbar
                 app
-                color="green darken-4 "
+                color=""
                 dense
                 dark
+                style="background-color: #1b5e20"
             >
-                <!--<v-icon>home</v-icon>-->
-                <!--<v-toolbar-title>-->
-                    <!--<img src="/public/img/logo_salic.png" alt="logo" height="30px">-->
-                <!--</v-toolbar-title>-->
-                <v-spacer></v-spacer>
-                <v-menu
-                    :nudge-width="100"
-                    v-for="item in dadosMenu"
-                    :key="item.id"
-                    offset-y
-                >
-                    <v-toolbar-title
-                        slot="activator"
-                    >
-                        <v-btn flat>
-                            <span v-html="item.label"></span>
-                            <v-icon right dark>arrow_drop_down</v-icon>
-                        </v-btn>
-                    </v-toolbar-title>
-                    <v-list
-                        v-for="(menu, index) in item.menu"
-                        :key="index"
-                    >
-                        <v-list-tile>
-                            <v-list-tile-title>
-                                <a
-                                    :href="('/' + menu.url.module + '/' + menu.url.controller + '/' + menu.url.action)"
-                                    v-html="menu.label">
-                                </a>
-                            </v-list-tile-title>
-                        </v-list-tile>
-                    </v-list>
-                </v-menu>
-                <v-spacer></v-spacer>
-                <v-layout row wrap align-center>
-                    <v-flex xs6>
-                        <v-select
-                            :items="perfisDisponiveis"
-                            menu-props="auto"
-                            hide-details
-                            label="Select"
-                            single-line
-                        ></v-select>
-                    </v-flex>
-                </v-layout>
-                <v-spacer></v-spacer>
-                <v-menu
-                    :nudge-width="100"
-                    offset-y
-                >
-                    <v-toolbar-title
-                        slot="activator"
-                    >
-                        <span>Olá, Romulo</span>
-                        <v-icon dark>arrow_drop_down</v-icon>
-                    </v-toolbar-title>
-                    <v-list>
-                        <v-list-tile>
-                            <v-list-tile-title>
-                                Teste
-                            </v-list-tile-title>
-                        </v-list-tile>
-                    </v-list>
-                </v-menu>
-            </v-toolbar>
-            <v-toolbar
-                app
-                color="green darken-4 "
-                dense
-                dark
-            >
-                <!--<v-icon>home</v-icon>-->
                 <a href="/principal">
                     <img
                         src="/public/img/logo_salic.png"
@@ -86,9 +16,6 @@
                         height="40"
                     />
                 </a>
-                <!--<v-toolbar-title>-->
-                    <!--<img src="/public/img/logo_salic.png" alt="logo" height="30px">-->
-                <!--</v-toolbar-title>-->
                 <v-spacer></v-spacer>
                 <v-menu
                     :nudge-width="100"
@@ -217,5 +144,9 @@
         width: 500px;
         height: 750px;
         overflow: scroll;
+    }
+
+    .teste {
+
     }
 </style>
