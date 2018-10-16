6ed2e66d0a791d6682ede0d0947fb83d46028091
Wouerner Brandão <wouerner@protonmail.com>
2018-10-16 20:07:28 -0300

    [UPDATE] Merge dos trabalhos e atualização de botões.

---
+++ front/src/modules/avaliacaoResultados/components/analise/AssinarButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-btn
+        dark
+        icon
+        flat
+        small
+        color="green"
+        :to="{ name: 'AnalisePlanilha', params:{ id: idPronac }}"
+    >
+        <v-icon>edit</v-icon>
+    </v-btn>
+</template>
+
+<script>
+    export default {
+        name: 'AssinarButton',
+        props: [
+            'idPronac',
+        ],
+        data() {
+            return {
+            };
+        },
+        methods: {
+        },
+        computed: {
+        },
+    };
+</script>
