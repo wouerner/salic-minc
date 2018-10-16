b83f82e194446f894d51c91a374fe07b1052a448
guiamado <amado.guigui@gmail.com>
2018-10-16 11:16:28 -0300

    [FIX] Diligencias: Arruma tabela de diligencias proposta #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/components/VisualizarDiligenciaProposta.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/components/VisualizarDiligenciaProposta.vue
@@ -13,7 +13,7 @@
                     <td class="center">
                         <button
                             class="waves-effect waves-darken btn white black-text"
-                            @click="setActiveTab(diligencia.idPreprojeto, diligencia.idAvaliacaoProposta)"
+                            @click="setActiveTab(diligencia.idPreprojeto, diligencia.idAvaliacaoProposta, index)"
                         >
                             <i class="material-icons">visibility</i>
                         </button>
@@ -21,53 +21,53 @@
                     <td>{{ diligencia.idPreprojeto }}</td>
                     <td>{{ diligencia.dataSolicitacao }}</td>
                 </tr>
+                <tr v-if="activeTab === index && ativo && Object.keys(dadosDiligencia).length > 0">
+                    <td colspan="3">
+                        <template>
+                        <table class="tabela">
+                            <tbody>
+                            <tr>
+                                <th>Nr PROPOSTA</th>
+                                <th>NOME DA PROPOSTA</th>
+                            </tr>
+                            <tr>
+                                <td>{{ dadosDiligencia.idPreprojeto }}</td>
+                                <td>{{ dadosDiligencia.nomeProjeto }}</td>
+                            </tr>
+                            <tr>
+                                <th>DATA DA SOLICITA&Ccedil;&Atilde;O</th>
+                                <th>DATA DA RESPOSTA</th>
+                            </tr>
+                            <tr>
+                                <td>{{ dadosDiligencia.dataSolicitacao }}</td>
+                                <td>{{ dadosDiligencia.dataResposta }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <table v-if="dadosDiligencia.Solicitacao" class="tabela">
+                            <tbody>
+                            <tr>
+                                <th>SOLICITA&Ccedil;&Atilde;O</th>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 20px" v-html="dadosDiligencia.Solicitacao"></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <table v-if="dadosDiligencia.Resposta" class="tabela">
+                            <tbody>
+                            <tr>
+                                <th>RESPOSTA:</th>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 20px" v-html="dadosDiligencia.Resposta"></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        </template>
+                    </td>
+                </tr>
             </tbody>
-        </table>
-        <table>
-            <tr v-if="activeTab && Object.keys(dadosDiligencia).length > 0">
-                <td colspan="3">
-                    <table class="tabela">
-                        <tbody>
-                        <tr>
-                            <th>Nr PROPOSTA</th>
-                            <th>NOME DA PROPOSTA</th>
-                        </tr>
-                        <tr>
-                            <td>{{ dadosDiligencia.idPreprojeto }}</td>
-                            <td>{{ dadosDiligencia.nomeProjeto }}</td>
-                        </tr>
-                        <tr>
-                            <th>DATA DA SOLICITA&Ccedil;&Atilde;O</th>
-                            <th>DATA DA RESPOSTA</th>
-                        </tr>
-                        <tr>
-                            <td>{{ dadosDiligencia.dataSolicitacao }}</td>
-                            <td>{{ dadosDiligencia.dataResposta }}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <table v-if="dadosDiligencia.Solicitacao" class="tabela">
-                        <tbody>
-                        <tr>
-                            <th>SOLICITA&Ccedil;&Atilde;O</th>
-                        </tr>
-                        <tr>
-                            <td style="padding-left: 20px" v-html="dadosDiligencia.Solicitacao"></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <table v-if="dadosDiligencia.Resposta" class="tabela">
-                        <tbody>
-                        <tr>
-                            <th>Resposta:</th>
-                        </tr>
-                        <tr>
-                            <td style="padding-left: 20px" v-html="dadosDiligencia.Solicitacao"></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
         </table>
     </div>
 </template>
@@ -84,18 +84,19 @@ export default {
                     return {};
                 },
             },
-            activeTab: false,
+            activeTab: -1,
+            ativo: false,
         };
     },
     methods: {
-        setActiveTab(idPreProjeto, idAvaliacaoProposta) {
-            this.activeTab = !this.activeTab;
-            // if (this.activeTab === index) {
-            //     this.activeTab = -1;
-            // } else {
-            //     this.activeTab = index;
-            // }
-            this.obterDiligencias(idPreProjeto, idAvaliacaoProposta);
+        setActiveTab(idPreProjeto, idAvaliacaoProposta, index) {
+            if (this.activeTab === index) {
+                this.ativo = !this.ativo;
+            } else {
+                this.activeTab = index;
+                this.ativo = true;
+                this.obterDiligencias(idPreProjeto, idAvaliacaoProposta);
+            }
         },
         obterDiligencias(idPreProjeto, idAvaliacaoProposta) {
             const self = this;
