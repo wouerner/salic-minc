f7e540aa79d894fc00babbc9ecc87947f6d5e85e
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-18 12:29:27 -0300

    [UPDATE] Histórico de Encaminhamento: Ajustando o compomente, os dados na tela e migrando algumas coisas para o Vuex

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
@@ -1,39 +1,47 @@
 <template>
     <div id="conteudo">
-        <div v-if="dados.informacoes">
-            <IdentificacaoProjeto :pronac="dados.informacoes.Pronac"
-                                  :nomeProjeto="dados.informacoes.NomeProjeto">
+        <div>
+            <IdentificacaoProjeto
+                    :pronac="dadosProjeto.Pronac"
+                    :nomeProjeto="dadosProjeto.NomeProjeto">
             </IdentificacaoProjeto>
-            <div v-if="dados.Encaminhamentos.length > 0">
-                <table class="tabela">
-                    <thead>
-                        <tr class="destacar">
-                            <th class="center">PRODUTO</th>
-                            <th class="center">UNIDADE</th>
-                            <th class="center">OBSERVA&Ccedil;&Atilde;O</th>
-                            <th class="center">DT. ENVIO</th>
-                            <th class="center">DT. RETORNO</th>
-                            <th class="center">QT. DIAS</th>
+            <table class="tabela" v-if="dados.Encaminhamentos.length > 0">
+                <thead>
+                    <tr class="destacar">
+                        <th class="center">PRODUTO</th>
+                        <th class="center">UNIDADE</th>
+                        <th class="center">OBSERVA&Ccedil;&Atilde;O</th>
+                        <th class="center">DT. ENVIO</th>
+                        <th class="center">DT. RETORNO</th>
+                        <th class="center">QT. DIAS</th>
+                    </tr>
+                </thead>
+                    <tbody v-for="(dado, index) in dados.Encaminhamentos" :key="index">
+                        <tr>
+                            <td class="center">{{ dado.Produto }}</td>
+                            <td class="center">{{ dado.Unidade }}</td>
+                            <td class="center" v-html="dado.Observacao">{{ dado.Observacao }}</td>
+                            <td class="center">{{ dado.DtEnvio }}</td>
+                            <td class="center">{{ dado.DtRetorno }}</td>
+                            <td class="center">{{ dado.qtDias }}</td>
                         </tr>
-                    </thead>
-                        <tbody v-for="(dado, index) in dados.Encaminhamentos" :key="index">
-                            <tr>
-                                <td class="center">{{ dado.Produto }}</td>
-                                <td class="center">{{ dado.Unidade }}</td>
-                                <td class="center" v-html="dado.Observacao">{{ dado.Observacao }}</td>
-                                <td class="center">{{ dado.DtEnvio }}</td>
-                                <td class="center">{{ dado.DtRetorno }}</td>
-                                <td class="center">{{ dado.qtDias }}</td>
-                            </tr>
-                        </tbody>
-                </table>
+                    </tbody>
+            </table>
+            <div v-else>
+                <fieldset>
+                    <legend>Hist&oacute;rico Encaminhamento</legend>
+                    <div class="center">
+                        <em>Dados n&atilde;o  informado.</em>
+                    </div>
+                </fieldset>
             </div>
-            <div v-else><h3>Sem Encaminhamentos.</h3></div>
         </div>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 import IdentificacaoProjeto from './IdentificacaoProjeto';
+
 export default {
     name: 'HistoricoEncaminhamento',
     props: ['idPronac'],
@@ -43,25 +51,29 @@ export default {
     data() {
         return {
             dados: {
-                    type: Object,
-                    default() {
-                        return {};
-                    },
+                type: Object,
+                default() {
+                    return {};
                 },
+            },
         };
     },
+    computed: {
+        ...mapGetters({
+            dadosProjeto: 'projeto/projeto',
+        }),
+    },
     mounted() {
-        if (typeof this.$route.params.idPronac !== 'undefined') {
+        if (typeof this.dadosProjeto.idPronac !== 'undefined') {
             this.buscar_dados();
         }
     },
     methods: {
         buscar_dados() {
             const self = this;
-            const idPronac = self.$route.params.idPronac
             /* eslint-disable */
             $3.ajax({
-                url: '/projeto/historico-encaminhamento-rest/index/idPronac/' + idPronac,
+                url: '/projeto/historico-encaminhamento-rest/index/idPronac/' + self.dadosProjeto.idPronac,
             }).done(function (response) {
                 self.dados = response.data;
             });
