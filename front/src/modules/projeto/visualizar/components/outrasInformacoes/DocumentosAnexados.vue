fa45e63c34298da34d1bf14c4641c899b9111da4
guiamado <amado.silva133@gmail.com>
2018-10-25 16:26:34 -0200

    [ADD] Documentos Anexados: Adicionando tela de loading #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/DocumentosAnexados.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/DocumentosAnexados.vue
@@ -1,55 +1,67 @@
 <template>
-    <div id="conteudo" v-if="documentosAnexados.documentos">
-        <IdentificacaoProjeto
-                :pronac="dadosProjeto.Pronac"
-                :nomeProjeto="dadosProjeto.NomeProjeto">
-        </IdentificacaoProjeto>
-        <table v-if="Object.keys(documentosAnexados.documentos).length > 0">
-            <thead>
-            <tr class="destacar">
-                <th class="center">N&ordm;</th>
-                <th class="center">CLASSIFICA&ccedil;&atilde;O</th>
-                <th class="center">DATA</th>
-                <th class="center">TIPO DE DOCUMENTO</th>
-                <th class="center">DOCUMENTO</th>
-            </tr>
-            </thead>
-            <tbody v-for="(dado, index) in documentosAnexados.documentos" :key="index">
-            <tr>
-                <td class="center">{{ index + 1 }}</td>
-                <td class="center">{{ dado.Anexado }}</td>
-                <td class="center">{{ dado.Data }}</td>
-                <td class="center">{{ dado.Descricao }}</td>
-                <td class="center">
-                    <a class=""
-                       :href="`/consultardadosprojeto/abrir-documentos-anexados?id=${dado.idArquivo}&tipo=${dado.AgenteDoc}&idPronac=${dadosProjeto.idPronac}`"
-                       target="_blank"
-                       data-position="top" data-delay="50" data-tooltip="Visualizar">
-                        {{ dado.NoArquivo }}
-                    </a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-        <div v-else>
-            <fieldset>
-                <legend>Certid&otilde;es Negativas</legend>
-                <div class="center">
-                    <em>Dados n&atilde;o informado.</em>
-                </div>
-            </fieldset>
+    <div id="conteudo">
+        <div v-if="loading">
+            <Carregando :text="'Carregando Documentos Anexados'"></Carregando>
+        </div>
+        <div v-else-if="documentosAnexados">
+            <IdentificacaoProjeto
+                    :pronac="dadosProjeto.Pronac"
+                    :nomeProjeto="dadosProjeto.NomeProjeto">
+            </IdentificacaoProjeto>
+            <table v-if="Object.keys(documentosAnexados.documentos).length > 0">
+                <thead>
+                <tr class="destacar">
+                    <th class="center">N&ordm;</th>
+                    <th class="center">CLASSIFICA&ccedil;&atilde;O</th>
+                    <th class="center">DATA</th>
+                    <th class="center">TIPO DE DOCUMENTO</th>
+                    <th class="center">DOCUMENTO</th>
+                </tr>
+                </thead>
+                <tbody v-for="(dado, index) in documentosAnexados.documentos" :key="index">
+                <tr>
+                    <td class="center">{{ index + 1 }}</td>
+                    <td class="center">{{ dado.Anexado }}</td>
+                    <td class="center">{{ dado.Data }}</td>
+                    <td class="center">{{ dado.Descricao }}</td>
+                    <td class="center">
+                        <a class=""
+                        :href="`/consultardadosprojeto/abrir-documentos-anexados?id=${dado.idArquivo}&tipo=${dado.AgenteDoc}&idPronac=${dadosProjeto.idPronac}`"
+                        target="_blank"
+                        data-position="top" data-delay="50" data-tooltip="Visualizar">
+                            {{ dado.NoArquivo }}
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <div v-else>
+                <fieldset>
+                    <legend>Certid&otilde;es Negativas</legend>
+                    <div class="center">
+                        <em>Dados n&atilde;o informado.</em>
+                    </div>
+                </fieldset>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import { mapGetters, mapActions } from 'vuex';
+    import Carregando from '@/components/Carregando';
     import IdentificacaoProjeto from './IdentificacaoProjeto';
 
     export default {
         name: 'DocumentosAnexados',
         props: ['idPronac'],
+        data() {
+            return {
+                loading: true,
+            };
+        },
         components: {
+            Carregando,
             IdentificacaoProjeto,
         },
         mounted() {
@@ -61,6 +73,9 @@
             dados(value) {
                 this.informacoes = value.informacoes;
             },
+            documentosAnexados(value) {
+                this.loading = false;
+            }
         },
         computed: {
             ...mapGetters({
