dcd575cfa21591794d47eb7a97b9a875ee3a4266
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-29 16:34:47 -0300

    [UPDATE] Recursos: Migrando estrutrua para o Vue.js

---
+++ front/src/modules/projeto/visualizar/components/analiseAprovacao/Recursos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="conteudo">
+
+    </div>
+</template>
+<script>
+
+    import { mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name: 'CertidoesNegativas',
+        data() {
+            return {
+                loading: true,
+            };
+        },
+    };
+</script>
+
