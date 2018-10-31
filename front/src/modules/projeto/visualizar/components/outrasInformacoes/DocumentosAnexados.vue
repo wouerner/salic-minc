f398baecae45c7d39aee42f90feb6a6f551d9faa
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-31 18:06:39 -0300

    [ ADD ] Migrando tela Documentos Anexados para a estrutura do Vuetify #1456
    Co-authored-by: Guigui <amado.silva133@gmail.com>
    Co-authored-by: Ruan <ruannawe95@gmail.com>

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/DocumentosAnexados.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/DocumentosAnexados.vue
@@ -1,73 +1,26 @@
-<!--<template>-->
-<!--<div id="conteudo">-->
-<!--<div v-if="loading">-->
-<!--<Carregando :text="'Carregando Documentos Anexados'"></Carregando>-->
-<!--</div>-->
-<!--<div v-else-if="documentosAnexados">-->
-<!--<IdentificacaoProjeto-->
-<!--:pronac="dadosProjeto.Pronac"-->
-<!--:nomeProjeto="dadosProjeto.NomeProjeto">-->
-<!--</IdentificacaoProjeto>-->
-<!--<table v-if="Object.keys(documentosAnexados.documentos).length > 0">-->
-<!--<thead>-->
-<!--<tr class="destacar">-->
-<!--<th class="center">N&ordm;</th>-->
-<!--<th class="center">CLASSIFICA&ccedil;&atilde;O</th>-->
-<!--<th class="center">DATA</th>-->
-<!--<th class="center">TIPO DE DOCUMENTO</th>-->
-<!--<th class="center">DOCUMENTO</th>-->
-<!--</tr>-->
-<!--</thead>-->
-<!--<tbody v-for="(dado, index) in documentosAnexados.documentos" :key="index">-->
-<!--<tr>-->
-<!--<td class="center">{{ index + 1 }}</td>-->
-<!--<td class="center">{{ dado.Anexado }}</td>-->
-<!--<td class="center">{{ dado.Data }}</td>-->
-<!--<td class="center">{{ dado.Descricao }}</td>-->
-<!--<td class="center">-->
-<!--<a class=""-->
-<!--:href="`/consultardadosprojeto/abrir-documentos-anexados?id=${dado.idArquivo}&tipo=${dado.AgenteDoc}&idPronac=${dadosProjeto.idPronac}`"-->
-<!--target="_blank"-->
-<!--data-position="top" data-delay="50" data-tooltip="Visualizar">-->
-<!--{{ dado.NoArquivo }}-->
-<!--</a>-->
-<!--</td>-->
-<!--</tr>-->
-<!--</tbody>-->
-<!--</table>-->
-<!--<div v-else>-->
-<!--<fieldset>-->
-<!--<legend>Certid&otilde;es Negativas</legend>-->
-<!--<div class="center">-->
-<!--<em>Dados n&atilde;o informado.</em>-->
-<!--</div>-->
-<!--</fieldset>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>-->
-<!--</template>-->
 <template>
     <div>
         <div v-if="loading">
             <Carregando :text="'Documentos Anexados'"></Carregando>
         </div>
         <div v-else-if="documentosAnexados.documentos">
+            <IdentificacaoProjeto :pronac="dadosProjeto.Pronac"
+                                  :nomeProjeto="dadosProjeto.NomeProjeto">
+            </IdentificacaoProjeto>
             <v-data-table
                     :headers="headers"
-                    :items="documentosAnexados.documentos"
+                    :items="indexItems"
+                    item-key="id"
                     :search="search"
                     :pagination.sync="pagination"
                     class="elevation-1"
+                    rows-per-page-text="Itens por Página"
             >
                 <template slot="items" slot-scope="props">
-                    <td>{{ props.item.NoArquivo }}</td>
+                    <td>{{ props.item.id + 1 }}</td>
                     <td class="text-xs">{{ props.item.Anexado }}</td>
                     <td class="text-xs">{{ props.item.Data }}</td>
                     <td class="text-xs">{{ props.item.Descricao }}</td>
-                    <!--<v-btn fab dark small color="blue" style="">-->
-                        <!--<v-icon dark>cloud_download</v-icon>-->
-                    <!--</v-btn>-->
-
                     <v-tooltip left>
                         <v-btn
                             slot="activator"
@@ -78,54 +31,20 @@
                             <v-icon dark>cloud_download</v-icon>
                         </v-btn>
                         <span>{{ props.item.NoArquivo }}</span>
-                    </v-tooltip>                <!--<td class="text-xs">{{ props.item.NoArquivo }}</td>-->
+                    </v-tooltip>
+                </template>
+                <template slot="pageText" slot-scope="props">
+                    Itens {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
                 </template>
             </v-data-table>
         </div>
     </div>
 </template>
 <script>
-    import {mapGetters, mapActions} from 'vuex';
+    import { mapGetters, mapActions } from 'vuex';
     import Carregando from '@/components/Carregando';
     import IdentificacaoProjeto from './IdentificacaoProjeto';
 
-    // export default {
-    //     name: 'DocumentosAnexados',
-    //     props: ['idPronac'],
-    //     data() {
-    //         return {
-    //             loading: true,
-    //         };
-    //     },
-    //     components: {
-    //         Carregando,
-    //         IdentificacaoProjeto,
-    //     },
-    //     mounted() {
-    //         if (typeof this.dadosProjeto.idPronac !== 'undefined') {
-    //             this.buscarDocumentosAnexados(this.dadosProjeto.idPronac);
-    //         }
-    //     },
-    //     watch: {
-    //         dados(value) {
-    //             this.informacoes = value.informacoes;
-    //         },
-    //         documentosAnexados() {
-    //             this.loading = false;
-    //         },
-    //     },
-    //     computed: {
-    //         ...mapGetters({
-    //             dadosProjeto: 'projeto/projeto',
-    //             documentosAnexados: 'projeto/documentosAnexados',
-    //         }),
-    //     },
-    //     methods: {
-    //         ...mapActions({
-    //             buscarDocumentosAnexados: 'projeto/buscarDocumentosAnexados',
-    //         }),
-    //     },
-    // };
     export default {
         name: 'DocumentosAnexados',
         props: ['idPronac'],
@@ -139,14 +58,14 @@
                 pagination: {
                     sortBy: 'fat',
                 },
+                indexDocumentosAnexados: 0,
                 selected: [],
                 loading: true,
                 headers: [
                     {
                         text: 'N°',
                         align: 'left',
-                        sortable: false,
-                        value: 'numero',
+                        value: 'id',
                     },
                     {
                         text: 'CLASSIFICAÇÃO',
@@ -184,12 +103,23 @@
             ...mapActions({
                 buscarDocumentosAnexados: 'projeto/buscarDocumentosAnexados',
             }),
+            iterador(index) {
+                this.indexDocumentosAnexados = index + 1;
+                return this.indexDocumentosAnexados;
+            },
         },
         computed: {
             ...mapGetters({
                 dadosProjeto: 'projeto/projeto',
                 documentosAnexados: 'projeto/documentosAnexados',
             }),
+            indexItems() {
+                const currentItems = this.documentosAnexados.documentos;
+                return currentItems.map((item, index) => ({
+                    id: index,
+                    ...item,
+                }));
+            },
         },
     };
 </script>
