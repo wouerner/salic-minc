226143abe215556e953fe00f0b060f2283638dbf
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-31 15:41:24 -0300

    [ ADD ] Formata cpf e cnpj com filter
    Co-authored-by: Guigui <amado.silva133@gmail.com>
    Co-authored-by: Ruan <ruannawe95@gmail.com>

--- front/src/filters/cnpj.js
+++ front/src/filters/cnpj.js
@@ -1,6 +1,9 @@
 export default (value) => {
-    if (value.length > 11) {
-        return value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+    const currentValue = value.trim();
+
+    if (currentValue.length > 11) {
+        return currentValue.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
     }
-    return value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+
+    return currentValue.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 };
--- front/src/modules/projeto/visualizar/components/outrasInformacoes/ProvidenciaTomada.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/ProvidenciaTomada.vue
@@ -1,41 +1,4 @@
 <template>
-    <!--<div id="conteudo">-->
-    <!--<div v-if="loading">-->
-    <!--<Carregando :text="'Carregando Providencia Tomada'"></Carregando>-->
-    <!--</div>-->
-    <!--<div v-else-if="dados">-->
-    <!--<div v-if="dados.providenciaTomada">-->
-    <!--<table class="tabela">-->
-    <!--<thead>-->
-    <!--<tr class="destacar">-->
-    <!--<th class="center">DT. SITUA&Ccedil;&Atilde;O</th>-->
-    <!--<th class="center">SITUA&Ccedil;&Atilde;O</th>-->
-    <!--<th class="center">PROVID&Ecirc;NCIA TOMADA</th>-->
-    <!--<th class="center">CPF</th>-->
-    <!--<th class="center">NOME</th>-->
-    <!--</tr>-->
-    <!--</thead>-->
-    <!--<tbody v-for="(dado, index) in dados.providenciaTomada" :key="index">-->
-    <!--<tr>-->
-    <!--<td class="center">{{ dado.DtSituacao }}</td>-->
-    <!--<td class="center">{{ dado.Situacao }}</td>-->
-    <!--<td class="center">{{ dado.ProvidenciaTomada }}</td>-->
-    <!--<td class="center">{{ dado.cnpjcpf }}</td>-->
-    <!--<td class="center">{{ dado.usuario }}</td>-->
-    <!--</tr>-->
-    <!--</tbody>-->
-    <!--</table>-->
-    <!--</div>-->
-    <!--<div v-else>-->
-    <!--<fieldset>-->
-    <!--<legend>Provid&ecirc;ncia Tomada</legend>-->
-    <!--<div class="center">-->
-    <!--<em>Dados n&atilde;o informado.</em>-->
-    <!--</div>-->
-    <!--</fieldset>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
     <div>
        <v-data-table
                 :headers="headers"
@@ -43,12 +6,11 @@
                 class="elevation-1"
         >
             <template slot="items" slot-scope="props">
-                <td>{{ props.item.DtSituacao }}</td>
-                <td>{{ props.item.Situacao }}</td>
-                <td style="width: 400px">{{ props.item.ProvidenciaTomada }}</td>
-                <td>{{ props.item.cnpjcpf }}</td>
+                <td style="width: 190px">{{ props.item.DtSituacao }}</td>
+                <td style="width: 50px">{{ props.item.Situacao }}</td>
+                <td style="width: 700px">{{ props.item.ProvidenciaTomada }}</td>
+                <td style="width: 190px">{{ props.item.cnpjcpf | cnpjFilter }}</td>
                 <td>{{ props.item.usuario }}</td>
-                <!--<td class="text-xs-right">{{ props.item.iron }}</td>-->
             </template>
             <template slot="pageText" slot-scope="props">
                 Lignes {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
@@ -57,44 +19,13 @@
     </div>
 
 </template>
+
 <script>
     import { mapActions, mapGetters } from 'vuex';
     import Carregando from '@/components/Carregando';
+    import cnpjFilter from '@/filters/cnpj';
     import IdentificacaoProjeto from './IdentificacaoProjeto';
 
-    // export default {
-    //     name: 'ProvidenciaTomada',
-    //     components: {
-    //         Carregando,
-    //         IdentificacaoProjeto,
-    //     },
-    //     data() {
-    //         return {
-    //             loading: true,
-    //         };
-    //     },
-    //     mounted() {
-    //         if (typeof this.dadosProjeto.idPronac !== 'undefined') {
-    //             this.buscarProvidenciaTomada(this.dadosProjeto.idPronac);
-    //         }
-    //     },
-    //     watch: {
-    //         dados() {
-    //             this.loading = false;
-    //         }
-    //     },
-    //     computed: {
-    //         ...mapGetters({
-    //             dadosProjeto: 'projeto/projeto',
-    //             dados: 'projeto/providenciaTomada',
-    //         }),
-    //     },
-    //     methods: {
-    //         ...mapActions({
-    //             buscarProvidenciaTomada: 'projeto/buscarProvidenciaTomada',
-    //         }),
-    //     },
-    // };
     export default {
         components: {
             Carregando,
@@ -166,6 +97,8 @@
                 buscarProvidenciaTomada: 'projeto/buscarProvidenciaTomada',
             }),
         },
-    }
+        filters: {
+            cnpjFilter,
+        },
+    };
 </script>
-
