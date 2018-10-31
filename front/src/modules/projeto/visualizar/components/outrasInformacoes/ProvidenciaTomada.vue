13c48a6c9a188d4b08f7c72d3b2b6e27528e234b
Paulophmp <paulo.mendes00@hotmail.com>
2018-10-31 16:51:03 -0300

    [ ADD ] Ajustando o cabeçalho com vuetify
    Co-authored-by: Guigui <amado.silva133@gmail.com>

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/ProvidenciaTomada.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/ProvidenciaTomada.vue
@@ -1,22 +1,30 @@
 <template>
     <div>
-       <v-data-table
-                :headers="headers"
-                :items="dados.providenciaTomada"
-                class="elevation-1"
-                rows-per-page-text="Itens por Página"
-       >
-            <template slot="items" slot-scope="props">
-                <td style="width: 190px">{{ props.item.DtSituacao }}</td>
-                <td style="width: 50px">{{ props.item.Situacao }}</td>
-                <td style="width: 700px">{{ props.item.ProvidenciaTomada }}</td>
-                <td style="width: 190px">{{ props.item.cnpjcpf | cnpjFilter }}</td>
-                <td>{{ props.item.usuario }}</td>
-            </template>
-            <template slot="pageText" slot-scope="props">
-                Itens {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
-            </template>
-        </v-data-table>
+        <div v-if="loading">
+            <Carregando :text="'Carregando Certidoes Negativas'"></Carregando>
+        </div>
+        <div v-else-if="dados.providenciaTomada">
+            <IdentificacaoProjeto :pronac="dadosProjeto.Pronac"
+                                  :nomeProjeto="dadosProjeto.NomeProjeto">
+            </IdentificacaoProjeto>
+           <v-data-table
+                    :headers="headers"
+                    :items="dados.providenciaTomada"
+                    class="elevation-1 container-fluid"
+                    rows-per-page-text="Itens por Página"
+           >
+                <template slot="items" slot-scope="props">
+                    <td style="width: 190px">{{ props.item.DtSituacao }}</td>
+                    <td style="width: 50px">{{ props.item.Situacao }}</td>
+                    <td style="width: 700px">{{ props.item.ProvidenciaTomada }}</td>
+                    <td style="width: 190px">{{ props.item.cnpjcpf | cnpjFilter }}</td>
+                    <td>{{ props.item.usuario }}</td>
+                </template>
+                <template slot="pageText" slot-scope="props">
+                    Itens {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
+                </template>
+            </v-data-table>
+        </div>
     </div>
 
 </template>
