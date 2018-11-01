e79c5dbcaebcce1297a800dac118aba4bba2adab
guiamado <amado.silva133@gmail.com>
2018-11-01 14:18:53 -0200

    [ADD] Historico Encaminhamento: Adiciona Estrutura Vuetify #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="conteudo">
+    <!-- <div id="conteudo">
         <div v-if="loading">
             <Carregando :text="'Carregando Historico de Encaminhamentos'"></Carregando>
         </div>
@@ -39,6 +39,34 @@
                 </fieldset>
             </div>
         </div>
+    </div> -->
+    <div>
+        <div v-if="loading">
+            <Carregando :text="'Carregando Certidoes Negativas'"></Carregando>
+        </div>
+        <div v-else-if="dados.Encaminhamentos">
+            <IdentificacaoProjeto :pronac="dadosProjeto.Pronac"
+                                  :nomeProjeto="dadosProjeto.NomeProjeto">
+            </IdentificacaoProjeto>
+           <v-data-table
+                    :headers="headers"
+                    :items="dados.Encaminhamentos"
+                    class="elevation-1 container-fluid"
+                    rows-per-page-text="Itens por Página"
+           >
+                <template slot="items" slot-scope="props">
+                    <td style="width: 190px">{{ props.item.Produto }}</td>
+                    <td style="width: 50px">{{ props.item.Unidade }}</td>
+                    <td style="width: 700px" v-html="props.item.Observacao"></td>
+                    <td style="width: 190px">{{ props.item.DtEnvio }}</td>
+                    <td style="width: 190px">{{ props.item.DtRetorno }}</td>
+                    <td>{{ props.item.qtDias }}</td>
+                </template>
+                <template slot="pageText" slot-scope="props">
+                    Itens {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
+                </template>
+            </v-data-table>
+        </div>
     </div>
 </template>
 <script>
@@ -50,7 +78,39 @@
         name: 'HistoricoEncaminhamento',
         data() {
             return {
+                search: '',
+                pagination: {
+                    sortBy: 'fat',
+                },
+                selected: [],
                 loading: true,
+                headers: [
+                    {
+                        text: 'PRODUTO',
+                        align: 'left',
+                        value: 'Produto',
+                    },
+                    {
+                        text: 'UNIDADE',
+                        value: 'Unidade',
+                    },
+                    {
+                        text: 'OBSERVAÇÃO',
+                        value: 'Observacao',
+                    },
+                    {
+                        text: 'DT. ENVIO',
+                        value: 'DtEnvio',
+                    },
+                    {
+                        text: 'DT. RETORNO',
+                        value: 'DtRetorno',
+                    },
+                    {
+                        text: 'QT. DIAS',
+                        value: 'qtDias',
+                    },
+                ],
             };
         },
         components: {
