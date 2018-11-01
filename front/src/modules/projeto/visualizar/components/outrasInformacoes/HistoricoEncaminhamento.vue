bb2ccc15b46547ee2970e4497a904751e4280b25
guiamado <amado.silva133@gmail.com>
2018-11-01 14:21:22 -0200

    [REFACTOR] Historico Encaminhamento: Retira comentarios descenessarios #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/HistoricoEncaminhamento.vue
@@ -1,45 +1,4 @@
 <template>
-    <!-- <div id="conteudo">
-        <div v-if="loading">
-            <Carregando :text="'Carregando Historico de Encaminhamentos'"></Carregando>
-        </div>
-        <div v-else-if="dados">
-            <IdentificacaoProjeto
-                    :pronac="dadosProjeto.Pronac"
-                    :nomeProjeto="dadosProjeto.NomeProjeto">
-            </IdentificacaoProjeto>
-            <table class="tabela" v-if="dados.Encaminhamentos.length > 0">
-                <thead>
-                <tr class="destacar">
-                    <th class="center">PRODUTO</th>
-                    <th class="center">UNIDADE</th>
-                    <th class="center">OBSERVA&Ccedil;&Atilde;O</th>
-                    <th class="center">DT. ENVIO</th>
-                    <th class="center">DT. RETORNO</th>
-                    <th class="center">QT. DIAS</th>
-                </tr>
-                </thead>
-                <tbody v-for="(dado, index) in dados.Encaminhamentos" :key="index">
-                <tr>
-                    <td class="center">{{ dado.Produto }}</td>
-                    <td class="center">{{ dado.Unidade }}</td>
-                    <td class="center" v-html="dado.Observacao">{{ dado.Observacao }}</td>
-                    <td class="center">{{ dado.DtEnvio }}</td>
-                    <td class="center">{{ dado.DtRetorno }}</td>
-                    <td class="center">{{ dado.qtDias }}</td>
-                </tr>
-                </tbody>
-            </table>
-            <div v-else>
-                <fieldset>
-                    <legend>Hist&oacute;rico Encaminhamento</legend>
-                    <div class="center">
-                        <em>Dados n&atilde;o informado.</em>
-                    </div>
-                </fieldset>
-            </div>
-        </div>
-    </div> -->
     <div>
         <div v-if="loading">
             <Carregando :text="'Carregando Certidoes Negativas'"></Carregando>
