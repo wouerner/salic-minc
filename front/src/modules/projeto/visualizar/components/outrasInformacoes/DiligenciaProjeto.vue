4300e21033f9b6033cc1f06dfed5fde4bad680ba
Paulo Henrique <paulo.mendes00@hotmail.com>
2018-11-01 12:00:14 -0300

    [UPDATE] Diligências do Projeto: Migrando para a estrutura Vuetify. #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/DiligenciaProjeto.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/DiligenciaProjeto.vue
@@ -1,49 +1,101 @@
 <template>
-    <div id="conteudo">
-        <div v-if="loading">
-            <Carregando :text="'Carregando Diligencias do Projeto'"></Carregando>
-        </div>
-        <div v-else>
-            <IdentificacaoProjeto
-                    :pronac="dadosProjeto.Pronac"
-                    :nomeProjeto="dadosProjeto.NomeProjeto"
-            >
-            </IdentificacaoProjeto>
-            <div v-if="dados.diligenciaProposta">
-                <fieldset style="margin: 0px;">
-                    <legend>Dilig&ecirc;ncia Proposta</legend>
-                    <VisualizarDiligenciaProposta
-                            :idPronac="idPronac"
-                            :diligencias="dados.diligenciaProposta"
-                    >
-                    </VisualizarDiligenciaProposta>
-                </fieldset>
-            </div>
-            <div v-if="dados.diligenciaAdequacao">
-                <fieldset style="margin: 0px;">
-                    <legend>Dilig&ecirc;ncias da Adequa&ccedil;&atilde;o do Projeto</legend>
-                    <VisualizarDiligenciaAdequacao
-                            :idPronac="idPronac"
-                            :diligencias="dados.diligenciaAdequacao"
-                    >
-                    </VisualizarDiligenciaAdequacao>
-                </fieldset>
-            </div>
-            <div v-if="dados.diligenciaProjeto">
-                <fieldset style="margin: 0px;">
-                    <legend>Dilig&ecirc;ncia Projeto</legend>
-                    <VisualizarDiligenciaProjeto
-                            :idPronac="idPronac"
-                            :diligencias="dados.diligenciaProjeto"
-                    >
-                    </VisualizarDiligenciaProjeto>
-                </fieldset>
-            </div>
-        </div>
+    <div v-if="loading">
+        <Carregando :text="'Carregando Diligências do Projeto'"></Carregando>
+    </div>
+    <div v-else>
+        <IdentificacaoProjeto
+                :pronac="dadosProjeto.Pronac"
+                :nomeProjeto="dadosProjeto.NomeProjeto"
+        >
+        </IdentificacaoProjeto>
+        <template>
+            <v-expansion-panel popout>
+                <v-expansion-panel-content
+                >
+                    <div slot="header">Diligência Proposta</div>
+                    <v-card>
+                        <v-card-text class="grey lighten-3">
+                            <VisualizarDiligenciaProposta
+                                    :idPronac="idPronac"
+                                    :diligencias="dados.diligenciaProposta"
+                            >
+                            </VisualizarDiligenciaProposta>
+                        </v-card-text>
+                    </v-card>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </template>
+        <template>
+            <v-expansion-panel popout>
+                <v-expansion-panel-content
+                >
+                    <div slot="header">Diligência Adeqação Projeto</div>
+                    <v-card>
+                        <v-card-text class="grey lighten-3">
+                            <VisualizarDiligenciaAdequacao
+                                    :idPronac="idPronac"
+                                    :diligencias="dados.diligenciaAdequacao"
+                            >
+                            </VisualizarDiligenciaAdequacao>
+                        </v-card-text>
+                    </v-card>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </template>
+
+        <template>
+            <v-expansion-panel popout>
+                <v-expansion-panel-content
+                >
+                    <div slot="header">Diligência Projeto</div>
+                    <v-card>
+                        <v-card-text class="grey lighten-3">
+                            <VisualizarDiligenciaProjeto
+                                    :idPronac="idPronac"
+                                    :diligencias="dados.diligenciaProjeto"
+                            >
+                            </VisualizarDiligenciaProjeto>
+                        </v-card-text>
+                    </v-card>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </template>
     </div>
 </template>
+<!--<div v-if="dados.diligenciaProposta">-->
+<!--<fieldset style="margin: 0px;">-->
+<!--<legend>Dilig&ecirc;ncia Proposta</legend>-->
+<!--<VisualizarDiligenciaProposta-->
+<!--:idPronac="idPronac"-->
+<!--:diligencias="dados.diligenciaProposta"-->
+<!--&gt;-->
+<!--</VisualizarDiligenciaProposta>-->
+<!--</fieldset>-->
+<!--</div>-->
+<!--<div v-if="dados.diligenciaAdequacao">-->
+<!--<fieldset style="margin: 0px;">-->
+<!--<legend>Dilig&ecirc;ncias da Adequa&ccedil;&atilde;o do Projeto</legend>-->
+<!--<VisualizarDiligenciaAdequacao-->
+<!--:idPronac="idPronac"-->
+<!--:diligencias="dados.diligenciaAdequacao"-->
+<!--&gt;-->
+<!--</VisualizarDiligenciaAdequacao>-->
+<!--</fieldset>-->
+<!--</div>-->
+<!--<div v-if="dados.diligenciaProjeto">-->
+<!--<fieldset style="margin: 0px;">-->
+<!--<legend>Dilig&ecirc;ncia Projeto</legend>-->
+<!--<VisualizarDiligenciaProjeto-->
+<!--:idPronac="idPronac"-->
+<!--:diligencias="dados.diligenciaProjeto"-->
+<!--&gt;-->
+<!--</VisualizarDiligenciaProjeto>-->
+<!--</fieldset>-->
+<!--</div>-->
+
+
 <script>
-    import { mapGetters, mapActions } from 'vuex';
+    import {mapGetters, mapActions} from 'vuex';
     import Carregando from '@/components/Carregando';
     import IdentificacaoProjeto from './IdentificacaoProjeto';
     import VisualizarDiligenciaProposta from './components/VisualizarDiligenciaProposta';
