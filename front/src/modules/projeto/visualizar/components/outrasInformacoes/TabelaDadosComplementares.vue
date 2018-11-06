4d4a49dffcaa8959dab7ae1ed33974c0e1d29d1f
guiamado <amado.guigui@gmail.com>
2018-11-06 11:59:24 -0200

    [FIX] Corrige bug da string

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
@@ -8,30 +8,30 @@
                 </v-layout>
                 <v-card>
                     <v-card-text>
-                        <div v-if="dsDadoComplementar" v-html="dsDadoComplementar">
+                        <div v-if="dsDadoComplementar && dsDadoComplementar.length > 1" v-html="dsDadoComplementar">
                             <td>{{ dsDadoComplementar }}</td>
                         </div>
-                            <div v-else-if="custosVinculados">
-                                <table class="tabela">
-                                    <thead>
+                        <div v-else-if="custosVinculados">
+                            <table class="tabela">
+                                <thead>
                                     <tr>
                                         <th>ITEM</th>
                                         <th>PERCENTUAL</th>
                                     </tr>
-                                    </thead>
-                                    <tbody v-for="(custosVinculado, index) in custosVinculados" :key="index">
+                                </thead>
+                                <tbody v-for="(custosVinculado, index) in custosVinculados" :key="index">
                                     <tr>
                                         <td>{{ custosVinculado.Descricao }}</td>
                                         <td>{{ custosVinculado.Percentual }}</td>
                                     </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div v-else>
-                                <v-alert :value="true" color="info" icon="warning">
-                                    Nenhum dado encontrado
-                                </v-alert>
-                            </div>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div v-else>
+                            <v-alert :value="true" color="info" icon="warning">
+                                Nenhum dado encontrado
+                            </v-alert>
+                        </div>
                     </v-card-text>
                 </v-card>
             </v-expansion-panel-content>
