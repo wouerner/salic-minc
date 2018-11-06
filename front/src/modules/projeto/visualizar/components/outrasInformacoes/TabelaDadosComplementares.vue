7522d888842435731b85427bc0d329d16e09845c
Paulo Henrique <paulo.mendes00@hotmail.com>
2018-11-06 11:17:00 -0200

    [UPDATE] Dados Complementares do Projeto: Migrando para a estrutura Vuetify. #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
@@ -1,60 +1,48 @@
 <template>
-    <div id="conteudo">
-        <table class="tabela">
-            <tbody>
-            <tr>
-                <td align="left">
-                    <input type="button"
-                           class="btn_adicionar"
-                           id="objetivos"
-                           @click="setActiveTab(1);">
-                    <b v-html="dadoComplementar">{{ dadoComplementar }}</b>
-                </td>
-            </tr>
-            <tr v-if="activeTab === 1" align="left" style="padding: 5px">
-                <div v-if="dsDadoComplementar" v-html="dsDadoComplementar">
-                    <td>{{ dsDadoComplementar }}</td>
-                </div>
-                <div v-else-if="custosVinculados">
-                    <table class="tabela">
-                        <thead>
-                        <tr>
-                            <th>ITEM</th>
-                            <th>PERCENTUAL</th>
-                        </tr>
-                        </thead>
-                        <tbody v-for="(custosVinculado, index) in custosVinculados" :key="index">
-                        <tr>
-                            <td>{{ custosVinculado.Descricao }}</td>
-                            <td>{{ custosVinculado.Percentual }}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div v-else>N&atilde;o se aplica.</div>
-            </tr>
-            </tbody>
-        </table>
-    </div>
+    <v-flex>
+        <v-expansion-panel popout focusable>
+            <v-expansion-panel-content class="elevation-1">
+                <v-layout slot="header" class="green--text">
+                    <v-icon class="mr-3 green--text">subject</v-icon>
+                    <span v-html="dadoComplementar">{{ dadoComplementar }}</span>
+                </v-layout>
+                <v-card>
+                    <v-card-text>
+                        <div v-if="dsDadoComplementar" v-html="dsDadoComplementar">
+                            <td>{{ dsDadoComplementar }}</td>
+                        </div>
+                            <div v-else-if="custosVinculados">
+                                <table class="tabela">
+                                    <thead>
+                                    <tr>
+                                        <th>ITEM</th>
+                                        <th>PERCENTUAL</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody v-for="(custosVinculado, index) in custosVinculados" :key="index">
+                                    <tr>
+                                        <td>{{ custosVinculado.Descricao }}</td>
+                                        <td>{{ custosVinculado.Percentual }}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div v-else>
+                                <v-alert :value="true" color="info" icon="warning">
+                                    Nenhum dado encontrado
+                                </v-alert>
+                            </div>
+                    </v-card-text>
+                </v-card>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-flex>
 </template>
+
 <script>
     export default {
         name: 'TabelaDadosComplementares',
         props: ['dadoComplementar', 'custosVinculados', 'dsDadoComplementar'],
-        data() {
-            return {
-                activeTab: -1,
-            };
-        },
-        methods: {
-            setActiveTab(index) {
-                if (this.activeTab === index) {
-                    this.activeTab = -1;
-                } else {
-                    this.activeTab = index;
-                }
-            },
-        },
     };
 </script>
 
