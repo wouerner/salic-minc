f2f91d54539a43759ef700260529f815bff9828a
leonardo <volthier@gmail.com>
2018-11-06 17:25:11 -0200

    [UPDATE] Identação template teste inicial!

--- front/src/modules/avaliacaoResultados/components/ParecerTecnico/HistoricoDiligencias.vue
+++ front/src/modules/avaliacaoResultados/components/ParecerTecnico/HistoricoDiligencias.vue
@@ -1,32 +1,54 @@
 <template>
-    <v-timeline>
-        <v-timeline-item
-            v-for="(year, i) in years"
-            :color="year.color"
-            :key="i"
-            small
-        >
+
+    <v-layout row justify-center>
+        <v-dialog v-model="dialog" scrollable max-width="800px">
+            <v-btn slot="activator" color="cyan" dark><v-icon>assignment_late</v-icon></v-btn>
+            <v-card>
+                <v-card-title>Histórico Diligências</v-card-title>
+                <v-divider></v-divider>
+                <v-card-text style="height: 300px;">
+                    <v-timeline>
+                        <v-timeline-item
+                            v-for="(year, i) in years"
+                            :color="year.color"
+                            :key="i"
+                            small
+                        >
       <span
           slot="opposite"
           :class="`headline font-weight-bold ${year.color}--text`"
-          v-text="year.year"
-      ></span>
-            <div class="py-3">
-                <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">Lorem ipsum</h2>
-                <div>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-                </div>
-            </div>
-        </v-timeline-item>
-    </v-timeline>
+          v-text="year.year">
+      </span>
+                            <div class="py-3">
+                                <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">Lorem ipsum </h2>
+                                <div>
+                                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+                                </div>
+                            </div>
+                        </v-timeline-item>
+                    </v-timeline>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-layout>
+
 </template>
 
 <script>
 
-export default {
+    import BtnEncaminhar from '../BtnEncaminhar';
+
+    export default {
     name: 'HistoricoDiligencias',
+    components: { BtnEncaminhar },
     data() {
         return {
+            dialog: false,
             years: [
                 {
                     color: 'cyan',
