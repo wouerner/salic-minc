26b162012b4087cf9daea6d2c21df894bb5e600a
Paulophmp <paulo.mendes00@hotmail.com>
2018-11-07 16:33:36 -0200

    [UPDATE] Ajustando tabelas  #1456

--- front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
+++ front/src/modules/projeto/visualizar/components/outrasInformacoes/TabelaDadosComplementares.vue
@@ -1,42 +1,46 @@
 <template>
-    <v-flex>
-        <v-expansion-panel popout focusable>
-            <v-expansion-panel-content class="elevation-1">
-                <v-layout slot="header" class="green--text">
-                    <v-icon class="mr-3 green--text">subject</v-icon>
-                    <span v-html="dadoComplementar">{{ dadoComplementar }}</span>
-                </v-layout>
-                <v-card>
-                    <v-card-text>
-                        <div v-if="dsDadoComplementar && dsDadoComplementar.length > 1" v-html="dsDadoComplementar">
-                            <td>{{ dsDadoComplementar }}</td>
-                        </div>
-                        <div v-else-if="custosVinculados">
-                            <table class="tabela">
-                                <thead>
-                                    <tr>
-                                        <th>ITEM</th>
-                                        <th>PERCENTUAL</th>
-                                    </tr>
-                                </thead>
-                                <tbody v-for="(custosVinculado, index) in custosVinculados" :key="index">
-                                    <tr>
-                                        <td>{{ custosVinculado.Descricao }}</td>
-                                        <td>{{ custosVinculado.Percentual }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div v-else>
-                            <v-alert :value="true" color="info" icon="warning">
-                                Nenhum dado encontrado
-                            </v-alert>
-                        </div>
-                    </v-card-text>
-                </v-card>
-            </v-expansion-panel-content>
-        </v-expansion-panel>
-    </v-flex>
+    <v-expansion-panel popout focusable>
+        <v-expansion-panel-content class="elevation-1">
+            <v-layout slot="header" class="green--text">
+                <v-icon class="mr-3 green--text">subject</v-icon>
+                <span v-html="dadoComplementar">{{ dadoComplementar }}</span>
+            </v-layout>
+            <v-card>
+                <v-card-text>
+                    <div v-if="dsDadoComplementar && dsDadoComplementar.length > 1" v-html="dsDadoComplementar">
+                        <td>{{ dsDadoComplementar }}</td>
+                    </div>
+                    <div v-else-if="custosVinculados">
+                        <v-container grid-list-md>
+                            <v-layout justify-space-around row wrap>
+                                <v-flex xs6>
+                                    <b>ITEM</b>
+                                </v-flex>
+                                <v-flex xs2>
+                                    <b>PERCENTUAL</b>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout justify-space-around align-center row
+                                      v-for="(custosVinculado, index) in custosVinculados" :key="index"
+                            >
+                                <v-flex xs6>
+                                    <td>{{ custosVinculado.Descricao }}</td>
+                                </v-flex>
+                                <v-flex xs2>
+                                    <td>{{ custosVinculado.Percentual }}</td>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </div>
+                    <div v-else>
+                        <v-alert :value="true" color="info" icon="warning">
+                            Nenhum dado encontrado
+                        </v-alert>
+                    </div>
+                </v-card-text>
+            </v-card>
+        </v-expansion-panel-content>
+    </v-expansion-panel>
 </template>
 
 <script>
