5fc69055beca3cc496905492521275b212dd8758
guiamado <amado.guigui@gmail.com>
2018-11-08 17:55:20 -0200

    [FIX] Documentos Anexados/Assinados: Corrige datas #1669

--- application/modules/projeto/service/documentos-anexados/DocumentosAnexados.php
+++ application/modules/projeto/service/documentos-anexados/DocumentosAnexados.php
@@ -66,9 +66,15 @@ class DocumentosAnexados implements \MinC\Servico\IServicoRestZend
                     $item->Anexado = 'Documento do Projeto';
                     break;
             }
+            $data = ' ';
+
+            if (!empty($item->Data)) {
+                $data = new \DateTime($item->Data);
+                $data = $data->format('d/m/Y H:i:s');
+            }
             $itemArray = [
                 'Anexado' => $item->Anexado,
-                'Data' => $item->Data,
+                'Data' => $data,
                 'Descricao' => $item->Descricao,
                 'NoArquivo' => $item->NoArquivo,
                 'idArquivo' => $item->idDocumentosAgentes,
