2db5177dfc7d4aec40579732af356d47bd53c0da
leonardo <volthier@gmail.com>
2018-11-21 17:20:34 -0200

    [FIX] Fix data resposta

--- front/src/modules/avaliacaoResultados/components/components/HistoricoDiligencias.vue
+++ front/src/modules/avaliacaoResultados/components/components/HistoricoDiligencias.vue
@@ -108,16 +108,16 @@ export default {
             };
             const prazoPadrao = 40;
             // diligenciado
-            if (obj.DtSolicitacao && typeof obj.DtResposta === 'undefined' &&
+            if (obj.DtSolicitacao && obj.DtResposta === '' &&
                 prazo <= prazoPadrao && obj.stEnviado === 'S') {
                 status = { color: 'yellow', desc: 'Diligenciado' };
                 return status;
                 // diligencia não respondida
-            } else if (obj.DtSolicitacao && typeof obj.DtResposta === 'undefined' && prazo > prazoPadrao) {
+            } else if (obj.DtSolicitacao && obj.DtResposta === '' && prazo > prazoPadrao) {
                 status = { color: 'red', desc: 'Diligencia não respondida' };
                 return status;
                 // diligencia respondida com ressalvas
-            } else if (obj.DtSolicitacao && typeof obj.DtResposta !== 'undefined') {
+            } else if (obj.DtSolicitacao && obj.DtResposta !== '') {
                 if (obj.stEnviado === 'N' && prazo > prazoPadrao) {
                     status = { color: 'red', desc: 'Diligencia não respondida' };
                     return status;
