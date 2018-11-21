6a3e8cb07c141468271d651da7ab7e280e22eff6
Paulophmp <paulo.mendes00@hotmail.com>
2018-11-21 15:41:18 -0200

    [ UPDATE ]

--- front/src/modules/projeto/visualizar/components/execucao/DadosReadequacoes.vue
+++ front/src/modules/projeto/visualizar/components/execucao/DadosReadequacoes.vue
@@ -10,8 +10,8 @@
                         v-for="(dado, index) in dados.dadosReadequacoes"
                         :key="index"
                 >
-                    <v-layout slot="header" class="green--text">
-                        <v-icon class="mr-3 green--text">subject</v-icon>
+                    <v-layout slot="header" class="primary--text">
+                        <v-icon class="mr-3 primary--text">subject</v-icon>
                         <span>{{ dado.dsReadequacao }}</span>
                     </v-layout>
                     <v-container fluid>
