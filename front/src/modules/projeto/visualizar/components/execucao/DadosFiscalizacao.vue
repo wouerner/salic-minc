460baeb785046cdf6c3bb1c95590744a09b1a97d
guiamado <amado.guigui@gmail.com>
2018-11-21 10:54:55 -0200

    [ADD] Dados Fiscalizacao: Adiciona vuetify data table

--- front/src/modules/projeto/visualizar/components/execucao/DadosFiscalizacao.vue
+++ front/src/modules/projeto/visualizar/components/execucao/DadosFiscalizacao.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
-        <v-card>
-            <v-card-title>
-                <h6>Dados Fiscalizacao</h6>
-            </v-card-title>
-        </v-card>
+        <v-data-table
+            :headers="headers"
+            :items="dados"
+            class="elevation-1 container-fluid"
+            rows-per-page-text="Items por Página"
+            hide-actions
+        >
+            <template slot="items" slot-scope="props">
+                <td class="text-xs-center">
+                    <v-btn flat icon>
+                        <v-tooltip bottom>
+                            <v-icon
+                                    slot="activator"
+                                    @click="showItem(props.item)"
+                                    class="material-icons"
+                                    color="green"
+                                    dark>add
+                            </v-icon>
+                            <span>Visualizar Dados Fiscalizacao</span>
+                        </v-tooltip>
+                    </v-btn>
+                </td>
+                <td class="text-xs-center" v-html="props.item.dtInicio"></td>
+                <td class="text-xs-center">{{ props.item.dtFim }}</td>
+                <td class="text-xs-center">{{ props.item.cpfTecnico }}</td>
+                <td class="text-xs-center">{{ props.item.nmTecnico }}</td>
+            </template>
+        </v-data-table>
     </div>
 </template>
 <script>
@@ -15,43 +38,58 @@
         name: 'DadosFiscalizacao',
         data() {
             return {
+                loading: true,
+                headers: [
+                    {
+                        text: 'VISUALIZAR',
+                        align: 'center',
+                        sortable: false,
+                        value: 'dados',
+                    },
+                    {
+                        text: 'DT. INICIO',
+                        align: 'center',
+                        value: 'dtInicio',
+                    },
+                    {
+                        text: 'DT. FIM',
+                        align: 'center',
+                        value: 'dtFim',
+                    },
+                    {
+                        text: 'CPF TECNICO',
+                        align: 'center',
+                        value: 'cpfTecnico',
+                    },
+                    {
+                        text: 'NOME TECNICO',
+                        align: 'center',
+                        value: 'nmTecnico',
+                    },
+                ],
             };
         },
-        // filters: {
-        //     formatarData(date) {
-        //         if (date.length === 0) {
-        //             return '-';
-        //         }
-        //         return moment(date).format('DD/MM/YYYY');
-        //     },
-        // },
-        // components: {
-        //     Carregando,
-        // },
-        // mounted() {
-        //     if (typeof this.dadosProjeto.idPronac !== 'undefined') {
-        //         this.buscarMarcasAnexadas(this.dadosProjeto.idPronac);
-        //     }
-        // },
-        // watch: {
-        //     loadingButton() {
-        //         setTimeout(() => (this.loadingButton = -1), 2000);
-        //     },
-        //     dados() {
-        //         this.loading = false;
-        //     },
-        // },
-        // computed: {
-        //     ...mapGetters({
-        //         dadosProjeto: 'projeto/projeto',
-        //         dados: 'projeto/marcasAnexadas',
-        //     }),
-        // },
-        // methods: {
-        //     ...mapActions({
-        //         buscarMarcasAnexadas: 'projeto/buscarMarcasAnexadas',
-        //     }),
-        // },
+        mounted() {
+            if (typeof this.dadosProjeto.idPronac !== 'undefined') {
+                this.buscarDadosFiscalizacaoLista(this.dadosProjeto.idPronac);
+            }
+        },
+        watch: {
+            dados() {
+                this.loading = false;
+            },
+        },
+        computed: {
+            ...mapGetters({
+                dadosProjeto: 'projeto/projeto',
+                dados: 'projeto/dadosFiscalizacaoLista',
+            }),
+        },
+        methods: {
+            ...mapActions({
+                buscarDadosFiscalizacaoLista: 'projeto/buscarDadosFiscalizacaoLista',
+            }),
+        },
     };
 </script>
 
