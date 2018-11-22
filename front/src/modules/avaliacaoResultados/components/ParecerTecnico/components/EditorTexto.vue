b2d3e6f0fde06e796c7b1d38eb14754436ebbde8
luizfpinheiro <luizfelipenet@hotmail.com>
2018-11-22 18:10:01 -0200

    [ADD] Componente editor separado para reuso - Ideia inicial

---
+++ front/src/modules/avaliacaoResultados/components/ParecerTecnico/components/EditorTexto.vue
@@ -0,0 +1,35 @@
+<template>    
+    <div id="app">
+        <vue-editor
+            :editorToolbar="customToolbar"
+            :placeholder="'Texto do Parecer *'"
+        >
+        </vue-editor>
+    </div>
+</template>
+
+<script>
+    import { VueEditor } from "vue2-editor";
+    
+    export default {
+        components: {
+            VueEditor
+        },
+        data() {
+            return {
+                customToolbar:  [
+                    [{ 'font': [] }],
+                    [{ 'header': [false, 1, 2, 3, 4, 5, 6, ] }],
+                    [{ 'size': ['small', false, 'large', 'huge'] }],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    [{'align': ''}, {'align': 'center'}, {'align': 'right'}, {'align': 'justify'}],
+                    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                    [{ 'indent': '-1'}, { 'indent': '+1' }],
+                    [{ 'color': [] }],
+                ]
+            }
+        },
+
+    };
+
+</script>
