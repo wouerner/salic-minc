caec3090c3e3da6d18396ea30844d77e6183059f
Paulophmp <paulo.mendes00@hotmail.com>
2018-11-26 11:51:17 -0200

    [ ADD ] Adicinando teste com Cypress no Sub-Menu: Pedido de Porrogação.
    Co-authored-by: Wouerner Brandão <wouerner@protonmail.com>

--- cypress/integration/dados-projeto-execucao/tests-execucao.spec.js
+++ cypress/integration/dados-projeto-execucao/tests-execucao.spec.js
@@ -1,15 +1,4 @@
 describe('Testes dos Sub-menu Execucao', function () {
-    // before(function () { // Inicia todos os testes na Tela de Análise de Resultados - Perfil Técnico
-    //     cy.get('#combousuario div input').click()
-    //         .get('ul li span').eq(59).click();
-    //
-    //     cy.get('ul li a.dropdown-button').contains('Avaliação de Resultados').click()
-    //         .get('#prestacao-contas li a').contains('Analisar Parecer (Nova)').click();
-    //
-    //     cy.wait(4000);
-    //
-    // });
-
     it('Verifica se há pelo menos um item no sub-menu - "pedido-prorrogacao" ', function () {
         cy.get('#pronac').type('171313{enter}');
         cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-header > span').click();
@@ -17,39 +6,4 @@ describe('Testes dos Sub-menu Execucao', function () {
 
         cy.get('.m9 > h1').should('not.be.empty').contains('Pedido de Prorrogação');
     });
-
-    // it('Renderização da tabela de análise de resultados - Perfil Técnico', function () {
-    //     cy.get('div.v-card .theme--light');
-    // });
-    //
-    // it('Verifica se há pelo menos um item na tabela - Aba "Assinar" ', function () {
-    //     cy.get('div.v-tabs__div a').contains('Assinar').click();
-    //     cy.get('tbody > :nth-child(1) .text-xs-right').should('not.be.empty');
-    // });"
-    //
-    // it('Verifica se há pelo menos um item na tabela - Aba "Histórico" ', function () {
-    //     cy.get('div.v-tabs__div a').contains('Historico').click();
-    //     cy.get('tbody > :nth-child(1) .text-xs-right').should('not.be.empty');
-    // });
-    //
-    // it('Click para abrir modal Histórico de Encaminhamentos ', function () {
-    //     let a = cy.get(':nth-child(4) > .v-card > .v-card__text > :nth-child(1) > :nth-child(2) > .v-table__overflow > .v-datatable > tbody > :nth-child(1) > :nth-child(6) > .v-dialog__container > .v-dialog__activator > .v-tooltip > span > .v-btn > .v-btn__content > .v-icon')
-    //     a.click();
-    // });
-    //
-    // it('Click para fechar modal Histórico de Encaminhamentos ', function () {
-    //     cy.get('.v-dialog__content--active > .v-dialog > .v-card > .v-card__actions > .v-btn > .v-btn__content').click();
-    // });
-    //
-    // it('Click para Analisar/Visualizar Projeto', function () {
-    //     let a = cy.get(':nth-child(4) > .v-card > .v-card__text > :nth-child(1) > :nth-child(2) > .v-table__overflow > .v-datatable > tbody > :nth-child(1) > :nth-child(6) > .v-btn--router > .v-btn__content > .v-tooltip > span > .v-icon')
-    //     a.click();
-    // });
-
-    // it('Click para voltar da Analisar projeto -> Tabela de Análise Resultados', function() {
-    //   let a = cy.get('i.v-icon .material-icons .theme--light').contains('arrow_back');
-    //   a.click();
-    // });
-
-
 });
