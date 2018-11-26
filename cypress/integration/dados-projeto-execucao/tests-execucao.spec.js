8155e5b6e80d7eb786b0ef75e4378f27bf2085d6
Paulophmp <paulo.mendes00@hotmail.com>
2018-11-26 16:22:21 -0200

    [ ADD ] Adicinando teste com Cypress no Sub-Menu: Pedido de Porrogação, Marcas Anexadas e Dados das Readequações.
    Co-authored-by: Wouerner Brandão <wouerner@protonmail.com>

--- cypress/integration/dados-projeto-execucao/tests-execucao.spec.js
+++ cypress/integration/dados-projeto-execucao/tests-execucao.spec.js
@@ -1,9 +1,23 @@
 describe('Testes dos Sub-menu Execucao', function () {
-    it('Verifica se há pelo menos um item no sub-menu - "pedido-prorrogacao" ', function () {
+    it('Verifica se há pelo menos um item no sub-menu - "Marcas Anexadas" ', function () {
         cy.get('#pronac').type('171313{enter}');
         cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-header > span').click();
-        cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-body > ul > :nth-child(5) > .waves-effect').click();
 
+        //Marcas Anexadas
+        cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-body > ul > :nth-child(4) > .waves-effect').click();
+        cy.get('.m9 > h1').should('not.be.empty').contains('Marcas Anexadas');
+
+        cy.wait(3500);
+
+        //Pedido de Prorrogação
+        cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-body > ul > :nth-child(5) > .waves-effect').click();
         cy.get('.m9 > h1').should('not.be.empty').contains('Pedido de Prorrogação');
+
+        cy.wait(3500);
+
+        //Dados das Readequações
+        cy.get(':nth-child(6) > .collapsible > .bold > .collapsible-body > ul > :nth-child(3) > .waves-effect').click(); //
+        cy.get('.m9 > h1').should('not.be.empty').contains('Dados das Readequações');
+
     });
 });
