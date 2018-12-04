4d0cb42a7e90617384efa0b5d2db6953ff195332
Cleber Santos <oclebersantos@gmail.com>
2018-12-04 18:51:52 -0200

    [update] fiscalizacao: altera fiscalizacao ao devolver documento #1737

--- application/modules/fiscalizacao/service/assinatura/acao/Devolver.php
+++ application/modules/fiscalizacao/service/assinatura/acao/Devolver.php
@@ -8,5 +8,12 @@ class Devolver implements IAcaoDevolver
 {
     public function executar(\MinC\Assinatura\Model\Assinatura $assinatura)
     {
+        $modeloTbDocumentoAssinatura = $assinatura->modeloTbDocumentoAssinatura;
+
+        $FiscalizacaoDAO = new \Fiscalizacao_Model_DbTable_TbFiscalizacao();
+        $FiscalizacaoDAO->alteraSituacaoProjeto(
+            \Fiscalizacao_Model_TbFiscalizacao::ST_FISCALIZACAO_OFICIALIZADA
+            , $modeloTbDocumentoAssinatura->getIdAtoDeGestao()
+        );
     }
 }
