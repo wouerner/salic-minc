58bc70d4ef37c12ff8f0e09155ae05010a4143dc
guiamado <amado.silva133@gmail.com>
2018-12-20 15:35:42 -0200

    [ADD] Extratos bancarios: adiciona campo de data final e tratações da escolha da data

--- front/src/modules/projeto/visualizar/components/dadosBancarios/ExtratosBancarios.vue
+++ front/src/modules/projeto/visualizar/components/dadosBancarios/ExtratosBancarios.vue
@@ -5,6 +5,24 @@
         </div>
         <div v-else>
             <v-card>
+                <span v-if="validaCampo().desc !== ''" color="alert">
+                    <div v-if="validaCampo().id === 1">
+                        <v-alert
+                            :value="true"
+                            type="warning"
+                            >
+                            {{ validaCampo().desc }}
+                        </v-alert>
+                    </div>
+                    <div v-if="validaCampo().id === 3">
+                        <v-alert
+                            :value="true"
+                            type="success"
+                            >
+                            {{ validaCampo().desc }}
+                        </v-alert>
+                    </div>
+                </span>
                 <template>
                     <v-layout row wrap>
                         <v-flex xs12 sm6 md4>
@@ -23,7 +41,7 @@
                                 <v-text-field
                                 slot="activator"
                                 v-model="datestring"
-                                label="Escolha a data inicial"
+                                label="Escolha a data inicial da Pesquisa"
                                 prepend-icon="event"
                                 readonly
                                 ></v-text-field>
@@ -36,10 +54,47 @@
                                 >
                                     <v-spacer></v-spacer>
                                     <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-                                    <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                                    <v-btn flat color="primary" @click="validaCampo();$refs.menu.save(date)">OK</v-btn>
                                 </v-date-picker>
                             </v-menu>
                         </v-flex>
+
+                        <v-flex xs12 sm6 md4>
+                            <v-menu
+                                ref="menuFim"
+                                :close-on-content-click="false"
+                                v-model="menuFim"
+                                :nudge-right="40"
+                                :return-value.sync="dateFim"
+                                lazy
+                                transition="scale-transition"
+                                offset-y
+                                full-width
+                                min-width="290px"
+                            >
+                                <v-text-field
+                                slot="activator"
+                                v-model="datestringFim"
+                                label="Escolha a data Final da Pesquisa"
+                                prepend-icon="event"
+                                readonly
+                                ></v-text-field>
+                                <v-date-picker
+                                    class="calendario-vuetify"
+                                    :max="new Date().toISOString().substr(0, 10)"
+                                    v-model="dateFim"
+                                    no-title
+                                    scrollable
+                                >
+                                    <v-spacer></v-spacer>
+                                    <v-btn flat color="primary" @click="menuFim = false">Cancel</v-btn>
+                                    <v-btn flat color="primary" @click="validaCampo();$refs.menuFim.save(dateFim)">OK</v-btn>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-flex>
+                        <div class="pt-4 pl-4">
+                            <v-btn color="teal" class="white--text" :change="validaCampo().validacao" :disabled="!validaCampo().validacao">Pesquisar</v-btn>
+                        </div>
                     </v-layout>
                 </template>
                 <v-card-title>
@@ -111,6 +166,9 @@
                 date: new Date().toISOString().substr(0, 10),
                 menu: false,
                 datestring:'',
+                dateFim: new Date().toISOString().substr(0, 10),
+                menuFim: false,
+                datestringFim:'',
                 pagination: {
                     sortBy: 'fat',
                 },
@@ -169,6 +227,7 @@
                 this.buscarExtratosBancarios(this.dadosProjeto.idPronac);
                 this.loading = false;
             }
+            // this.validaCampo();
         },
         filters: {
             FormatarData(date) {
@@ -188,7 +247,11 @@
             date(val) {
                 this.datestring = this.formatDate(val);
                 console.log(val);
-            }
+            },
+            dateFim(val) {
+                this.datestringFim = this.formatDate(val);
+                console.log(val);
+            },
         },
         methods: {
             ...mapActions({
@@ -199,6 +262,50 @@
                     return str.substring(8, 10)+'/'+str.substring(5, 7)+'/'+str.substring(0, 4);
                 }
                 return '';
+            },
+            validaCampo() {
+                // this.menu = true;
+                let status = {
+                    desc: '',
+                    validacao: false,
+                    id: 0
+                };
+                if( this.datestring === '' && this.datestringFim === '') {
+                    console.log('ahahah');
+                    status = { desc: 'hahaaa', validacao: false, id: 2 };
+                    return status;
+                }
+                if( this.datestring === '' && this.datestringFim !== '') {
+                    console.log('primeiro preencher data inicial');
+                    status = { desc: 'preencher data inicial', validacao: false, id: 1 };
+                    return status;
+                }
+                else if(this.datestring !== '' && this.datestringFim === '') {
+                    console.log('segundo agora Escolha a data final');
+                    status = { desc: 'agora Escolha a data final', validacao: false, id: 1 };
+                    return status;
+                }
+                if(this.datestring !== '' &&  this.datestringFim !== '') {
+                    if( Date.parse(this.datestring) > Date.parse(this.datestringFim)){
+                        console.log('data inicial nao pode ser maior que data final');
+                        status = { desc: 'data inicial nao pode ser maior que data final', validacao: false, id: 1 };
+                        return status;
+                    }
+                    else if ( !(Date.parse(this.datestring) < Date.parse(this.datestringFim))) {
+                        console.log('data final nao pode ser menor que data inicial222222');
+                        status = { desc: 'data final nao pode ser menor que data inicial2222', validacao: false, id: 1 };
+                        return status;
+                    }
+                }
+                // if(this.datestring !== '' &&  this.datestringFim !== '' && Date.parse(this.datestring) > !Date.parse(this.datestringFim)) {
+                //     console.log('data inicial nao pode ser maior que data final');
+                //     status = { desc: 'data inicial nao pode ser maior que data final', validacao: false };
+                //     return status;
+                // }
+                console.log('tudo certo:'+Date.parse(this.datestring),'Tudo certo fim:' + Date.parse(this.datestringFim));
+                status = { desc: 'tudo certo', validacao: true, id: 3 };
+                return status;
+
             },
         },
     };
