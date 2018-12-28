65177a3ef5545beaf441575a4866c2de47d50b9d
guiamado <amado.silva133@gmail.com>
2018-12-28 14:55:17 -0200

    [REFACTOR] Extratos Bancarios: Coloca parte de filtros em outro componente

--- front/src/modules/projeto/visualizar/components/dadosBancarios/ExtratosBancarios.vue
+++ front/src/modules/projeto/visualizar/components/dadosBancarios/ExtratosBancarios.vue
@@ -5,106 +5,10 @@
         </div>
         <div v-else>
             <v-card>
-                <span v-if="validaCampo().desc !== ''" color="alert">
-                    <div v-if="validaCampo().id === 1">
-                        <v-alert
-                            :value="true"
-                            type="warning"
-                            class="black--text"
-                            >
-                            {{ validaCampo().desc }}
-                        </v-alert>
-                    </div>
-                </span>
-                <template>
-                    <v-container fluid>
-                        <v-layout row wrap>
-                            <v-flex xs12 sm6 md4>
-                                <v-menu
-                                    ref="menu"
-                                    :close-on-content-click="false"
-                                    v-model="menu"
-                                    :nudge-right="40"
-                                    lazy
-                                    transition="scale-transition"
-                                    offset-y
-                                    full-width
-                                    min-width="290px"
-                                >
-                                    <v-text-field
-                                        slot="activator"
-                                        v-model="datestring"
-                                        label="Escolha a data inicial da Pesquisa"
-                                        prepend-icon="event"
-                                        @blur="date = parseDate(datestring)"
-                                        mask="##/##/####"
-                                        return-masked-value
-                                    ></v-text-field>
-                                    <v-date-picker
-                                        class="calendario-vuetify"
-                                        :max="new Date().toISOString().substr(0, 10)"
-                                        v-model="date"
-                                        no-title
-                                        scrollable
-                                        locale="pt-br"
-                                        @input="menu = false"
-                                    >
-                                    </v-date-picker>
-                                </v-menu>
-                            </v-flex>
-
-                            <v-flex xs12 sm6 md4>
-                                <v-menu
-                                    ref="menuFim"
-                                    :close-on-content-click="false"
-                                    v-model="menuFim"
-                                    :nudge-right="40"
-                                    lazy
-                                    transition="scale-transition"
-                                    offset-y
-                                    full-width
-                                    min-width="290px"
-                                    class="pl-4"
-                                >
-                                    <v-text-field
-                                        slot="activator"
-                                        v-model="datestringFim"
-                                        label="Escolha a data Final da Pesquisa"
-                                        prepend-icon="event"
-                                        @blur="dateFim = parseDate(datestringFim)"
-                                        mask="##/##/####"
-                                        return-masked-value
-                                    ></v-text-field>
-                                    <v-date-picker
-                                        class="calendario-vuetify"
-                                        :max="new Date().toISOString().substr(0, 10)"
-                                        v-model="dateFim"
-                                        no-title
-                                        scrollable
-                                        locale="pt-br"
-                                        @input="menuFim = false"
-                                    >
-                                    </v-date-picker>
-                                </v-menu>
-                            </v-flex>
-                            <div class="pt-4 pl-4">
-                                <v-btn color="teal" class="white--text" :disabled="!validaCampo().validacao" @click="filtrarData()">Pesquisar</v-btn>
-                            </div>
-                        </v-layout>
-                        <v-layout>
-                            <v-flex xs4>
-                                <v-select
-                                    v-model="search"
-                                    :items="tpConta"
-                                    item-text="text"
-                                    item-value="id"
-                                    label="Tipo Conta"
-                                ></v-select>
-                                <v-spacer></v-spacer>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </template>
+                <filtros
+                    v-on:eventoSearch="search = $event"
+                >
+                </filtros>
                 <v-data-table
                     :headers="headers"
                     :items="dadosExtratosBancarios"
@@ -141,32 +45,13 @@
     import Carregando from '@/components/CarregandoVuetify';
     import { utils } from '@/mixins/utils';
     import moment from 'moment';
+    import Filtros  from './components/Filtros';
 
     export default {
         name: 'ExtratosBancarios',
         data() {
             return {
                 search: '',
-                tpConta: [
-                    {
-                        id:'Captação',
-                        text:'Captação'
-                    },
-                    {
-                        id:'Movimentação',
-                        text:'Movimentação'
-                    },
-                    {
-                        id:'',
-                        text:'Todos'
-                    }
-                ],
-                date: '',
-                menu: false,
-                datestring: '',
-                dateFim: '',
-                menuFim: false,
-                datestringFim:'',
                 pagination: {
                     sortBy: 'fat',
                 },
@@ -219,6 +104,7 @@
         mixins: [utils],
         components: {
             Carregando,
+            Filtros,
         },
         mounted() {
             if (typeof this.dadosProjeto.idPronac !== 'undefined') {
@@ -238,93 +124,10 @@
                 dadosExtratosBancarios: 'projeto/extratosBancarios',
             }),
         },
-        watch: {
-            date(val) {
-                this.datestring = this.formatDate(val);
-            },
-            dateFim(val) {
-                this.datestringFim = this.formatDate(val);
-            },
-        },
         methods: {
             ...mapActions({
                 buscarExtratosBancarios: 'projeto/buscarExtratosBancarios',
             }),
-            formatDate (date) {
-                if (!date) return null
-
-                const [year, month, day] = date.split('-')
-                return `${day}/${month}/${year}`
-            },
-            parseDate (date) {
-                if (!date) return null
-
-                const [day, month, year] = date.split('/')
-                return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
-            },
-            filtrarData() {
-                const params = {
-                    idPronac: this.dadosProjeto.idPronac,
-                    dtLancamento: this.date,
-                    dtLancamentoFim: this.dateFim,
-                    tpConta: ''
-                };
-
-                this.buscarExtratosBancarios(params);
-            },
-            filtrarTipoConta() {
-                const params = {
-                    idPronac: this.dadosProjeto.idPronac,
-                    dtLancamento: '',
-                    dtLancamentoFim: '',
-                    tpConta: this.search
-                };
-                if (params.tpConta === 'Captação') {
-                    params.tpConta = 'captacao';
-                }
-                if (params.tpConta === 'Movimentação') {
-                    params.tpConta = 'movimentacao';
-                }
-
-                this.buscarExtratosBancarios(params);
-            },
-            validaCampo() {
-                let status = {
-                    desc: '',
-                    validacao: false,
-                    id: 0
-                };
-                if( this.date === '' && this.dateFim === '') {
-                    status = { desc: 'hahaaa', validacao: false, id: 2 };
-                    return status;
-                }
-                if( this.date === '' && this.dateFim !== '') {
-                    status = { desc: 'preencher data inicial', validacao: false, id: 1 };
-                    return status;
-                }
-                else if(this.date !== '' && this.dateFim === '') {
-                    status = { desc: 'agora Escolha a data final', validacao: false, id: 1 };
-                    return status;
-                }
-                if( this.date === this.dateFim ) {
-                    status = { desc: 'hahaaa', validacao: true, id: 3 };
-                    return status;
-                }
-                if(this.date !== '' &&  this.dateFim !== '') {
-                    if( Date.parse(this.date) > Date.parse(this.dateFim)){
-                        status = { desc: 'data inicial nao pode ser maior que data final', validacao: false, id: 1 };
-                        return status;
-                    }
-                    else if ( !(Date.parse(this.date) < Date.parse(this.dateFim))) {
-                        status = { desc: 'data final nao pode ser menor que data inicial', validacao: false, id: 1 };
-                        return status;
-                    }
-                }
-                
-                status = { desc: 'tudo certo', validacao: true, id: 3 };
-                return status;
-
-            },
         },
     };
 </script>
