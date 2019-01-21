d569c3c23629de196d7776d8a43849c8b2bd58d7
luizfpinheiro <luizfelipenet@hotmail.com>
2019-01-21 16:50:55 -0200

    [UPDATE] Ideia inicial de uso do mock server nos testes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,8 +4,22 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("mudarPerfil", (codGrupo, codOrgao) => { 
-    cy.request(`http://localhost/autenticacao/perfil/alterarperfil?codGrupo=${codGrupo}&codOrgao=${codOrgao}`);
+Cypress.Commands.add("mudarPerfil", (codGrupo, codOrgao) => {
+  cy.request(`http://localhost/autenticacao/perfil/alterarperfil?codGrupo=${codGrupo}&codOrgao=${codOrgao}`);
 
-    cy.visit('http://localhost/principal');
+  cy.visit('http://localhost/principal');
+
 })
+
+
+Cypress.Commands.add('projetosInicio', () => {
+  // Mock Server
+  // cy.request('http://localhost:4000/avaliacao-resultados/fluxo-projeto?estadoid=5&idAgente=236').then((response) => {
+  //     var mock = response.body
+  //   })
+
+    cy.wait('@projetosInicio').then((response) => {
+      response.body = {} // mock 
+    })
+
+});
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-before( () => {
+before(() => {
     cy.visit('localhost');
-        
     var login = cy.get('#Login').type('239.691.561-49'); // Login
     var senha = cy.get('#Senha').type('123456'); // Senha
     cy.get('form').submit();
@@ -11,6 +10,19 @@ before( () => {
     cy.visit('http://localhost/principal');
 
     cy.wait(1000);
+
+
+    cy.server()
+    cy.route({
+      method: 'GET',      // Route all GET requests
+      url: '/navegacao/menu-principal',
+    }).as('menuPrincipal')
+
+    cy.server()
+    cy.route({
+      method: 'GET',      // Route all GET requests
+      url: '/avaliacao-resultados/fluxo-projeto?estadoid=5&idAgente=236',
+    }).as('projetosInicio')
 });
 
 
