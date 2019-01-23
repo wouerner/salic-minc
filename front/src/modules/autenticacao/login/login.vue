5ffbdfc02315dc5f1972a840b3811247766be648
guiamado <amado.silva133@gmail.com>
2019-01-23 15:48:25 -0200

    [FIX] Corrigindo padrão de assignments
    Co-authored-by: Wouerner Brandão <wouerner@protonmail.com>

--- front/src/modules/autenticacao/login/login.vue
+++ front/src/modules/autenticacao/login/login.vue
@@ -129,18 +129,18 @@ export default {
             Soma = 0;
             if (strCPF === '00000000000') return false;
 
-            for (let i = 1; i <= 9; i++) Soma += parseInt(strCPF.substring(i - 1, i), 10) * (11 - i);
+            for (let i = 1; i <= 9; i += 1) Soma += parseInt(strCPF.substring(i - 1, i), 10) * (11 - i);
             Resto = (Soma * 10) % 11;
 
-            if ((Resto == 10) || (Resto == 11)) Resto = 0;
-            if (Resto != parseInt(strCPF.substring(9, 10), 10)) return false;
+            if ((Resto === 10) || (Resto === 11)) Resto = 0;
+            if (Resto !== parseInt(strCPF.substring(9, 10), 10)) return false;
 
             Soma = 0;
-            for (let i = 1; i <= 10; i++) Soma += parseInt(strCPF.substring(i - 1, i), 10) * (12 - i);
+            for (let i = 1; i <= 10; i += 1) Soma += parseInt(strCPF.substring(i - 1, i), 10) * (12 - i);
             Resto = (Soma * 10) % 11;
 
-            if ((Resto == 10) || (Resto == 11)) Resto = 0;
-            if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+            if ((Resto === 10) || (Resto === 11)) Resto = 0;
+            if (Resto !== parseInt(strCPF.substring(10, 11), 10)) return false;
             return true;
         },
     },
