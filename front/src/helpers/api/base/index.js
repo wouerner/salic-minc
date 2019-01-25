56303f9b24b113c50dc4cafa9d0c4231fb4d61e0
luizfpinheiro <luizfelipenet@hotmail.com>
2019-01-25 17:18:15 -0200

    [UPDATE] Testes da Avaliação de Resultados usando API Mock Server

--- front/src/helpers/api/base/index.js
+++ front/src/helpers/api/base/index.js
@@ -3,18 +3,24 @@ import axios from 'axios';
 axios.interceptors.request.use((config) => {
     const user = JSON.parse(localStorage.getItem('user'));
     const conf = config;
-
+    
     if (user) {
         conf.headers.Authorization = `Bearer ${user.token}`;
     }
-
+    
     return conf;
 }, err => Promise.reject(err));
 
-export const getRequest = (path, queryParams = '') => axios.get(`${path}${queryParams}`);
+const API_ENDPOINT = 'http://localhost:4000';
 
-export const postRequest = (path, data) => axios.post(path, data);
+const instance = axios.create({
+  baseURL: API_ENDPOINT,
+});
 
-export const putRequest = (path, bodyFormData, id) => axios.post(`${path}/${id}`, bodyFormData);
+export const getRequest = (path, queryParams = '') => instance.get(`${path}${queryParams}`);
 
-export const deleteRequest = (path, id) => axios.delete(`${path}/${id}`);
+export const postRequest = (path, data) => instance.post(path, data);
+
+export const putRequest = (path, bodyFormData, id) => instance.post(`${path}/${id}`, bodyFormData);
+
+export const deleteRequest = (path, id) => instance.delete(`${path}/${id}`);
