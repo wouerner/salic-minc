389cf97eab0bcbcbaadf12eaade32872a8226c36
guiamado <amado.silva133@gmail.com>
2019-02-05 16:38:33 -0200

    [ADD] Relação pagamentos: Adiciona datatable expand

--- front/src/modules/projeto/visualizar/components/prestacaoContas/RelacaoPagamento.vue
+++ front/src/modules/projeto/visualizar/components/prestacaoContas/RelacaoPagamento.vue
@@ -6,6 +6,68 @@
         <div v-else-if="dados">
             <v-data-table
                 :pagination.sync="pagination"
+                :headers="listagem"
+                :items="indexItems"
+                :rows-per-page-items="[10, 25, 50, {'text': 'Todos', value: -1}]"
+                :expand="expand"
+                item-key="id"
+                class="elevation-1 container-fluid"
+            >
+                <template
+                    slot="items"
+                    slot-scope="props">
+                    <tr @click="props.expanded = !props.expanded">
+                        <td class="text-xs-center">{{ props.item.id + 1 }}</td>
+                        <td class="text-xs-left"><b>{{ props.item.conteudo }}</b></td>
+                    </tr>
+                </template>
+                <template slot="expand" slot-scope="props">
+                    <v-data-table
+                        :pagination.sync="pagination"
+                        :headers="headers"
+                        :items="dados"
+                        :rows-per-page-items="[10, 25, 50, {'text': 'Todos', value: -1}]"
+                        item-key="id"
+                        class="elevation-1 container-fluid"
+                    >
+                        <template
+                            slot="items"
+                            slot-scope="props">
+                            <td class="text-xs-left"><b>{{ props.item.Item }}</b></td>
+                            <td
+                                class="text-xs-center pl-5"
+                                style="width: 200px">
+                                {{ props.item.CNPJCPF | cnpjFilter }}
+                            </td>
+                            <td class="text-xs-left">{{ props.item.Fornecedor }}</td>
+                            <td class="text-xs-left">{{ props.item.tbDocumento }}</td>
+                            <td class="text-xs-right">{{ props.item.nrComprovante }}</td>
+                            <td class="text-xs-center pl-5">{{ props.item.DtPagamento | formatarData }}</td>
+                            <td class="text-xs-center pl-5">{{ props.item.DtEmissao | formatarData }}</td>
+                            <td
+                                class="text-xs-left"
+                                v-html="props.item.tpFormaDePagamento"/>
+                            <td class="text-xs-right">{{ props.item.nrDocumentoDePagamento }}</td>
+                            <td class="text-xs-left">{{ props.item.dsJustificativa }}</td>
+                            <td class="text-xs-right">{{ props.item.vlPagamento | filtroFormatarParaReal }}</td>
+                            <td class="text-xs-left">
+                                <v-btn
+                                    :href="`/upload`+
+                                        `/abrir`+
+                                    `?id=${props.item.idArquivo}`"
+                                    style="text-decoration: none"
+                                    round
+                                    small
+                                >
+                                    {{ props.item.nmArquivo }}
+                                </v-btn>
+                            </td>
+                        </template>
+                    </v-data-table>
+                </template>
+            </v-data-table>
+            <v-data-table
+                :pagination.sync="pagination"
                 :headers="headers"
                 :items="dados"
                 :rows-per-page-items="[10, 25, 50, {'text': 'Todos', value: -1}]"
@@ -44,9 +106,6 @@
                             {{ props.item.nmArquivo }}
                         </v-btn>
                     </td>
-                    <td
-                        class="text-xs-left"
-                        v-html="props.item.siComprovanteTrimestral"/>
                 </template>
             </v-data-table>
         </div>
@@ -76,7 +135,21 @@ export default {
                 // descending: true,
             },
             selected: [],
+            dadosLista: [],
             loading: true,
+            expand: false,
+            listagem: [
+                {
+                    text: 'Nº',
+                    align: 'center',
+                    value: 'id',
+                },
+                {
+                    text: 'Item',
+                    align: 'left',
+                    value: 'conteudo',
+                },
+            ],
             headers: [
                 {
                     text: 'Item',
@@ -146,6 +219,14 @@ export default {
             dadosProjeto: 'projeto/projeto',
             dados: 'prestacaoContas/relacaoPagamento',
         }),
+        indexItems() {
+            const currentItems = this.montaArray();
+            return currentItems.map((item, index) => ({
+                id: index,
+                ...item,
+                conteudo: item,
+            }));
+        },
     },
     watch: {
         dados() {
@@ -161,6 +242,19 @@ export default {
         ...mapActions({
             buscarRelacaoPagamento: 'prestacaoContas/buscarRelacaoPagamento',
         }),
+        montaArray() {
+            let dadosListagem = [];
+            let arrayTeste = this.dados;
+            arrayTeste = arrayTeste.filter(function(element, i, array) {
+                return array.map(x => x['Item']).indexOf(element['Item']) === i;
+            })
+
+            arrayTeste.forEach(element => {
+                dadosListagem.push(element['Item']);
+            });
+
+            return dadosListagem;
+        },
     },
 };
 </script>
