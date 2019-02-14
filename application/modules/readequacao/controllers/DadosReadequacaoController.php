2ea8a0aba16b6dd0657ecfd35fa3af0f96c6feb7
fernao <fernao@riseup.net>
2019-02-14 12:21:57 -0200

    [UPDATE] Readequação: rota dados-readequacao funcionando da forma correta (index para listagem/busca) e com /$idProponente para consulta #1415 e #1961

--- application/modules/readequacao/controllers/DadosReadequacaoController.php
+++ application/modules/readequacao/controllers/DadosReadequacaoController.php
@@ -8,13 +8,16 @@ class Readequacao_DadosReadequacaoController extends MinC_Controller_Rest_Abstra
     public function __construct(Zend_Controller_Request_Abstract $request, Zend_Controller_Response_Abstract $response, array $invokeArgs = array())
     {
         $profiles = [
-            // Autenticacao_Model_Grupos::PROPONENTE,
-            // Autenticacao_Model_Grupos::TECNICO_ACOMPANHAMENTO,
-            // Autenticacao_Model_Grupos::COORDENADOR_ACOMPANHAMENTO,
-            // Autenticacao_Model_Grupos::COORDENADOR_GERAL_ACOMPANHAMENTO,
+            Autenticacao_Model_Grupos::PROPONENTE,
+            Autenticacao_Model_Grupos::TECNICO_ACOMPANHAMENTO,
+            Autenticacao_Model_Grupos::COORDENADOR_ACOMPANHAMENTO,
+            Autenticacao_Model_Grupos::COORDENADOR_GERAL_ACOMPANHAMENTO,
         ];
-
+        
         $permissionsPerMethod  = [
+            'post' => [
+                Autenticacao_Model_Grupos::PROPONENTE
+            ]
         ];
         $this->setProtectedMethodsProfilesPermission($permissionsPerMethod);
         
@@ -24,28 +27,33 @@ class Readequacao_DadosReadequacaoController extends MinC_Controller_Rest_Abstra
     public function getAction() {
         $data = [];
         $code = 200;
-        $readequacaoService = new ReadequacaoService($this->getRequest(), $this->getResponse());
+        $idReadequacao = $this->getRequest()->getParam('id');
         
-        if ($this->getRequest()->getParam('idReadequacao')) {
-            $idReadequacao = $this->getRequest()->getParam('idReadequacao');           
-            $data = $readequacaoService->buscar($idReadequacao);
-            
-        } else if ($this->getRequest()->getParam('idPronac')) {
-            $idPronac = $this->getRequest()->getParam('idPronac');
-            if (strlen($idPronac) > 7) {
-                $idPronac = Seguranca::dencrypt($idPronac);
-            }
-            
-            $idTipoReadequacao = $this->getRequest()->getParam('idTipoReadequacao');
-            $stEstagioAtual = $this->getRequest()->getParam('stEstagioAtual');
-            
-            $data = $readequacaoService->buscarReadequacoes($idPronac, $idTipoReadequacao, $stEstagioAtual);
-        }
-            
+        $readequacaoService = new ReadequacaoService($this->getRequest(), $this->getResponse());
+        $data = $readequacaoService->buscar($idReadequacao);
+        
         $this->renderJsonResponse($data, $code);
     }
 
-    public function indexAction(){}
+    public function indexAction(){
+        $data = [];
+        $code = 200;
+        
+        $idReadequacao = $this->getRequest()->getParam('idReadequacao');
+
+        $idPronac = $this->getRequest()->getParam('idPronac');
+        if (strlen($idPronac) > 7) {
+            $idPronac = Seguranca::dencrypt($idPronac);
+        }
+        
+        $idTipoReadequacao = $this->getRequest()->getParam('idTipoReadequacao');
+        $stEstagioAtual = $this->getRequest()->getParam('stEstagioAtual');
+        
+        $readequacaoService = new ReadequacaoService($this->getRequest(), $this->getResponse());
+        $data = $readequacaoService->buscarReadequacoes($idPronac, $idTipoReadequacao, $stEstagioAtual);
+        
+        $this->renderJsonResponse($data, $code);
+    }
 
     public function headAction(){}
 
