166694eac7522a3c0d41523dcd01a9dc4ed536c6
luizfpinheiro <luizfelipenet@hotmail.com>
2019-03-21 18:56:34 -0300

    [UPDATE] Visualizar Readequação - novo estilo para a visualização

--- front/src/modules/readequacao/components/VisualizarReadequacaoButton.vue
+++ front/src/modules/readequacao/components/VisualizarReadequacaoButton.vue
@@ -37,16 +37,125 @@
                     <v-spacer/>
                     <v-toolbar-title>{{ dadosProjeto.Pronac }} - {{ dadosProjeto.NomeProjeto }}</v-toolbar-title>
                 </v-toolbar>
+
+                <v-divider></v-divider>
+
                 <v-layout
                     row
                     wrap
                     class="mt-5 pt-3"
                 >
-                    <v-flex
-                        xs10
-                        offset-xs1
-                    >
-                        <v-expansion-panel
+                    <v-flex xs5 offset-xs1>
+                        <v-list two-line subheader>
+                            <v-subheader inset>Dados da Solicitação</v-subheader>
+
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">person</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Nome do Solicitante</v-list-tile-title>
+                                    <v-list-tile-sub-title>Beltrano Ciclano - {{dadosReadequacao.idSolicitante}} </v-list-tile-sub-title>
+                                </v-list-tile-content>
+
+                            </v-list-tile>
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">mode_comment</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Solicitação</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{dadosReadequacao.dsSolicitacao}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                    </v-flex>
+
+
+                    <v-flex xs5>
+                        <v-list two-line subheader>
+                            <v-subheader inset></v-subheader>
+
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">date_range</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Data da Solicitação</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{dadosReadequacao.dtSolicitacao}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+
+                            </v-list-tile>
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">assignment</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Justificativa</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{dadosReadequacao.dsJustificativa}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                    </v-flex>
+
+                    <v-flex xs5 offset-xs1>
+                        <v-list two-line subheader>
+                            <v-subheader inset>Dados da Avaliação</v-subheader>
+
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">person</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Nome do Avaliador</v-list-tile-title>
+                                    <v-list-tile-sub-title>Avaliador - {{dadosReadequacao.idAvaliador}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+
+                            </v-list-tile>
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">rate_review</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Avaliação</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{dadosReadequacao.dsAvaliacao}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                    </v-flex>
+
+                    <v-flex xs5>
+                        <v-list two-line subheader>
+                            <v-subheader inset></v-subheader>
+
+                            <v-list-tile avatar>
+
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">date_range</v-icon>
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Data da Avaliação</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{dadosReadequacao.dtAvaliador}}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+
+                            </v-list-tile>
+                        </v-list>
+                    </v-flex>
+
+                        <!-- <v-expansion-panel
                             v-model="panel"
                             expand
                         >
@@ -57,12 +166,15 @@
                                     class="title"
                                 >DADOS DA SOLICITAÇÃO</div>
                                 <v-card>
-                                    <v-card-title class="subheading font-weight-bold">
-                                        Solicitação
-                                    </v-card-title>
-                                    <v-card-text>
-                                        <span v-html="dadosReadequacao.dsSolicitacao"></span>
-                                    </v-card-text>
+                                    <v-flex xs4 sm2>
+                                        <v-text-field
+                                            :value="dadosReadequacao.dsSolicitacao"
+                                            label="Solicitante"
+                                            outline
+                                            readonly
+                                        >
+                                        </v-text-field>
+                                    </v-flex>
                                 </v-card>
                                 <v-card>
                                     <v-card-title class="subheading font-weight-bold">
@@ -99,8 +211,7 @@
                                     </v-card-text>
                                 </v-card>
                             </v-expansion-panel-content>
-                        </v-expansion-panel>
-                    </v-flex>
+                        </v-expansion-panel> -->
                 </v-layout>
             </v-card>
         </v-dialog>
