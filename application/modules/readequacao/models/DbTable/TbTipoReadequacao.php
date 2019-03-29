0a86e1f0572510ea8077d7274dbe13a1163e887e
fernao <fernao@riseup.net>
2019-03-29 19:08:29 -0300

    [UPDATE] Readequação: corrige lista de readaequações disponíveis - lista somente as que não estão em andamento

--- application/modules/readequacao/models/DbTable/TbTipoReadequacao.php
+++ application/modules/readequacao/models/DbTable/TbTipoReadequacao.php
@@ -10,6 +10,9 @@ class Readequacao_Model_DbTable_TbTipoReadequacao extends MinC_Db_Table_Abstract
     protected $_schema = "SAC";
     protected $_name   = "tbTipoReadequacao";
 
+    const ST_ESTADO_ATIVA = 0;
+    const ST_ESTADO_INATIVA = 1;
+    
     public function buscarTiposReadequacoesPermitidos($idPronac, $order = 2)
     {
         $select = $this->select();
@@ -22,15 +25,11 @@ class Readequacao_Model_DbTable_TbTipoReadequacao extends MinC_Db_Table_Abstract
         );
 
         $select->where('siReadequacao = ?', 0);
-        $select->where('stEstado = ?', Readequacao_Model_DbTable_TbReadequacao::ST_ESTADO_EM_ANDAMENTO);
+        $select->where('stEstado = ?', SELF::ST_ESTADO_ATIVA);
 
         $select->where(new Zend_Db_Expr("idTipoReadequacao NOT IN (
             SELECT idTipoReadequacao FROM SAC.dbo.tbReadequacao WHERE idPronac = $idPronac 
-            AND siEncaminhamento IN (" . implode(',', [
-                Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_NAO_ENVIA_MINC,
-                Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_CADASTRADA_PROPONENTE,
-                Readequacao_Model_tbTipoEncaminhamento::SI_ENCAMINHAMENTO_FINALIZADA_SEM_PORTARIA
-            ]) . "))"));
+            AND stEstado = " . Readequacao_Model_DbTable_TbReadequacao::ST_ESTADO_EM_ANDAMENTO . ")"));
 
         $select->order($order);
         
