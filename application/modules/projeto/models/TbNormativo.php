fa246c2ab11eb8a71836343c4ba4ec74f1551646
Cleber Santos <oclebersantos@gmail.com>
2019-04-24 19:55:45 -0300

    [update] custos vinculados: atualiza regra da remuneracao para captacao IN 2019 #2066

---
+++ application/modules/projeto/models/TbNormativo.php
@@ -0,0 +1,15 @@
+<?php
+
+class Projeto_Model_TbNormativo extends MinC_Db_Model
+{
+    protected $_idNormativo;
+    protected $_nmNormativo;
+    protected $_dsJustificativa;
+    protected $_dsComentario;
+    protected $_dtPublicacao;
+    protected $_dtRevogacao;
+    protected $_stEstado;
+
+    const INSTRUCAO_NORMATIVA_2019 = 10;
+
+}
