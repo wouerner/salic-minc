5569962f29bde617e6b717c5f36a39045e7d2299
fernao <fernao@riseup.net>
2019-04-24 15:10:18 -0300

    [UPDATE] Readequação: ajusta visualização da avaliação, que cortava #1967

--- front/src/modules/readequacao/components/VisualizarReadequacaoButton.vue
+++ front/src/modules/readequacao/components/VisualizarReadequacaoButton.vue
@@ -175,7 +175,7 @@
                     </v-flex>
                     <v-flex
                         v-if="existeAvaliacao"
-                        xs5
+                        xs10
                         offset-xs1
                     >
                         <v-list
@@ -201,32 +201,47 @@
                             </v-list-tile>
                             <v-list-tile avatar>
                                 <v-list-tile-avatar>
-                                    <v-icon class="green lighten-1 white--text">rate_review</v-icon>
-                                </v-list-tile-avatar>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>Avaliação</v-list-tile-title>
-                                    <v-list-tile-sub-title v-html="dadosReadequacao.dsAvaliacao"/>
-                                </v-list-tile-content>
-                            </v-list-tile>
-                        </v-list>
-                    </v-flex>
-                    <v-flex
-                        v-if="existeAvaliacao"
-                        xs5
-                    >
-                        <v-list
-                            two-line
-                            subheader
-                        >
-                            <v-subheader inset/>
-                            <v-list-tile avatar>
-                                <v-list-tile-avatar>
                                     <v-icon class="green lighten-1 white--text">date_range</v-icon>
                                 </v-list-tile-avatar>
                                 <v-list-tile-content>
                                     <v-list-tile-title>Data da Avaliação</v-list-tile-title>
                                     <v-list-tile-sub-title>{{ dadosReadequacao.dtAvaliador | formatarData }}</v-list-tile-sub-title>
                                 </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                    </v-flex>
+                    <v-flex
+                        v-if="existeAvaliacao"
+                        xs10
+                        offset-xs1
+                    >
+                        <v-list
+                            two-line
+                        >
+                            <v-list-tile
+                                avatar
+                                @click="visualizarAvaliacao = !visualizarAvaliacao"
+                            >
+                                <visualizar-campo-detalhado
+                                    v-if="visualizarAvaliacao"
+                                    :dialog="true"
+                                    :dados="{ titulo: 'Avaliação', descricao: dadosReadequacao.dsAvaliacao }"
+                                    @fechar-visualizacao="visualizarAvaliacao = false"
+                                />
+                                <v-list-tile-avatar>
+                                    <v-icon class="green lighten-1 white--text">assignment</v-icon>
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Avaliação</v-list-tile-title>
+                                </v-list-tile-content>
+                                <v-list-tile-action>
+                                    <v-btn
+                                        icon
+                                        ripple
+                                    >
+                                        <v-icon color="grey lighten-1">info</v-icon>
+                                    </v-btn>
+                                </v-list-tile-action>
                             </v-list-tile>
                         </v-list>
                     </v-flex>
@@ -285,7 +300,7 @@ export default {
             dialog: false,
             loading: true,
             panel: [true, true],
-            visualizarSolicitacao: false,
+            visualizarAvaliacao: false,
             visualizarJustificativa: false,
             outrosTiposSolicitacoes: [
                 Const.TIPO_READEQUACAO_REMANEJAMENTO_PARCIAL,
