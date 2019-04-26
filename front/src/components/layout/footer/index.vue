e258fbce14bc2e938c5679a6524bfdc53e7a5627
Cleber Santos <oclebersantos@gmail.com>
2019-04-26 00:40:06 -0300

    [update] geral: altera labels de rouanet para lei de incentivo a cultura #2066

--- front/src/components/layout/footer/index.vue
+++ front/src/components/layout/footer/index.vue
@@ -18,10 +18,10 @@
                         class="orange-text text-lighten-3 tooltipped"
                         data-position="top"
                         data-delay="50"
-                        data-tooltip="Ir para o portal de notícias, tutoriais e legislações da Lei Rouanet"
+                        data-tooltip="Ir para o portal de notícias, tutoriais e legislações da Lei de Incentivo &agrave; Cultura"
                         target="_blank"
                         href="http://rouanet.cultura.gov.br/"
-                        data-tooltip-id="11c539d7-9913-16c0-3c16-ce4e6364817f">LEI ROUANET</a>
+                        data-tooltip-id="11c539d7-9913-16c0-3c16-ce4e6364817f">LEI DE INCENTIVO &Agrave; CULTURA</a>
                 </v-flex>
                 <v-flex
                     xs3
