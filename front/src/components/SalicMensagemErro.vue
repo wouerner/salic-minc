5110c9b4414a20855a5949029a02224c86585fb0
fernao <fernao@riseup.net>
2019-04-29 15:41:50 -0300

    [UPDATE] Componente mensagem erro: atualiza layout para vuetify

--- front/src/components/SalicMensagemErro.vue
+++ front/src/components/SalicMensagemErro.vue
@@ -1,20 +1,34 @@
 <template>
-    <div
-        class="card center-align"
-        style="margin: 20px auto; width:80%">
-        <div class="card-content">
-            <div class="row">
-                <div class="col s2 right-align"><i class="medium red-text material-icons">not_interested</i></div>
-                <div class="col s10 left-align">
-                    <br>
-                    <b v-html="texto"/>
-                    <a
-                        v-if="urlRetorno"
-                        href="urlRetorno"> {{ msgUrlRetorno }}</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-container
+        grid
+    >
+        <v-layout
+            row
+            wrap
+            align-center
+        >
+            <v-flex
+                offset-xs1
+                xs1
+                class="text-xs-center"
+            >
+                <v-icon
+                    size="56"
+                    class="red--text mr-2"
+                >not_interested
+                </v-icon>
+            </v-flex>
+            <v-flex
+                xs3
+                class="text-xs-left"
+            >
+                <strong v-html="texto"/>
+                <a
+                    v-if="urlRetorno"
+                    href="urlRetorno"> {{ msgUrlRetorno }}</a>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
